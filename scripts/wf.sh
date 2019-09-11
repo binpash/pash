c266f8cfde4703120a6d7a720eff3587e9f0d6d9
Nikos Vasilakis <nikos@vasilak.is>
2019-09-11 11:14:02 -0400

    Minor fixes

--- scripts/wf.sh
+++ scripts/wf.sh
@@ -1,4 +1,4 @@
 # Word frequencies:
-cat ./input.txtINPUT | tr -cs A-Za-z'\n' | tr A-Z a-z | sort | uniq -c | sort -rn | sed ${1}q
+cat ./input.txt | tr -cs A-Za-z'\n' | tr A-Z a-z | sort | uniq -c | sort -rn | sed ${1}q
 
 
