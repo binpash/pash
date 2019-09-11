0a5bcba59664c135c3e4003a20ceb82fbeff16b5
Nikos Vasilakis <nikos@vasilak.is>
2019-09-11 11:14:02 -0400

    Minor fixes

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,6 +1,7 @@
-# Fine markdown files, compile them, and expose them over the network
+# Find markdown files, compile them, and expose them over the network
+
 find . '*.md' | # Parallelizable, given a distributed FS
-    xargs mdc | # xargs is higher-order command; trivially parallelizable
+    xargs mdc | # xargs is higher-order, trivially parallelizable; mdc is third-party
     nc -l 80    # netcat could default-but-configurably parallelizable
 
 
