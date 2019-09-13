eb0f64b0943b280754ebcaa61052595a9722d371
Nikos Vasilakis <nikos@vasilak.is>
2019-09-13 13:25:55 -0400

    Update circular

--- scripts/circular/sine.sh
+++ scripts/circular/sine.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-[ -f /etc/resolv.conf ] || ( echo 1 >temp.txt )
+F="temp.txt"
+[ -f $F ] && (rm $F && echo 1 >$F )
 tail -f temp.txt | while read n; do echo "1+s(3*$n)" | bc -l; sleep 1; done | tee -a temp.txt
