249e761072f19ba51d91f600e2e7e34b92b9e488
Lazar Cvetkovic <l.cvetkovic.997@gmail.com>
2019-09-24 12:03:56 -0500

    - coordinator now creates new processes instead of threads - testing yet to be done - removed unused stuff from old classes

--- dshell/core/worker/DistributedTask.java
+++ dshell/core/worker/DistributedTask.java
@@ -9,11 +9,14 @@ import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.rmi.Remote;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
 public class DistributedTask {
     private final static int NUMBER_OF_THREADS = 8;
+    private final static String WORKER_NODE = "";
 
     public static void main(String[] args) throws Exception {
         if (args.length < 1)
@@ -32,9 +35,39 @@ public class DistributedTask {
                     while (true) {
                         Object received = ois.readObject();
 
-                        if (received instanceof RemoteExecutionData)
-                            executor.execute(new WorkerThread((RemoteExecutionData) received));
-                        else if (received instanceof SystemMessage.EndOfREM)
+                        if (received instanceof RemoteExecutionData) {
+                            RemoteExecutionData red = (RemoteExecutionData) received;
+                            List<String> argsToSend = new ArrayList<>();
+
+                            argsToSend.add("java");                             // JRE
+                            argsToSend.add("-cp");                              // classpath
+                            argsToSend.add("./dshell.jar");                     // executable location
+                            argsToSend.add("dshell.core.worker.WorkerProcess"); // main method class container
+
+                            argsToSend.add(Integer.toString(red.getOperator().getInputArity()));
+                            argsToSend.add(Integer.toString(red.getOperator().getOutputArity()));
+                            argsToSend.add(red.getOperator().getProgram());
+                            argsToSend.add(Integer.toString(red.getOperator().getCommandLineArguments().length));
+                            for (String commandLineArgument : red.getOperator().getCommandLineArguments())
+                                argsToSend.add(commandLineArgument);
+
+                            argsToSend.add(Boolean.toString(red.isInitialOperator()));
+
+                            argsToSend.add(Integer.toString(red.getInputPort()));
+
+                            argsToSend.add(Integer.toString(red.getOutputHost().length));
+                            for (int i = 0; i < red.getOutputHost().length; i++) {
+                                argsToSend.add(red.getOutputHost()[i]);
+                                argsToSend.add(Integer.toString(red.getOutputPort()[i]));
+                            }
+
+                            argsToSend.add(red.getCallbackHost());
+                            argsToSend.add(Integer.toString(red.getCallBackPort()));
+
+                            ProcessBuilder builder = new ProcessBuilder(argsToSend);
+                            builder.start();
+                            // process will terminate itself upon completion of main method
+                        } else if (received instanceof SystemMessage.EndOfREM)
                             break;
                         else
                             throw new RuntimeException("Not supported type of input data received by worker coordinator.");
