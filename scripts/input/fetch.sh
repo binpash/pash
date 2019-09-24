32a71be8f78fab42aa972c18a351fc6da12826ad
Nikos Vasilakis <nikos@vasilak.is>
2019-09-23 22:43:00 -0400

    Add input generation scripts

---
+++ scripts/input/fetch.sh
@@ -0,0 +1,2 @@
+curl -s 'http://ndr.md/corpus/dummy/i1M.txt' > i1M.txt
+curl -s 'http://ndr.md/corpus/dummy/i1G.txt' > i1G.txt
---
+++ scripts/input/gen.sh
@@ -0,0 +1,6 @@
+touch i1G.txt
+for (( i = 0; i < 1000; i++ )); do
+  cat i1M.txt >> i1G.txt
+done
+
+# more logic to truncate file --- trivial on Linux, more difficult on OS X
