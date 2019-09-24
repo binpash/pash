ec51c68e3abd5810088cdb8cc2add5716493f66b
Nikos Vasilakis <nikos@vasilak.is>
2019-09-24 11:49:39 -0400

    Add some documentation to scripts

--- scripts/minimal.sh
+++ scripts/minimal.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
-cat ./input/i1G.txt | tr A-Z a-z | grep '\(.\).*\1\(.\).*\2\(.\).*\3\(.\).*\4' > /dev/null
+# Timings on my machine (nv):
+# 1M: 1.25s
+# 1G: > half an hour
+IN=./input/i1G.txt # Change G to M for small input
+cat $IN | tr A-Z a-z | grep '\(.\).*\1\(.\).*\2\(.\).*\3\(.\).*\4' > ./output/1.txt
