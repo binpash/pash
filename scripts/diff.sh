83d82ceb6e4d904ef684e63d1ce031354842c289
Nikos Vasilakis <nikos@vasilak.is>
2019-10-03 17:28:42 -0400

    Fix diff

--- scripts/diff.sh
+++ scripts/diff.sh
@@ -2,12 +2,11 @@
 # https://crashingdaily.wordpress.com/2008/03/06/diff-two-stdout-streams/
 # This would work with coreutils.
 shuf() { awk 'BEGIN {srand(); OFMT="%.17f"} {print rand(), $0}' "$@" | sort -k1,1n | cut -d ' ' -f2-; }
-
-alias gen='head -n 2 ./input/input.txt'
+gen() { head -n 2 ./input/input.txt; }
 # alias gen='cat ./input/input.txt'
 
 mkfifo s1 s2
 gen | shuf | sort | tr [:lower:] [:upper:] > s1 & 
-gen | shuf | sort | tr [:lower:] [:upper:] > s2 &
+gen | shuf | sort | tr [:upper:] [:lower:] > s2 &
 diff -B s1 s2
 rm s1 s2
