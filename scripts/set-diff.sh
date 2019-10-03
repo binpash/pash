632453c899a2dd89075daf58c0fdeb176b29c520
Nikos Vasilakis <nikos@vasilak.is>
2019-10-03 17:00:53 -0400

    Correct diff

--- scripts/set-diff.sh
+++ scripts/set-diff.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-p="../linux_command_statistics/c_stats"
-echo $1 $2 $3
-comm  -3 <(cut -d ' ' -f 1 $p/$1.txt | sort ) <( cut -d ' ' -f 1 $p/$2.txt | sort)
+# Show diff between different groups of commands
+f1=${1:-posix}
+f2=${2:-coreutils}
+p="../stats/c_stats"
+comm -3 <(cut -d ' ' -f 1 $p/$f1.txt | sort ) <( cut -d ' ' -f 1 $p/$f2.txt | sort)
