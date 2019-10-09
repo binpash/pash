b2c282781551c279e37add71f34130911534d0cf
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2019-10-08 21:46:52 -0400

    Clean up obsolete comments, separate files, and draft next TODOs

---
+++ front_end/distr_plan.py
@@ -0,0 +1,5 @@
+
+## This file should receive the name of a file that holds an IR, read
+## the IR, read some configuration file with node information, and
+## then should make a distribution plan for it.
+    
