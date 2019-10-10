14b29f216e45be00717705c8360ddc2490883ba2
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2019-10-10 15:37:12 -0400

    Make a skeleton for the distribution planner

--- front_end/distr_plan.py
+++ front_end/distr_plan.py
@@ -1,5 +1,22 @@
+import sys
+import pickle
+from ir import *
 
-## This file should receive the name of a file that holds an IR, read
-## the IR, read some configuration file with node information, and
-## then should make a distribution plan for it.
+## This file receives the name of a file that holds an IR, reads the
+## IR, read some configuration file with node information, and then
+## should make a distribution plan for it.
+
+def main():
+    # print command line arguments
+    ir_filename = sys.argv[1]
+    with open(ir_filename, "rb") as ir_file:
+        ir_node = pickle.load(ir_file)
+
+    print(ir_node)
+
+    ## TODO: Do something with the node
+    
+        
+if __name__ == "__main__":
+    main()
     
