887faa51a0439cb823085e298754b50f1a4d5742
Lazar M. Cvetković <l.cvetkovic.997@gmail.com>
2019-10-30 18:17:35 -0500

    - another concurrent bugfixing

--- dshell/core/worker/SocketToProcessThread.java
+++ dshell/core/worker/SocketToProcessThread.java
@@ -3,8 +3,6 @@ package dshell.core.worker;
 import dshell.core.interfaces.Consumer;
 import dshell.core.misc.SystemMessage;
 
-import java.util.Queue;
-
 public class SocketToProcessThread implements Runnable {
     private InternalBuffer internalBuffer;
     private int inputChannel;
@@ -19,16 +17,13 @@ public class SocketToProcessThread implements Runnable {
     @Override
     public void run() {
         Object data;
-        boolean endNotReceived = true;
 
-        while (endNotReceived) {
+        while (true) {
             // NOTE: internalBuffer.read() is a blocking method
             data = internalBuffer.read();
 
-            if (data instanceof SystemMessage.EndOfData) {
-                endNotReceived = false;
+            if (data instanceof SystemMessage.EndOfData)
                 return;
-            }
             else {
                 // invoking the operator's computation; after the computation, the data is sent via socket to next node
                 // if this is split operator the data splitting will be done inside an operator and the data will be
