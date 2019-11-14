ac282615078fac41f74ff4c72f80eb77fa6ee744
Nikos Vasilakis <nikos@vasilak.is>
2019-11-14 18:50:20 -0500

    Serialize to a simple JSON

--- front_end/ir.py
+++ front_end/ir.py
@@ -562,6 +562,23 @@ class IR:
         output = "File ids:\n{}\n".format(all_file_ids) + output
         return output
 
+    def serialize_as_JSON(self):
+        output = "nodes: {\n"# <-- FIXME confirm this should be single `}`
+        all_file_ids = ""
+        for i, node in enumerate(self.nodes):
+            serialized_input_file_ids = " ".join([fid.serialize()
+                                                  for fid in node.get_flat_input_file_ids()])
+            serialized_output_file_ids = " ".join([fid.serialize()
+                                                   for fid in node.get_flat_output_file_ids()])
+            all_file_ids += serialized_input_file_ids + " "
+            all_file_ids += serialized_output_file_ids + " "
+            output += "\"{}\": {{in: [{}], out: [{}], command: \"{}\" }}\n".format(i,
+                                                               ", ".join(serialized_input_file_ids),
+                                                               ", ".join(serialized_output_file_ids),
+                                                               node.serialize())
+        output = "{{ fids:\n[{}]\n,".format(", ".join(all_file_ids)) + output + "}" # <-- FIXME confirm this should be single `}`
+        return output
+
     def set_ast(self, ast):
         self.ast = ast
     
