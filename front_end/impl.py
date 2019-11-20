da3151b89b89e2425167a35312b29c72e2a58dbe
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2019-11-19 21:45:57 -0500

    Instead of executing in python, write the script to a file

--- front_end/impl.py
+++ front_end/impl.py
@@ -6,7 +6,7 @@ def execute(graph_json, output_file):
     ## TODO: Remove environment hardcoding
     env={"IN": "../scripts/input/i1M.txt"}
 
-    print("Execute:")
+    print("Translate:")
     # print(graph_json)
     fids = graph_json["fids"]
     in_fids = graph_json["in"]
@@ -16,14 +16,19 @@ def execute(graph_json, output_file):
 
     start_time = time.time()
 
+    output_script_commands = []
+
     ## Setup pipes
     for fid in fids:
         # print(fid)
-        remove_fifo_if_exists(fid)
-        make_fifo(fid)
+        rm_com = remove_fifo_if_exists(fid)
+        mkfifo_com = make_fifo(fid)
+        output_script_commands.append(rm_com)
+        output_script_commands.append(mkfifo_com)
 
     ## Execute nodes
     processes = [execute_node(node, env) for node_id, node in nodes.items()]
+    output_script_commands += processes
 
     ## Collect outputs
     collect_output_args = ["cat"]
@@ -32,43 +37,50 @@ def execute(graph_json, output_file):
     collect_output_args.append(">")
     collect_output_args.append('"{}"'.format(output_file))
     output_script = " ".join(collect_output_args)
-    print("Collect output:")
-    print(output_script)
-    out_p = subprocess.Popen(output_script, shell=True,
-                             executable="/bin/bash")
+    # print("Collect output:")
+    # print(output_script)
+    # out_p = subprocess.Popen(output_script, shell=True,
+    #                          executable="/bin/bash")
+    output_script_commands.append(output_script)
 
     ## Wait for all processes to die
-    for proc in processes:
-        ret = proc.wait()
-        if(not ret == 0):
-            print("-- Error!", proc, ret)
+    # for proc in processes:
+    #     ret = proc.wait()
+    #     if(not ret == 0):
+    #         print("-- Error!", proc, ret)
 
     ## Kill pipes
     for fid in fids:
         # print(fid)
-        remove_fifo_if_exists(fid)
+        rm_com = remove_fifo_if_exists(fid)
+        output_script_commands.append(rm_com)
 
     end_time = time.time()
 
-    print("Distributed graph execution time:", end_time - start_time)
+    # print("Distributed translation execution time:", end_time - start_time)
+    return "\n".join(output_script_commands)
 
 def remove_fifo_if_exists(pipe):
     assert(pipe[0] == "#")
-    if os.path.exists(pipe):
-        os.remove(pipe)
+    # print('rm -f "{}"'.format(pipe))
+    # if os.path.exists(pipe):
+    #     os.remove(pipe)
+    return 'rm -f "{}"'.format(pipe)
 
 def make_fifo(pipe):
     assert(pipe[0] == "#")
-    print ('mkfifo "{}"'.format(pipe))
-    os.mkfifo(pipe)
+    # print ('mkfifo "{}"'.format(pipe))
+    # os.mkfifo(pipe)
+    return 'mkfifo "{}"'.format(pipe)
 
 def execute_node(node, env):
     # print(node)
     script = node_to_script(node)
-    print(script)
-    p = subprocess.Popen(script, shell=True,
-                         executable="/bin/bash", env=env)
-    return p
+    # print("{} &".format(script))
+    # p = subprocess.Popen(script, shell=True,
+    #                      executable="/bin/bash", env=env)
+    # return p
+    return "{} &".format(script)
 
 def node_to_script(node):
     # print(node)
