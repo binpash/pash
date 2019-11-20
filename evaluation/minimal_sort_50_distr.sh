b37cbc86257e87f32535e1544917b23e1ec2c178
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2019-11-19 23:01:14 -0500

    Add scale up scripts for the minimal sort

---
+++ evaluation/minimal_sort_50_distr.sh
@@ -0,0 +1,606 @@
+mkdir -p /tmp/dish_output/
+rm -f "#file105"
+mkfifo "#file105"
+rm -f "#file190"
+mkfifo "#file190"
+rm -f "#file118"
+mkfifo "#file118"
+rm -f "#file206"
+mkfifo "#file206"
+rm -f "#file151"
+mkfifo "#file151"
+rm -f "#file115"
+mkfifo "#file115"
+rm -f "#file170"
+mkfifo "#file170"
+rm -f "#file155"
+mkfifo "#file155"
+rm -f "#file58"
+mkfifo "#file58"
+rm -f "#file168"
+mkfifo "#file168"
+rm -f "#file182"
+mkfifo "#file182"
+rm -f "#file85"
+mkfifo "#file85"
+rm -f "#file94"
+mkfifo "#file94"
+rm -f "#file187"
+mkfifo "#file187"
+rm -f "#file162"
+mkfifo "#file162"
+rm -f "#file154"
+mkfifo "#file154"
+rm -f "#file140"
+mkfifo "#file140"
+rm -f "#file107"
+mkfifo "#file107"
+rm -f "#file156"
+mkfifo "#file156"
+rm -f "#file138"
+mkfifo "#file138"
+rm -f "#file174"
+mkfifo "#file174"
+rm -f "#file161"
+mkfifo "#file161"
+rm -f "#file172"
+mkfifo "#file172"
+rm -f "#file177"
+mkfifo "#file177"
+rm -f "#file119"
+mkfifo "#file119"
+rm -f "#file77"
+mkfifo "#file77"
+rm -f "#file109"
+mkfifo "#file109"
+rm -f "#file200"
+mkfifo "#file200"
+rm -f "#file149"
+mkfifo "#file149"
+rm -f "#file202"
+mkfifo "#file202"
+rm -f "#file191"
+mkfifo "#file191"
+rm -f "#file199"
+mkfifo "#file199"
+rm -f "#file132"
+mkfifo "#file132"
+rm -f "#file186"
+mkfifo "#file186"
+rm -f "#file72"
+mkfifo "#file72"
+rm -f "#file124"
+mkfifo "#file124"
+rm -f "#file145"
+mkfifo "#file145"
+rm -f "#file117"
+mkfifo "#file117"
+rm -f "#file192"
+mkfifo "#file192"
+rm -f "#file181"
+mkfifo "#file181"
+rm -f "#file96"
+mkfifo "#file96"
+rm -f "#file142"
+mkfifo "#file142"
+rm -f "#file61"
+mkfifo "#file61"
+rm -f "#file83"
+mkfifo "#file83"
+rm -f "#file135"
+mkfifo "#file135"
+rm -f "#file75"
+mkfifo "#file75"
+rm -f "#file79"
+mkfifo "#file79"
+rm -f "#file121"
+mkfifo "#file121"
+rm -f "#file147"
+mkfifo "#file147"
+rm -f "#file166"
+mkfifo "#file166"
+rm -f "#file184"
+mkfifo "#file184"
+rm -f "#file201"
+mkfifo "#file201"
+rm -f "#file133"
+mkfifo "#file133"
+rm -f "#file144"
+mkfifo "#file144"
+rm -f "#file63"
+mkfifo "#file63"
+rm -f "#file103"
+mkfifo "#file103"
+rm -f "#file90"
+mkfifo "#file90"
+rm -f "#file204"
+mkfifo "#file204"
+rm -f "#file136"
+mkfifo "#file136"
+rm -f "#file130"
+mkfifo "#file130"
+rm -f "#file141"
+mkfifo "#file141"
+rm -f "#file106"
+mkfifo "#file106"
+rm -f "#file65"
+mkfifo "#file65"
+rm -f "#file197"
+mkfifo "#file197"
+rm -f "#file114"
+mkfifo "#file114"
+rm -f "#file68"
+mkfifo "#file68"
+rm -f "#file205"
+mkfifo "#file205"
+rm -f "#file97"
+mkfifo "#file97"
+rm -f "#file80"
+mkfifo "#file80"
+rm -f "#file148"
+mkfifo "#file148"
+rm -f "#file88"
+mkfifo "#file88"
+rm -f "#file104"
+mkfifo "#file104"
+rm -f "#file176"
+mkfifo "#file176"
+rm -f "#file91"
+mkfifo "#file91"
+rm -f "#file111"
+mkfifo "#file111"
+rm -f "#file198"
+mkfifo "#file198"
+rm -f "#file131"
+mkfifo "#file131"
+rm -f "#file171"
+mkfifo "#file171"
+rm -f "#file86"
+mkfifo "#file86"
+rm -f "#file100"
+mkfifo "#file100"
+rm -f "#file134"
+mkfifo "#file134"
+rm -f "#file76"
+mkfifo "#file76"
+rm -f "#file175"
+mkfifo "#file175"
+rm -f "#file120"
+mkfifo "#file120"
+rm -f "#file110"
+mkfifo "#file110"
+rm -f "#file123"
+mkfifo "#file123"
+rm -f "#file89"
+mkfifo "#file89"
+rm -f "#file194"
+mkfifo "#file194"
+rm -f "#file84"
+mkfifo "#file84"
+rm -f "#file71"
+mkfifo "#file71"
+rm -f "#file78"
+mkfifo "#file78"
+rm -f "#file95"
+mkfifo "#file95"
+rm -f "#file173"
+mkfifo "#file173"
+rm -f "#file60"
+mkfifo "#file60"
+rm -f "#file164"
+mkfifo "#file164"
+rm -f "#file196"
+mkfifo "#file196"
+rm -f "#file81"
+mkfifo "#file81"
+rm -f "#file160"
+mkfifo "#file160"
+rm -f "#file165"
+mkfifo "#file165"
+rm -f "#file99"
+mkfifo "#file99"
+rm -f "#file189"
+mkfifo "#file189"
+rm -f "#file62"
+mkfifo "#file62"
+rm -f "#file102"
+mkfifo "#file102"
+rm -f "#file56"
+mkfifo "#file56"
+rm -f "#file152"
+mkfifo "#file152"
+rm -f "#file82"
+mkfifo "#file82"
+rm -f "#file66"
+mkfifo "#file66"
+rm -f "#file183"
+mkfifo "#file183"
+rm -f "#file128"
+mkfifo "#file128"
+rm -f "#file179"
+mkfifo "#file179"
+rm -f "#file167"
+mkfifo "#file167"
+rm -f "#file143"
+mkfifo "#file143"
+rm -f "#file57"
+mkfifo "#file57"
+rm -f "#file74"
+mkfifo "#file74"
+rm -f "#file87"
+mkfifo "#file87"
+rm -f "#file185"
+mkfifo "#file185"
+rm -f "#file158"
+mkfifo "#file158"
+rm -f "#file146"
+mkfifo "#file146"
+rm -f "#file125"
+mkfifo "#file125"
+rm -f "#file169"
+mkfifo "#file169"
+rm -f "#file188"
+mkfifo "#file188"
+rm -f "#file69"
+mkfifo "#file69"
+rm -f "#file73"
+mkfifo "#file73"
+rm -f "#file70"
+mkfifo "#file70"
+rm -f "#file127"
+mkfifo "#file127"
+rm -f "#file163"
+mkfifo "#file163"
+rm -f "#file150"
+mkfifo "#file150"
+rm -f "#file113"
+mkfifo "#file113"
+rm -f "#file93"
+mkfifo "#file93"
+rm -f "#file203"
+mkfifo "#file203"
+rm -f "#file129"
+mkfifo "#file129"
+rm -f "#file178"
+mkfifo "#file178"
+rm -f "#file139"
+mkfifo "#file139"
+rm -f "#file180"
+mkfifo "#file180"
+rm -f "#file153"
+mkfifo "#file153"
+rm -f "#file108"
+mkfifo "#file108"
+rm -f "#file193"
+mkfifo "#file193"
+rm -f "#file92"
+mkfifo "#file92"
+rm -f "#file116"
+mkfifo "#file116"
+rm -f "#file122"
+mkfifo "#file122"
+rm -f "#file112"
+mkfifo "#file112"
+rm -f "#file101"
+mkfifo "#file101"
+rm -f "#file137"
+mkfifo "#file137"
+rm -f "#file126"
+mkfifo "#file126"
+rm -f "#file159"
+mkfifo "#file159"
+rm -f "#file157"
+mkfifo "#file157"
+rm -f "#file195"
+mkfifo "#file195"
+rm -f "#file59"
+mkfifo "#file59"
+rm -f "#file64"
+mkfifo "#file64"
+rm -f "#file67"
+mkfifo "#file67"
+rm -f "#file98"
+mkfifo "#file98"
+cat $IN > "#file57" &
+cat $IN > "#file58" &
+cat $IN > "#file59" &
+cat $IN > "#file60" &
+cat $IN > "#file61" &
+cat $IN > "#file62" &
+cat $IN > "#file63" &
+cat $IN > "#file64" &
+cat $IN > "#file65" &
+cat $IN > "#file66" &
+cat $IN > "#file67" &
+cat $IN > "#file68" &
+cat $IN > "#file69" &
+cat $IN > "#file70" &
+cat $IN > "#file71" &
+cat $IN > "#file72" &
+cat $IN > "#file73" &
+cat $IN > "#file74" &
+cat $IN > "#file75" &
+cat $IN > "#file76" &
+cat $IN > "#file77" &
+cat $IN > "#file78" &
+cat $IN > "#file79" &
+cat $IN > "#file80" &
+cat $IN > "#file81" &
+cat $IN > "#file82" &
+cat $IN > "#file83" &
+cat $IN > "#file84" &
+cat $IN > "#file85" &
+cat $IN > "#file86" &
+cat $IN > "#file87" &
+cat $IN > "#file88" &
+cat $IN > "#file89" &
+cat $IN > "#file90" &
+cat $IN > "#file91" &
+cat $IN > "#file92" &
+cat $IN > "#file93" &
+cat $IN > "#file94" &
+cat $IN > "#file95" &
+cat $IN > "#file96" &
+cat $IN > "#file97" &
+cat $IN > "#file98" &
+cat $IN > "#file99" &
+cat $IN > "#file100" &
+cat $IN > "#file101" &
+cat $IN > "#file102" &
+cat $IN > "#file103" &
+cat $IN > "#file104" &
+cat $IN > "#file105" &
+cat $IN > "#file106" &
+cat "#file57" | tr A-Z a-z > "#file107" &
+cat "#file58" | tr A-Z a-z > "#file108" &
+cat "#file59" | tr A-Z a-z > "#file109" &
+cat "#file60" | tr A-Z a-z > "#file110" &
+cat "#file61" | tr A-Z a-z > "#file111" &
+cat "#file62" | tr A-Z a-z > "#file112" &
+cat "#file63" | tr A-Z a-z > "#file113" &
+cat "#file64" | tr A-Z a-z > "#file114" &
+cat "#file65" | tr A-Z a-z > "#file115" &
+cat "#file66" | tr A-Z a-z > "#file116" &
+cat "#file67" | tr A-Z a-z > "#file117" &
+cat "#file68" | tr A-Z a-z > "#file118" &
+cat "#file69" | tr A-Z a-z > "#file119" &
+cat "#file70" | tr A-Z a-z > "#file120" &
+cat "#file71" | tr A-Z a-z > "#file121" &
+cat "#file72" | tr A-Z a-z > "#file122" &
+cat "#file73" | tr A-Z a-z > "#file123" &
+cat "#file74" | tr A-Z a-z > "#file124" &
+cat "#file75" | tr A-Z a-z > "#file125" &
+cat "#file76" | tr A-Z a-z > "#file126" &
+cat "#file77" | tr A-Z a-z > "#file127" &
+cat "#file78" | tr A-Z a-z > "#file128" &
+cat "#file79" | tr A-Z a-z > "#file129" &
+cat "#file80" | tr A-Z a-z > "#file130" &
+cat "#file81" | tr A-Z a-z > "#file131" &
+cat "#file82" | tr A-Z a-z > "#file132" &
+cat "#file83" | tr A-Z a-z > "#file133" &
+cat "#file84" | tr A-Z a-z > "#file134" &
+cat "#file85" | tr A-Z a-z > "#file135" &
+cat "#file86" | tr A-Z a-z > "#file136" &
+cat "#file87" | tr A-Z a-z > "#file137" &
+cat "#file88" | tr A-Z a-z > "#file138" &
+cat "#file89" | tr A-Z a-z > "#file139" &
+cat "#file90" | tr A-Z a-z > "#file140" &
+cat "#file91" | tr A-Z a-z > "#file141" &
+cat "#file92" | tr A-Z a-z > "#file142" &
+cat "#file93" | tr A-Z a-z > "#file143" &
+cat "#file94" | tr A-Z a-z > "#file144" &
+cat "#file95" | tr A-Z a-z > "#file145" &
+cat "#file96" | tr A-Z a-z > "#file146" &
+cat "#file97" | tr A-Z a-z > "#file147" &
+cat "#file98" | tr A-Z a-z > "#file148" &
+cat "#file99" | tr A-Z a-z > "#file149" &
+cat "#file100" | tr A-Z a-z > "#file150" &
+cat "#file101" | tr A-Z a-z > "#file151" &
+cat "#file102" | tr A-Z a-z > "#file152" &
+cat "#file103" | tr A-Z a-z > "#file153" &
+cat "#file104" | tr A-Z a-z > "#file154" &
+cat "#file105" | tr A-Z a-z > "#file155" &
+cat "#file106" | tr A-Z a-z > "#file156" &
+cat "#file107" | sort  > "#file157" &
+cat "#file108" | sort  > "#file158" &
+cat "#file109" | sort  > "#file159" &
+cat "#file110" | sort  > "#file160" &
+cat "#file111" | sort  > "#file161" &
+cat "#file112" | sort  > "#file162" &
+cat "#file113" | sort  > "#file163" &
+cat "#file114" | sort  > "#file164" &
+cat "#file115" | sort  > "#file165" &
+cat "#file116" | sort  > "#file166" &
+cat "#file117" | sort  > "#file167" &
+cat "#file118" | sort  > "#file168" &
+cat "#file119" | sort  > "#file169" &
+cat "#file120" | sort  > "#file170" &
+cat "#file121" | sort  > "#file171" &
+cat "#file122" | sort  > "#file172" &
+cat "#file123" | sort  > "#file173" &
+cat "#file124" | sort  > "#file174" &
+cat "#file125" | sort  > "#file175" &
+cat "#file126" | sort  > "#file176" &
+cat "#file127" | sort  > "#file177" &
+cat "#file128" | sort  > "#file178" &
+cat "#file129" | sort  > "#file179" &
+cat "#file130" | sort  > "#file180" &
+cat "#file131" | sort  > "#file181" &
+cat "#file132" | sort  > "#file182" &
+cat "#file133" | sort  > "#file183" &
+cat "#file134" | sort  > "#file184" &
+cat "#file135" | sort  > "#file185" &
+cat "#file136" | sort  > "#file186" &
+cat "#file137" | sort  > "#file187" &
+cat "#file138" | sort  > "#file188" &
+cat "#file139" | sort  > "#file189" &
+cat "#file140" | sort  > "#file190" &
+cat "#file141" | sort  > "#file191" &
+cat "#file142" | sort  > "#file192" &
+cat "#file143" | sort  > "#file193" &
+cat "#file144" | sort  > "#file194" &
+cat "#file145" | sort  > "#file195" &
+cat "#file146" | sort  > "#file196" &
+cat "#file147" | sort  > "#file197" &
+cat "#file148" | sort  > "#file198" &
+cat "#file149" | sort  > "#file199" &
+cat "#file150" | sort  > "#file200" &
+cat "#file151" | sort  > "#file201" &
+cat "#file152" | sort  > "#file202" &
+cat "#file153" | sort  > "#file203" &
+cat "#file154" | sort  > "#file204" &
+cat "#file155" | sort  > "#file205" &
+cat "#file156" | sort  > "#file206" &
+sort -m "#file157" "#file158" "#file159" "#file160" "#file161" "#file162" "#file163" "#file164" "#file165" "#file166" "#file167" "#file168" "#file169" "#file170" "#file171" "#file172" "#file173" "#file174" "#file175" "#file176" "#file177" "#file178" "#file179" "#file180" "#file181" "#file182" "#file183" "#file184" "#file185" "#file186" "#file187" "#file188" "#file189" "#file190" "#file191" "#file192" "#file193" "#file194" "#file195" "#file196" "#file197" "#file198" "#file199" "#file200" "#file201" "#file202" "#file203" "#file204" "#file205" "#file206" > "#file56" &
+cat "#file56" > /tmp/dish_output//0
+rm -f "#file105"
+rm -f "#file190"
+rm -f "#file118"
+rm -f "#file206"
+rm -f "#file151"
+rm -f "#file115"
+rm -f "#file170"
+rm -f "#file155"
+rm -f "#file58"
+rm -f "#file168"
+rm -f "#file182"
+rm -f "#file85"
+rm -f "#file94"
+rm -f "#file187"
+rm -f "#file162"
+rm -f "#file154"
+rm -f "#file140"
+rm -f "#file107"
+rm -f "#file156"
+rm -f "#file138"
+rm -f "#file174"
+rm -f "#file161"
+rm -f "#file172"
+rm -f "#file177"
+rm -f "#file119"
+rm -f "#file77"
+rm -f "#file109"
+rm -f "#file200"
+rm -f "#file149"
+rm -f "#file202"
+rm -f "#file191"
+rm -f "#file199"
+rm -f "#file132"
+rm -f "#file186"
+rm -f "#file72"
+rm -f "#file124"
+rm -f "#file145"
+rm -f "#file117"
+rm -f "#file192"
+rm -f "#file181"
+rm -f "#file96"
+rm -f "#file142"
+rm -f "#file61"
+rm -f "#file83"
+rm -f "#file135"
+rm -f "#file75"
+rm -f "#file79"
+rm -f "#file121"
+rm -f "#file147"
+rm -f "#file166"
+rm -f "#file184"
+rm -f "#file201"
+rm -f "#file133"
+rm -f "#file144"
+rm -f "#file63"
+rm -f "#file103"
+rm -f "#file90"
+rm -f "#file204"
+rm -f "#file136"
+rm -f "#file130"
+rm -f "#file141"
+rm -f "#file106"
+rm -f "#file65"
+rm -f "#file197"
+rm -f "#file114"
+rm -f "#file68"
+rm -f "#file205"
+rm -f "#file97"
+rm -f "#file80"
+rm -f "#file148"
+rm -f "#file88"
+rm -f "#file104"
+rm -f "#file176"
+rm -f "#file91"
+rm -f "#file111"
+rm -f "#file198"
+rm -f "#file131"
+rm -f "#file171"
+rm -f "#file86"
+rm -f "#file100"
+rm -f "#file134"
+rm -f "#file76"
+rm -f "#file175"
+rm -f "#file120"
+rm -f "#file110"
+rm -f "#file123"
+rm -f "#file89"
+rm -f "#file194"
+rm -f "#file84"
+rm -f "#file71"
+rm -f "#file78"
+rm -f "#file95"
+rm -f "#file173"
+rm -f "#file60"
+rm -f "#file164"
+rm -f "#file196"
+rm -f "#file81"
+rm -f "#file160"
+rm -f "#file165"
+rm -f "#file99"
+rm -f "#file189"
+rm -f "#file62"
+rm -f "#file102"
+rm -f "#file56"
+rm -f "#file152"
+rm -f "#file82"
+rm -f "#file66"
+rm -f "#file183"
+rm -f "#file128"
+rm -f "#file179"
+rm -f "#file167"
+rm -f "#file143"
+rm -f "#file57"
+rm -f "#file74"
+rm -f "#file87"
+rm -f "#file185"
+rm -f "#file158"
+rm -f "#file146"
+rm -f "#file125"
+rm -f "#file169"
+rm -f "#file188"
+rm -f "#file69"
+rm -f "#file73"
+rm -f "#file70"
+rm -f "#file127"
+rm -f "#file163"
+rm -f "#file150"
+rm -f "#file113"
+rm -f "#file93"
+rm -f "#file203"
+rm -f "#file129"
+rm -f "#file178"
+rm -f "#file139"
+rm -f "#file180"
+rm -f "#file153"
+rm -f "#file108"
+rm -f "#file193"
+rm -f "#file92"
+rm -f "#file116"
+rm -f "#file122"
+rm -f "#file112"
+rm -f "#file101"
+rm -f "#file137"
+rm -f "#file126"
+rm -f "#file159"
+rm -f "#file157"
+rm -f "#file195"
+rm -f "#file59"
+rm -f "#file64"
+rm -f "#file67"
+rm -f "#file98"
--- front_end/translate_evaluation_scripts.sh
+++ front_end/translate_evaluation_scripts.sh
@@ -4,3 +4,8 @@ distr_output_dir=/tmp/dish_output/
 
 ## Call the translate for all the scripts
 ./translate_script.sh "../evaluation/minimal_sort_2" $distr_output_dir
+./translate_script.sh "../evaluation/minimal_sort_4" $distr_output_dir
+./translate_script.sh "../evaluation/minimal_sort_10" $distr_output_dir
+./translate_script.sh "../evaluation/minimal_sort_20" $distr_output_dir
+./translate_script.sh "../evaluation/minimal_sort_50" $distr_output_dir
+./translate_script.sh "../evaluation/minimal_sort_100" $distr_output_dir
