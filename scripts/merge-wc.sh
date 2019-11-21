1296ae6c1e1ee5620c4a29e0f83fc79b23400b2a
Nikos Vasilakis <nikos@vasilak.is>
2019-11-20 21:34:01 -0500

    Update wc

--- scripts/merge-wc.sh
+++ scripts/merge-wc.sh
@@ -1,1 +1,1 @@
-paste -d '+' <(cat * | wc | tr -s ' '  '\n' | tail -n +2) <(cat * | wc | tr -s ' '  '\n' | tail -n +2) | bc
+paste -d '+' <(cat * | wc | tr -s ' '  '\n' | tail -n +2) <(cat * | wc | tr -s ' '  '\n' | tail -n +2) | bc | tr -s '\n'  ' ' | sed 's/^/   /' | sed 's/$/\ /'
