e2a275ca8fa3dc23d85a35e21aa7614409d7b85c
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2019-11-22 01:41:54 -0500

    Add more scripts for evaluation

--- front_end/execute_evaluation_scripts.sh
+++ front_end/execute_evaluation_scripts.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
 
-./execute_evaluation_script.sh "minimal_sort_2" 
-./execute_evaluation_script.sh "minimal_sort_4" 
-./execute_evaluation_script.sh "minimal_sort_10" 
-./execute_evaluation_script.sh "minimal_sort_20" 
-./execute_evaluation_script.sh "minimal_sort_50" 
-./execute_evaluation_script.sh "minimal_sort_100"
+./execute_compile_evaluation_script.sh "minimal_sort_2" $distr_output_dir 1 0
+./execute_compile_evaluation_script.sh "minimal_sort_4" $distr_output_dir 1 0
+./execute_compile_evaluation_script.sh "minimal_sort_10" $distr_output_dir 1 0
+./execute_compile_evaluation_script.sh "minimal_sort_20" $distr_output_dir 1 0
+./execute_compile_evaluation_script.sh "minimal_sort_50" $distr_output_dir 1 0
+./execute_compile_evaluation_script.sh "minimal_sort_100" $distr_output_dir 1 0
 
-./execute_evaluation_script.sh "minimal_grep_1"
-./execute_evaluation_script.sh "minimal_grep_2"
-./execute_evaluation_script.sh "minimal_grep_4"
-./execute_evaluation_script.sh "minimal_grep_10"
-./execute_evaluation_script.sh "minimal_grep_20"
-./execute_evaluation_script.sh "minimal_grep_50"
-./execute_evaluation_script.sh "minimal_grep_100"
-./execute_evaluation_script.sh "minimal_grep_200" 
+./execute_compile_evaluation_script.sh "minimal_grep_2" $distr_output_dir 1 0
+./execute_compile_evaluation_script.sh "minimal_grep_4" $distr_output_dir 1 0
+./execute_compile_evaluation_script.sh "minimal_grep_10" $distr_output_dir 1 0
+./execute_compile_evaluation_script.sh "minimal_grep_20" $distr_output_dir 1 0
+./execute_compile_evaluation_script.sh "minimal_grep_50" $distr_output_dir 1 0
+./execute_compile_evaluation_script.sh "minimal_grep_100" $distr_output_dir 1 0
+./execute_compile_evaluation_script.sh "minimal_grep_200" $distr_output_dir 1 0
 
-./execute_evaluation_script.sh "spell_1" 
+# ./execute_compile_evaluation_script.sh "spell_1" 
 
-./execute_evaluation_script.sh "topn_2" $distr_output_dir 2 200000
+## This has to be tuned (fan-out and batch size)
+./execute_compile_evaluation_script.sh "topn_2" $distr_output_dir 1 0
+./execute_compile_evaluation_script.sh "topn_4" $distr_output_dir 1 0
+./execute_compile_evaluation_script.sh "topn_10" $distr_output_dir 1 0
+./execute_compile_evaluation_script.sh "topn_20" $distr_output_dir 1 0
+./execute_compile_evaluation_script.sh "topn_50" $distr_output_dir 1 0
+./execute_compile_evaluation_script.sh "topn_100" $distr_output_dir 1 0
 
-./execute_evaluation_script.sh "wf_2" 
+./execute_compile_evaluation_script.sh "wf_2" $distr_output_dir 1 0
+./execute_compile_evaluation_script.sh "wf_4" $distr_output_dir 1 0
+./execute_compile_evaluation_script.sh "wf_10" $distr_output_dir 1 0
+./execute_compile_evaluation_script.sh "wf_20" $distr_output_dir 1 0
+./execute_compile_evaluation_script.sh "wf_50" $distr_output_dir 1 0
+./execute_compile_evaluation_script.sh "wf_100" $distr_output_dir 1 0
--- front_end/translate_evaluation_scripts.sh
+++ front_end/translate_evaluation_scripts.sh
@@ -3,21 +3,21 @@
 distr_output_dir=/tmp/dish_output/
 
 ## Call the translate for all the scripts
-./translate_script.sh "../evaluation/minimal_sort_2" $distr_output_dir
-./translate_script.sh "../evaluation/minimal_sort_4" $distr_output_dir
-./translate_script.sh "../evaluation/minimal_sort_10" $distr_output_dir
-./translate_script.sh "../evaluation/minimal_sort_20" $distr_output_dir
-./translate_script.sh "../evaluation/minimal_sort_50" $distr_output_dir
-./translate_script.sh "../evaluation/minimal_sort_100" $distr_output_dir
+./translate_script.sh "../evaluation/minimal_sort_2" $distr_output_dir 1 0
+./translate_script.sh "../evaluation/minimal_sort_4" $distr_output_dir 1 0
+./translate_script.sh "../evaluation/minimal_sort_10" $distr_output_dir 1 0
+./translate_script.sh "../evaluation/minimal_sort_20" $distr_output_dir 1 0
+./translate_script.sh "../evaluation/minimal_sort_50" $distr_output_dir 1 0
+./translate_script.sh "../evaluation/minimal_sort_100" $distr_output_dir 1 0
 
 ./translate_script.sh "../evaluation/minimal_grep_1" $distr_output_dir 1 0
-./translate_script.sh "../evaluation/minimal_grep_2" $distr_output_dir
-./translate_script.sh "../evaluation/minimal_grep_4" $distr_output_dir
-./translate_script.sh "../evaluation/minimal_grep_10" $distr_output_dir
-./translate_script.sh "../evaluation/minimal_grep_20" $distr_output_dir
-./translate_script.sh "../evaluation/minimal_grep_50" $distr_output_dir
-./translate_script.sh "../evaluation/minimal_grep_100" $distr_output_dir
-./translate_script.sh "../evaluation/minimal_grep_200" $distr_output_dir
+./translate_script.sh "../evaluation/minimal_grep_2" $distr_output_dir 1 0
+./translate_script.sh "../evaluation/minimal_grep_4" $distr_output_dir 1 0
+./translate_script.sh "../evaluation/minimal_grep_10" $distr_output_dir 1 0
+./translate_script.sh "../evaluation/minimal_grep_20" $distr_output_dir 1 0
+./translate_script.sh "../evaluation/minimal_grep_50" $distr_output_dir 1 0
+./translate_script.sh "../evaluation/minimal_grep_100" $distr_output_dir 1 0
+./translate_script.sh "../evaluation/minimal_grep_200" $distr_output_dir 1 0
 
 ./translate_script.sh "../evaluation/spell_1" $distr_output_dir
 
