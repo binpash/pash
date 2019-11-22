fefa38d242c4bd314180de3c177aaf78a11ac5b2
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2019-11-22 01:06:39 -0500

    stash

--- front_end/execute_compile_evaluation_script.sh
+++ front_end/execute_compile_evaluation_script.sh
@@ -4,9 +4,8 @@
 ## distributed, and an environment
 experiment=$1
 distr_output_dir=$2
-cat_at_end=$3
-fan_out=$4
-batch_size=$5
+fan_out=$3
+batch_size=$4
 
 
 directory="../evaluation/"
@@ -26,13 +25,10 @@ export $(cut -d= -f1 $env_file)
 # { time /bin/bash $seq_script > /tmp/seq_output ; } 2> >(tee "${results}${experiment}_seq.time" >&2)
 
 echo "Distributed:"
-if [ "$cat_at_end" = true ] ;
-then
-    echo "Execute with cat"
-    { time { ./translate_script.sh $prefix $distr_output_dir $fan_out $batch_size ; /bin/bash $distr_script ; ./cat_output_files.sh $distr_output_dir > /tmp/distr_output_complete ; } ; } 2> >(tee "${results}${experiment}_compile_cat_distr.time" >&2)
-else
-    { time { ./translate_script.sh $prefix $distr_output_dir $fan_out $batch_size ; /bin/bash $distr_script ; } ; } 2> >(tee "${results}${experiment}_compile_distr.time" >&2)
-fi
+{ time ./translate_script.sh $prefix $distr_output_dir $fan_out $batch_size ; } 2> >(tee "${results}${experiment}_compile_distr.time" >&2)
+{ time /bin/bash $distr_script ; } 2> >(tee "${results}${experiment}_distr.time" >&2)
+{ time ./cat_output_files.sh $distr_output_dir > /tmp/distr_output_complete ; } 2> >(tee "${results}${experiment}_cat_distr.time" >&2)
+
 
 
 
