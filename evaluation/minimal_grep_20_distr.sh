88de206afe8eb590d008290665713bd78be91ea7
Nikos Vasilakis <nikos@vasilak.is>
2019-11-22 10:45:35 -0500

    New results without fan out

--- evaluation/minimal_grep_20_distr.sh
+++ evaluation/minimal_grep_20_distr.sh
@@ -1,146 +1,146 @@
-rm -rf /tmp/dish_output/
-mkdir -p /tmp/dish_output/
+rm -rf 1
+mkdir -p 1
 mkdir -p /dev/shm/dish
-rm -f "#file58"
-mkfifo "#file58"
-rm -f "#file84"
-mkfifo "#file84"
-rm -f "#file40"
-mkfifo "#file40"
-rm -f "#file46"
-mkfifo "#file46"
-rm -f "#file61"
-mkfifo "#file61"
-rm -f "#file34"
-mkfifo "#file34"
-rm -f "#file37"
-mkfifo "#file37"
-rm -f "#file50"
-mkfifo "#file50"
-rm -f "#file64"
-mkfifo "#file64"
-rm -f "#file68"
-mkfifo "#file68"
-rm -f "#file53"
-mkfifo "#file53"
-rm -f "#file72"
-mkfifo "#file72"
-rm -f "#file86"
-mkfifo "#file86"
-rm -f "#file43"
-mkfifo "#file43"
-rm -f "#file76"
-mkfifo "#file76"
-rm -f "#file65"
-mkfifo "#file65"
-rm -f "#file47"
-mkfifo "#file47"
-rm -f "#file81"
-mkfifo "#file81"
-rm -f "#file33"
-mkfifo "#file33"
-rm -f "#file74"
-mkfifo "#file74"
-rm -f "#file27"
-mkfifo "#file27"
-rm -f "#file56"
-mkfifo "#file56"
-rm -f "#file69"
-mkfifo "#file69"
-rm -f "#file75"
-mkfifo "#file75"
-rm -f "#file73"
-mkfifo "#file73"
-rm -f "#file78"
-mkfifo "#file78"
 rm -f "#file54"
 mkfifo "#file54"
+rm -f "#file36"
+mkfifo "#file36"
+rm -f "#file78"
+mkfifo "#file78"
+rm -f "#file75"
+mkfifo "#file75"
+rm -f "#file34"
+mkfifo "#file34"
+rm -f "#file43"
+mkfifo "#file43"
 rm -f "#file29"
 mkfifo "#file29"
+rm -f "#file62"
+mkfifo "#file62"
+rm -f "#file41"
+mkfifo "#file41"
+rm -f "#file50"
+mkfifo "#file50"
+rm -f "#file48"
+mkfifo "#file48"
+rm -f "#file38"
+mkfifo "#file38"
+rm -f "#file46"
+mkfifo "#file46"
+rm -f "#file63"
+mkfifo "#file63"
+rm -f "#file40"
+mkfifo "#file40"
+rm -f "#file86"
+mkfifo "#file86"
+rm -f "#file31"
+mkfifo "#file31"
+rm -f "#file66"
+mkfifo "#file66"
+rm -f "#file61"
+mkfifo "#file61"
+rm -f "#file33"
+mkfifo "#file33"
+rm -f "#file44"
+mkfifo "#file44"
+rm -f "#file82"
+mkfifo "#file82"
+rm -f "#file70"
+mkfifo "#file70"
+rm -f "#file68"
+mkfifo "#file68"
+rm -f "#file85"
+mkfifo "#file85"
+rm -f "#file58"
+mkfifo "#file58"
+rm -f "#file37"
+mkfifo "#file37"
+rm -f "#file79"
+mkfifo "#file79"
+rm -f "#file59"
+mkfifo "#file59"
+rm -f "#file57"
+mkfifo "#file57"
+rm -f "#file71"
+mkfifo "#file71"
+rm -f "#file53"
+mkfifo "#file53"
+rm -f "#file64"
+mkfifo "#file64"
+rm -f "#file30"
+mkfifo "#file30"
+rm -f "#file84"
+mkfifo "#file84"
+rm -f "#file73"
+mkfifo "#file73"
+rm -f "#file67"
+mkfifo "#file67"
+rm -f "#file80"
+mkfifo "#file80"
+rm -f "#file28"
+mkfifo "#file28"
+rm -f "#file56"
+mkfifo "#file56"
+rm -f "#file42"
+mkfifo "#file42"
+rm -f "#file52"
+mkfifo "#file52"
+rm -f "#file39"
+mkfifo "#file39"
+rm -f "#file74"
+mkfifo "#file74"
+rm -f "#file77"
+mkfifo "#file77"
+rm -f "#file35"
+mkfifo "#file35"
+rm -f "#file83"
+mkfifo "#file83"
+rm -f "#file55"
+mkfifo "#file55"
+rm -f "#file65"
+mkfifo "#file65"
+rm -f "#file72"
+mkfifo "#file72"
+rm -f "#file47"
+mkfifo "#file47"
+rm -f "#file76"
+mkfifo "#file76"
+rm -f "#file81"
+mkfifo "#file81"
+rm -f "#file51"
+mkfifo "#file51"
+rm -f "#file27"
+mkfifo "#file27"
+rm -f "#file32"
+mkfifo "#file32"
+rm -f "#file45"
+mkfifo "#file45"
 rm -f "#file49"
 mkfifo "#file49"
 rm -f "#file60"
 mkfifo "#file60"
-rm -f "#file44"
-mkfifo "#file44"
-rm -f "#file66"
-mkfifo "#file66"
-rm -f "#file83"
-mkfifo "#file83"
-rm -f "#file28"
-mkfifo "#file28"
-rm -f "#file62"
-mkfifo "#file62"
-rm -f "#file51"
-mkfifo "#file51"
-rm -f "#file70"
-mkfifo "#file70"
-rm -f "#file38"
-mkfifo "#file38"
-rm -f "#file39"
-mkfifo "#file39"
-rm -f "#file45"
-mkfifo "#file45"
-rm -f "#file63"
-mkfifo "#file63"
-rm -f "#file82"
-mkfifo "#file82"
-rm -f "#file48"
-mkfifo "#file48"
-rm -f "#file52"
-mkfifo "#file52"
-rm -f "#file71"
-mkfifo "#file71"
-rm -f "#file42"
-mkfifo "#file42"
-rm -f "#file77"
-mkfifo "#file77"
-rm -f "#file85"
-mkfifo "#file85"
-rm -f "#file67"
-mkfifo "#file67"
-rm -f "#file55"
-mkfifo "#file55"
-rm -f "#file36"
-mkfifo "#file36"
-rm -f "#file57"
-mkfifo "#file57"
-rm -f "#file80"
-mkfifo "#file80"
-rm -f "#file30"
-mkfifo "#file30"
-rm -f "#file59"
-mkfifo "#file59"
-rm -f "#file32"
-mkfifo "#file32"
-rm -f "#file79"
-mkfifo "#file79"
-rm -f "#file41"
-mkfifo "#file41"
-rm -f "#file31"
-mkfifo "#file31"
-rm -f "#file35"
-mkfifo "#file35"
-cat $IN > "#file27" &
-cat $IN > "#file28" &
-cat $IN > "#file29" &
-cat $IN > "#file30" &
-cat $IN > "#file31" &
-cat $IN > "#file32" &
-cat $IN > "#file33" &
-cat $IN > "#file34" &
-cat $IN > "#file35" &
-cat $IN > "#file36" &
-cat $IN > "#file37" &
-cat $IN > "#file38" &
-cat $IN > "#file39" &
-cat $IN > "#file40" &
-cat $IN > "#file41" &
-cat $IN > "#file42" &
-cat $IN > "#file43" &
-cat $IN > "#file44" &
-cat $IN > "#file45" &
-cat $IN > "#file46" &
+rm -f "#file69"
+mkfifo "#file69"
+cat ${IN} > "#file27" &
+cat ${IN} > "#file28" &
+cat ${IN} > "#file29" &
+cat ${IN} > "#file30" &
+cat ${IN} > "#file31" &
+cat ${IN} > "#file32" &
+cat ${IN} > "#file33" &
+cat ${IN} > "#file34" &
+cat ${IN} > "#file35" &
+cat ${IN} > "#file36" &
+cat ${IN} > "#file37" &
+cat ${IN} > "#file38" &
+cat ${IN} > "#file39" &
+cat ${IN} > "#file40" &
+cat ${IN} > "#file41" &
+cat ${IN} > "#file42" &
+cat ${IN} > "#file43" &
+cat ${IN} > "#file44" &
+cat ${IN} > "#file45" &
+cat ${IN} > "#file46" &
 cat "#file27" | tr A-Z a-z > "#file47" &
 cat "#file28" | tr A-Z a-z > "#file48" &
 cat "#file29" | tr A-Z a-z > "#file49" &
@@ -181,85 +181,85 @@ cat "#file63" | grep "\(.\).*\1\(.\).*\2\(.\).*\3\(.\).*\4" > "#file83" &
 cat "#file64" | grep "\(.\).*\1\(.\).*\2\(.\).*\3\(.\).*\4" > "#file84" &
 cat "#file65" | grep "\(.\).*\1\(.\).*\2\(.\).*\3\(.\).*\4" > "#file85" &
 cat "#file66" | grep "\(.\).*\1\(.\).*\2\(.\).*\3\(.\).*\4" > "#file86" &
-cat "#file67" > /tmp/dish_output//0 &
-cat "#file68" > /tmp/dish_output//1 &
-cat "#file69" > /tmp/dish_output//2 &
-cat "#file70" > /tmp/dish_output//3 &
-cat "#file71" > /tmp/dish_output//4 &
-cat "#file72" > /tmp/dish_output//5 &
-cat "#file73" > /tmp/dish_output//6 &
-cat "#file74" > /tmp/dish_output//7 &
-cat "#file75" > /tmp/dish_output//8 &
-cat "#file76" > /tmp/dish_output//9 &
-cat "#file77" > /tmp/dish_output//10 &
-cat "#file78" > /tmp/dish_output//11 &
-cat "#file79" > /tmp/dish_output//12 &
-cat "#file80" > /tmp/dish_output//13 &
-cat "#file81" > /tmp/dish_output//14 &
-cat "#file82" > /tmp/dish_output//15 &
-cat "#file83" > /tmp/dish_output//16 &
-cat "#file84" > /tmp/dish_output//17 &
-cat "#file85" > /tmp/dish_output//18 &
-cat "#file86" > /tmp/dish_output//19 &
+cat "#file67" > 1/0 &
+cat "#file68" > 1/1 &
+cat "#file69" > 1/2 &
+cat "#file70" > 1/3 &
+cat "#file71" > 1/4 &
+cat "#file72" > 1/5 &
+cat "#file73" > 1/6 &
+cat "#file74" > 1/7 &
+cat "#file75" > 1/8 &
+cat "#file76" > 1/9 &
+cat "#file77" > 1/10 &
+cat "#file78" > 1/11 &
+cat "#file79" > 1/12 &
+cat "#file80" > 1/13 &
+cat "#file81" > 1/14 &
+cat "#file82" > 1/15 &
+cat "#file83" > 1/16 &
+cat "#file84" > 1/17 &
+cat "#file85" > 1/18 &
+cat "#file86" > 1/19 &
 wait
-rm -f "#file58"
-rm -f "#file84"
-rm -f "#file40"
-rm -f "#file46"
-rm -f "#file61"
-rm -f "#file34"
-rm -f "#file37"
-rm -f "#file50"
-rm -f "#file64"
-rm -f "#file68"
-rm -f "#file53"
-rm -f "#file72"
-rm -f "#file86"
-rm -f "#file43"
-rm -f "#file76"
-rm -f "#file65"
-rm -f "#file47"
-rm -f "#file81"
-rm -f "#file33"
-rm -f "#file74"
-rm -f "#file27"
-rm -f "#file56"
-rm -f "#file69"
-rm -f "#file75"
-rm -f "#file73"
-rm -f "#file78"
 rm -f "#file54"
+rm -f "#file36"
+rm -f "#file78"
+rm -f "#file75"
+rm -f "#file34"
+rm -f "#file43"
 rm -f "#file29"
+rm -f "#file62"
+rm -f "#file41"
+rm -f "#file50"
+rm -f "#file48"
+rm -f "#file38"
+rm -f "#file46"
+rm -f "#file63"
+rm -f "#file40"
+rm -f "#file86"
+rm -f "#file31"
+rm -f "#file66"
+rm -f "#file61"
+rm -f "#file33"
+rm -f "#file44"
+rm -f "#file82"
+rm -f "#file70"
+rm -f "#file68"
+rm -f "#file85"
+rm -f "#file58"
+rm -f "#file37"
+rm -f "#file79"
+rm -f "#file59"
+rm -f "#file57"
+rm -f "#file71"
+rm -f "#file53"
+rm -f "#file64"
+rm -f "#file30"
+rm -f "#file84"
+rm -f "#file73"
+rm -f "#file67"
+rm -f "#file80"
+rm -f "#file28"
+rm -f "#file56"
+rm -f "#file42"
+rm -f "#file52"
+rm -f "#file39"
+rm -f "#file74"
+rm -f "#file77"
+rm -f "#file35"
+rm -f "#file83"
+rm -f "#file55"
+rm -f "#file65"
+rm -f "#file72"
+rm -f "#file47"
+rm -f "#file76"
+rm -f "#file81"
+rm -f "#file51"
+rm -f "#file27"
+rm -f "#file32"
+rm -f "#file45"
 rm -f "#file49"
 rm -f "#file60"
-rm -f "#file44"
-rm -f "#file66"
-rm -f "#file83"
-rm -f "#file28"
-rm -f "#file62"
-rm -f "#file51"
-rm -f "#file70"
-rm -f "#file38"
-rm -f "#file39"
-rm -f "#file45"
-rm -f "#file63"
-rm -f "#file82"
-rm -f "#file48"
-rm -f "#file52"
-rm -f "#file71"
-rm -f "#file42"
-rm -f "#file77"
-rm -f "#file85"
-rm -f "#file67"
-rm -f "#file55"
-rm -f "#file36"
-rm -f "#file57"
-rm -f "#file80"
-rm -f "#file30"
-rm -f "#file59"
-rm -f "#file32"
-rm -f "#file79"
-rm -f "#file41"
-rm -f "#file31"
-rm -f "#file35"
+rm -f "#file69"
 rm -rf "/dev/shm/dish"
--- evaluation/wf_2_distr.sh
+++ evaluation/wf_2_distr.sh
@@ -1,30 +1,30 @@
-rm -rf /tmp/distr_output
-mkdir -p /tmp/distr_output
+rm -rf 1
+mkdir -p 1
 mkdir -p /dev/shm/dish
-rm -f "#file17"
-mkfifo "#file17"
-rm -f "#file18"
-mkfifo "#file18"
-rm -f "#file22"
-mkfifo "#file22"
-rm -f "#file19"
-mkfifo "#file19"
-rm -f "#file12"
-mkfifo "#file12"
-rm -f "#file24"
-mkfifo "#file24"
-rm -f "#file10"
-mkfifo "#file10"
-rm -f "#file21"
-mkfifo "#file21"
-rm -f "#file16"
-mkfifo "#file16"
-rm -f "#file20"
-mkfifo "#file20"
 rm -f "#file23"
 mkfifo "#file23"
+rm -f "#file10"
+mkfifo "#file10"
+rm -f "#file19"
+mkfifo "#file19"
+rm -f "#file21"
+mkfifo "#file21"
+rm -f "#file22"
+mkfifo "#file22"
+rm -f "#file17"
+mkfifo "#file17"
+rm -f "#file24"
+mkfifo "#file24"
 rm -f "#file14"
 mkfifo "#file14"
+rm -f "#file12"
+mkfifo "#file12"
+rm -f "#file16"
+mkfifo "#file16"
+rm -f "#file18"
+mkfifo "#file18"
+rm -f "#file20"
+mkfifo "#file20"
 cat "#file10" | uniq -c > "#file12" &
 cat "#file12" | sort -rn > "#file14" &
 cat "#file14" | sed ${1}q > "#file16" &
@@ -37,18 +37,18 @@ cat "#file20" | tr A-Z a-z > "#file22" &
 cat "#file21" | sort  > "#file23" &
 cat "#file22" | sort  > "#file24" &
 sort -m --parallel=2 "#file23" "#file24" > "#file10" &
-cat "#file16" > /tmp/distr_output/0 &
+cat "#file16" > 1/0 &
 wait
-rm -f "#file17"
-rm -f "#file18"
-rm -f "#file22"
-rm -f "#file19"
-rm -f "#file12"
-rm -f "#file24"
-rm -f "#file10"
-rm -f "#file21"
-rm -f "#file16"
-rm -f "#file20"
 rm -f "#file23"
+rm -f "#file10"
+rm -f "#file19"
+rm -f "#file21"
+rm -f "#file22"
+rm -f "#file17"
+rm -f "#file24"
 rm -f "#file14"
+rm -f "#file12"
+rm -f "#file16"
+rm -f "#file18"
+rm -f "#file20"
 rm -rf "/dev/shm/dish"
