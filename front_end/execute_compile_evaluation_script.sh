f9a4edc47171ae69e9887e58f52ab1085500f933
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2019-11-22 00:05:02 -0500

    Extend script to count time with cat too

--- front_end/execute_compile_evaluation_script.sh
+++ front_end/execute_compile_evaluation_script.sh
@@ -4,8 +4,10 @@
 ## distributed, and an environment
 experiment=$1
 distr_output_dir=$2
-fan_out=$3
-batch_size=$4
+cat_at_end=$3
+fan_out=$4
+batch_size=$5
+
 
 directory="../evaluation/"
 results="${directory}results/"
@@ -24,7 +26,13 @@ export $(cut -d= -f1 $env_file)
 # { time /bin/bash $seq_script > /tmp/seq_output ; } 2> >(tee "${results}${experiment}_seq.time" >&2)
 
 echo "Distributed:"
-cat $distr_script | grep -v "rm -f" | grep -v "mkfifo"
-{ time { ./translate_script.sh $prefix $distr_output_dir $fan_out $batch_size ; /bin/bash $distr_script ; } ; } 2> >(tee "${results}${experiment}_compile_distr.time" >&2)
+if [ "$cat_at_end" = true ] ;
+then
+    echo "Execute with cat"
+    { time { ./translate_script.sh $prefix $distr_output_dir $fan_out $batch_size ; /bin/bash $distr_script ; ./cat_output_files.sh $distr_output_dir > /tmp/distr_output_complete ; } ; } 2> >(tee "${results}${experiment}_compile_cat_distr.time" >&2)
+else
+    { time { ./translate_script.sh $prefix $distr_output_dir $fan_out $batch_size ; /bin/bash $distr_script ; } ; } 2> >(tee "${results}${experiment}_compile_distr.time" >&2)
+fi
+
 
 
