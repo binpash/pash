95eba7d0b15f3aa93b15b4904dd1bf9a75c00433
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2019-11-21 23:36:16 -0500

    New distributed scripts

--- evaluation/minimal_grep_1_distr.sh
+++ evaluation/minimal_grep_1_distr.sh
@@ -1,35 +1,18 @@
+rm -rf /tmp/dish_output/
 mkdir -p /tmp/dish_output/
-rm -f "#file14"
-mkfifo "#file14"
-rm -f "#file12"
-mkfifo "#file12"
-rm -f "#file8"
-mkfifo "#file8"
-rm -f "#file15"
-mkfifo "#file15"
-rm -f "#file9"
-mkfifo "#file9"
+mkdir -p /dev/shm/dish
+rm -f "#file5"
+mkfifo "#file5"
+rm -f "#file7"
+mkfifo "#file7"
 rm -f "#file2"
 mkfifo "#file2"
-rm -f "#file13"
-mkfifo "#file13"
 cat $IN > "#file2" &
-{ head -n 100 "#file2" > "#file8" ; cat "#file2" > "#file9" ; } &
-cat "#file8" | tr A-Z a-z > "#file12" &
-cat "#file9" | tr A-Z a-z > "#file13" &
-cat "#file12" | grep "\(.\).*\1\(.\).*\2\(.\).*\3\(.\).*\4" > "#file14" &
-cat "#file13" | grep "\(.\).*\1\(.\).*\2\(.\).*\3\(.\).*\4" > "#file15" &
-cat "#file14" > /tmp/dish_output//0 &
-cat "#file15" > /tmp/dish_output//1 &
-for job in `jobs -p` 
-do 
- echo $job
- wait $job 
-done
-rm -f "#file14"
-rm -f "#file12"
-rm -f "#file8"
-rm -f "#file15"
-rm -f "#file9"
+cat "#file2" | tr A-Z a-z > "#file5" &
+cat "#file5" | grep "\(.\).*\1\(.\).*\2\(.\).*\3\(.\).*\4" > "#file7" &
+cat "#file7" > /tmp/dish_output//0 &
+wait
+rm -f "#file5"
+rm -f "#file7"
 rm -f "#file2"
-rm -f "#file13"
+rm -rf "/dev/shm/dish"
