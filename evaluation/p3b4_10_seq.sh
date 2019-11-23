159575b17ce48cc473e93da8c25a7b339d8f5a1d
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2019-11-22 22:53:29 -0500

    max-temp running script

---
+++ evaluation/p3b4_10_seq.sh
@@ -0,0 +1,4 @@
+curl -s "$IN_DIR/p3a_10_1.txt" "$IN_DIR/p3a_10_2.txt" "$IN_DIR/p3a_10_3.txt" "$IN_DIR/p3a_10_4.txt" "$IN_DIR/p3a_10_5.txt" "$IN_DIR/p3a_10_6.txt" "$IN_DIR/p3a_10_7.txt" "$IN_DIR/p3a_10_8.txt" "$IN_DIR/p3a_10_9.txt" "$IN_DIR/p3a_10_10.txt" | gunzip |   cut -c 89-92 |
+  grep -v 999 |
+  sort -rn |
+  head -n1
---
+++ evaluation/p3b_1_seq.sh
@@ -0,0 +1,1 @@
+curl -s "$IN_DIR/p3a.txt" | gunzip
