4aa540139c2581bebb7fac127addf1373077d426
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2019-11-22 23:01:10 -0500

    Minor change

--- evaluation/p3b_10_seq.sh
+++ evaluation/p3b_10_seq.sh
@@ -1,1 +1,1 @@
-curl -s "$IN_DIR/p3a_10_1.txt" "$IN_DIR/p3a_10_2.txt" "$IN_DIR/p3a_10_3.txt" "$IN_DIR/p3a_10_4.txt" "$IN_DIR/p3a_10_5.txt" "$IN_DIR/p3a_10_6.txt" "$IN_DIR/p3a_10_7.txt" "$IN_DIR/p3a_10_8.txt" "$IN_DIR/p3a_10_9.txt" "$IN_DIR/p3a_10_10.txt" | gunzip
+cat "$IN_DIR/p3a_10_1.txt" "$IN_DIR/p3a_10_2.txt" "$IN_DIR/p3a_10_3.txt" "$IN_DIR/p3a_10_4.txt" "$IN_DIR/p3a_10_5.txt" "$IN_DIR/p3a_10_6.txt" "$IN_DIR/p3a_10_7.txt" "$IN_DIR/p3a_10_8.txt" "$IN_DIR/p3a_10_9.txt" "$IN_DIR/p3a_10_10.txt" | xargs -n1 curl -s | gunzip
