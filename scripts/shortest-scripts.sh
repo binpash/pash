00c2ca8b19471d716ea9ca6c3c034272ebcc2ee7
Nikos Vasilakis <nikos@vasilak.is>
2019-11-23 00:23:14 -0500

    Update shortest scripts

--- scripts/shortest-scripts.sh
+++ scripts/shortest-scripts.sh
@@ -3,4 +3,5 @@
 # From "Wicked Cool Shell Scripts", 2nd Ed., pg. 7
 # +p.95 multiple sed
 # +p.XX crawler
-file /usr/bin/* | grep "shell script" | cut -d: -f1 | xargs wc -l | sort -n | head -15
+
+cat ./input/cmds10x.txt | xargs file | grep "shell script" | cut -d: -f1 | xargs wc -l | sort -n | head -15
