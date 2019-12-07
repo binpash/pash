ba0f2cdfb39ee6ac5915d3d863f1bb39d900a8a3
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2019-12-07 14:28:43 -0500

    Rename main to dish

--- compiler/execute_compile_evaluation_script.sh
+++ compiler/execute_compile_evaluation_script.sh
@@ -27,7 +27,7 @@ cat $seq_script | grep -v "rm -f" | grep -v "mkfifo"
 echo "Distributed:"
 { time ./translate_script.sh $prefix $distr_output_dir $fan_out $batch_size ; } 2> >(tee "${results}${experiment}_compile_distr.time" >&2)
 
-## Obsolete as main.py executes output script
+## Obsolete as dish.py executes output script
 # { time /bin/bash $distr_script ; } 2> >(tee "${results}${experiment}_distr.time" >&2)
 ## TODO: Move in impl.py and add a flag in main that enables/disables
 ## concatenating the output files
