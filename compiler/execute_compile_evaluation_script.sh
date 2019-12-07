96c1d91d31d0a36d28c2fc563aec38d1135a084d
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2019-12-07 14:08:16 -0500

    Modify execute_compile script as main now executes script

--- compiler/execute_compile_evaluation_script.sh
+++ compiler/execute_compile_evaluation_script.sh
@@ -26,8 +26,12 @@ cat $seq_script | grep -v "rm -f" | grep -v "mkfifo"
 
 echo "Distributed:"
 { time ./translate_script.sh $prefix $distr_output_dir $fan_out $batch_size ; } 2> >(tee "${results}${experiment}_compile_distr.time" >&2)
-{ time /bin/bash $distr_script ; } 2> >(tee "${results}${experiment}_distr.time" >&2)
-{ time ./cat_output_files.sh $distr_output_dir > /tmp/distr_output_complete ; } 2> >(tee "${results}${experiment}_cat_distr.time" >&2)
+
+## Obsolete as main.py executes output script
+# { time /bin/bash $distr_script ; } 2> >(tee "${results}${experiment}_distr.time" >&2)
+## TODO: Move in impl.py and add a flag in main that enables/disables
+## concatenating the output files
+# { time ./cat_output_files.sh $distr_output_dir > /tmp/distr_output_complete ; } 2> >(tee "${results}${experiment}_cat_distr.time" >&2)
 
 
 
