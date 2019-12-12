1a65a9fdf68f6349328556c189583d656948e11f
Achilles Benetopoulos <abenetopoulos@gmail.com>
2019-12-12 20:44:04 +0200

    Separate config by component

--- compiler/distr_plan.py
+++ compiler/distr_plan.py
@@ -16,14 +16,21 @@ config = {}
 
 def load_config():
     global config
+    dish_config = {}
+    CONFIG_KEY = 'distr_planner'
 
     ## TODO allow this to be passed as an argument
     config_file_path = 'config.yaml'
     with open(config_file_path) as config_file:
-        config = yaml.load(config_file, Loader=yaml.FullLoader)
+        dish_config = yaml.load(config_file, Loader=yaml.FullLoader)
 
-    if not config:
+    if not dish_config:
         raise Exception('No valid configuration could be loaded from {}'.format(config_file_path))
+
+    if CONFIG_KEY not in dish_config:
+        raise Exception('Missing `{}` config in {}'.format(CONFIG_KEY, config_file_path))
+
+    config = dish_config[CONFIG_KEY]
 
 def main():
     load_config()
