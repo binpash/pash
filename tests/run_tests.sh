ebbe788a86382cbc1d2d362e956e00a8b0a404a8
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2019-12-14 19:48:36 -0500

    A note in tests and remove an uneccessary script

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,6 +4,7 @@
 TestsArray=("minimal_sort_2" \
             "minimal_grep_2")
 
+## TODO: Exit if any diff_test fails
 for test_name in ${TestsArray[*]}; do
      ./diff_test.sh $test_name /tmp/distr_output 1 100
 done
