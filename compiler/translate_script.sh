208a8cc8745781b5a167ab80d804e83f26c72d36
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2019-12-14 19:26:24 -0500

    Add a new environment variable that points to the top of the directory and clean up some obsolete scripts

--- compiler/translate_script.sh
+++ compiler/translate_script.sh
@@ -26,5 +26,5 @@ seq_script="${script_prefix}_seq.sh"
 distr_script="${script_prefix}_distr.sh"
 
 ## TODO: This currently assumes that there is only one IR in the script
-python3 dish.py --output_optimized $seq_script $distr_script &&
+python3.8 $DISH_TOP/compiler/dish.py --output_optimized $seq_script $distr_script &&
 cat $distr_script
