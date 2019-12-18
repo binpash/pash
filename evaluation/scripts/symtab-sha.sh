5d90d22560b610941a03f192d6f5f6f26b67ca50
Nikos Vasilakis <nikos@vasilak.is>
2019-12-18 03:19:53 -0500

    Add symbtable hashing for SGX

---
+++ evaluation/scripts/symtab-sha.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# To build and sign an SGX enclave, this script extracts the executable's symbol
+# table and calculates its SHA256 hashsum.
+
+readelf -x .symtab enclave.signed.so |
+  tail -n +3 |
+  head -n -1 |
+  # next three implement `awk '{print $2$3$4$5}'`
+  sed 's/^[[:space:]]*//' | 
+  cut -d ' ' -f2-5 |
+  tr -d ' ' |
+  tr -d "\n" |
+  xxd -r -p |
+  sha256sum
