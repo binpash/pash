59d8d0fdc12978b6fb385c3774f963819a3aeaf5
Nikos Vasilakis <nikos@vasilak.is>
2019-12-27 13:27:25 -0400

    Update / correct scripts

--- evaluation/scripts/merge-wc.sh
+++ evaluation/scripts/merge-wc.sh
@@ -1,1 +1,17 @@
-paste -d '+' <(cat * | wc | tr -s ' '  '\n' | tail -n +2) <(cat * | wc | tr -s ' '  '\n' | tail -n +2) | bc | tr -s '\n'  ' ' | sed 's/^/   /' | sed 's/$/\ /'
+#!/bin/bash
+
+# Part of a distributed-`wc` wrapper, merging two `wc` results
+
+paste -d '+' 
+    <(cat * |
+      wc |
+      tr -s ' '  '\n' |
+      tail -n +2)
+    <(cat * |
+      wc |
+      tr -s ' '  '\n' |
+      tail -n +2) |
+  bc |
+  tr -s '\n'  ' ' |
+  sed 's/^/   /' |
+  sed 's/$/\ /'
--- evaluation/scripts/page-count.sh
+++ evaluation/scripts/page-count.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
 # A bash script for determining how many pages are in a folder of OpenOffice documents
 # From "Wicked Cool Shell Scripts", 2nd Ed., pg. 7
-echo "$(exiftool *.odt | grep Page-count | cut -d ":" -f2 | tr '\n' '+')""0" | bc
+
+# Require: libimage-exiftool-perl, bc
+# Data:
+#   http://ndr.md/data/dummy/large.pdf
+# More data:
+#   https://arxiv.org/help/bulk_data
+
+echo "$(exiftool *.pdf |
+    grep Page-count |
+    cut -d ":" -f2 |
+    tr '\n' '+')""0" |
+  bc |
+  sed 's/^/\n/'
