dc1e24f73dc73a83fd7b1105e9909515bc969c0f
Nikos Vasilakis <nikos@vasilak.is>
2019-12-27 13:27:58 -0400

    Update / correct scripts

--- evaluation/scripts/wf.sh
+++ evaluation/scripts/wf.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+
 # Word frequencies:
-cat ./input.txt | tr -cs A-Za-z'\n' | tr A-Z a-z | sort | uniq -c | sort -rn | sed ${1}q
+
+INPUT=./input/i1G.txt
+cat ./input.txt | tr -cs A-Za-z'\n' | tr A-Z a-z | sort | uniq -c | sort
 
 
