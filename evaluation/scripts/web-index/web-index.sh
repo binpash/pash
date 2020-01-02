2159309f566fb60f59092f7d3de4ae5bd0bfc73e
Nikos Vasilakis <nikos@vasilak.is>
2020-01-02 16:18:36 -0500

    Checkpoint

--- evaluation/scripts/web-index/web-index.sh
+++ evaluation/scripts/web-index/web-index.sh
@@ -11,10 +11,13 @@
 # iconv -c -t ascii//TRANSLIT
 # ```
 
+# https://webcache.googleusercontent.com/search?q=cache:l-R5pHOys9MJ:https://stackoverflow.com/questions/207047/what-linux-unix-commands-are-outdated-and-have-powerful-alternatives+&cd=13&hl=en&ct=clnk&gl=us
+# https://github.com/flonatel/pipexec
+
 cat  >seed.txt <<EOM
 # Do not start this experiment without redirecting **all** HTTP 
 # requests to one of our servers.
-https://en.wikipedia.org
+http://en.wikipedia.org
 EOM
 
 PROXY=$([ "$(hostname)" == "deathstar" ] && echo "gamma.ndr.md" || echo "localhost")
@@ -26,13 +29,16 @@ rm s1 s2
 mkfifo s1 s2
 
 echo $PROXY
-# Download page and clone stream as s1, s2
-# curl http://example.com/2.txt --connect-to '::158.130.4.114:8080'
+
+# to enable line buffering: stdbuf -oL
 tail -f $SOURCE |
+# removes buffering
+tee >(tee /dev/null >&2) |
 grep --line-buffered -v '^#' |
 sed -u 's/^https/http/' |
 head -n 2 |
-xargs -n 1 curl -s --connect-to "::${PROXY}:8080"  |
+# to debug curl, remove `-s`
+xargs -0 -n 1 -d '\n' curl --connect-to "::${PROXY}:8080"  |
 # {
 #   # this lambda is only for hitting multiple servers to increase throughput
 #   read url;
@@ -41,11 +47,9 @@ xargs -n 1 curl -s --connect-to "::${PROXY}:8080"  |
 #   echo $url >&2;
 #   curl -s  --connect-to "::${hosts[$((i++ % 2))]}:8080" 
 # } |
-# grep -in dish | # To test if it hits our local HTTP server
-# head -n 1 |
   tee s1 >/dev/null &
 
-echo "https://en.wikipedia.org" >> $SOURCE &
+echo "http://en.wikipedia.org" >> $SOURCE &
 
 # # s1---URL manipulation: Get all URLs, diff with SEEN, and write back to SOURCE and SEEN
 # FIXME: sort won't work here because it's gonna wait indefinitely---but { read v; echo $v | comm .. } should work
@@ -55,24 +59,32 @@ cat s1 |
 # grep -Eo 'href="[^\"]+"' | 
 # grep -Eo '(http|https)://[^/"]+' |
 grep '^http' |
-# tr 'x' 'x' # | tee -a $SOURCE >> $SEEN # maybe could do >>
+# filtering in-place, because we can't do sort
+# realized that xargs is a window operator!
+# it's impossible to use <( ) with xargs because it's evaluated before 
+# xargs expands `{}` --- so the solution is to call bash
+# quotes are used to avoid command injection
+# TODO: Alternatively, this can be written as a (possibly anon.) function
+xargs -0 -n 1 -d '\n' -I {} bash -c 'echo "$(grep -Fxv -f ./test.txt <(echo "{}"))"' |
+# grep -w amp
 # {
 #   read v;
-#   echo "-->  $v" >&2;
-#   echo $v; | comm -23 <(cat $SEEN | sort) -;
+# # echo "-->  $v" >&2;
+#   echo $v # |  -
 # } |
-grep -v '^#' |
-tr 'x' 'x' | tee -a $SOURCE >> $SEEN
+grep --line-buffered -v '^#' |
+sed -u 's/^https/http/' |
+# # tr 'x' 'x' |
+# Note the append on SEEN and write on SOURCE
+tee -a $SEEN -a $SOURCE > /dev/null
 
-
-# 
-# # s2---NLP manipulation:  get text
-# cat s2 | pandoc --from html --to plain | tr -cs A-Za-z '\n' | tr A-Z a-z | grep -vwFf stopwords.txt | ./stem-words.js # stem-to-roots
-# tee 
-#   >(tee shifted |  tail +2 | paste shifted - | sort | uniq -c | sort -rn >> 2grams.txt) # 2-grams
-#   >(tee shifted |  tail +3 | paste shifted - | sort | uniq -c | sort -rn >> 3grams.txt) # 3-grams
-#   >(tee shifted |  tail +4 | paste shifted - | sort | uniq -c | sort -rn >> 4grams.txt) # 4-grams
-#  | sort | uniq -c | sort -rn >> 1grams.txt # 1-gram frequencies
-# 
-# # lynx -dump -stdin
-# rm s1 s2
+#  # s2---NLP manipulation:  get text
+#  cat s2 | pandoc --from html --to plain | tr -cs A-Za-z '\n' | tr A-Z a-z | grep -vwFf stopwords.txt | ./stem-words.js # stem-to-roots
+#  tee 
+#    >(tee shifted |  tail +2 | paste shifted - | sort | uniq -c | sort -rn >> 2grams.txt) # 2-grams
+#    >(tee shifted |  tail +3 | paste shifted - | sort | uniq -c | sort -rn >> 3grams.txt) # 3-grams
+#    >(tee shifted |  tail +4 | paste shifted - | sort | uniq -c | sort -rn >> 4grams.txt) # 4-grams
+#   | sort | uniq -c | sort -rn >> 1grams.txt # 1-gram frequencies
+#  
+#  # lynx -dump -stdin
+#  rm s1 s2
