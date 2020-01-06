a4f7133c705ffd203334ed1def3206c324362381
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-01-06 22:41:30 +0200

    WIP in max-temp evaluation script

--- compiler/execute_max_temp_dish_evaluation.sh
+++ compiler/execute_max_temp_dish_evaluation.sh
@@ -5,12 +5,16 @@ p1="${directory}/p1.sh"
 p2="${directory}/p2.sh"
 p3="${directory}/p3.sh"
 p4="${directory}/p4.sh"
+p5="${directory}/p5.sh"
 
 output_dir="/tmp/max-temp-output/"
 p1_out="${output_dir}/p1.out"
 p2_out="${output_dir}/p2.out"
 p3_out="${output_dir}/p3.out"
 p4_out="${output_dir}/p4.out"
+p5_out="${output_dir}/p5.out"
+
+intermediary_dir="../evaluation/intermediary/"
 
 ## Make the temporary output dir
 mkdir -p $output_dir
@@ -18,33 +22,36 @@ mkdir -p $output_dir
 # TODO: Maybe time p1, p2?
 /bin/bash $p1 > $p1_out
 cat $p1_out | /bin/bash $p2 > $p2_out
+
+## TODO: p3 is currently only working for 2005
 cat $p2_out | /bin/bash $p3 > $p3_out
 cat $p3_out | /bin/bash $p4 > $p4_out
+cat $p4_out | /bin/bash $p5 > $p5_out
 
-# ## We assume that each evaluation script has a sequential, a
-# ## distributed, and an environment
-# experiment=$1
+## Split the intermediate output files for all scripts
+split -n 2 -d $p3_out ${p3_out}_2_
+split -n 10 -d $p3_out ${p3_out}_10_
+split -n 2 -d $p4_out ${p4_out}_2_
+split -n 10 -d $p4_out ${p4_out}_10_
 
-# directory="../evaluation/"
-# results="${directory}results/"
-# prefix="${directory}${experiment}"
-# env_file="${prefix}_env.sh"
-# seq_script="${prefix}_seq.sh"
-# distr_script="${prefix}_distr.sh"
+## Setup the _env files of the experiments accordingly
+echo "IN_DIR=${output_dir}" > ${intermediary_dir}/p4_1_env.sh
+echo "IN_DIR=${output_dir}" > ${intermediary_dir}/p4_2_env.sh
+echo "IN_DIR=${output_dir}" > ${intermediary_dir}/p4_10_env.sh
+echo "IN_DIR=${output_dir}" > ${intermediary_dir}/p5_1_env.sh
+echo "IN_DIR=${output_dir}" > ${intermediary_dir}/p5_2_env.sh
+echo "IN_DIR=${output_dir}" > ${intermediary_dir}/p5_10_env.sh
 
-# echo "Environment:"
-# # cat $env_file
-# . $env_file
-# export $(cut -d= -f1 $env_file)
+## TODO: Rename and remake the scripts in distr/seq to p4, p5
+## TODO: Rename the input file names in the scripts to match those 
 
-# echo "Sequential:"
-# cat $seq_script | grep -v "rm -f" | grep -v "mkfifo"
-# { time /bin/bash $seq_script > /tmp/seq_output ; } 2> >(tee "${results}${experiment}_seq.time" >&2)
-
-# echo "Distributed:"
-# { time ./translate_script.sh $seq_script $distr_script ; } 2> >(tee "${results}${experiment}_compile_distr.time" >&2)
-
+# ./execute_compile_evaluation_script.sh "p4_1"
+./execute_compile_evaluation_script.sh "p4_2"
+./execute_compile_evaluation_script.sh "p4_10"
+# ./execute_compile_evaluation_script.sh "p5_1"
+./execute_compile_evaluation_script.sh "p5_2"
+./execute_compile_evaluation_script.sh "p5_10"
 
 
-
+## (Maybe) TODO: Create the intermediary seq, distr scripts
 
