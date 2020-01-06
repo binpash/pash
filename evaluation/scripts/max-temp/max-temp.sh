b10e66d79750f5cf313a0d22746ab838e8f66b2b
Nikos Vasilakis <nikos@vasilak.is>
2020-01-06 16:10:29 -0500

    Correct URL after recent changes

--- evaluation/scripts/max-temp/max-temp.sh
+++ evaluation/scripts/max-temp/max-temp.sh
@@ -2,14 +2,14 @@
 
 #NOTE: The `head -n 1 below is for minimizing the number of pages to be seen
 
-# This is similar to {1995..2005}, but this requires shell expansion rules that
+# `seq` is similar to {1995..2005}, but this requires shell expansion rules that
 # are quite convoluted
 seq 2005 2005 | 
-  sed 's;^;http://ndr.md/noaa/;' |
+  sed 's;^;http://ndr.md/data/noaa/;' |
   sed 's;$;/;' |
   xargs -n 1 curl -s |
   grep gz |
-  head -n 1 | # <-- remove this live for full scale
+  head -n 1 | # <-- remove this line for full scale
   tr -s ' ' |
   cut -d ' ' -f9 |
   sed 's;^;http://ndr.md/noaa/2005/;' |
@@ -19,8 +19,4 @@ seq 2005 2005 |
   grep -v 999 |
   sort -rn |
   head -n1
-
-# what can you do with a named function?
-
-# can you use command substitution?
-
+  
