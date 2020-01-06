652aeebc8390e2704703087e287a4fb856996b3e
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-01-06 21:21:31 +0200

    Start working on a script that executes the max-temp evaluation

--- evaluation/scripts/max-temp/p3.sh
+++ evaluation/scripts/max-temp/p3.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cat p2.txt | 
-  sed 's;^;http://ndr.md/noaa/2005/;' |
-  xargs -n1 curl -s |
-  gunzip > p3.txt
+sed 's;^;http://ndr.md/noaa/2005/;' |
+    xargs -n1 curl -s |
+    gunzip
 
--- evaluation/scripts/max-temp/p4.sh
+++ evaluation/scripts/max-temp/p4.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cat p3.txt |
-  cut -c 89-92 |
-  grep -v 999 |
-  sort -rn |
-  head -n1 > p4.txt
+cut -c 89-92 |
+    grep -v 999 |
+    sort -rn |
+    head -n1
