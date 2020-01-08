3b68675f911c06d7f150bb8d7c8eac3bdbc6fb01
Nikos Vasilakis <nikos@vasilak.is>
2020-01-08 12:53:02 -0500

    Results from max-temp

--- evaluation/intermediary/p4_2_distr.sh
+++ evaluation/intermediary/p4_2_distr.sh
@@ -1,14 +1,14 @@
 rm -rf /tmp/distr_output
 mkdir -p /tmp/distr_output
 mkdir -p /dev/shm/dish
-rm -f "#file13"
-mkfifo "#file13"
-rm -f "#file14"
-mkfifo "#file14"
-rm -f "#file9"
-mkfifo "#file9"
 rm -f "#file10"
 mkfifo "#file10"
+rm -f "#file9"
+mkfifo "#file9"
+rm -f "#file14"
+mkfifo "#file14"
+rm -f "#file13"
+mkfifo "#file13"
 rm -f "#file11"
 mkfifo "#file11"
 rm -f "#file12"
@@ -22,10 +22,10 @@ cat "#file12" | gunzip  > "#file14" &
 cat "#file13" > /tmp/distr_output/0 &
 cat "#file14" > /tmp/distr_output/1 &
 wait
-rm -f "#file13"
-rm -f "#file14"
-rm -f "#file9"
 rm -f "#file10"
+rm -f "#file9"
+rm -f "#file14"
+rm -f "#file13"
 rm -f "#file11"
 rm -f "#file12"
 rm -rf "/dev/shm/dish"
