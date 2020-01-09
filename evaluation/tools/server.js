1fc7160a84b931bd566cbdc60aa2c469cd6e1cbb
Nikos Vasilakis <nikos@vasilak.is>
2020-01-09 18:03:43 -0500

    Update distribution scripts
    (Related to #11)

--- evaluation/tools/server.js
+++ evaluation/tools/server.js
@@ -6,71 +6,80 @@ const crypto = require('crypto');
 const HASH = "f4f8b9dc33fe805bcd0a854b19554f1daa596cf46ae3ef811a27b7d490dae42c";
 const PORT = 6001
 
-// nc -l 5000 | tr '[:lower:]' '[:upper:]' | nc -C 158.130.4.212 5555
-
 let jobs = [];
 
-// example object:
-// {dt: "passphrase", workerPort: 1234, serverIP: "192.168.0.1", serverPort: 4321, program: "..."  }
 let validateObj = (obj) => {
-  let keys = ["dt", "workerPort", "serverIP", "serverPort", "program"];
+  let keys = ["dt", "program"];
   for (var i = 0; i < keys.length; i++) {
     if (!obj[keys[i]]) {
-      return {"success": false, "msg": "object missing " + keys[i]}
+      return {success: false, msg: "object missing " + keys[i]}
     }
   };
 
   let h = crypto.createHash('sha256').update(obj.dt).digest('hex');
   if (h !== HASH) {
-    return {"success": false, "msg": "security token failed"}
+    return {success: false, msg: "security token failed"}
   }
+  return {success: true};
 }
 
 let validateURI = (uri) => {
   const parsed = parseInt(uri, 10);
   if (isNaN(parsed)) {
-    return {"success": false, "msg": "Did not call `/<int>`"};
+    return {success: false, msg: "Did not call `/<int>`"};
   } 
-  if ((parsed < 1) || (parsed > jobs.lenth - 1)) {
-    return {"success": false, "msg": "No such ID"};
+  if ((parsed < 1) || (parsed > jobs.length - 1)) {
+    return {success: false, msg: "No such ID"};
   }
-  reutn {"success": true, "msg": data[parsed]};
+  return {success: true, msg: data[parsed]};
 }
 
 let newJob = (obj) => {
-  const jobID = jobs.lenth;
-  const fileName = jobID + ".sh";
-  fs.writeFileSync(fileName, obj.program, "utf-8");
-  const pipeline = `
-  nc -l ${obj.workerPort} |
-        ./${fileName} |
-         nc -C ${serverIP} ${serverPort}`
-  let c = exec(pipeline, (error, stdout, stderr) => {
+  console.log(jobs, jobs.length, obj);
+  if (!validateObj(obj).success) {
+    return validateObj(obj)
+  }
+  const jobID = jobs.length;
+  jobs[jobID] = obj;
+  const filename = jobID + ".sh";
+  try {
+    fs.writeFileSync(filename, obj.program, "utf-8");
+  } catch (e) {
+    return e
+  }
+//  const pipeline = `
+//  nc -l ${obj.workerPort} |
+//        ./${fileName} |
+//         nc -C ${serverIP} ${serverPort}`
+  let c = cp.exec(`sh ./${filename}`, (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`);
-      return;
+      jobs[jobID].error = error;
+      return
+//    cb({success: false});
     }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
+    jobs[jobID].complete = true;
+    if (stdout) {
+      jobs[jobID].stdout = stdout;
+    }
+    if (stderr) {
+      jobs[jobID].stderr = stderr;
+    }
   });
-}
-
-function roughScale(x, base) {
-  const parsed = parseInt(x, base);
-  if (isNaN(parsed)) { return 0 }
-  return parsed * 100;
+  // return a second after the process started
+  return {success: true}
 }
 
 http.createServer( (request, response) => {
-  if (request.method === 'PUT' && request.url === '/newjob') {
+  if (request.method === 'PUT' && request.url === '/') {
     // PUT /control adds to the global timeseries
     let body = [];
     request.on('data', (chunk) => {
       body.push(chunk);
     }).on('end', () => {
       body = Buffer.concat(body).toString();
-      response.end(JSON.stringify(newJob(JSON.parse(body)));
-      );
+      console.log(body);
+      response.end(JSON.stringify(newJob(JSON.parse(body))));
     });
   } else if (request.url === '/') {
     // otherwise, return all current jobs
