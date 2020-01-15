485f405c41a934938464ffa7bf1d10be328de5e1
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-01-15 21:03:35 +0200

    Complete generalized map-reduce for bigrams (except for backend)

--- compiler/ir.py
+++ compiler/ir.py
@@ -518,18 +518,30 @@ class BigramGMap(Command):
 class BigramGReduce(Command):
     def __init__(self, file_ids):
         command = string_to_argument("bigram_aux_reduce")
-        options = [string_to_argument(fid.pipe_name()) for fid in file_ids]
+        options = self.make_options(file_ids)
         ## TODO: Generalize the model to arbitrarily many outputs to
         ## get rid of this hack, where inputs, outputs are just
         ## written as options.
         in_stream = []
-        out_stream = []
+        out_stream = [("option", 6)]
         ## TODO: When we generalize the model to have arbitrary
         ## outputs we can have them also be outputs.
-        opt_indices = [("option", i) for i in range(0,9)]
+        opt_indices = [("option", i) for i in range(0,9) if not i == 6]
         category = "pure"
         super().__init__(None, command, options, in_stream, out_stream,
                          opt_indices, category)
+
+    ## TODO: Make this atrocity prettier
+    def make_options(self, file_ids):
+        options = []
+        for i in range(0, 9):
+            fid = file_ids[i]
+            if(not i == 6):
+                opt = string_to_argument(fid.pipe_name())
+            else:
+                opt = fid
+            options.append(opt)
+        return options
 
 class SortGReduce(Command):
     def __init__(self, file_ids):
