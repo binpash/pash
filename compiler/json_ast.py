5cd1de812a5b99d8c60aeae8bd973937c858126f
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-01-22 11:31:27 -0500

    Fix the parsing bug after the changes

--- compiler/json_ast.py
+++ compiler/json_ast.py
@@ -8,41 +8,9 @@ JSON_TO_SHELL_APP="../parser/json_to_shell.native"
 
 ### --- From JSON --- ###
 
-## The json dumper in ocaml seems to print <, >, and parentheses
-## instead of {, }, [,]. Therefore we need to replace the characters
-## with the correct ones.
-def to_standard_json(string):
-    # FIXME: This function is not needed anymore
-    # string = string.replace("<", "{")
-    # string = string.replace(">", "}")
-    # string = string.replace("(", "[")
-    # string = string.replace(")", "]")
-
-    # # After these replacements, single names are written like this:
-    # # {"Name"} and the parser complains. We just need to remove the
-    # # braces.
-    # #
-    # # Note: I have noticed that the names are always constructors that
-    # # have no arguments, so they should all be letter characters.
-    # #
-    # # Warning: This is not robust at all, but will do for now
-    # # TODO (nv): is it possible to change this on the OCaml side?
-    # string = re.sub(r'\{\"([A-Za-z]+)\"\}', r'"\1"', string)
-    
-    return string
-
-def from_standard_json(string):
-    string = string.replace("{", "<")
-    string = string.replace("}", ">")
-    ## TODO: How should I handle tuples?
-
-    return string
-
 ## Returns the ast as a object
-def parse_json_line(line):
-    std_json_line = to_standard_json(line)
-    # print(std_json_line)
-    ast_object = json.loads(std_json_line)
+def parse_json_line(json_line):
+    ast_object = json.loads(json_line)
     return ast_object
 
 def parse_json_ast_string(json_string):
@@ -69,19 +37,17 @@ def serialize_asts_to_json(asts):
     serialized_asts = [serialize_ast_json(ast) for ast in asts]
     return "\n".join(serialized_asts)
 
+## FIXME: This is not working properly. json_to_shell produces a fata
+## error.
 def serialize_ast_json(ast):
     standard_json = json.dumps(ast)
-    ocaml_json = from_standard_json(standard_json)
-    ## TODO: It seems that it works without reverting all the changes
-    ## from standard_json. Is it correct?
-    # print("Output json:", ocaml_json)
-    return ocaml_json
+    return standard_json
 
 ### --- AST to Shell --- ###
 
 
 def json_to_shell(json_string):
-    subproc = run(['../parser/json_to_shell.native'], stdout=PIPE, input=json_string, encoding='ascii')
+    subproc = run([JSON_TO_SHELL_APP], stdout=PIPE, input=json_string, encoding='ascii')
     return subproc.stdout
 
 def ast_to_shell(ast):
