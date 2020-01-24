e2297b2c575fbe60a036f3bd851e7ac6d67d8d88
Nikos Vasilakis <nikos@vasilak.is>
2020-01-24 10:34:16 -0500

    Move into tools

--- evaluation/tools/page-per-line.sh
+++ evaluation/tools/page-per-line.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# Squash all HTML for each URL into a single line, streaming fashion
+# It also prefixes with the URL
+
 page_per_line () {
   curl -s $1 | tr -d "\n\r" | tr -d '\n' | sed "s/^/$0 /" | sed -e '/.$/a\'
 }
 
 export -f page_per_line
+
 # xargs:
 # add `-t` for debugging
 cat ./urls.txt | xargs -0 -d '\n' -n 1 bash -c 'page_per_line "$@"' _
