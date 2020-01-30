93ac1395bcf91f6620cea5b9d1384bdb3135e2ae
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-01-30 18:46:46 -0500

    Progress on distributed_back_end

--- compiler/distr_back_end.py
+++ compiler/distr_back_end.py
@@ -20,6 +20,11 @@ def distr_execute(graph, output_dir, output_script_name, output_optimized, compi
 
 def map_graph_to_physical_nodes(graph, number_nodes):
 
+    ## This keeps a list of file ids that will be connected to remote
+    ## channels in each node. It keeps triples of file id, previous,
+    ## and next physical node.
+    remote_channels = []
+
     ## This will be a list of graphs, one for each physical node. Graphs are in
     ## the exact same format as the initial graph.
     distributed_graphs = []
@@ -32,20 +37,41 @@ def map_graph_to_physical_nodes(graph, number_nodes):
     ##    a physical node we stop, and complete the graph for the current node.
     curr_physical_node = 0
     work_stack = graph.source_nodes()
-    print(work_stack)
-    exit(0)
-    while len(work_stack) > 0 and curr_physical_node < number_nodes:
-        curr = work_stack.pop(0)
-        next_nodes = next_nodes(curr, graph)
-        work_stack += next_nodes
 
-        if(curr in node_map):
-            ## TODO: Stop and close the distributed graph for curr_physical_node
-            curr_physical_node += 1
+    ## WARNING: At the moment the work stack is not exactly correct,
+    ## as it contains reduce nodes too. This won't be a correctness
+    ## problem though, as all of the nodes will be assigned to some
+    ## physical one.
+    print(work_stack)
+    while (len(work_stack) > 0):
+        curr = work_stack.pop(0)
+        next_nodes = graph.get_next_nodes(curr)
+        # print("Next:", curr, next_nodes)
+        work_stack = next_nodes + work_stack
+
+        ## If the node is a sink of the graph, then we have to stop,
+        ## so we fix its physical node to trigger the creation of the
+        ## distributed graph.
+        if(len(next_nodes) == 0):
+            curr.physical_node = curr_physical_node
+
+        if(hasattr(curr, 'physical_node')):
+            ## TODO: Stop and close the distributed graph for
+            ## curr_physical_node.  The final nodes are all being put
+            ## into the central (last) node.
+            if(curr_physical_node + 1 < number_nodes):
+                curr_physical_node += 1
+                input_nodes_and_edges = graph.get_previous_nodes_and_edges(curr)
+                new_remote_channels = [(edge, node.physical_node, curr.physical_node)
+                                       for node, edge in input_nodes_and_edges
+                                       if not node.physical_node == curr.physical_node]
+                remote_channels += new_remote_channels
         else:
             curr.physical_node = curr_physical_node
 
-    
+        print("Physical:", curr, curr.physical_node)
+
+    print(remote_channels)
 
     return distributed_graphs
 
