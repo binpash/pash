ddc31d3f2327228283caa78ca21a0027af8b3ef2
Nikos Vasilakis <nikos@vasilak.is>
2020-02-02 13:55:11 -0500

    Web index pandoc stack size

--- evaluation/intermediary/web-index_p2_2_seq.sh
+++ evaluation/intermediary/web-index_p2_2_seq.sh
@@ -3,7 +3,7 @@ cat "$IN_DIR/p1.out_2_00" "$IN_DIR/p1.out_2_01" |
   sed "s#^#$WIKI#" |
   xargs cat |
   iconv -c -t ascii//TRANSLIT |
-  pandoc +RTS -K50000000 -RTS --from html --to plain --quiet |
+  pandoc +RTS -K64m -RTS --from html --to plain --quiet |
   tr -cs A-Za-z '\n' |
   tr A-Z a-z |
   grep -vwFf $WEB_INDEX_DIR/stopwords.txt |
