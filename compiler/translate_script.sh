a0ffb8d11429aafd7a93be450dde6a36eb6ee2c6
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-02-12 18:36:53 -0500

    Print the optimized script in the back_end

--- compiler/translate_script.sh
+++ compiler/translate_script.sh
@@ -4,5 +4,4 @@ seq_script=$1
 distr_script=$2
 DISH_TOP=${DISH_TOP:-$(git rev-parse --show-toplevel --show-superproject-working-tree)}
 ## TODO: This currently assumes that there is only one IR in the script
-python3.8 $DISH_TOP/compiler/dish.py --output_optimized $seq_script $distr_script &&
-cat $distr_script
+python3.8 $DISH_TOP/compiler/dish.py --output_optimized $seq_script $distr_script
