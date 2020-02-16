a443296201e8286077b6d5a2e626c1cd4f5fcb3b
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-02-16 17:13:51 -0500

    Move globals in a different file to prevent duplication

--- compiler/dish.py
+++ compiler/dish.py
@@ -6,47 +6,15 @@ from ast_to_ir import *
 from distr_plan import *
 from ir import *
 from json_ast import *
-
-## TODO: Can we get rid of this duplication? All of these are in both
-## distr_plan.py and dish.py
-GIT_TOP_CMD = [ 'git', 'rev-parse', '--show-toplevel', '--show-superproject-working-tree']
-if 'DISH_TOP' in os.environ:
-    DISH_TOP = os.environ['DISH_TOP']
-else:
-    DISH_TOP = subprocess.run(GIT_TOP_CMD, capture_output=True,
-            text=True).stdout.rstrip()
-
-PARSER_BINARY = os.path.join(DISH_TOP, "parser/parse_to_json.native")
-PRINTER_BINARY = os.path.join(DISH_TOP, "parser/json_to_shell.native")
-
-config = {}
-
-def load_config():
-    global config
-    dish_config = {}
-    CONFIG_KEY = 'distr_planner'
-
-    ## TODO: allow this to be passed as an argument
-    config_file_path = '{}/compiler/config.yaml'.format(DISH_TOP)
-    with open(config_file_path) as config_file:
-        dish_config = yaml.load(config_file, Loader=yaml.FullLoader)
-
-    if not dish_config:
-        raise Exception('No valid configuration could be loaded from {}'.format(config_file_path))
-
-    if CONFIG_KEY not in dish_config:
-        raise Exception('Missing `{}` config in {}'.format(CONFIG_KEY, config_file_path))
-
-    config = dish_config[CONFIG_KEY]
-
+import config
 
 def main():
     ## Parse arguments
     args = parse_args()
 
-    global config
-    if not config:
-        load_config()
+    # global config
+    if not config.config:
+        config.load_config()
 
     ## 1. Execute the POSIX shell parser that returns the AST in JSON
     input_script_path = args.input
@@ -56,7 +24,7 @@ def main():
     ast_objects = parse_json_ast_string(json_ast_string)
 
     ## 3. Compile ASTs to our intermediate representation
-    compiled_asts = compile_asts(ast_objects, config)
+    compiled_asts = compile_asts(ast_objects, config.config)
 
     ## 4. Translate the new AST back to shell syntax
     input_script_wo_extension, input_script_extension = os.path.splitext(input_script_path)
@@ -69,12 +37,12 @@ def main():
         execute_script(args.output, args.output_optimized, args.compile_optimize_only)
 
 def parse_shell(input_script_path):
-    parser_output = subprocess.run([PARSER_BINARY, input_script_path], capture_output=True, text=True)
+    parser_output = subprocess.run([config.PARSER_BINARY, input_script_path], capture_output=True, text=True)
     parser_output.check_returncode()
     return parser_output.stdout
 
 def from_ir_to_shell(ir_filename, new_shell_filename):
-    printer_output = subprocess.run([PRINTER_BINARY, ir_filename], capture_output=True, text=True)
+    printer_output = subprocess.run([config.PRINTER_BINARY, ir_filename], capture_output=True, text=True)
     printer_output.check_returncode()
     compiled_script = printer_output.stdout
     with open(new_shell_filename, 'w') as new_shell_file:
