d1d15e8ead9e4085c7256f93196d875a144c3217
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-03-25 12:23:19 -0400

    Generate all intermediary scripts in the evaluation script

--- compiler/execute_evaluation_scripts.sh
+++ compiler/execute_evaluation_scripts.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+microbenchmarks_dir="../evaluation/microbenchmarks/"
+intermediary_dir="../evaluation/intermediary/"
+
+mkdir -p $intermediary_dir
+
 n_inputs=(
+    1
     2
     4
     10
@@ -24,6 +30,12 @@ microbenchmarks=(
 ##       before some specific benchmarks
 for microbenchmark in "${microbenchmarks[@]}"; do
     for n_in in "${n_inputs[@]}"; do
+
+        ## Generate the intermediary script
+        python3 generate_microbenchmark_intermediary_scripts.py \
+                $microbenchmarks_dir $microbenchmark $n_in $intermediary_dir
+
+        ## Execute the intermediary script
         ./execute_compile_evaluation_script.sh "${microbenchmark}_${n_in}"
     done
 done
