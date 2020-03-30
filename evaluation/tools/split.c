b5e3674d5a4f5dfe2d159bd55ce2ae6ebe8817a8
Achilles Benetopoulos <abenetopoulos@gmail.com>
2020-03-29 20:40:34 -0400

    Improve split
    Added debug logs; fixed logic around timing of opening output files.

--- evaluation/tools/split.c
+++ evaluation/tools/split.c
@@ -4,54 +4,77 @@
 
 #define MAX_LINE_LENGTH 1e6
 
+#define LOC __FILE__ // TODO expand
+
+#ifdef DEBUG
+#define PRINTDBG(fmt, ...) printf(fmt, ##__VA_ARGS__)
+#else
+#define PRINTDBG(fmt, ...)
+#endif
+
 void SplitInput(char* input, char* output1, char* output2, int batchSize) {
-    FILE* inputFile = fopen(input, "r");
-    if (!inputFile) {
-        return;
+  PRINTDBG("will split input\n");
+  FILE* inputFile = fopen(input, "r");
+  if (!inputFile) {
+    perror(LOC);
+    exit(1);
+  }
+  PRINTDBG("opened input file %s\n", input);
+
+  PRINTDBG("will open outputFile1 from %s \n", output1);
+  FILE* outputFile1 = fopen(output1, "w");
+  if (!outputFile1) {
+    perror(LOC);
+    exit(1);
+  }
+  PRINTDBG("opened output file %s\n", output1);
+  FILE* outputFile = outputFile1;
+
+  char* inputBuffer = NULL;
+  unsigned int readLines = 0;
+
+  size_t len = 0;
+  while (getline(&inputBuffer, &len, inputFile) > 0) {
+    if (++readLines == batchSize) {
+      fclose(outputFile);
+      if (!(outputFile = fopen(output2, "w"))) {
+        perror(LOC);
+        exit(1);
+      }
+      PRINTDBG("successfully opened second output file %s\n", output2);
     }
+    fputs(inputBuffer, outputFile);
+  }
 
-    FILE* outputFile1 = fopen(output1, "w");
-    FILE* outputFile2 = fopen(output2, "w");
-    if (!(outputFile1 && outputFile2)) {
-        return;
-    }
-    FILE* outputFile;
 
-    char* inputBuffer = NULL;
-    unsigned int readLines = 0;
-
-    while (getline(&inputBuffer, 0, inputFile) > 0) {
-        outputFile = ++readLines >= batchSize ? outputFile2 : outputFile1;
-        fputs(inputBuffer, outputFile);
-    }
-
-    fclose(inputFile);
-    fclose(outputFile1);
-    fclose(outputFile2);
-    free(inputBuffer);
+  PRINTDBG("done reading, will close all open files from %s, %s, %s\n", input, output1, output2);
+  fclose(inputFile);
+  fclose(outputFile);
+  free(inputBuffer);
 }
 
 int main(int argc, char* argv[]) {
-    // arg#1 -> input file name
-    // arg#2 -> output file name 1
-    // arg#3 -> output file name 2
-    // arg#4 -> batch_size
-    if (argc < 5) {
-        printf("ERROR: missing input!");
-        exit(1);
-    }
+  // arg#1 -> input file name
+  // arg#2 -> output file name 1
+  // arg#3 -> output file name 2
+  // arg#4 -> batch_size
+  if (argc < 5) {
+    fprintf(stderr, "missing input!\n");
+    exit(1);
+  }
 
-    char* inputFileName = calloc(strlen(argv[1]) + 1, sizeof(char));
-    strcpy(inputFileName, argv[1]);
+  char* inputFileName = calloc(strlen(argv[1]) + 1, sizeof(char));
+  strcpy(inputFileName, argv[1]);
 
-    char* outputFileName1 = calloc(strlen(argv[2]) + 1, sizeof(char));
-    strcpy(outputFileName1, argv[2]);
-    char* outputFileName2 = calloc(strlen(argv[3]) + 1, sizeof(char));
-    strcpy(outputFileName2, argv[3]);
+  char* outputFileName1 = calloc(strlen(argv[2]) + 1, sizeof(char));
+  strcpy(outputFileName1, argv[2]);
+  char* outputFileName2 = calloc(strlen(argv[3]) + 1, sizeof(char));
+  strcpy(outputFileName2, argv[3]);
 
-    int batchSize = atoi(argv[4]);
+  int batchSize = atoi(argv[4]);
 
-    SplitInput(inputFileName, outputFileName1, outputFileName2, batchSize);
+  SplitInput(inputFileName, outputFileName1, outputFileName2, batchSize);
 
-    return 0;
+  PRINTDBG("SplitInput is done, whatever that's worth\n");
+  return 0;
 }
