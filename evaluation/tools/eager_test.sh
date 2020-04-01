cda9ba899e42f95fca749ff43b7f9ffd460ec147
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-04-01 09:35:15 -0400

    add timing

--- evaluation/tools/eager_test.sh
+++ evaluation/tools/eager_test.sh
@@ -3,14 +3,14 @@
 mkfifo s1 s2
 
 # IN=test_in.txt
-IN=../scripts/input/1M.txt
+IN=../scripts/input/10M.txt
 
 cat $IN > s1 &
 cat s2 > test_out.txt &
-./eager s1 s2 intermediate
+./eager s1 s2 intermediate &
 
 wait
 
 rm s1 s2
 
-diff -s $IN test_out.txt
+# diff -s $IN test_out.txt
