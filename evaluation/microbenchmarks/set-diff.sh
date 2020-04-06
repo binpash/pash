904bcc66557c4456716f58517fbe9ae6fee628cc
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-04-06 11:34:08 -0400

    Start tackling set-diff

---
+++ evaluation/microbenchmarks/set-diff.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Show the set-difference between two streams (i.e., elements in the first that are not in the second).
+
+# For default data, it uses the current set 
+
+# p="../../distributability/c_stats/"
+# A=${1:-${p}posix.txt}
+# B=${2:-${p}coreutils.txt}
+
+# TODO: Find a good input set
+
+mkfifo s1 s2
+
+cat $IN |
+    cut -d ' ' -f 1 |
+    sort > s1 &
+
+cat $IN |
+    cut -d ' ' -f 1 |
+    sort > s2 &
+
+comm -23 s1 s2
+
+rm s1 s2
