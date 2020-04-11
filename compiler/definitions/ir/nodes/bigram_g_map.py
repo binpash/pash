fd2914ce17d0860acd8d1a1508e7ee3f65b01dad
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-04-11 17:46:30 -0400

    Solve a first bug in bigram

--- compiler/definitions/ir/nodes/bigram_g_map.py
+++ compiler/definitions/ir/nodes/bigram_g_map.py
@@ -1,18 +1,32 @@
 from definitions.ir.command import *
 
 class BigramGMap(Command):
-    num_outputs = 3
+    num_outputs = config.bigram_g_map_num_outputs
     def __init__(self, file_ids):
+        assert(self.num_outputs == 3)
         command = string_to_argument("bigram_aux_map")
-        options = [string_to_argument(fid.pipe_name()) for fid in file_ids]
+        options = self.make_options(file_ids)
+        # options = [string_to_argument(fid.pipe_name()) for fid in file_ids]
         ## TODO: Generalize the model to arbitrarily many outputs to
         ## get rid of this hack, where inputs, outputs are just
         ## written as options.
-        in_stream = []
+        in_stream = [("option", 0)]
         out_stream = []
         ## TODO: When we generalize the model to have arbitrary
         ## outputs we can have them also be outputs.
-        opt_indices = [("option", 0), ("option", 1), ("option", 2), ("option", 3)]
+        opt_indices = [("option", 1), ("option", 2), ("option", 3)]
         category = "stateless"
         super().__init__(None, command, options, in_stream, out_stream,
                          opt_indices, category)
+
+    ## TODO: Make this atrocity prettier
+    def make_options(self, file_ids):
+        options = []
+        for i in range(4):
+            fid = file_ids[i]
+            if(not i == 0):
+                opt = string_to_argument(fid.pipe_name())
+            else:
+                opt = fid
+            options.append(opt)
+        return options
