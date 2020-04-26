2588548f86a390386c90a4ea8ac4b8e905a9b5a2
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-04-26 14:20:35 -0400

    Progress on executing all unix50 benchmarks

--- compiler/ast_to_ir.py
+++ compiler/ast_to_ir.py
@@ -291,7 +291,8 @@ def compile_node_for(ast_node, fileIdGen, config):
 
 def compile_arg_char(arg_char, fileIdGen, config):
     key, val = get_kv(arg_char)
-    if (key == 'C'):
+    if (key in ['C',   # Single character
+                'E']): # Escape
         return arg_char
     elif (key == 'B'):
         ## TODO: I probably have to redirect the input of the compiled
--- compiler/execute_unix_benchmarks.sh
+++ compiler/execute_unix_benchmarks.sh
@@ -11,23 +11,24 @@ mkdir -p $intermediary_dir
 mkdir -p "../evaluation/results/${results_subdir}/"
 
 ## Make inputs larger and generate scripts and their envs
-input_size_increase=10000
+input_size_increase=10
 python3 generate_unix50_scripts.py $unix50_dir $unix50_intermediary $input_size_increase
 
 n_inputs=(
-    1
+    # 1
     2
-    4
-    10
-    20
-    50
-    100
+    # 4
+    # 8
+    # 16
+    # 32
+    # 64
 )
 
 ## Note: Maybe we need to tune the configuration (fan-out, batch-size)
 ##       before some specific benchmarks
 for unix50_pipeline in $(ls ${unix50_intermediary} | grep -v "_env" | cut -f 1 -d '.' | sort); do
     echo $unix50_pipeline
+    exec_seq="-s"
     for n_in in "${n_inputs[@]}"; do
 
         ## Generate the intermediary script
@@ -35,6 +36,9 @@ for unix50_pipeline in $(ls ${unix50_intermediary} | grep -v "_env" | cut -f 1 -
                 $unix50_intermediary $unix50_pipeline $n_in $intermediary_dir
 
         ## Execute the intermediary script
-        ./execute_compile_evaluation_script.sh "${unix50_pipeline}_${n_in}" "${results_subdir}" > /dev/null
+        ./execute_compile_evaluation_script.sh $exec_seq -e "${unix50_pipeline}" "${n_in}" "${results_subdir}"  > /dev/null
+        # Only execute the sequential once
+        exec_seq=""
+
     done
 done
