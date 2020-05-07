e2ee5fbab061f96b9d5886c263782b996151bd7d
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-05-06 20:59:02 -0400

    Modify the web-index benchmark

--- compiler/execute_web_index_dish_evaluation.sh
+++ compiler/execute_web_index_dish_evaluation.sh
@@ -18,7 +18,9 @@ input_dir="${HOME}/wikipedia/"
 ## Make the temporary output dir
 mkdir -p $output_dir
 
-cp "$input_dir/index_h_50000.txt" $p1_out
+# cp "$input_dir/index_h_50000.txt" $p1_out
+cp "$input_dir/index_h_1000.txt" $p1_out
+
 
 { time ( cat $p1_out | /bin/bash $p2 > $p2_out ) ; } 2> >(tee "${results_dir}/web-index_p2_seq.time" >&2)
 
@@ -36,49 +38,50 @@ echo "Intermediate files have been successfully produced."
 echo "IN_DIR=${output_dir}" > ${intermediary_dir}/web-index_p2_1_env.sh
 echo "WIKI=${input_dir}" >> ${intermediary_dir}/web-index_p2_1_env.sh
 echo "WEB_INDEX_DIR=${directory}" >> ${intermediary_dir}/web-index_p2_1_env.sh
-cp ${intermediary_dir}/web-index_p2_1_env.sh ${intermediary_dir}/web-index_p2_2_env.sh
-cp ${intermediary_dir}/web-index_p2_1_env.sh ${intermediary_dir}/web-index_p2_10_env.sh
-cp ${intermediary_dir}/web-index_p2_1_funs.sh ${intermediary_dir}/web-index_p2_2_funs.sh
-cp ${intermediary_dir}/web-index_p2_1_funs.sh ${intermediary_dir}/web-index_p2_10_funs.sh
+# cp ${intermediary_dir}/web-index_p2_1_env.sh ${intermediary_dir}/web-index_p2_2_env.sh
+# cp ${intermediary_dir}/web-index_p2_1_env.sh ${intermediary_dir}/web-index_p2_10_env.sh
+# cp ${intermediary_dir}/web-index_p2_1_funs.sh ${intermediary_dir}/web-index_p2_2_funs.sh
+# cp ${intermediary_dir}/web-index_p2_1_funs.sh ${intermediary_dir}/web-index_p2_10_funs.sh
 
-cp ${intermediary_dir}/web-index_p2_1_env.sh ${intermediary_dir}/web-index_p3_1gram_1_env.sh
-cp ${intermediary_dir}/web-index_p2_1_env.sh ${intermediary_dir}/web-index_p3_1gram_2_env.sh
-cp ${intermediary_dir}/web-index_p2_1_env.sh ${intermediary_dir}/web-index_p3_1gram_10_env.sh
-cp ${intermediary_dir}/web-index_p2_1_funs.sh ${intermediary_dir}/web-index_p3_1gram_1_funs.sh
-cp ${intermediary_dir}/web-index_p2_1_funs.sh ${intermediary_dir}/web-index_p3_1gram_2_funs.sh
-cp ${intermediary_dir}/web-index_p2_1_funs.sh ${intermediary_dir}/web-index_p3_1gram_10_funs.sh
+# cp ${intermediary_dir}/web-index_p2_1_env.sh ${intermediary_dir}/web-index_p3_1gram_1_env.sh
+# cp ${intermediary_dir}/web-index_p2_1_env.sh ${intermediary_dir}/web-index_p3_1gram_2_env.sh
+# cp ${intermediary_dir}/web-index_p2_1_env.sh ${intermediary_dir}/web-index_p3_1gram_10_env.sh
+# cp ${intermediary_dir}/web-index_p2_1_funs.sh ${intermediary_dir}/web-index_p3_1gram_1_funs.sh
+# cp ${intermediary_dir}/web-index_p2_1_funs.sh ${intermediary_dir}/web-index_p3_1gram_2_funs.sh
+# cp ${intermediary_dir}/web-index_p2_1_funs.sh ${intermediary_dir}/web-index_p3_1gram_10_funs.sh
 
-cp ${intermediary_dir}/web-index_p2_1_env.sh ${intermediary_dir}/web-index_p3_2gram_1_env.sh
-cp ${intermediary_dir}/web-index_p2_1_env.sh ${intermediary_dir}/web-index_p3_2gram_2_env.sh
-cp ${intermediary_dir}/web-index_p2_1_env.sh ${intermediary_dir}/web-index_p3_2gram_10_env.sh
-cp ${intermediary_dir}/web-index_p2_1_funs.sh ${intermediary_dir}/web-index_p3_2gram_1_funs.sh
-cp ${intermediary_dir}/web-index_p2_1_funs.sh ${intermediary_dir}/web-index_p3_2gram_2_funs.sh
-cp ${intermediary_dir}/web-index_p2_1_funs.sh ${intermediary_dir}/web-index_p3_2gram_10_funs.sh
+# cp ${intermediary_dir}/web-index_p2_1_env.sh ${intermediary_dir}/web-index_p3_2gram_1_env.sh
+# cp ${intermediary_dir}/web-index_p2_1_env.sh ${intermediary_dir}/web-index_p3_2gram_2_env.sh
+# cp ${intermediary_dir}/web-index_p2_1_env.sh ${intermediary_dir}/web-index_p3_2gram_10_env.sh
+# cp ${intermediary_dir}/web-index_p2_1_funs.sh ${intermediary_dir}/web-index_p3_2gram_1_funs.sh
+# cp ${intermediary_dir}/web-index_p2_1_funs.sh ${intermediary_dir}/web-index_p3_2gram_2_funs.sh
+# cp ${intermediary_dir}/web-index_p2_1_funs.sh ${intermediary_dir}/web-index_p3_2gram_10_funs.sh
 
-cp ${intermediary_dir}/web-index_p2_1_env.sh ${intermediary_dir}/web-index_p3_3gram_1_env.sh
-cp ${intermediary_dir}/web-index_p2_1_env.sh ${intermediary_dir}/web-index_p3_3gram_2_env.sh
-cp ${intermediary_dir}/web-index_p2_1_env.sh ${intermediary_dir}/web-index_p3_3gram_10_env.sh
-cp ${intermediary_dir}/web-index_p2_1_funs.sh ${intermediary_dir}/web-index_p3_3gram_1_funs.sh
-cp ${intermediary_dir}/web-index_p2_1_funs.sh ${intermediary_dir}/web-index_p3_3gram_2_funs.sh
-cp ${intermediary_dir}/web-index_p2_1_funs.sh ${intermediary_dir}/web-index_p3_3gram_10_funs.sh
+# cp ${intermediary_dir}/web-index_p2_1_env.sh ${intermediary_dir}/web-index_p3_3gram_1_env.sh
+# cp ${intermediary_dir}/web-index_p2_1_env.sh ${intermediary_dir}/web-index_p3_3gram_2_env.sh
+# cp ${intermediary_dir}/web-index_p2_1_env.sh ${intermediary_dir}/web-index_p3_3gram_10_env.sh
+# cp ${intermediary_dir}/web-index_p2_1_funs.sh ${intermediary_dir}/web-index_p3_3gram_1_funs.sh
+# cp ${intermediary_dir}/web-index_p2_1_funs.sh ${intermediary_dir}/web-index_p3_3gram_2_funs.sh
+# cp ${intermediary_dir}/web-index_p2_1_funs.sh ${intermediary_dir}/web-index_p3_3gram_10_funs.sh
+
+cp ${intermediary_dir}/web-index_p2_1_env.sh ${intermediary_dir}/web-index_full_2_env.sh
+cp ${intermediary_dir}/web-index_p2_1_funs.sh ${intermediary_dir}/web-index_full_2_funs.sh
 
 ## Setup the fifo pipes needed in the example.
 
 ## Note: The sequential p2 stage takes about 7 seconds for 100! urls
 ##       This means that for the whole wikipedia (1mil urls) it will take about
 ##       20+ hours to complete.
-# ./execute_compile_evaluation_script.sh "web-index_p2_1"
-./execute_compile_evaluation_script.sh "web-index_p2_2"
-./execute_compile_evaluation_script.sh "web-index_p2_10"
+# ./execute_compile_evaluation_script.sh -e -s "web-index_p2" "2"
+# ./execute_compile_evaluation_script.sh -e "web-index_p2" "10"
 
-./execute_compile_evaluation_script.sh "web-index_p3_1gram_1"
-./execute_compile_evaluation_script.sh "web-index_p3_1gram_2"
-./execute_compile_evaluation_script.sh "web-index_p3_1gram_10"
+# ./execute_compile_evaluation_script.sh -e -s "web-index_p3_1gram" "2"
+# ./execute_compile_evaluation_script.sh -e "web-index_p3_1gram" "10"
 
-./execute_compile_evaluation_script.sh "web-index_p3_2gram_1"
-./execute_compile_evaluation_script.sh "web-index_p3_2gram_2"
-./execute_compile_evaluation_script.sh "web-index_p3_2gram_10"
+# ./execute_compile_evaluation_script.sh -e -s "web-index_p3_2gram" "2"
+# ./execute_compile_evaluation_script.sh -e "web-index_p3_2gram" "10"
 
-./execute_compile_evaluation_script.sh "web-index_p3_3gram_1"
-./execute_compile_evaluation_script.sh "web-index_p3_3gram_2"
-./execute_compile_evaluation_script.sh "web-index_p3_3gram_10"
+# ./execute_compile_evaluation_script.sh -e -s "web-index_p3_3gram" "2"
+# ./execute_compile_evaluation_script.sh -e "web-index_p3_3gram" "10"
+
+./execute_compile_evaluation_script.sh -e -s "web-index_full" "2"
---
+++ evaluation/intermediary/web-index_full_2_seq.sh
@@ -0,0 +1,44 @@
+mkfifo shift{1,2,3} {1,2,3}grams
+
+
+cat "$IN_DIR/p1.out_2_00" "$IN_DIR/p1.out_2_01" |
+  sed "s#^#$WIKI#" |
+  xargs cat |
+  iconv -c -t ascii//TRANSLIT |
+  pandoc +RTS -K64m -RTS --from html --to plain --quiet |
+  tr -cs A-Za-z '\n' |
+  tr A-Z a-z |
+  grep -vwFf $WEB_INDEX_DIR/stopwords.txt |
+  $WEB_INDEX_DIR/stem-words.js |
+  tee '3grams' '2grams' '1grams' > /dev/null &
+
+cat '1grams' |
+    sort |
+    uniq -c |
+    sort -rn >> 1-grams.txt &
+
+cat '2grams' |
+    tr -cs A-Za-z '\n' |
+    tr A-Z a-z |
+    tee shift1 |
+    tail +2 |
+    paste shift1 - |
+    sort |
+    uniq -c |
+    sort -rn >> 2-grams.txt &
+
+cat '3grams' |
+    tr -cs A-Za-z '\n' |
+    tr A-Z a-z |
+    tee shift2 |
+    tail +2 |
+    paste shift2 - |
+    tee shift3 |
+    cut -f 1 |
+    tail +3 |
+    paste shift3 - |
+    sort |
+    uniq -c |
+    sort -rn >> 3-grams.txt 
+
+rm shift{1,2,3} {1,2,3}grams
