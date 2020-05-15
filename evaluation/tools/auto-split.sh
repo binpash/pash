2167caa646bc15ca9484bf03e3f87385ecdd67c3
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-05-15 11:46:05 -0400

    Automatic no-task-par split

---
+++ evaluation/tools/auto-split.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+input="$1"
+shift
+outputs="$@"
+n_outputs="$#"
+
+temp=$(mktemp -u)
+
+cat $input > $temp
+total_lines=$(wc -l $temp | cut -f 1 -d ' ')
+batch_size=$( expr $total_lines / $n_outputs )
+# echo "Input: $input"
+# echo "Ouputs: $outputs"
+# echo "Number of outputs: $n_outputs"
+# echo "Total Lines: $total_lines"
+# echo "Batch Size: $batch_size"
+split $input $batch_size $outputs
