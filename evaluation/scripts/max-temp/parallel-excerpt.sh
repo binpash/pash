61b864f6c16c332cbb0de0e577130ed9b06d7cf3
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-05-22 17:04:43 -0400

    Update parallel excerpt

--- evaluation/scripts/max-temp/parallel-excerpt.sh
+++ evaluation/scripts/max-temp/parallel-excerpt.sh
@@ -1,3 +1,24 @@
+mkfifo $t{0,1...}
+curl $base/$y > $t0 &
+cat $t0 | split $t1 $t2 &
+cat $t1 | xargs -n1 curl -s > $t3 &
+cat $t2 | xargs -n1 curl -s > $t4 &
+...
+cat $t9 | sort -rn > $t11 &
+cat $t10 | sort -rn > $t12 &
+cat $t11 | eager > $t13 &
+cat $t12 | eager > $t14 &
+sort -m -rn $t13 $t14 > $t15 &
+cat $t15 | head -n1 > $out1 &
+wait $!
+
+
+
+
+
+mkfifo $t{0,1...}
+curl $base/$y > $t0 &
+cat $t0 | split $t1 $t2 &
 cat $t1 | xargs -n1 curl -s > $t3 &
 cat $t2 | xargs -n1 curl -s > $t4 &
 cat $t3 | gunzip  > $t5 &
@@ -8,5 +29,10 @@ cat $t7 | grep -v 999 > $t9 &
 cat $t8 | grep -v 999 > $t10 &
 cat $t9 | sort -rn > $t11 &
 cat $t10 | sort -rn > $t12 &
-sort -m -rn $t11 $t12 > $t13 &
-cat $t13 | head -n1 > $out1 &
+cat $t11 | eager > $t13 &
+cat $t12 | eager > $t14 &
+sort -m -rn $t13 $t14 > $t15 &
+cat $t15 | head -n1 > $out1 &
+wait $!
+ps --ppid $$ | awk '{print $1}' | grep -E '[0-9]' | xargs -n 1 kill -SIGPIPE
+rm $t{0,1...}
