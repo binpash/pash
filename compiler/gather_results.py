10f1d732c532708c56095b3ebec0091a4b75ada6
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-09-21 20:53:06 +0300

    Add average line in Unix50

--- compiler/gather_results.py
+++ compiler/gather_results.py
@@ -611,23 +611,35 @@ def make_unix50_bar_chart(all_results, scaleup_numbers, parallelism):
     speedup_color = 'tab:blue'
     seq_time_color = 'tab:red'
 
+
+
     fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
     # print(fig.get_size_inches())
     fig.set_size_inches(12, 6)
 
-    ## Plot speedup
-    ax1.set_ylabel('Speedup', color=speedup_color)
-    ax1.set_xlabel('Pipeline')
+    ## Plot line at 1
     plt.xlim(-1, len(individual_results))
-    plt.hlines([1], -1, len(individual_results) + 1, linewidth=0.8)
-    ax1.bar(ind-w, individual_results, width=2*w, align='center', color=speedup_color)
-    ax1.tick_params(axis='y', labelcolor=speedup_color)
 
-    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-    ax2.set_ylabel('Sequential Time (s)', color=seq_time_color)
-    ax2.set_yscale("log")
-    ax2.bar(ind+w, absolute_seq_times_s, width=2*w, align='center', color=seq_time_color)
-    ax2.tick_params(axis='y', labelcolor=seq_time_color)
+    ## Plot speedup
+    ax2.set_ylabel('Speedup', color=speedup_color)
+    ax2.set_xlabel('Pipeline')
+    ax2.bar(ind-w, individual_results, width=2*w, align='center', color=speedup_color)
+    ax2.tick_params(axis='y', labelcolor=speedup_color)
+
+    ax1.set_ylabel('Sequential Time (s)', color=seq_time_color)
+    ax1.set_yscale("log")
+    ax1.bar(ind+w, absolute_seq_times_s, width=2*w, align='center', color=seq_time_color)
+    ax1.tick_params(axis='y', labelcolor=seq_time_color)
+
+    ## Plot average line
+    ax2.hlines([1], -1, len(individual_results) + 1, linewidth=0.8, linestyles="dotted")
+    ax2.hlines([mean], -1, len(individual_results) + 1, linewidth=1.2)
+
+    ax1.yaxis.tick_right()
+    ax1.yaxis.set_label_position("right")
+    ax2.yaxis.tick_left()
+    ax2.yaxis.set_label_position("left")
     # plt.yscale("log")
     # plt.yticks(range(1, 18, 2))
     # plt.ylim((0.1, 20))
