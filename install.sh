d926ba693c6c9f1490c16851cd7c6401a13d6898
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-10-03 22:03:51 +0300

    Add a top level installation script

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Ensure that the script fails if something failed
+set -e
+
+echo "Requires libtool, m4, automake, opam, pkg-config, libffi-dev, python3.8"
+echo "Ensure that you have them by running:"
+echo "  sudo apt install libtool m4 automake opam pkg-config libffi-dev python3.8"
+echo "  opam init"
+echo "Press 'y' if you have these dependencies installed."
+while : ; do
+    read -n 1 k <&1
+    if [[ $k = y ]] ; then
+        echo "Proceeding..."
+        break
+    fi
+done
+
+# Build the parser (requires libtool, m4, automake, opam)
+echo "Building parser..."
+eval $(opam config env)
+cd parser
+make opam-dependencies
+make libdash
+make
+cd ../
+
+echo "Building runtime..."
+# Build runtime tools: eager, split
+cd evaluation/tools/
+make
+cd ../../
+
+# Generate inputs
+echo "Generating input files"
+echo "TODO: Running gen.sh currently doesn't work."
