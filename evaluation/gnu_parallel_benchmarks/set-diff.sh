97a6b2b22886a6d6f16d8f46b6a3c418aac54985
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-11-14 15:44:04 +0200

    Add a comment in set-diff.sh

--- evaluation/gnu_parallel_benchmarks/set-diff.sh
+++ evaluation/gnu_parallel_benchmarks/set-diff.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Issue: Deadlocks as s2 is read from more than one parallel copy
 mkfifo s1 s2
 TEMP_C1="/tmp/{/}.out1"
 TEMP_C2="/tmp/{/}.out2"
@@ -7,6 +8,6 @@ parallel "cat {} | cut -d ' ' -f 1 | sort > $TEMP_C1" ::: $IN &
 sort -m ${TEMP1} > s1 &
 parallel "cat {} | cut -d ' ' -f 1 | sort > $TEMP_C2" ::: $IN &
 sort -m ${TEMP2} > s2 &
-cat s1 | parallel -k --pipe --jobs ${JOBS} --block 250M "comm -23 - s2"
+cat s1 | parallel -k --pipe --jobs ${JOBS} --block 100K "comm -23 - s2"
 rm ${TEMP1} ${TEMP2}
 rm s1 s2
