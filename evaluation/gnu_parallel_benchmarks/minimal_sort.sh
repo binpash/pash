b87d02ed777175e5f64d9fb46ec92ce3090bc44f
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-11-14 07:37:24 -0500

    Add minimal sort results for gnu_parallel

--- evaluation/gnu_parallel_benchmarks/minimal_sort.sh
+++ evaluation/gnu_parallel_benchmarks/minimal_sort.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # cat $IN | tr A-Z a-z | sort
-TEMP_C1="/tmp/{/}.out"
+TEMP_C1="/tmp/{/}.out1"
 mkfifo ${TEMP1}
 parallel "cat {} | tr A-Z a-z | sort > $TEMP_C1" ::: $IN & 
 sort -m ${TEMP1}
