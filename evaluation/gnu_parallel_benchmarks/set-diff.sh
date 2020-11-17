7b3727e84a81b3a6b0533e0f5c83dc7054a844c9
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-11-17 11:07:24 -0500

    Small change on set-diff

--- evaluation/gnu_parallel_benchmarks/set-diff.sh
+++ evaluation/gnu_parallel_benchmarks/set-diff.sh
@@ -22,6 +22,6 @@ parallel "cat {} | cut -d ' ' -f 1 | sort > $TEMP_C1" ::: $IN &
 sort -m ${TEMP1} > s1 &
 parallel "cat {} | cut -d ' ' -f 1 | sort > $TEMP_C2" ::: $IN &
 sort -m ${TEMP2} > s2
-cat s1 | parallel -k --pipe --jobs ${JOBS} --block 100K "comm -23 - s2"
+cat s1 | parallel -k --pipe --jobs ${JOBS} --block 250M "comm -23 - s2"
 rm ${TEMP1} ${TEMP2}
 rm s1 s2
