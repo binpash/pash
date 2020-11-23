4f84f5c87f972b196a4c01a49ff4516b2d2694f5
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-11-23 03:44:50 +0200

    A comment in the gnu parallel script

--- evaluation/gnu_parallel_benchmarks/spell.sh
+++ evaluation/gnu_parallel_benchmarks/spell.sh
@@ -1,4 +1,4 @@
-## Issue 1: Very ugly. (Developer effort)
+## Issue 1: Very ugly. It is akward to handle multiple parallel wires. (Developer effort)
 ## Issue 2: We have to know a valid block size to use (Performance might vary 10K -- 27m, 250K -- 4m, 250M -- 3m)
 ## Issue 3: adding -k is necessary for it to be correct (if not added it is usually correct but sometimes it isn't... Difficult to spot the bug)
 TEMP_C1="/tmp/{/}.out1"
