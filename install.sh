3c5ea1c414e85cc18e065cbb875d63caa57933d7
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-11-27 17:49:03 +0200

    Add an environment variable to the installations cript

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@
 # Ensure that the script fails if something failed
 set -e
 
-echo "Requires libtool, m4, automake, opam, pkg-config, libffi-dev, python3.8"
+## TODO: If an option is set, also run the sudo
+
+echo "Requires libtool, m4, automake, opam, pkg-config, libffi-dev, python3.8, pip for python3"
 echo "Ensure that you have them by running:"
-echo "  sudo apt install libtool m4 automake opam pkg-config libffi-dev python3.8"
+echo "  sudo apt install libtool m4 automake opam pkg-config libffi-dev python3.8 python3-pip"
 echo "  opam init"
 echo "Press 'y' if you have these dependencies installed."
 while : ; do
@@ -20,7 +22,7 @@ done
 echo "Building parser..."
 eval $(opam config env)
 cd parser
-make opam-dependencies
+yes | make opam-dependencies
 make libdash
 make
 cd ../
@@ -44,3 +46,6 @@ cd ../../../
 # Export necessary environment variables
 export PASH_TOP=$PWD
 export PASH_PARSER=${PASH_TOP}/parser/parse_to_json.native
+
+## This is necessary for the parser to link to libdash
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib/"
