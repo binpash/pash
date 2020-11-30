bb856f7ad55fec1ca900ddd9940ffcc53ab0ff5a
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-11-30 23:28:08 +0200

    Add a comment about a bug in the no_in_script.sh

--- evaluation/microbenchmarks/no_in_script.sh
+++ evaluation/microbenchmarks/no_in_script.sh
@@ -1,2 +1,5 @@
-# N=1000
-seq 1 1000 | sort -r
+N=1000 
+# BUG: seq 1 $N | sort -rn
+# In order to make the above work, we need to somehow get the value of N in PaSh.
+# Even exporting doesn't seems to work, even though this works normally if we have the variable in the environment script.
+seq 1 1000 | sort -rn
