584f516ea40c76d820d27d5a9e805f9a573ed43f
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-12-03 15:49:24 +0200

    A couple notes on the pash_runtime

--- compiler/pash_runtime.sh
+++ compiler/pash_runtime.sh
@@ -18,8 +18,35 @@ done
 pash_sequential_script_file=$1
 # >&2 echo "Sequential file in: $1 contains"
 # >&2 cat $1
-# ## TODO: Don't write to /tmp_distr_output/0 anymore
-# source $1 > /tmp/distr_output/0
+# source $1
+
+## NOTE: The intuition about why quick-abort works is that if the compilation succeeds, then the
+##       script is a DFG, meaning that we know exactly how it affects its environment after completing.
+##       Therefore, we can go back and stop the already running script without risking unsafe behavior.
+
+## TODO: We need to *at least* capture the stdout and stderr of the executed bash script 
+##       if we ever want to have a change of running a compiled version in its place.
+##       In order to do that, we can pipe its stdout and stderr to an eager command (so that it doesn't get lost)
+##       and if we actually need it, we can pipe from the output of the eager command to stdout and stderr.
+##
+##       If it ends out that we don't need it we can just kill the command and dump its
+##       outputs to /dev/null.
+
+## TODO: If the output of the DFG is more than just stdout, then we actually need to first delete
+##       what the outputs (since they will be rewritten from the DFG).
+
+## ISSUE: What about if the DFG appends(!) to its output (instead of just writing). Then
+##        we can't handle it correctly.
+
+## ISSUE: We can't actually `source` the execution of the sequential if we later want to kill it.
+##
+## POSSIBLE SOLUTION: 
+##        Maybe we can develop a signal handler in the PaSh runtime that receives a signal and starts executing the parallel version.
+##        Then we can fork of a process that runs the python `pash_runtime` and if it is completed, it can send the signal to 
+##        current shell.
+## 
+## TODO: If we choose to follow this solution we have to make sure that there is no race condition.
+
 
 pash_compiled_script_file=$(mktemp -u)
 python3.8 pash_runtime.py ${pash_compiled_script_file} "${@:2}"
