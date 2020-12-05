aa53752e0112f1191f365b3e0d40b3983b365e00
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-12-05 14:33:08 +0200

    Return exist status from script

--- compiler/pash_wrap_vars.sh
+++ compiler/pash_wrap_vars.sh
@@ -6,5 +6,10 @@ output_vars_file=$2
 # >&2 echo $input_vars_file
 
 source pash_source_declare_vars.sh $input_vars_file
+
+>&2 echo "Executing script:"
+>&2 cat "${@:3}"   
 source "${@:3}"
+pash_exec_status=$?
 declare -p > $output_vars_file
+(exit "$pash_exec_status")
