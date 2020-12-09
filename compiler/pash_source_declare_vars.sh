ec60aaac2809a1e1d589c0f26df22d4c58fc2c50
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-12-09 11:12:28 +0200

    Update sourcing variable filtering

--- compiler/pash_source_declare_vars.sh
+++ compiler/pash_source_declare_vars.sh
@@ -7,11 +7,11 @@
 
 ## TODO: Fix this to not source pash variables so as to not invalidate PaSh progress
 
-## TODO: Do the same filtering in both
+## TODO: Fix this filtering
 
 ## TODO: Error handling if the argument is empty?
 if [ "$PASH_REDIR" == '&2' ]; then
     >&2 source <(cat $1 | grep -v "BASH" | grep -v "LINENO" | grep -v "EUID" | grep -v "GROUPS" | grep -v "pash")
 else
-    >>"$PASH_REDIR" 2>&1 source $1
+    >>"$PASH_REDIR" 2>&1 source <(cat $1 | grep -v "BASH" | grep -v "LINENO" | grep -v "EUID" | grep -v "GROUPS" | grep -v "pash")
 fi
