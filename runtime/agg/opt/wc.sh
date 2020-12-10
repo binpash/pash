cd7d3a92888de13b37c1105b7d40162bca783f15
Nikos Vasilakis <nikos@vasilak.is>
2020-12-10 00:56:07 -0500

    Add aggregators

---
+++ runtime/agg/opt/wc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+A="paste -d '+' "
+for i in "$@"; do
+  # cat "$i" | tr -s ' '  '\n' | tail -n +2
+  A="$A <(cat $i | tr -s ' '  '\n' | tail -n +2) "
+done
+A="$A | bc | tr -s '\n'  ' ' | sed 's/^/   /' | sed 's/$/\n /'"
+
+eval $A
+
