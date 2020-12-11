3a563e1fbe0b92c6bf8c5a9d513afa1a63b2201e
Nikos Vasilakis <nikos@vasilak.is>
2020-12-11 12:02:31 -0500

    First version CI

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -5,6 +5,11 @@ set -ex
 # Placeholder for CI
 REPORT_DIR=../../reports
 C=5
+cd ..
+PASH_TOP="$PWD"
+cd -
+
+SMOOSH_RESULTS=""
 
 trim() {
   awk 'length > 40{$0 = substr($0, 1, 37) "..."} {print $0}' 
@@ -16,10 +21,18 @@ install() {
   scripts/install.sh
 }
 
-tests() {
+pash_tests() {
   cd ../compiler 
-  ./test_evaluation_scripts.sh
-  cd ../
+  ./test_evaluation_scripts.sh | tee  >(grep '^Summary' | cut -d ' ' -f2 > pash_tests.sum)
+  PASH_RESULTS=$(cat pash_tests.sum)
+  cd ../scripts
+}
+
+smoosh_tests() {
+  cd ../../smoosh
+  TEST_SHELL="python3.8 $PASH_TOP/compiler/pash.py --split_fan_out 2 --log_file /tmp/log_file" make -C tests veryclean
+  TEST_SHELL="python3.8 $PASH_TOP/compiler/pash.py --split_fan_out 2 --log_file /tmp/log_file" make -C tests  | tee >(grep 'tests passed' | cut -d ' ' -f2 > smoosh_tests.sum)
+  SMOOSH_RESULTS=$(cat smoosh_tests.sum)
 }
 
 git pull
@@ -45,10 +58,11 @@ trap 'err_report $LINENO' ERR
 
 echo $(date '+%F %T') "Start CI" > $RF
 echo "Lots of tests"   >> $RF
-tests >> $RF
+pash_tests >> $RF
+smoosh_tests >> $RF
 echo $(date '+%F %T') "End CI"  >> $RF
 
-RES="pass"
+RES="$(echo $PASH_RESULTS '|' $SMOOSH_RESULTS)"
 
 FORMAT="%s  %s  %-40s  %s  %s\n"
 SUM="$(printf "$FORMAT" "$(date '+%F;%T')" "$REV" "$MSG" "$RES" "$TIME")"
