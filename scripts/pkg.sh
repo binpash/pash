8c0c16c8420cad0247d0266c63cf010a80f24eef
Nikos Vasilakis <nikos@vasilak.is>
2020-12-10 22:36:15 -0500

    Avoid keeping all versions

--- scripts/pkg.sh
+++ scripts/pkg.sh
@@ -3,10 +3,9 @@
 echo $(pwd)
 REV=0
 
-cd pash
 git pull
 REV=$(git rev-parse --short HEAD)
-cd ..
+cd ../../
 
 # TODO: for a clear release, remove all versioning artifacts
 # cp -r pash release
@@ -15,8 +14,11 @@ cd ..
 # cd ..
 
 tar -cvzf pash.tar.gz pash/
-mv pash.tar.gz get/pash-${REV}.tar.gz
-ln -sf ./pash-${REV}.tar.gz get/latest 
+# uncomment the following line to keep all versions
+# mv pash.tar.gz get/pash-${REV}.tar.gz
+# ln -sf ./pash-${REV}.tar.gz get/latest 
+mv pash.tar.gz get/
+ln -sf ./pash.tar.gz get/latest 
 
 # in the future, we might want to have versions
 # ln -s pash.tar.gz latest
