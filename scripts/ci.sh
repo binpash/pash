d3026ec0cddf98dee31350b55180841476f9e2d5
Nikos Vasilakis <nikos@vasilak.is>
2020-12-10 21:55:59 -0500

    Update CI

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,20 +2,32 @@
 
 # Placeholder for CI
 REPORT_DIR=../../reports
-SUMMARY=$REPORT_DIR/summary.txt
+C=5
 
-run () {
-REV=$(git rev-parse --short HEAD)
-REPORT=$REPORT_DIR/$REV.txt
-PASS="fail"
-TIME="0s"
-echo $(date '+%F %T') "Start CI" > $REPORT
-echo "Lots of tests"   >> $REPORT
-echo $(date '+%F %T') "End CI"  >> $REPORT
-
-cat <(echo $(date '+%F %T') $REV $PASS $TIME) <(cat $SUMMARY) > $SUMMARY
+trim() {
+  awk 'length > 40{$0 = substr($0, 1, 37) "..."} {printf "%-20s", $0}' 
 }
 
-git pull
+run () {
+  git pull
+
+  #all this runs after pull
+  REV=$(git rev-parse --short HEAD)
+  MSG="$(git log -1 --pretty=%B | trim)"
+  sleep 30
+
+  RF=$REPORT_DIR/$REV
+  SF=$REPORT_DIR/summary
+  PASS="fail"
+  TIME="0s"
+  echo $(date '+%F %T') "Start CI" > $RF
+  echo "Lots of tests"   >> $RF
+  echo $(date '+%F %T') "End CI"  >> $RF
+
+  FORMAT="%s  %s  %s  %s  %s\n"
+  SUM="$(printf "$FORMAT" "$(date '+%F;%T')" "$REV" "$MSG" "$PASS" "$TIME")"
+  cat <(echo "$SUM") <(cat $SF) > $SF
+}
+
 run
 
