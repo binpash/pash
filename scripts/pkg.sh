90635f5341b7163a96faac8750626c74126b49e2
Nikos Vasilakis <nikos@vasilak.is>
2020-12-12 11:36:19 -0500

    Switch to light clone for pkg

--- scripts/pkg.sh
+++ scripts/pkg.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+set -ex
+
 echo $(pwd)
 REV=0
 
-git pull
 REV=$(git rev-parse --short HEAD)
 cd ../../
+
+git clone --depth 1 git@github.com:andromeda/pash.git
 
 # TODO: for a clear release, remove all versioning artifacts
 # cp -r pash release
@@ -13,12 +16,14 @@ cd ../../
 # rm -rf .gitignore .gitsubmodules .git
 # cd ..
 
-tar -cvzf pash.tar.gz pash/ > /dev/null
+mv pash pash-shallow
+tar -cvzf pash-shallow.tar.gz pash-shallow/ > /dev/null
 # uncomment the following line to keep all versions
 # mv pash.tar.gz get/pash-${REV}.tar.gz
 # ln -sf ./pash-${REV}.tar.gz get/latest 
-mv pash.tar.gz get/
-ln -sf ./pash.tar.gz get/latest 
+mv pash-shallow.tar.gz get/
+ln -sf ./pash-shallow.tar.gz get/latest 
+rm -rf pash-shallow
 
 # in the future, we might want to have versions
 # ln -s pash.tar.gz latest
