e12c207565fc6130c0ede6d4043b1e013a4ad61c
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-12-16 14:37:46 +0200

    Add a comment to remember to check something in Arg

--- compiler/definitions/ir/arg.py
+++ compiler/definitions/ir/arg.py
@@ -3,8 +3,9 @@ from ir_utils import *
 class Arg:
     def __init__(self, arg_char_list):
         if(isinstance(arg_char_list, Arg)):
-           ## TODO: We might need to copy here?
-           self.arg_char_list = arg_char_list.arg_char_list
+            ## TODO: Make sure that this does not happen using an assertion.
+            ## TODO: We might need to copy here?
+            self.arg_char_list = arg_char_list.arg_char_list
         else:
            self.arg_char_list = arg_char_list
 
