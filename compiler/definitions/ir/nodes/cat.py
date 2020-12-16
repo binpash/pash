a6e5d36860ce1f9c01a7a0f2b51a491136666e08
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-12-16 14:39:43 +0200

    Remove obsolete code

--- compiler/definitions/ir/nodes/cat.py
+++ compiler/definitions/ir/nodes/cat.py
@@ -21,25 +21,3 @@ def make_cat_node(input_file_ids, output_file_id):
     ast = None
     return Cat(ast, command, options, in_stream, out_stream,
                opt_indices, category, stdout=stdout)
-
-# from definitions.ir.command import *
-
-# class Cat(Command):
-#     def __init__(self, ast, command, options, in_stream, out_stream,
-#                  opt_indices, category, stdin=None, stdout=None, redirections=[]):
-#         super().__init__(ast, command, options, in_stream, out_stream,
-#                          opt_indices, category, stdin=stdin, stdout=stdout, redirections=redirections)
-
-# def make_cat_node(input_file_ids, output_file_id):
-#     command = string_to_argument("cat")
-#     options = input_file_ids
-#     in_stream = [("option", i)  for i in range(len(input_file_ids))]
-#     out_stream = ["stdout"]
-#     stdout = output_file_id
-#     opt_indices = []
-#     category = "stateless"
-#     ## TODO: Fill the AST
-#     ast = None
-#     return Cat(ast, command, options, in_stream, out_stream,
-#                opt_indices, category, stdout=stdout)
-
