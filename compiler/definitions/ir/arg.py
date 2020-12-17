52436deccb0153f6b6e3573d909615d1f33d3843
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-12-17 19:34:14 +0200

    Add a comparison method for arguments

--- compiler/definitions/ir/arg.py
+++ compiler/definitions/ir/arg.py
@@ -1,4 +1,5 @@
 from ir_utils import *
+from util import *
 
 class Arg:
     def __init__(self, arg_char_list):
@@ -11,6 +12,12 @@ class Arg:
 
     def __repr__(self):
         return format_arg_chars(self.arg_char_list)
+
+    def __eq__(self, other):
+        if(isinstance(other, Arg)):
+            return self.arg_char_list == other.arg_char_list
+        log("Warning: Comparing Arg:", self, "with a non Arg argument:", other, "of type:", type(other))
+        return False
 
     def opt_serialize(self):
         return self.__repr__()
