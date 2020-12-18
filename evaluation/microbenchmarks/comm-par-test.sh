d53f2ca47138e4ba2216886567a1b877e9c9ef3c
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-12-18 02:35:48 +0200

    Add another test

--- evaluation/microbenchmarks/comm-par-test.sh
+++ evaluation/microbenchmarks/comm-par-test.sh
@@ -3,6 +3,10 @@
 # Tests the parallelization of comm with a configuration input and a stream input.
 mkfifo s1 s2
 
+## TODO: Add a grep after the second cat triggers an error
+## TODO: Add a new line in the second s2 to have difference in output
+## TODO: In theory we could parallelize a node that has a sequence in it!
+
 cat $IN > s1 &
 cat $IN > s2 &
 comm -23 s1 s2
