619a19e6ba63b132407e53a57a3c72ba5c482811
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2020-12-22 16:19:35 +0200

    Extend DFG node to work with configuration inputs

--- compiler/definitions/ir/nodes/cat.py
+++ compiler/definitions/ir/nodes/cat.py
@@ -2,13 +2,11 @@ from definitions.ir.dfg_node import *
 from definitions.input_consumption_mode import *
 
 class Cat(DFGNode):
-    def __init__(self, inputs, outputs, com_name, com_category, input_consumption_mode = InputConsumptionMode.ORDERED,
+    def __init__(self, inputs, outputs, com_name, com_category,
                  com_options = [], com_redirs = [], com_assignments=[]):
         assert(str(com_name) == "cat")
         assert(com_category == "stateless")
-        assert(input_consumption_mode == InputConsumptionMode.ORDERED)
         super().__init__(inputs, outputs, com_name, com_category,
-                         input_consumption_mode=input_consumption_mode,
                          com_options=com_options, 
                          com_redirs=com_redirs, 
                          com_assignments=com_assignments)
