45d49ff17966344088ae98eac910db9a4bce55bb
Nikos Vasilakis <nikos@vasilak.is>
2021-01-03 19:29:41 -0500

    Update scripts (closes #78)

--- scripts/up.sh
+++ scripts/up.sh
@@ -21,11 +21,22 @@ if [ $PLATFORM = 'darwin' ]; then
   echo 'PaSh is not yet well supported on OS X'
 fi
 
-mkdir pash
-cd pash
+INSTALL=0
+while getopts 'i' opt; do
+    case $opt in
+        i) INSTALL=1 ;;
+        *) echo 'Error in command line parsing' >&2
+           exit 1
+    esac
+done
+shift "$(( OPTIND - 1 ))"
+
+[ -d "./pash" ] && mv ./pash ./pash-old-${RANDOM}
 curl -s $URL | tar xzf - --strip-components=1 -C .
-cd scripts
 # FIXME: it's unclear we should always run as root
 # A first step would be to confirm dependencies are met
 # ./install.sh -p
+if [[ $INSTAll == 1 ]]; then
+  echo "This option does not work currently. Please open a ticket"
+fi
 echo 'Run `./scripts/install.sh`, with `-p` for installing packages if needed'
