5509fe0a0549bf11b3adb52fcaa4d146c2ab1e0a
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-01-05 04:19:55 +0200

    Fix the buggy script

--- evaluation/scripts/buggy_comm_script.sh
+++ evaluation/scripts/buggy_comm_script.sh
@@ -1,11 +1,21 @@
-mkfifo s1 s2 s3 s4
+#!/bin/bash
 
-IN=../evaluation/scripts/input/1M.txt
+mkfifo s1 s2 s3 s4 s5
 
-cat $IN | sort | tee s4 >s3 &
-comm -23 <(cat $IN | sort) s3 > s1 &
-comm -23 <(cat $IN | sort) s4 > s2 &
-cat s1 s2
+PREV_IN=../evaluation/scripts/input/1M.txt
+IN=/tmp/1M.txt
 
-rm s1 s2 s3 s4
+cat $PREV_IN > $IN
+echo "end" >> $IN
 
+cat $IN | grep "king" | tee s4 >s3 &
+comm -23 $IN s3 > s1 &
+comm -23 $IN s4 > s2 &
+{ ../runtime/eager s2 s5 "/tmp/eager_intermediate_#file1" & }
+cat s1 s5 > /tmp/buggy.out
+
+comm -23 <(cat $IN $IN) <(cat $IN | grep "king") > /tmp/seq.out
+
+rm s1 s2 s3 s4 s5
+
+diff /tmp/buggy.out /tmp/seq.out
