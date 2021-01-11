57572b96cda54f7eeb401f31ace3d3b7218bab18
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-01-11 09:20:26 -0500

    Clean up and simplify the web-index evaluation script

--- compiler/execute_web_index_dish_evaluation.sh
+++ compiler/execute_web_index_dish_evaluation.sh
@@ -2,42 +2,18 @@
 
 PASH_TOP=${PASH_TOP:-$(git rev-parse --show-toplevel --show-superproject-working-tree)}
 
-
 eval_dir="../evaluation/"
 directory="${eval_dir}/scripts/web-index/"
-p1="${directory}/p1.sh"
-p2="${directory}/p2.sh"
-
-output_dir="/tmp/web-index-output"
-p1_out="${output_dir}/p1.out"
-
-## TODO: Move the scripts outside of the intermediary
-intermediary_dir="${eval_dir}/intermediary/"
 results_dir="${eval_dir}/results/"
-
 input_dir="${HOME}/wikipedia/"
 
-## Make the temporary output dir
-mkdir -p $output_dir
-
-# cp "$input_dir/index_h_100000.txt" $p1_out
-cp "$input_dir/index_h_100.txt" $p1_out
-
-
-## TODO: If we have time, we need to remove this split
-
-## Split the input file
-split -n l/2 -d $p1_out ${p1_out}_2_
-split -n l/16 -d $p1_out ${p1_out}_16_
-
-echo "Intermediate files have been successfully produced."
-
-export IN_DIR="${output_dir}"
+# export IN="$input_dir/index_h_100000.txt" # A couple hours in bash 
+# export IN="$input_dir/index_h_1000.txt" # About 1.5 minutes in bash 
+export IN="$input_dir/index_h_100.txt" # About 10 seconds in bash 
 export WIKI="${input_dir}"
 export WEB_INDEX_DIR="${directory}"
 
-web_index_2_script="${intermediary_dir}/web-index_full_2_seq.sh"
-web_index_16_script="${intermediary_dir}/web-index_full_16_seq.sh"
+web_index_script="${eval_dir}/scripts/web-index.sh"
 
 seq_output=/tmp/seq_output
 pash_width_2_output=/tmp/pash_2_output
@@ -50,14 +26,14 @@ pash_width_16_time="$results_dir/web-index-16-pash.time"
 ##       This means that for the whole wikipedia (1mil urls) it will take about
 ##       20+ hours to complete.
 echo "Executing the script with bash..."
-{ time /bin/bash $web_index_2_script > $seq_output ; } 2> >(tee "${seq_time}" >&2)
+{ time /bin/bash $web_index_script > $seq_output ; } 2> >(tee "${seq_time}" >&2)
 
 echo "Executing the script with pash -w 2 (log in: /tmp/pash_2_log)"
-{ time $PASH_TOP/pa.sh -w 2 --log_file /tmp/pash_2_log --output_time $web_index_2_script ; } 1> "$pash_width_2_output" 2> >(tee "${pash_width_2_time}" >&2)
+{ time $PASH_TOP/pa.sh -w 2 --log_file /tmp/pash_2_log --output_time $web_index_script ; } 1> "$pash_width_2_output" 2> >(tee "${pash_width_2_time}" >&2)
 echo "Checking for output equivalence..."
 diff -s $seq_output $pash_width_2_output | head
 
 echo "Executing the script with pash -w 16 (log in: /tmp/pash_16_log)"
-{ time $PASH_TOP/pa.sh -w 16 --log_file /tmp/pash_16_log --output_time $web_index_16_script ; } 1> "$pash_width_16_output" 2> >(tee "${pash_width_16_time}" >&2)
+{ time $PASH_TOP/pa.sh -w 16 --log_file /tmp/pash_16_log --output_time $web_index_script ; } 1> "$pash_width_16_output" 2> >(tee "${pash_width_16_time}" >&2)
 echo "Checking for output equivalence..."
 diff -s $seq_output $pash_width_16_output | head
