c67e64429589d3723b986f9f2031fb1a8b8ef668
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-01-11 06:17:29 +0200

    Now eager also removes its intermediate files, finally :)

---
+++ runtime/eager.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+input=${1?"ERROR: Eager: No input file given"}
+output=${2?"ERROR: Eager: No output file given"}
+intermediate_file=${3?"ERROR: Eager: No intermediate file given"}
+
+# Set a default DISH_TOP in this directory if it doesn't exist
+PASH_TOP=${PASH_TOP:-$(git rev-parse --show-toplevel)}
+
+$PASH_TOP/runtime/eager "$input" "$output" "$intermediate_file"
+rm "$intermediate_file"
