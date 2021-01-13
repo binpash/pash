fb00299504caa9dc1d67ac14b35acb6d975cf8b8
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-01-13 09:54:58 -0500

    Simplify unix50 execution script

--- compiler/execute_unix_benchmarks.sh
+++ compiler/execute_unix_benchmarks.sh
@@ -3,7 +3,15 @@
 unix50_dir="../evaluation/unix50/"
 unix50_intermediary="${unix50_dir}/intermediary/"
 intermediary_dir="../evaluation/intermediary/"
-results_subdir="unix50"
+results_subdir_prefix="unix50"
+
+# maximum_input_size="$((10 * 1024 * 1024 * 1024))" # 10 GB
+n_in=16
+maximum_input_size="$((1024 * 1024 * 1024))" # 1 GB
+n_in=4
+
+
+results_subdir="${results_subdir_prefix}_${n_in}_${maximum_input_size}"
 
 rm -r $unix50_intermediary
 mkdir -p $unix50_intermediary
@@ -13,39 +21,17 @@ mkdir -p "../evaluation/results/${results_subdir}/"
 ## Make inputs larger and generate scripts and their envs
 ##
 ## TODO: The generation of scripts has to be transparent.
-## TODO: Simplify this parameter
 ## TODO: See if results are similar with a smaller input size
-# maximum_input_size="$((10 * 1000 * 1000 * 1000))" # 10 GB
-maximum_input_size="$((100 * 1000 * 1000))" # 100 MB
 python3 generate_unix50_scripts.py $unix50_dir $unix50_intermediary $maximum_input_size
 
-n_inputs=(
-    # 1
-    # 2
-    4
-    # 8
-    # 16
-    # 32
-    # 64
-)
-
-## Note: Maybe we need to tune the configuration (fan-out, batch-size)
-##       before some specific benchmarks
 for unix50_pipeline in $(ls ${unix50_intermediary} | grep -v "_env" | cut -f 1 -d '.' | sort); do
     echo $unix50_pipeline
-    exec_seq="-s"
-    for n_in in "${n_inputs[@]}"; do
 
-        ## Generate the intermediary script
-        echo "Generating input and intermediary scripts... be patient..."
-        python3 generate_microbenchmark_intermediary_scripts.py \
-                $unix50_intermediary $unix50_pipeline $n_in $intermediary_dir
+    echo "Generating input and intermediary scripts... be patient..."
+    python3 generate_microbenchmark_intermediary_scripts.py \
+            $unix50_intermediary $unix50_pipeline $n_in $intermediary_dir
 
-        ## Execute the intermediary script
-        ./execute_compile_evaluation_script.sh $exec_seq -e "${unix50_pipeline}" "${n_in}" "${results_subdir}"  > /dev/null
-        rm -f /tmp/eager*
-
-        # Only execute the sequential once
-        exec_seq=""
-    done
+    echo "Executing script with bash and pash..."
+    ./execute_compile_evaluation_script.sh -s -a "${unix50_pipeline}" "${n_in}" "${results_subdir}"  > /dev/null
+    rm -f /tmp/eager*
 done
