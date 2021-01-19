ef9b722586807e90b7c39d9209e3907717cb79a6
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-01-19 23:00:29 +0200

    Move evaluation scripts to eurosys directory

--- evaluation/eurosys/execute_unix_benchmarks.sh
+++ evaluation/eurosys/execute_unix_benchmarks.sh
@@ -5,6 +5,7 @@ unix50_intermediary="${unix50_dir}/intermediary/"
 intermediary_dir="../evaluation/intermediary/"
 results_subdir_prefix="unix50"
 
+## TODO: Choose with flag
 # maximum_input_size="$((10 * 1024 * 1024 * 1024))" # 10 GB
 n_in=16
 maximum_input_size="$((1024 * 1024 * 1024))" # 1 GB
@@ -19,9 +20,6 @@ mkdir -p $intermediary_dir
 mkdir -p "../evaluation/results/${results_subdir}/"
 
 ## Make inputs larger and generate scripts and their envs
-##
-## TODO: The generation of scripts has to be transparent.
-## TODO: See if results are similar with a smaller input size
 python3 generate_unix50_scripts.py $unix50_dir $unix50_intermediary $maximum_input_size
 
 for unix50_pipeline in $(ls ${unix50_intermediary} | grep -v "_env" | cut -f 1 -d '.' | sort); do
