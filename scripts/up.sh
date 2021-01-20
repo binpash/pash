aeee865746137008674b231f58d75b9a7ea6481d
Nikos Vasilakis <nikos@vasilak.is>
2021-01-20 11:32:21 -0500

    Update configs

--- scripts/up.sh
+++ scripts/up.sh
@@ -7,6 +7,7 @@ PLATFORM=$(uname | tr '[:upper:]' '[:lower:]')
 URL='http://get.pash.ndr.md/'
 VERSION='latest'
 DOWNLOADER='curl'
+INSTALL='false'
 alias curl='curl -s'
 
 download () {
@@ -21,6 +22,15 @@ if [ $PLATFORM = 'darwin' ]; then
   echo 'PaSh is not yet well supported on OS X'
 fi
 
+while getopts 'p' opt; do
+    case $opt in
+        p) INSTALL="true" ;;
+        *) echo 'Error in command line parsing' >&2
+           exit 1
+    esac
+done
+shift "$(( OPTIND - 1 ))"
+
 mkdir pash
 cd pash
 curl -s $URL | tar xzf - --strip-components=1 -C .
@@ -28,4 +38,8 @@ cd scripts
 # FIXME: it's unclear we should always run as root
 # A first step would be to confirm dependencies are met
 # ./install.sh -p
-echo 'Run `./scripts/install.sh`, with `-p` for installing packages if needed'
+if [[ INSTAll == "true" ]]; then
+  echo "ATTEMPT INSTALL"
+else
+  echo 'Run `./scripts/install.sh`, with `-p` for installing packages if needed'
+fi
