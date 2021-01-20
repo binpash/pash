fd5905d18788b0960b07c1f8a00ad97c357a65fe
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-01-20 18:43:33 -0500

    Fix discrepancies in Unix50 script

--- evaluation/eurosys/execute_unix_benchmarks.sh
+++ evaluation/eurosys/execute_unix_benchmarks.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-unix50_dir="../evaluation/unix50/"
+## Necessary to set PASH_TOP
+export PASH_TOP=${PASH_TOP:-$(git rev-parse --show-toplevel --show-superproject-working-tree)}
+
+unix50_dir="$PASH_TOP/evaluation/unix50/"
 unix50_intermediary="${unix50_dir}/intermediary/"
-intermediary_dir="../evaluation/intermediary/"
+intermediary_dir="$PASH_TOP/evaluation/intermediary/"
 results_subdir_prefix="unix50"
 
 ## TODO: Choose with flag
-# maximum_input_size="$((10 * 1024 * 1024 * 1024))" # 10 GB
+maximum_input_size="$((10 * 1024 * 1024 * 1024))" # 10 GB
 n_in=16
-maximum_input_size="$((1024 * 1024 * 1024))" # 1 GB
-n_in=4
+# maximum_input_size="$((1024 * 1024 * 1024))" # 1 GB
+# n_in=4
 
 
 results_subdir="${results_subdir_prefix}_${n_in}_${maximum_input_size}"
@@ -17,7 +20,7 @@ results_subdir="${results_subdir_prefix}_${n_in}_${maximum_input_size}"
 rm -r $unix50_intermediary
 mkdir -p $unix50_intermediary
 mkdir -p $intermediary_dir
-mkdir -p "../evaluation/results/${results_subdir}/"
+mkdir -p "$PASH_TOP/evaluation/results/${results_subdir}/"
 
 ## Make inputs larger and generate scripts and their envs
 python3 generate_unix50_scripts.py $unix50_dir $unix50_intermediary $maximum_input_size
@@ -26,10 +29,10 @@ for unix50_pipeline in $(ls ${unix50_intermediary} | grep -v "_env" | cut -f 1 -
     echo $unix50_pipeline
 
     echo "Generating input and intermediary scripts... be patient..."
-    python3 generate_microbenchmark_intermediary_scripts.py \
+    python3 "$PASH_TOP/evaluation/generate_microbenchmark_intermediary_scripts.py" \
             $unix50_intermediary $unix50_pipeline $n_in $intermediary_dir
 
     echo "Executing script with bash and pash..."
-    ./execute_compile_evaluation_script.sh -s -a "${unix50_pipeline}" "${n_in}" "${results_subdir}"  > /dev/null
+    "$PASH_TOP/evaluation/execute_compile_evaluation_script.sh" -s -a "${unix50_pipeline}" "${n_in}" "${results_subdir}"  # > /dev/null
     rm -f /tmp/eager*
 done
