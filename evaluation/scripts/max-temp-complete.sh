dcefdf9502b5cf9c1f47f19992b080d4c9888d8b
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-01-21 21:04:04 +0200

    Slightly modify the max-temp script

--- evaluation/scripts/max-temp-complete.sh
+++ evaluation/scripts/max-temp-complete.sh
@@ -4,7 +4,7 @@ sed 's;^;http://ndr.md/data/noaa/;' |
     sed 's;$;/;' |
     xargs -n 1 curl -s |
     grep gz |
-    tr -s ' ' |
+    tr -s ' \n' |
     cut -d ' ' -f9 |
     sed 's;^\(.*\)\(20[0-9][0-9]\).gz;\2/\1\2\.gz;' |
     sed 's;^;http://ndr.md/data/noaa/;' |
