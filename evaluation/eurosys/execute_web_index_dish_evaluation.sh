709e2ea5c33ee6a5aa6103e58f99aac4c5d5463d
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-01-21 20:03:41 +0200

    Update evaluation scripts

--- evaluation/eurosys/execute_web_index_dish_evaluation.sh
+++ evaluation/eurosys/execute_web_index_dish_evaluation.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
-PASH_TOP=${PASH_TOP:-$(git rev-parse --show-toplevel --show-superproject-working-tree)}
+export PASH_TOP=${PASH_TOP:-$(git rev-parse --show-toplevel --show-superproject-working-tree)}
 
-eval_dir="../evaluation/"
+## There are two possible execution levels:
+## options -s: 1,000 urls (about 1.5 minutes in bash)
+## options -l: 100,000 urls (a couple hours in bash)
+export IN="$input_dir/index_h_1000.txt" # About 1.5 minutes in bash 
+# export IN="$input_dir/index_h_100.txt" # About 10 seconds in bash 
+
+while getopts 'slh' opt; do
+    case $opt in
+        s) export IN="$input_dir/index_h_1000.txt" ;;
+        l) export IN="$input_dir/index_h_100000.txt" ;;
+        h) echo "There are three possible execution levels:"
+           echo "option -s: 1,000 urls (about 1.5 minutes in bash)"
+           echo "option -l: 100,000 urls (a couple hours in bash) (EuroSys evaluation)"
+           exit 0 ;;
+        *) echo 'Error in command line parsing' >&2
+           exit 1
+    esac
+done
+shift "$(( OPTIND - 1 ))"
+
+eval_dir="$PASH_TOP/evaluation/"
 directory="${eval_dir}/scripts/web-index/"
 results_dir="${eval_dir}/results/"
 input_dir="${HOME}/wikipedia/"
 
-# export IN="$input_dir/index_h_100000.txt" # A couple hours in bash 
-# export IN="$input_dir/index_h_1000.txt" # About 1.5 minutes in bash 
-export IN="$input_dir/index_h_100.txt" # About 10 seconds in bash 
 export WIKI="${input_dir}"
 export WEB_INDEX_DIR="${directory}"
 
@@ -22,9 +39,6 @@ seq_time="$results_dir/web-index-seq.time"
 pash_width_2_time="$results_dir/web-index-2-pash.time"
 pash_width_16_time="$results_dir/web-index-16-pash.time"
 
-## Note: The sequential p2 stage takes about 7 seconds for 100! urls
-##       This means that for the whole wikipedia (1mil urls) it will take about
-##       20+ hours to complete.
 echo "Executing the script with bash..."
 { time /bin/bash $web_index_script > $seq_output ; } 2> >(tee "${seq_time}" >&2)
 
