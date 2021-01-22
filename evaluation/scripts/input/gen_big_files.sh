56785b81f9b74245b6d6a0f0089aa770d529073f
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-01-22 16:53:40 +0200

    Fix a missing step in file generation

--- evaluation/scripts/input/gen_big_files.sh
+++ evaluation/scripts/input/gen_big_files.sh
@@ -12,12 +12,6 @@ for (( i = 0; i < 10; i++ )); do
   cat 10G.txt >> 100G.txt                                                           
 done
 
-rm -f all_cmds_x100.txt
-touch all_cmds_x100.txt
-for (( i = 0; i < 10; i++ )); do                                               
-  cat all_cmds_x10.txt >> all_cmds_x100.txt                                                           
-done
-
 rm -f all_cmds_x1000.txt
 touch all_cmds_x1000.txt
 for (( i = 0; i < 100; i++ )); do                                               
