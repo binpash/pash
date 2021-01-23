d81c6e52aed0f1f3d227e0dbc6983380f31cdd85
Nikos Vasilakis <nikos@vasilak.is>
2021-01-22 22:10:22 -0500

    Update quickcheck

--- scripts/quickcheck.sh
+++ scripts/quickcheck.sh
@@ -3,11 +3,15 @@
 cd $PASH_TOP
 
 echo  confirms the necessary components for running the artifact
+echo
 echo Git commit ID: $(git rev-parse --short HEAD)
 echo \$PASH_TOP: $(echo $PASH_TOP)
-echo pash executable: $(which pash)
-echo Small input: $(wc -l ../evaluation/scripts/input/1M.txt)
+echo pash executable: $PASH_TOP/pa.sh
 
-../pa.sh --help
+# SMALL=$PASH_TOP/evaluation/scripts/input/3M.txt
+echo 'Inputs:'
+ls -lh $PASH_TOP/evaluation/scripts/input/* | awk '{print $5,$9}'
 
 
+echo
+$PASH_TOP/pa.sh --help
