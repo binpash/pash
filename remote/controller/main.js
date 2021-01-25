7989b75ee97cb2db5e43430b9ac41d03a2f65161
Sage Gerard <sage@sagegerard.com>
2021-01-25 13:06:46 -0500

    Revert "Small fixes from test run"
    This reverts commit ace0cb9465123ac6f8e8fbccaf9f7785946f2e45.

--- remote/controller/main.js
+++ remote/controller/main.js
@@ -3,7 +3,6 @@
 const { createServer } = require('http');
 const { parse: parseUrl } = require('url');
 const { log, err, respond } = require('./lib.js');
-const PORT = 2047;
 
 const createControlServer = (routes) =>
       createServer(async (req, res) => {
@@ -35,5 +34,5 @@ module.exports = {
 
 if (require.main === module) {
     createControlServer(require('./routes.js'))
-        .listen(PORT, "127.0.0.1", log("server listening on port " + PORT));
+        .listen(2047, log("server listening on port " + 2047));
 }
