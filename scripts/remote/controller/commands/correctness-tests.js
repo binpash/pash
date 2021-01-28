ea69635bb9343c4d9553ef091f3db0a61bbdf25d
Sage Gerard <sage@sagegerard.com>
2021-01-28 14:51:37 -0500

    Refactor correctness-tests.js to use syncDirectory

--- scripts/remote/controller/commands/correctness-tests.js
+++ scripts/remote/controller/commands/correctness-tests.js
@@ -1,32 +1,9 @@
-const Rsync = require('rsync');
-const { hours } = require('../lib.js');
+const { hours, syncRemoteDirectory } = require('../lib.js');
 
 module.exports = {
     makeCommandOptions: () => ({
         timeout: hours(1),
         shouldStopWaiting: (stdout, stderr) => /<<(fail|done)>>/.test(stdout),
-        postCompletion: async (ssh) => {
-            try {
-                // TODO: Stream rsync stdin/stdout to new files.
-                const rsync = (
-                    new Rsync()
-                        .flags('abziu')
-                        .set('e', `ssh -i ${rc('private_key')}`)
-                        .source(`${rc('user')}@${rc('host')}:reports/`) // Trailing '/' is meaningful: https://serverfault.com/a/529294
-                        .destination(rc('ci_reports_path', `${__dirname}/reports`))
-                );
-
-                await new Promise((resolve, reject) =>
-                    rsync.execute((error, code, cmd) => {
-                        if (error)
-                            reject(error)
-                        else
-                            resolve();
-                    }));
-            } catch (e) {
-                err(e);
-                throw e;
-            }
-        },
+        postCompletion: (ssh) => syncRemoteDirectory('reports', rc('ci_reports_path', `${__dirname}/reports`)),
     }),
 };
