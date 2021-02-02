9f93c82b4010bd1902af2dc9dbd4606031068ae4
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-02-01 20:03:13 -0500

    Add a fork in the ssh-install

--- scripts/ssh-install.sh
+++ scripts/ssh-install.sh
@@ -22,15 +22,16 @@ PRIVATE_KEY=$1
 ## The user and IP address or hostname of the server
 HOSTNAME=$2
 
+FORK="andromeda"
 BRANCH="main"
 USER="ubuntu"
 
 ssh -o StrictHostKeyChecking=no -i $PRIVATE_KEY "${USER}@${HOSTNAME}" /bin/bash <<EOF
 rm -rf pash
-git clone https://github.com/andromeda/pash.git
+git clone https://github.com/$FORK/pash.git
 cd pash
 git checkout $BRANCH
-echo "At branch: \$(git rev-parse --abbrev-ref HEAD)"
+echo "At branch: \$(git rev-parse --abbrev-ref HEAD) of \$(git remote get-url origin)"
 sed -i 's#git@github.com:angelhof/libdash.git#https://github.com/angelhof/libdash/#g' .gitmodules
 source scripts/install.sh -p
 cd compiler
