5dc1bfc93be8f393e7fdcb8fb2846242f5d96d51
Sage Gerard <sage@sagegerard.com>
2021-02-03 22:16:44 -0500

    Combine and move EXIT tranps in perf worker

--- scripts/remote/workers/run-perf.sh
+++ scripts/remote/workers/run-perf.sh
@@ -7,21 +7,22 @@ IMAGE_TAG=pash-perf-img
 CONTAINER_NAME=pash-perf
 
 cleanup() {
-  docker container stop $CONTAINER_NAME
-  docker container rm $CONTAINER_NAME
+    rm -r lock
+    docker container stop $CONTAINER_NAME
+    docker container rm $CONTAINER_NAME
 }
 
 if [ -d lock ]; then
     echo "Busy on existing job."
 else
+    cleanup
     mkdir lock
-    trap 'rm -r lock' EXIT
-
-    docker build -t $IMAGE_TAG - < "${1:-~/pash/scripts/Dockerfile}"
-    docker run -itd --name $CONTAINER_NAME $IMAGE_TAG /bin/bash
 
     trap 'cleanup' EXIT
     trap 'echo "<<fail>>"' ERR
+
+    docker build -t $IMAGE_TAG - < "${1:-~/pash/scripts/Dockerfile}"
+    docker run -itd --name $CONTAINER_NAME $IMAGE_TAG /bin/bash
 
     docker exec $CONTAINER_NAME /bin/bash -c "/pash/scripts/ci-perf.sh"
 
