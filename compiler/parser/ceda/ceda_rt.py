b8ca0ed9bb80386003ce4720c082df3a265f9bf5
Thurston Dang (pash docker) <tdang@mit.edu>
2021-02-13 00:39:53 +0000

    Benchmarking

--- compiler/parser/ceda/ceda_rt.py
+++ compiler/parser/ceda/ceda_rt.py
@@ -3,19 +3,35 @@ import sys;
 from parse_to_ast2 import parse_to_ast;
 from ast2shell import to_string;
 
+import cProfile;
 
-sys.setrecursionlimit (30001)
+
+sys.setrecursionlimit (9001);
 
 
-if (len (sys.argv) == 1):
-    new_asts = parse_to_ast ("-");
-else:
-    new_asts = parse_to_ast (sys.argv [1]);
+def print_asts (new_asts):
+    for ast in new_asts:
+        print (to_string (ast));
+#        to_string (ast);
 
+
+init = True
+
+#cProfile.runctx ("parse_to_ast (sys.argv [1], init)", globals (), locals ());
+#sys.exit (0);
+
+for f in range (10):
+    if (len (sys.argv) == 1):
+        new_asts = parse_to_ast ("-", init);
+    else:
+        new_asts = parse_to_ast (sys.argv [1], init);
+
+    init = False;
 
 #print ("NEW IMPLEMENTATION:");
 #print (new_asts);
 #print ("");
 
-for ast in new_asts:
-    print (to_string (ast));
+    print_asts (new_asts);
+
+#    cProfile.runctx ("print_asts (new_asts)", globals (), locals ());
