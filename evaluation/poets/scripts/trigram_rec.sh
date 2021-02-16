1b55a3a03b9cfe9d451fca489ee4cf3dff4c8a6a
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-02-16 19:08:50 +0200

    Correct input file path

--- evaluation/poets/scripts/trigram_rec.sh
+++ evaluation/poets/scripts/trigram_rec.sh
@@ -1,5 +1,5 @@
 # Recursive PaSh calls to trigrams (defined below)
 # trigram file scripts ???
-INPUT=${INPUT:-inputs/genesis}
+INPUT=${INPUT:-$PATH_TOP/evaluation/scripts/input/genesis}
 grep 'the land of' ${INPUT} | sh trigram | sort -nr | sed 5q 
 grep 'And he said' ${INPUT} | sh trigram | sort -nr | sed 5q
