bcdd97d69448f249ac5bef7dc304f560cd207d86
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-02-17 08:05:21 +0000

    fetch dataset script

---
+++ evaluation/poets/setup.sh
@@ -0,0 +1,3 @@
+mkdir -p ${PASH_TOP}/evaluation/scripts/input/poets
+wget -O  ${PASH_TOP}/evaluation/scripts/input/poets/genesis http://www.gutenberg.org/cache/epub/8001/pg8001.txt
+wget -O  ${PASH_TOP}/evaluation/scripts/input/poets/exodus http://www.gutenberg.org/cache/epub/8001/pg8001.txt 
