3081e6897f49a6c81dccc0402bd97322f9da0cb7
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-02-17 11:03:44 +0000

    Update on paths and commands

--- evaluation/posh/dis_cat.sh
+++ evaluation/posh/dis_cat.sh
@@ -1,3 +1,3 @@
-INPUT=${INPUT:-$PASH_TOP/evaluation/scripts/input/log1}
-INPUT2=${INPUT2:-$PASH_TOP/evaluation/scripts/input/log2}
+INPUT=${INPUT:-$PASH_TOP/evaluation/scripts/input/posh/log20170314.csv}
+INPUT2=${INPUT2:-$PASH_TOP/evaluation/scripts/input/posh/log20170315.csv}
 cat ${INPUT} ${INPUT2} | grep 'bar' > $PASH_TOP/evaluation/scripts/input/output.txt
