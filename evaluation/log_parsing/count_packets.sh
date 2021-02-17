b2feaee608cfd8e401304b5aecf093280e5038dc
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-02-17 13:04:59 +0000

    Heavy pcap commands

--- evaluation/log_parsing/count_packets.sh
+++ evaluation/log_parsing/count_packets.sh
@@ -1,3 +1,3 @@
 # count the packet number in a pcap file
-INPUT=${INPUT:-$PASH_TOP/evaluation/scripts/input.pcap}
+INPUT=${INPUT:-$PASH_TOP/evaluation/scripts/input/log_parsing/2019-02-28-20-50-15-192.168.1.193.pcap}
 tcpdump -nn -r ${INPUT} | wc -l
