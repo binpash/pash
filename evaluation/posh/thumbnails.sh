f2489f105b1ac7178d24ea6902c4cc1968ad2397
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-02-17 11:03:44 +0000

    Update on paths and commands

--- evaluation/posh/thumbnails.sh
+++ evaluation/posh/thumbnails.sh
@@ -1,4 +1,4 @@
-INPUT=${INPUT:-$PASH_TOP/evaluation/scripts/input/}
-mkdir -p $PASH_TOP/evaluation/scripts/input/output
-OUTPUT=${OUTPUT:-$PASH_TOP/evaluation/scripts/input/output}
-mogrify -format jpg -path ${OUTPUT} -thumbnail 100x100 ${INPUT}*.jpg
+# generate thumbnails with size 100x100 
+INPUT=${INPUT:-$PASH_TOP/evaluation/scripts/input/posh/jpg/}
+OUTPUT=${OUTPUT:-$PASH_TOP/evaluation/scripts/input/posh/output}
+find ${INPUT} -name "*.jpg" | xargs -n 1 mogrify -format jpg -path ${OUTPUT} -thumbnail 100x100
