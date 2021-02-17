f9bc77861452958e64cad1b1b65e4424a0439c6c
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-02-17 08:03:35 +0000

    Fixed grep count error

--- evaluation/poets/scripts/4letter_words.sh
+++ evaluation/poets/scripts/4letter_words.sh
@@ -1,4 +1,5 @@
 # four-letter words
-INPUT=${INPUT:-$PATH_TOP/evaluation/scripts/input/genesis}
-tr -sc '[A-Z][a-z]' '[\012*]' < ${INPUT} | grep -c '^....$'
-tr -sc '[A-Z][a-z]' '[\012*]' < ${INPUT} | sort -u | grep -c '^....$'
+INPUT=${INPUT:-$PASH_TOP/evaluation/scripts/input/poets/genesis}
+tr -sc '[A-Z][a-z]' '[\012*]' < ${INPUT} | grep -c '^....$' | paste -sd+ | bc
+tr -sc '[A-Z][a-z]' '[\012*]' < ${INPUT} | sort -u | grep -c '^....$' | paste -sd+ | bc
+
