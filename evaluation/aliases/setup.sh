ff58ccebc049a76187d05061c00ef176891a990d
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-02-18 08:58:59 +0000

    Cleanup. Fix Paths. Works on bash

--- evaluation/aliases/setup.sh
+++ evaluation/aliases/setup.sh
@@ -1,8 +1,6 @@
 # remove this line since it hangs timeout 2 -i brightness | cut -f2 -d "" "" > ~/.currentbrightness; cat\
 # execute once
 #sed -e '46106d' $1                                                               
-#rm -f $PASH_TOP/evaluation/scripts/input/aliases/*
-mkdir -p $PASH_TOP/evaluation/scripts/input/aliases/
 # likely-longest-pipelines.txt is locally stored
-# strip the first column
-cut -f1 -d" " --complement  likely-longest-pipelines.txt > $PASH_TOP/evaluation/scripts/input/aliases/generated.file
+# strip the number of pipe column
+cut -f1 -d" " --complement  likely-longest-pipelines.txt > $PASH_TOP/evaluation/scripts/input/generated.file
