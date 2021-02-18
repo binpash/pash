9a16cd305aff8cd751d59833d331cdcb15687822
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-02-18 09:48:44 +0000

    New benchmarks for new datasets

--- evaluation/log_parsing/pcap_scripts/setup.sh
+++ evaluation/log_parsing/pcap_scripts/setup.sh
@@ -6,5 +6,6 @@ then
     # too slow
     wget http://mawi.wide.ad.jp/mawi/samplepoint-F/2010/201011271400.dump.gz
     gunzip 201011271400.dump.gz
+    wget https://mcfp.felk.cvut.cz/publicDatasets/IoT-23-Dataset/IndividualScenarios/CTU-IoT-Malware-Capture-7-1/2018-07-20-17-31-20-192.168.100.108.pcap
 fi
 
