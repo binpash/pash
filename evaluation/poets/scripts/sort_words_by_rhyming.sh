047157d494e66d8aa72c0e6fc37d83658812520d
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-02-18 06:52:18 +0000

    Fixes on input paths. Reset scripts to original behavior

--- evaluation/poets/scripts/sort_words_by_rhyming.sh
+++ evaluation/poets/scripts/sort_words_by_rhyming.sh
@@ -1,3 +1,3 @@
 # Sort words in Genesis by rhyming order.
-INPUT=${INPUT:-$PASH_TOP/evaluation/scripts/input/poets/genesis}
+INPUT=${INPUT:-$PASH_TOP/evaluation/scripts/input/genesis}
 tr -sc '[A-Z][a-z]' '[\012*]' < ${INPUT} | sort | uniq -c | rev | sort | rev 
--- evaluation/poets/setup.sh
+++ evaluation/poets/setup.sh
@@ -1,3 +1,3 @@
-mkdir -p ${PASH_TOP}/evaluation/scripts/input/poets
-wget -O  ${PASH_TOP}/evaluation/scripts/input/poets/genesis http://www.gutenberg.org/cache/epub/8001/pg8001.txt
-wget -O  ${PASH_TOP}/evaluation/scripts/input/poets/exodus http://www.gutenberg.org/cache/epub/8001/pg8001.txt 
+mkdir -p ${PASH_TOP}/evaluation/scripts/input/
+wget -O  ${PASH_TOP}/evaluation/scripts/input/genesis http://www.gutenberg.org/cache/epub/8001/pg8001.txt
+wget -O  ${PASH_TOP}/evaluation/scripts/input/exodus http://www.gutenberg.org/cache/epub/8001/pg8001.txt 
