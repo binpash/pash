2f50535ba5cff1ce34cdda0c76e38cb115b4e083
Thurston Dang (pash docker) <tdang@mit.edu>
2021-02-19 16:07:16 +0000

    JSON to shell in Python

---
+++ compiler/parser/ceda/json_to_shell2.py
@@ -0,0 +1,21 @@
+import sys;
+
+sys.path.append("/pash/compiler");
+from json_ast import parse_json_ast;
+
+
+from ast2shell import *;
+
+
+def main ():
+    if (len (sys.argv) == 1):
+        asts = parse_json_ast ("/dev/stdin");
+    else:
+        asts = parse_json_ast (sys.argv [1]);
+
+    for ast in asts:
+        print (to_string (ast));
+
+
+if __name__ == "__main__":
+    main ();
