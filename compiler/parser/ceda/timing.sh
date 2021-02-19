c707eea6ee33ec7536d4797a54a1b94e7e12597e
Thurston Dang (pash docker) <tdang@mit.edu>
2021-02-19 16:11:01 +0000

    Add timings for Python via JSON

--- compiler/parser/ceda/timing.sh
+++ compiler/parser/ceda/timing.sh
@@ -13,15 +13,19 @@ fi
 echo "Input script: $input_script"
 echo
 
-echo "OCaml:"
+echo "OCaml (dash C AST -> libdash OCaml AST -> JSON -> Pash Python AST -> JSON -> shell:"
 time (../parse_to_json.native "$input_script" > /tmp/json.$$; cat /tmp/json.$$ | ../json_to_shell.native | md5sum)
 echo
 
-echo "C:"
-time (./parse_to_json2 "$input_script" > /tmp/json.$$; cat /tmp/json.$$ | ./json_to_shell2 | md5sum)
+echo "C (dash C AST -> libdash C AST -> JSON -> Pash Python AST -> JSON -> shell):"
+time (./parse_to_json2 "$input_script" > /tmp/json.$$ 2>/dev/null; cat /tmp/json.$$ | ./json_to_shell2 | md5sum)
 echo
 
-echo "Python:"
+echo "Python (dash C AST -> libdash C AST -> JSON -> Pash Python AST -> JSON -> shell):"
+time (python3 ./parse_to_json2.py "$input_script" > /tmp/json.$$ 2>/dev/null; cat /tmp/json.$$ | python3 ./json_to_shell2.py | md5sum)
+echo
+
+echo "Python (dash C AST -> Pash Python AST -> shell):"
 time (python3 ceda_rt.py "$input_script" | md5sum)
 echo
 
