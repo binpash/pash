87ea4b40464476e09808e0cd0a311431d0008404
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-02-21 09:19:00 -0500

    Modify some paths to start from PASH_TOP

--- compiler/parser/ceda/parse_to_json2.py
+++ compiler/parser/ceda/parse_to_json2.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
 
+import os
+import sys
+from parse_to_ast2 import parse_to_ast
 
-import sys;
+if 'PASH_TOP' in os.environ:
+    PASH_TOP = os.environ['PASH_TOP']
+else:
+    GIT_TOP_CMD = [ 'git', 'rev-parse', '--show-toplevel', '--show-superproject-working-tree']
+    PASH_TOP = subprocess.run(GIT_TOP_CMD, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True).stdout.rstrip()
 
-from parse_to_ast2 import parse_to_ast;
-
-sys.path.append("/pash/compiler");
-from json_ast import serialize_asts_to_json;
+sys.path.append(os.path.join(PASH_TOP, "compiler"))
+from json_ast import serialize_asts_to_json
 
 
 def main ():
     if (len (sys.argv) == 1):
-        new_asts = parse_to_ast ("-", True);
+        new_asts = parse_to_ast ("-", True)
     else:
-        new_asts = parse_to_ast (sys.argv [1], True);
+        new_asts = parse_to_ast (sys.argv [1], True)
 
-    json = serialize_asts_to_json (new_asts);
+    json = serialize_asts_to_json (new_asts)
 
-    print (json);
+    print (json)
 
 
 if __name__ == "__main__":
-    main ();
+    main ()
