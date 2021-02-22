703afdbb23fb5bb0dfb5891ea9d88c02d516e37e
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-02-22 16:22:11 -0500

    Fix ssh background poller to incrementally update the output log to show progress

--- evaluation/multi-instance-experiment/execute-and-collect.sh
+++ evaluation/multi-instance-experiment/execute-and-collect.sh
@@ -14,12 +14,15 @@ EVAL_DIR="$PASH_TOP/evaluation/multi-instance-experiment/"
 ## TODO: This only works if the user's name is ubuntu (and also is tied to the value in experiment-script.sh)
 ##       Is there any better way to refactor this so that this is not an issue?
 COMPLETION_FILE="/home/ubuntu/pash/done.txt"
+LOG_FILE="/home/ubuntu/pash/output.log"
 
 ## Execute script
 "${PASH_TOP}/evaluation/multi-instance-experiment/ssh-script.sh" "${KEY_PATH}" "${HOSTNAME}" < "${EVAL_DIR}/experiment-script.sh" 
 
+
+
 ## Wait for it to complete
-"${PASH_TOP}/scripts/ssh-bg-poller.sh" "${COMPLETION_FILE}" "${KEY_PATH}" "${HOSTNAME}"
+"${PASH_TOP}/scripts/ssh-bg-poller.sh" "${COMPLETION_FILE}" "${LOG_FILE}" "${KEY_PATH}" "${HOSTNAME}"
 
 ## Collect results
 "${EVAL_DIR}/collect-results-script.sh" "${LOCAL_DIR}" "${KEY_PATH}" "${HOSTNAME}"
--- scripts/ssh-bg-poller.sh
+++ scripts/ssh-bg-poller.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 DONE_FILE=${1?Done file argument not given}
-KEY_PATH=${2?Key path not given}
-HOSTNAME=${3?Hostname not given}
+LOG_FILE=${2?Done file argument not given}
+KEY_PATH=${3?Key path not given}
+HOSTNAME=${4?Hostname not given}
 
 check_if_file_exists()
 {
@@ -11,13 +12,19 @@ if [[ -f $DONE_FILE ]]; then
     cat $DONE_FILE
     exit 0
 else
+    cat $LOG_FILE
     exit 1
 fi
 EOF
 }
 
-until check_if_file_exists
+LOCAL_LOG="$(mktemp -u)"
+PREVIOUS_LOCAL_LOG="$(mktemp -u)"
+touch $PREVIOUS_LOCAL_LOG
+
+until check_if_file_exists > $LOCAL_LOG
 do
-#   echo "Not completed"
+  comm -23 --nocheck-order $LOCAL_LOG $PREVIOUS_LOCAL_LOG
+  cat $LOCAL_LOG > $PREVIOUS_LOCAL_LOG
   sleep 5
 done
