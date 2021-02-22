c52a32af70da509df2b573e02c94a623e6b93eb7
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-02-21 22:39:11 -0500

    Add two profiles for instances

---
+++ evaluation/multi-instance-experiment/instance-profiles/fast-disk.sh
@@ -0,0 +1,11 @@
+export PASH_AWS_EC2_AMI='ami-0dd9f0e7df0f0a138';
+export PASH_AWS_EC2_INSTANCE_TYPE='c5.2xlarge';
+export PASH_AWS_EC2_VPC_ID='vpc-54e6373f';
+export PASH_AWS_EC2_KEY_NAME='aws_pash';
+export PASH_AWS_EC2_SUBNET='subnet-5ec75912';
+export PASH_AWS_EC2_SECURITY_GROUP='sg-002037ce95957a98b';
+export PASH_AWS_EC2_DISK_SIZE_GB='10';
+export PASH_AWS_EC2_DISK_TYPE='gp3';
+## This is ignored by the current make-ec2.sh script
+export PASH_AWS_EC2_DISK_IOPS='15000';
+## What about throughput?
