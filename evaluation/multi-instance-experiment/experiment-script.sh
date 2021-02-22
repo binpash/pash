b383cceea2fa76c53cad7c25f17f3c2df31ecffa
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-02-22 16:22:51 -0500

    Close stdin, stdout, stderr so that bg worker can exit

--- evaluation/multi-instance-experiment/experiment-script.sh
+++ evaluation/multi-instance-experiment/experiment-script.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 cd pash/evaluation/eurosys
-nohup ../../scripts/bg-worker.sh ../../done.txt ../../output.log ./execute_eurosys_one_liners.sh -s &
+## WARNING: It is necessary to close file descriptors so that ssh can exit
+nohup ../../scripts/bg-worker.sh ../../done.txt ../../output.log ./execute_eurosys_one_liners.sh -s </dev/null >/dev/null 2>/dev/null &
