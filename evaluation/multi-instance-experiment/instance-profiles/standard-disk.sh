d1d3944b3ed9551bd24d61c54830b9cc2669ca9b
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-02-22 13:36:51 -0500

    Modify instance profiles to have 20GB of storage since they were running out

--- evaluation/multi-instance-experiment/instance-profiles/standard-disk.sh
+++ evaluation/multi-instance-experiment/instance-profiles/standard-disk.sh
@@ -4,7 +4,7 @@ export PASH_AWS_EC2_VPC_ID='vpc-54e6373f';
 export PASH_AWS_EC2_KEY_NAME='aws_pash';
 export PASH_AWS_EC2_SUBNET='subnet-5ec75912';
 export PASH_AWS_EC2_SECURITY_GROUP='sg-002037ce95957a98b';
-export PASH_AWS_EC2_DISK_SIZE_GB='10';
+export PASH_AWS_EC2_DISK_SIZE_GB='20';
 export PASH_AWS_EC2_DISK_TYPE='gp2';
 ## This is actually ignored (but needs to be here for the CLI to be invoked correctly)
 export PASH_AWS_EC2_DISK_IOPS='100';
