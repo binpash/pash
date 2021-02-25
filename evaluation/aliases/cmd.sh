1a5d8b251150218cd57a5d6daee9cc83dfd03026
DIMITRIOS KARNIKIS <dkarnikis@gmail.com>
2021-02-25 12:12:00 +0000

    Mp3 datasets and cmds. WIP parse gutenberg

--- evaluation/aliases/cmd.sh
+++ evaluation/aliases/cmd.sh
@@ -1,18 +1,23 @@
-# dataset http://www.repository.voxforge1.org/downloads/SpeechCorpus/Trunk/Audio/Original/48kHz_16bit/tomhannen-20080409.tgz
 convert_to_mp3()
 (
-    cd $1
-    for FILE in *.wav ;
-    do
-        echo $FILE
-        ffmpeg -i $FILE -f mp3 -ab 192000 ${FILE}.mp3 || break;
+   cd $1
+   find . -name "*.wav" | xargs -I {} -P 16 ffmpeg -i {} -ab 192000 ../mp3/{}.mp3
+   cd ../mp3
+   # sort based on name
+    for file in *.mp3; do
+      dir=${file:0:2}
+      mkdir -p "$dir"  &&      mv -iv "$file" "./$dir"
     done
+
+
+    cd - 
 )
 #convert_to_mp3 $1
 # generate a playlist
 gen_playlist()
 (
-find . -type f -name *.mp3 -o -name *.wav | sort > playlist.pls
+  cd $1
+  find . -type f -name *.mp3 -o -name *.wav | sort > playlist.pls
 )
 
 grep_words_ing()
@@ -26,3 +31,6 @@ delete_movies()
     # retrieve files with an extension, move them to a seperate folder
     mkdir -p ~/movies_to_delete && find  -type f -regex ".*\.\(mkv\|avi\|mov\|mpg\|wav\)"  -print0 | xargs -0 -I{} dirname {}  |xargs -I{} mv {} ~/movies_to_delete
 )
+
+convert_to_mp3 "$PASH_TOP/evaluation/scripts/input/aliases/wav"
+gen_playlist "$PASH_TOP/evaluation/scripts/input/aliases/"
--- evaluation/aliases/setup.sh
+++ evaluation/aliases/setup.sh
@@ -4,3 +4,20 @@
 # likely-longest-pipelines.txt is locally stored
 # strip the number of pipe column
 cut -f1 -d" " --complement  likely-longest-pipelines.txt > $PASH_TOP/evaluation/scripts/input/generated.file
+
+## mp3 dataset ##
+apt-get install ffmpeg
+PW=$PASH_TOP/evaluation/scripts/input/aliases
+mkdir -p $PW
+cd $PW
+wget http://www.repository.voxforge1.org/downloads/SpeechCorpus/Trunk/Audio/Original/48kHz_16bit/tomhannen-20080409.tgz
+tar xf tomhannen-20080409.tgz
+rm -rf wav mp3
+mkdir wav mp3
+cd tomhannen-20080409/wav
+# total 5.7 size of audio files
+for i in *.wav; do
+  FILE=$(basename "$i")
+  for x in {1..200}; do cp $i "../../wav/$i$x.wav"; done
+done
+rm -rf ../../tomhannen-20080409
