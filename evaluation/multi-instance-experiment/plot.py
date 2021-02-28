f63d476ec80cd873820109fd7c9152b877488807
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-02-28 18:33:39 -0500

    Plot bash and pash in the same plots

--- evaluation/multi-instance-experiment/plot.py
+++ evaluation/multi-instance-experiment/plot.py
@@ -28,25 +28,37 @@ experiments = ["minimal_grep",
 scaleup_numbers = [2, 16]
 
 ## TODO: Abstract this in an argument
-standard_disk_results_dir = os.path.join("results", "i-0347068fae17c256e", "eurosys_small")
-fast_disk_results_dir = os.path.join("results", "i-07814ed42a1ddd013", "eurosys_small")
+standard_disk_results_dir = os.path.join("results", "standard_disk_instance", "eurosys_small")
+fast_disk_results_dir = os.path.join("results", "fast_disk_instance", "eurosys_small")
 
 
 def plot(all_results, experiments, scaleup_numbers, pash_config='split'):
     fig = plt.figure()
     columns = 5
     rows = (len(experiments) // columns) + 1
-    gs = fig.add_gridspec(rows, columns, hspace=0.05)
+    # hspace=0.05
+    hspace=0.15
+    gs = fig.add_gridspec(rows, columns, hspace=hspace)
 
 
     for i, experiment in enumerate(experiments):
         ax = fig.add_subplot(gs[i])
         for config_name, config_results in all_results.items():
-            ys = config_results[experiment][pash_config]
-            ax.plot(scaleup_numbers, ys, label=config_name)
-            ax.text(.5,.91, gather_results.pretty_names[experiment],
+            speedups = config_results[experiment][pash_config]
+            seq_time_ms = config_results[experiment]['sequential']
+            seq_time = seq_time_ms / 1000.0
+            ys = [seq_time / speedup for speedup in speedups]
+            ax.plot(scaleup_numbers, ys, label=config_name+"-pash")
+            seq_ys = [seq_time for _ in scaleup_numbers]
+            ax.plot(scaleup_numbers, seq_ys, label=config_name + "-bash")
+        
+        text = "{}".format(gather_results.pretty_names[experiment])
+        # text_relative_y = .91
+        text_relative_y = 1.05
+        ax.text(.5, text_relative_y, text,
                 horizontalalignment='center',
                 transform=ax.transAxes)
+        ax.set_xticks(scaleup_numbers)
 
     plt.legend(loc='lower right', fontsize=16)
     # plt.title(pretty_names[experiment])
@@ -60,11 +72,13 @@ standard_disk_results = {}
 fast_disk_results = {}
 
 for experiment in experiments:
-    standard_disk_speedup_results, _, _sequential_time = gather_results.collect_scaleup_line_speedups(experiment, scaleup_numbers, standard_disk_results_dir)
+    standard_disk_speedup_results, _, standard_sequential_time = gather_results.collect_scaleup_line_speedups(experiment, scaleup_numbers, standard_disk_results_dir)
     standard_disk_results[experiment] = standard_disk_speedup_results
+    standard_disk_results[experiment]['sequential'] = standard_sequential_time
 
-    fast_disk_speedup_results, _, _sequential_time = gather_results.collect_scaleup_line_speedups(experiment, scaleup_numbers, fast_disk_results_dir)
+    fast_disk_speedup_results, _, fast_sequential_time = gather_results.collect_scaleup_line_speedups(experiment, scaleup_numbers, fast_disk_results_dir)
     fast_disk_results[experiment] = fast_disk_speedup_results
+    fast_disk_results[experiment]['sequential'] = fast_sequential_time
 
 all_results = {
     "standard-disk": standard_disk_results,
