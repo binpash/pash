799f72a47b18d3c28024e6cb0e1d165aaaa289a9
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-03-01 13:43:28 +0100

    Convert scripts from aliases

--- evaluation/aliases/cmd.sh
+++ evaluation/aliases/cmd.sh
@@ -20,69 +20,70 @@ gen_playlist()
   find . -type f -name *.mp3 -o -name *.wav | sort > playlist.pls
 )
 
-grep_words()
-(
-    # the first n books
-    dict_books=1000
-    # our dictionary file
-    my_dict=$1/my_dict
-    find $1 -name "*.txt" | head -n $dict_books > $1/${dict_books}.entries
-    ################################################
-    # Generate dictionary based on all the files 
-    ################################################
-    cat $1/${dict_books}.entries | xargs -n 1 cat | tr -sc 'A-Za-z' '\n' | tr 'A-Z' \
-    'a-z' | sort | uniq > $my_dict
-    echo "Created dictionary from allthe books"
-    #################################################
-    # grep words ending in ing
-    #################################################
-    cat $my_dict | grep 'ing$' | sort | uniq -c | sort -n -r  > $1/res
-    echo "Found all the -ings"
-    #################################################
-    # find all duplicate words across multiple files
-    #################################################
-    file_expr="$1/*.txt"; echo $file_expr > $1/f; sort $1/f | sed 's/^\s*//; s/\s*$//; /^\s*$/d' | \
-  uniq -d | while read dup_line; do grep -Hn "^\s*$dup_line\s*$" $file_expr; \
-done| sort -t: -k3 -k1,2 | awk -F: '{ file=$1; line=$2; $1=$2="";gsub(/(^[\t]+)| \
-([ \t]+$)/,"",$0); if (prev != "" && prev != $0) printf ("\n"); printf("\033[0;33m%s  \
-    (line %s)\033[0m: %s\n", file, line, $0); prev=$0; }' > $1/out
-    echo "Found all the duplicates"
-    #################################################
-    # Get words that don't exist in the dictionary
-    ################################################
-    # get all the words from the files that don't exist in dict 
-    #################################################
-    comm -1 $my_dict /usr/share/dict/words > $1/words
-    echo "Found words uniq to our dict"
-    ##################################################
-    # NLP ?
-    ##################################################
-    adj=$1/conv.data.adj
-    adv=$1/conv.data.adv
-    noun=$1/conv.data.noun
-    verb=$1/conv.data.verb
-    egrep -o "^[0-9]{8}\s[0-9]{2}\s[a-z]\s[0-9]{2}\s[a-zA-Z]*\s" $1/dict/data.adj | cut \
-    -d ' ' -f 5 > $adj
-    egrep -o "^[0-9]{8}\s[0-9]{2}\s[a-z]\s[0-9]{2}\s[a-zA-Z]*\s" $1/dict/data.adv | cut \
-    -d ' ' -f 5 > $adv
-    egrep -o "^[0-9]{8}\s[0-9]{2}\s[a-z]\s[0-9]{2}\s[a-zA-Z]*\s" $1/dict/data.noun | cut \
-    -d ' ' -f 5 > $noun
-    egrep -o "^[0-9]{8}\s[0-9]{2}\s[a-z]\s[0-9]{2}\s[a-zA-Z]*\s" $1/dict/data.verb | cut \
-    -d ' ' -f 5 > $verb
-    comm -12 $(cat $adj  | sort > $1/.tmp; echo $1/.tmp) $my_dict >$1/r1
-    comm -12 $(cat $adv  | sort > $1/.tmp; echo $1/.tmp) $my_dict >$1/r2
-    comm -12 $(cat $noun | sort > $1/.tmp; echo $1/.tmp) $my_dict >$1/r3
-    comm -12 $(cat $verb | sort > $1/.tmp; echo $1/.tmp) $my_dict >$1/r4
-    # Get verbs from the global dict
-    # get the verbs from each file that exists in global dict
-    # count the number of uniq verbs for each file
-    cat $1/${dict_books}.entries | xargs -I {} sh -c 'comm -12 $(cat "$1" | \
-      tr -sc "A-Za-z" "\n" | tr "A-Z" "a-z" | sort | uniq > "$1".lol; \
-      echo "$1".lol) $PASH_TOP/evaluation/scripts/input/aliases/r4 | wc -l > "$1".wc' sh {} 
-    echo "Generated word count files"
-    
-
-)
+# we can maybe move this to another script ?
+#grep_words()
+#(
+#    # the first n books
+#    dict_books=1000
+#    # our dictionary file
+#    my_dict=$1/my_dict
+#    find $1 -name "*.txt" | head -n $dict_books > $1/${dict_books}.entries
+#    ################################################
+#    # Generate dictionary based on all the files 
+#    ################################################
+#    cat $1/${dict_books}.entries | xargs -n 1 cat | tr -sc 'A-Za-z' '\n' | tr 'A-Z' \
+#    'a-z' | sort | uniq > $my_dict
+#    echo "Created dictionary from allthe books"
+#    #################################################
+#    # grep words ending in ing
+#    #################################################
+#    cat $my_dict | grep 'ing$' | sort | uniq -c | sort -n -r  > $1/res
+#    echo "Found all the -ings"
+#    #################################################
+#    # find all duplicate words across multiple files
+#    #################################################
+#    file_expr="$1/*.txt"; echo $file_expr > $1/f; sort $1/f | sed 's/^\s*//; s/\s*$//; /^\s*$/d' | \
+#  uniq -d | while read dup_line; do grep -Hn "^\s*$dup_line\s*$" $file_expr; \
+#done| sort -t: -k3 -k1,2 | awk -F: '{ file=$1; line=$2; $1=$2="";gsub(/(^[\t]+)| \
+#([ \t]+$)/,"",$0); if (prev != "" && prev != $0) printf ("\n"); printf("\033[0;33m%s  \
+#    (line %s)\033[0m: %s\n", file, line, $0); prev=$0; }' > $1/out
+#    echo "Found all the duplicates"
+#    #################################################
+#    # Get words that don't exist in the dictionary
+#    ################################################
+#    # get all the words from the files that don't exist in dict 
+#    #################################################
+#    comm -1 $my_dict /usr/share/dict/words > $1/words
+#    echo "Found words uniq to our dict"
+#    ##################################################
+#    # NLP ?
+#    ##################################################
+#    adj=$1/conv.data.adj
+#    adv=$1/conv.data.adv
+#    noun=$1/conv.data.noun
+#    verb=$1/conv.data.verb
+#    egrep -o "^[0-9]{8}\s[0-9]{2}\s[a-z]\s[0-9]{2}\s[a-zA-Z]*\s" $1/dict/data.adj | cut \
+#    -d ' ' -f 5 > $adj
+#    egrep -o "^[0-9]{8}\s[0-9]{2}\s[a-z]\s[0-9]{2}\s[a-zA-Z]*\s" $1/dict/data.adv | cut \
+#    -d ' ' -f 5 > $adv
+#    egrep -o "^[0-9]{8}\s[0-9]{2}\s[a-z]\s[0-9]{2}\s[a-zA-Z]*\s" $1/dict/data.noun | cut \
+#    -d ' ' -f 5 > $noun
+#    egrep -o "^[0-9]{8}\s[0-9]{2}\s[a-z]\s[0-9]{2}\s[a-zA-Z]*\s" $1/dict/data.verb | cut \
+#    -d ' ' -f 5 > $verb
+#    comm -12 $(cat $adj  | sort > $1/.tmp; echo $1/.tmp) $my_dict >$1/r1
+#    comm -12 $(cat $adv  | sort > $1/.tmp; echo $1/.tmp) $my_dict >$1/r2
+#    comm -12 $(cat $noun | sort > $1/.tmp; echo $1/.tmp) $my_dict >$1/r3
+#    comm -12 $(cat $verb | sort > $1/.tmp; echo $1/.tmp) $my_dict >$1/r4
+#    # Get verbs from the global dict
+#    # get the verbs from each file that exists in global dict
+#    # count the number of uniq verbs for each file
+#    cat $1/${dict_books}.entries | xargs -I {} sh -c 'comm -12 $(cat "$1" | \
+#      tr -sc "A-Za-z" "\n" | tr "A-Z" "a-z" | sort | uniq > "$1".lol; \
+#      echo "$1".lol) $PASH_TOP/evaluation/scripts/input/aliases/r4 | wc -l > "$1".wc' sh {} 
+#    echo "Generated word count files"
+#    
+#
+#)
 
 delete_movies()
 (   
@@ -90,6 +91,32 @@ delete_movies()
     mkdir -p ~/movies_to_delete && find  -type f -regex ".*\.\(mkv\|avi\|mov\|mpg\|wav\)"  -print0 | xargs -0 -I{} dirname {}  |xargs -I{} mv {} ~/movies_to_delete
 )
 
+# convert to jpg to psd
+# can we optimize this ?
+jpg_to_psd()
+(
+    cd $1
+    mkdir converted;
+    find . -name "*.jpg" | xargs -P 16 -I {} sh -c "convert {} converted/$(echo {} | sed -e 's/\.jpg/\.psd/g') &&  echo Converted {} to $(echo {} | sed -e 's/\.jpg/\.psd/g') ...;" sh {}
+)
+
+# Resize image 
+resize_image()
+(
+    find $1 -name *.jpg -o -name *.png -o -name *.jpeg -o -name *.JPG -o -name *.PNG \
+        -o -name *.JPEG | xargs -P 16 -I {} convert -resize 70% {} {}.70
+)
+
+# convert all rtf to txt
+rtf_to_txt()
+(
+    cd $1
+    find . -name "*.rtf" | xargs -I {} unrtf {} --text > /dev/null
+)
+
+
 #convert_to_mp3 "$PASH_TOP/evaluation/scripts/input/aliases/wav"
 #gen_playlist "$PASH_TOP/evaluation/scripts/input/aliases/"
-grep_words "$PASH_TOP/evaluation/scripts/input/aliases"
+resize_image "$PASH_TOP/evaluation/scripts/input/aliases/jpg"
+jpg_to_psd "$PASH_TOP/evaluation/scripts/input/aliases/jpg"
+#rtf_to_txt  "$PASH_TOP/evaluation/scripts/input/aliases/rtf"
--- evaluation/aliases/setup.sh
+++ evaluation/aliases/setup.sh
@@ -3,17 +3,17 @@
 #sed -e '46106d' $1                                                               
 # likely-longest-pipelines.txt is locally stored
 # strip the number of pipe column
-cut -f1 -d" " --complement  likely-longest-pipelines.txt > $PASH_TOP/evaluation/scripts/input/generated.file
+cut -f1 -d " " --complement  likely-longest-pipelines.txt > $PASH_TOP/evaluation/scripts/input/generated.file
 
 ## mp3 dataset ##
-apt-get install ffmpeg
+apt-get install ffmpeg unrtf imagemagick
 PW=$PASH_TOP/evaluation/scripts/input/aliases
 mkdir -p $PW
 cd $PW
 wget http://www.repository.voxforge1.org/downloads/SpeechCorpus/Trunk/Audio/Original/48kHz_16bit/tomhannen-20080409.tgz
 tar xf tomhannen-20080409.tgz
 rm -rf wav mp3
-mkdir wav mp3
+mkdir wav mp3 rtf
 cd tomhannen-20080409/wav
 # total 5.7 size of audio files
 for i in *.wav; do
@@ -28,4 +28,24 @@ rm *.tar.gz
 wget -O nlp.tar.gz https://wordnetcode.princeton.edu/3.0/WNdb-3.0.tar.gz
 tar xf nlp.tar.gz
 
+wget https://jeroen.github.io/files/sample.rtf
+for i in {0..10000}
+do
+    cp sample.rtf rtf/sample$i.rtf
+done
 
+if [ ! -f jpg1.tar.gz ]; then                                                 
+    echo "Fetching Dataset"                                                   
+    wget -O jpg1.tar.gz ftp://ftp.inrialpes.fr/pub/lear/douze/data/jpg1.tar.gz
+fi                                                                            
+tar xf jpg1.tar.gz                                                            
+mkdir -p tmp                                                                  
+cd jpg/                                                                       
+for filename in *.jpg; do                                                     
+    echo $filename                                                            
+    cp $filename ../tmp/${filename}_copy.jpg                                  
+done                                                                          
+
+mv ../tmp/* .
+rm -rf ../tmp
+cd ..
