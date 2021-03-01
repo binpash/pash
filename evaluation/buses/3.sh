1184f5afc41f5b932bc2a224f40e68718654ddd4
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-03-01 14:35:55 -0500

    Modify bus scripts to avoid awk

--- evaluation/buses/3.sh
+++ evaluation/buses/3.sh
@@ -14,14 +14,12 @@
 # Replace the line below with the two lines above to stream the latest file
 cat in.csv |                    # assumes saved input
   sed 's/T\(..\):..:../,\1/' |  # keep times only
-  awk -F, '{print $1,$2,$4}' |  # keep only day and bus no
+  cut -d ',' -f 1,2,4 |         # keep only time date and bus id
+  sort -u |                     # removing duplicate entries
+  cut -d ',' -f 3 |             # keep only bus ID
   sort |                        # preparing for uniq
-  uniq -c |                     # remove duplicate records due to time
-  awk '{print $4}' |            # keep all buses
-  sort |                        # preparing for uniq
-  uniq -c |                     # count unique dates
-  awk '{print $2,$1}' |         # print first date, then count
-  sort -k2n |                   # sort in reverse numerical order
-  tr ' ' '\t' > out             # replace space w/ tab as per original script
+  uniq -c |                     # count hours per bus
+  sort -k1n |                   # sort in reverse numerical order
+  awk -v OFS="\t" "{print \$2,\$1}"     # print first date, then count
 
 # diff out{1,}
