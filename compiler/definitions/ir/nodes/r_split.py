202e0fff1faa4deb35c965639e83e92e69c22d99
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-03-08 19:18:58 -0500

    Add a method to r_split to add an option

--- compiler/definitions/ir/nodes/r_split.py
+++ compiler/definitions/ir/nodes/r_split.py
@@ -1,12 +1,10 @@
-from definitions.ir.file_id import *
-
-from definitions.ir.dfg_node import *
-from ir_utils import string_to_argument, make_command
+import os
 
 import config
 
-import os
-
+from definitions.ir.dfg_node import *
+from definitions.ir.file_id import *
+from ir_utils import string_to_argument, make_command
 
 class RSplit(DFGNode):
     def __init__(self, inputs, outputs, com_name, com_category, com_options = [], 
@@ -15,6 +13,16 @@ class RSplit(DFGNode):
                          com_options=com_options, 
                          com_redirs=com_redirs, 
                          com_assignments=com_assignments)
+    
+    ## TODO: Generalize this code (for this and SortGReduce) to be able to add an option to any command.
+    def add_r_flag(self):
+        assert(len(self.com_options) <= 1)
+            
+        ## Add -r in r_split
+        new_opt = (0, Arg(string_to_argument("-r")))
+        shifted_options = [(i+1, opt) for i, opt in self.com_options]
+        self.com_options = [new_opt] + shifted_options
+
 
 ## TODO: Make a proper splitter subclass of Node
 def make_r_split(input_id, out_ids, r_split_batch_size):
