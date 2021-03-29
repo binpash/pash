65985a4b8f752bcfe76b6588fdbf825c3485c5fe
Tammam Mustafa <tammammusatafa@gmail.com>
2021-03-29 14:53:21 +0000

    plotting two df2 against each other for dgsh-tee results

--- evaluation/dgsh-tee/plot.py
+++ evaluation/dgsh-tee/plot.py
@@ -56,13 +56,70 @@ def plot_barchart(df, y_axis="exec_time", test_names=None, save_file="output.png
     
     fig.savefig(save_file)
 
+def plot_barchart_against(df1, df2, df1_name, df2_name, y_axis="exec_time", test_names=None, save_file="output.png"):
+    print(test_names)
+    if test_names is not None:
+        df1 = df1[df1["test_name"].isin(test_names)]
+        labels = sorted(test_names)
+    else:
+        labels = sorted(df1.test_name.unique())
+
+    x = np.arange(len(labels))  # the label locations
+    width = 0.2  # the width of the bars
+   
+    r_split_dgsh_tee1 = df1[(df1.split_type == "r-split") & (df1.no_eager == False) & (df1.dgsh_tee == True)]
+
+    auto_split_dgsh_tee1 = df1[(df1.split_type == "auto-split") & (df1.no_eager == False) & (df1.dgsh_tee == True)]
+
+    r_split_dgsh_tee2 = df2[(df2.split_type == "r-split") & (df2.no_eager == False) & (df2.dgsh_tee == True)]
+
+    auto_split_dgsh_tee2 = df2[(df2.split_type == "auto-split") & (df2.no_eager == False) & (df2.dgsh_tee == True)]
+    
+    rects = [r_split_dgsh_tee1, auto_split_dgsh_tee1, r_split_dgsh_tee2, auto_split_dgsh_tee2]
+    bar_height = [[] for i in range(len(rects))]
+    error_height = [[] for i in range(len(rects))]
+
+    for label in labels:
+        for i, rect in enumerate(rects):
+            try:
+                bar_height[i].append(rect.loc[rect["test_name"] == label][y_axis].mean())
+                error_height[i].append(rect.loc[rect["test_name"] == label][y_axis].std())
+            except:
+                print(label)
+        
+
+    fig, ax = plt.subplots(figsize=(20,10))
+    rects1 = ax.bar(x - 3/2*width, bar_height[0], width, yerr=error_height[0], label='r-split-dgsh-tee-'+df1_name)
+    rects2 = ax.bar(x - 1/2*width, bar_height[1], width, yerr=error_height[1], label='auto-split-dgsh-tee-'+df1_name)
+    rects3 = ax.bar(x + 1/2*width, bar_height[2], width, yerr=error_height[2], label='r-split-dgsh-tee-'+df2_name)
+    rects4 = ax.bar(x + 3/2*width, bar_height[3], width, yerr=error_height[3], label='auto-split-dgsh-tee-'+df2_name)
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('ms')
+    # ax.set_title('')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    plt.xticks(rotation=90)
+    ax.legend()
+    
+    fig.savefig(save_file)
+
 if __name__ == '__main__':
     #sample execution
+    # log_parser = LogParser()
+    # dirs_to_plot = argv[1:] if len(argv) > 1 else DEFAULT_LOG_FOLDER
+    # for d in dirs_to_plot:
+    #     log_parser.parse_folder(d)
+    # # parse more directories if needed
+    # df = log_parser.parse_folder("tmp_width4_1G")
     log_parser = LogParser()
-    dirs_to_plot = argv[1:] if len(argv) > 1 else DEFAULT_LOG_FOLDER
-    for d in dirs_to_plot:
-        log_parser.parse_folder(d)
-    # parse more directories if needed
-    # df = log_parser.parse_folder("tmp_width4_10M")
-    labels = ['minimal_sort', 'topn', 'wf', 'diff', 'set-diff', 'double_sort', 'sort']
-    plot_barchart(log_parser.get_df(), test_names=labels, save_file=("_".join([os.path.dirname(d) for d in dirs_to_plot])) + ".png")
+    df1 = log_parser.parse_folder("tmp_width4_1G_myDgsh_extra_eager")
+    df2 = log_parser.parse_folder("tmp_width4_1G_20M")
+    df1 = df1[(df1.split_type == "r-split") & (df1.no_eager == False) & (df1.dgsh_tee == True)]
+    df2 = df2[(df2.split_type == "r-split") & (df2.no_eager == False) & (df2.dgsh_tee == True)]
+    # labels = ['minimal_sort', 'topn', 'wf', 'diff', 'set-diff', 'double_sort', 'sort']
+    # df2 = df2.append(df1, ignore_index=True)
+    # plot_barchart(df2, test_names=labels, save_file="1G_width_20Mbuffer_spill" + ".png")
+    # print(df1[["test_name", "no_eager", "split_type", "exec_time", "cpu%", "width", "dgsh_tee"]].to_string(index = False))
+    # print(df2[["test_name", "no_eager", "split_type", "exec_time", "cpu%", "width", "dgsh_tee"]].to_string(index = False))
+    plot_barchart_against(df1,df2, "extra-eager", "original", save_file="extra_eager_vs_normal" + ".png")
