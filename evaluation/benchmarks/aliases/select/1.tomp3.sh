a0cc8a1e16f270a92c082f92cdd299aa2daf2e77
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-04-02 10:46:07 +0200

    Fix token parsing using xargs

--- evaluation/benchmarks/aliases/select/1.tomp3.sh
+++ evaluation/benchmarks/aliases/select/1.tomp3.sh
@@ -6,10 +6,7 @@ set -e
 IN=$PASH_TOP/evaluation/benchmarks/aliases/meta/wav
 OUT=$PASH_TOP/evaluation/benchmarks/aliases/meta/out
 
-#find $IN -name '*.wav' | 
-#    xargs -n1 basename |
-#    sed "s;\(.*\);-i $IN/\1 -ab 192000 $OUT/\1.mp3;" |
-#    xargs -0 -n1 echo ffmpeg
-
-find . -iname "*.wav" -printf "%p -ab 192000 ${OUT}/%f.mp3\n" | 
-    xargs -r -n4 ffmpeg -i
+find $IN -name '*.wav' | 
+    xargs -n1 basename |
+    sed "s;\(.*\);-i $IN/\1 -ab 192000 $OUT/\1.mp3;" |
+    xargs -L1  ffmpeg
--- evaluation/benchmarks/aliases/select/3.resiz.sh
+++ evaluation/benchmarks/aliases/select/3.resiz.sh
@@ -5,11 +5,7 @@ set -e
 
 IN=$PASH_TOP/evaluation/benchmarks/aliases/meta/jpg
 OUT=$PASH_TOP/evaluation/benchmarks/aliases/meta/out
-#find $IN -name "*.jpg" | 
-#  xargs -n1 basename |
-#  sed "s;\(.*\);$IN/\1 $OUT/\1.70;" |
-#  tr '\n' '\0' |
-#  xargs -0 -n1 convert -resize 70% 
-# can be optimized 
-find . -iname "*.jpg" -printf "%p  ${OUT}/%f.70\n" |
-    xargs -r -n2 convert -resize 70%
+find $IN -name "*.jpg" | 
+  xargs -n1 basename |
+  sed "s;\(.*\);-resize 70% $IN/\1 $OUT/\1.70;" |
+  xargs -L1  convert
