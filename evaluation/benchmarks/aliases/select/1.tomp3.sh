219b99a2559ee5fb964d4cc4027ae9eef5af6e3f
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-04-02 12:13:09 +0200

    Cleanup on aliases for run.seq.sh

--- evaluation/benchmarks/aliases/select/1.tomp3.sh
+++ evaluation/benchmarks/aliases/select/1.tomp3.sh
@@ -9,4 +9,4 @@ OUT=$PASH_TOP/evaluation/benchmarks/aliases/meta/out
 find $IN -name '*.wav' | 
     xargs -n1 basename |
     sed "s;\(.*\);-i $IN/\1 -ab 192000 $OUT/\1.mp3;" |
-    xargs -L1  ffmpeg
+    xargs -L1  ffmpeg -y -loglevel quiet -hide_banner
