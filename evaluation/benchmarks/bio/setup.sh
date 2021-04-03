146ed0d372b8fd41dc47be798520444aefbfb72c
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-04-03 17:48:11 +0200

    Bio setup on correct samtool version

--- evaluation/benchmarks/bio/setup.sh
+++ evaluation/benchmarks/bio/setup.sh
@@ -1,3 +1,6 @@
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 if [[ $1 == "-c" ]]; then
     rm -rf input
     rm -rf output
@@ -6,7 +9,19 @@ fi
 PW=${PASH_TOP}/evaluation/benchmarks/bio/input
 mkdir -p $PW
 mkdir -p ${PASH_TOP}/evaluation/benchmarks/bio/output
-apt-get install samtools
+# install dependencies
+pkgs='samtools'
+if ! dpkg -s $pkgs >/dev/null 2>&1; then
+    sudo apt-get install $pkgs
+    var=$(dpkg -s samtools | grep Version)
+    if [[ ! $var == "Version: 1.7-1" ]]; then                                
+        printf "${RED}Invalid Samtools Version\n"                            
+        printf "Samtools Version: 1.7 (using htslib 1.7-2) IS required{NC}\n"
+    fi                                                                       
+fi
+
+
+
 cat ./input.txt |while read s_line;
 	do
     
