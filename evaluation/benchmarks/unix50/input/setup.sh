b7cfcd5f621e95072e504f45babae0a7cab93197
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-04-04 08:31:30 -0400

    Setup unix50 inputs with cleaning and not redownloading if they are there

--- evaluation/benchmarks/unix50/input/setup.sh
+++ evaluation/benchmarks/unix50/input/setup.sh
@@ -2,12 +2,30 @@
 
 set -e
 
-## FIXME: Make sure that we don't redownload if the input is already downloaded.
+PASH_TOP=${PASH_TOP:-$(git rev-parse --show-toplevel)}
 
 ## FIXME: These inputs are already 1G when downloaded
 ## FIXME: Also, wget is not silent like curl in the other setup scripts.
-echo 1 10 11 12 2 3 4 5 6 7 8 9.1 9.2 9.3 9.4 9.5 9.6 9.7 9.8 9.9 | xargs -n 1 |
-  sed 's/$/.txt/' | sed 's;^;http://ndr.md/data/unix50/;' | xargs -n1 wget
+
+inputs=(
+  1 10 11 12 2 3 4 5 6 7 8 9.1 9.2 9.3 9.4 9.5 9.6 9.7 9.8 9.9
+)
+
+if [[ "$1" == "-c" ]]; then
+  for input in ${inputs[@]}
+  do
+    rm -f "${input}.txt"
+  done
+  exit
+fi
+
+for input in ${inputs[@]}
+do
+  if [ ! -f "${input}.txt" ]; then
+    wget "http://ndr.md/data/unix50/${input}.txt"
+    "$PASH_TOP/scripts/append_nl_if_not.sh" "${input}.txt"
+  fi
+done
 
 ## FIXME: Calling this script with --full is not idempotent.
 if [ "$#" -eq 1 ] && [ "$1" = "--full" ]; then
