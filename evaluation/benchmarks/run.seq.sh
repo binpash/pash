b2ede79dc5d3ec70362cacd55fb3ba81be457ea4
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-04-04 02:01:55 -0400

    Put poets scripts in a loop

--- evaluation/benchmarks/run.seq.sh
+++ evaluation/benchmarks/run.seq.sh
@@ -180,8 +180,11 @@ analytics-mts(){
 }
 
 poets(){
-  if [ -e ./seq.res ]; then
-    echo "skipping $(basename $(pwd))/seq.res"
+  times_file="seq.res"
+  outputs_suffix="seq.out"
+  outputs_dir="outputs"
+  if [ -e "poets/${times_file}" ]; then
+    echo "skipping poets/${times_file}"
     return 0
   fi
 
@@ -191,32 +194,52 @@ poets(){
   ./setup.sh
   cd ..
 
-  echo '' > seq.res
-  echo executing Unix-for-poets $(date) | tee -a ./seq.res
-  export IN=$PASH_TOP/evaluation/benchmarks/poets/input/genesis
-  echo 1syllable_words.sh:               $({ time ./6_4.sh > /dev/null; } 2>&1) | tee -a ./seq.res
-  echo 2syllable_words.sh:               $({ time ./6_5.sh > /dev/null; } 2>&1) | tee -a ./seq.res
-  echo 4letter_words.sh:                 $({ time ./6_2.sh > /dev/null; } 2>&1) | tee -a ./seq.res
-  echo bigrams_appear_twice.sh:          $({ time ./8.2_2.sh > /dev/null; } 2>&1) | tee -a ./seq.res
-  echo bigrams.sh:                       $({ time ./4_3.sh > /dev/null; } 2>&1) | tee -a ./seq.res
-  echo compare_exodus_genesis.sh:        $({ time ./8.3_3.sh > /dev/null; } 2>&1) | tee -a ./seq.res
-  echo count_consonant_seq.sh:           $({ time ./7_2.sh > /dev/null; } 2>&1) | tee -a ./seq.res
-# echo count_morphs.sh:                  $({ time ./7_1.sh > /dev/null; } 2>&1) | tee -a ./seq.res
-  echo count_trigrams.sh:                $({ time ./4_3b.sh > /dev/null; } 2>&1) | tee -a ./seq.res
-  echo count_vowel_seq.sh:               $({ time ./2_2.sh > /dev/null; } 2>&1) | tee -a ./seq.res
-  echo count_words.sh:                   $({ time ./1_1.sh > /dev/null; } 2>&1) | tee -a ./seq.res
-  echo find_anagrams.sh:                 $({ time ./8.3_2.sh > /dev/null; } 2>&1) | tee -a ./seq.res
-  echo merge_upper.sh:                   $({ time ./2_1.sh > /dev/null; } 2>&1) | tee -a ./seq.res
-  echo sort.sh:                          $({ time ./3_1.sh > /dev/null; } 2>&1) | tee -a ./seq.res
-  echo sort_words_by_folding.sh:         $({ time ./3_2.sh > /dev/null; } 2>&1) | tee -a ./seq.res
-  echo sort_words_by_num_of_syllables.sh:$({ time ./8_1.sh > /dev/null; } 2>&1) | tee -a ./seq.res
-  echo sort_words_by_rhyming.sh:         $({ time ./3_3.sh > /dev/null; } 2>&1) | tee -a ./seq.res
-# echo trigram_rec.sh:                   $({ time ./6_1.sh > /dev/null; } 2>&1) | tee -a ./seq.res
-  echo uppercase_by_token.sh:            $({ time ./6_1_1.sh > /dev/null; } 2>&1) | tee -a ./seq.res
-  echo uppercase_by_type.sh:             $({ time ./6_1_2.sh > /dev/null; } 2>&1) | tee -a ./seq.res
-  echo verses_2om_3om_2instances.sh:     $({ time ./6_7.sh > /dev/null; } 2>&1) | tee -a ./seq.res
-  echo vowel_sequencies_gr_1K.sh:        $({ time ./8.2_1.sh > /dev/null; } 2>&1) | tee -a ./seq.res
-  echo words_no_vowels.sh:               $({ time ./6_3.sh > /dev/null; } 2>&1) | tee -a ./seq.res
+  mkdir -p "$outputs_dir"
+
+  names_scripts=(
+    "1syllable_words;6_4"
+    "2syllable_words;6_5"
+    "4letter_words;6_2"
+    "bigrams_appear_twice;8.2_2"
+    "bigrams;4_3"
+    "compare_exodus_genesis;8.3_3"
+    "count_consonant_seq;7_2"
+    # "count_morphs;7_1"
+    "count_trigrams;4_3b"
+    "count_vowel_seq;2_2"
+    "count_words;1_1"
+    "find_anagrams;8.3_2"
+    "merge_upper;2_1"
+    "sort;3_1"
+    "sort_words_by_folding;3_2"
+    "sort_words_by_num_of_syllables;8_1"
+    "sort_words_by_rhyming;3_3"
+    # "trigram_rec;6_1"
+    "uppercase_by_token;6_1_1"
+    "uppercase_by_type;6_1_2"
+    "verses_2om_3om_2instances;6_7"
+    "vowel_sequencies_gr_1K;8.2_1"
+    "words_no_vowels;6_3"
+  )
+
+  touch "$times_file"
+  echo executing Unix-for-poets $(date) | tee -a "$times_file"
+  echo '' >> "$times_file"
+
+  for name_script in ${names_scripts[@]}
+  do
+    IFS=";" read -r -a name_script_parsed <<< "${name_script}"
+    name="${name_script_parsed[0]}"
+    script="${name_script_parsed[1]}"
+    export IN="$PASH_TOP/evaluation/benchmarks/poets/input/genesis"
+    printf -v pad %30s
+    padded_script="${name}.sh:${pad}"
+    padded_script=${padded_script:0:30}
+
+    outputs_file="${outputs_dir}/${script}.${outputs_suffix}"
+
+    echo "${padded_script}" $({ time ./${script}.sh > "$outputs_file"; } 2>&1) | tee -a "$times_file"
+  done
   cd ..
 }
 
