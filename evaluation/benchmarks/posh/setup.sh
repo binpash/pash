6f952ee3d1db99cca33971f46f52e516fd68e405
androbots <makesureyoudontreply@gmail.com>
2021-04-05 10:28:05 +0000

    POSH scripts

---
+++ evaluation/benchmarks/posh/setup.sh
@@ -0,0 +1,12 @@
+if [[ "$1" == "-c" ]]; then
+  rm -rf input
+fi
+mkdir input output
+cd input
+if [[ ! -d cr_data ]]; then
+  echo "Downloading 37GB"
+  wget https://sing.stanford.edu/deeptir/posh_data/posh_data.tar.gz
+  echo "extracting 140GB"
+  tar -xf posh_data.tar.gz
+fi
+
