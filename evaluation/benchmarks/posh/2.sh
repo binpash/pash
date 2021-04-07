a3df70a08235fe55318f08a1aa33009ce0204b3b
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-04-07 07:36:05 +0000

    fixes

--- evaluation/benchmarks/posh/2.sh
+++ evaluation/benchmarks/posh/2.sh
@@ -2,5 +2,5 @@
 # tag: generate thumbnails with size 100x100 
 set -e
 IN=${IN:-$PASH_TOP/evaluation/benchmarks/posh/input/cr_data/apps/images/originals}
-OU=${OUT:-$PASH_TOP/evaluation/benchmarks/posh/output}
+OUT=${OUT:-$PASH_TOP/evaluation/benchmarks/posh/input/output}
 mogrify  -format gif -path ${OUT} -thumbnail 100x100 ${IN}/*.jpg
