2c5ad342d71654b66ab067729da79667eb0fca6d
Nikos Vasilakis <nikos@vasilak.is>
2021-04-12 09:34:53 -0400

    Update tutorial scripts

--- evaluation/intro/demo-spell.sh
+++ evaluation/intro/demo-spell.sh
@@ -2,7 +2,9 @@
 
 # Would this expansion work:
 # cd "$(dirname "${BASH_SOURCE[0]}")"
-FILE="$PASH_TOP/evaluation/scripts/input/100M.txt"
-DICT="$PASH_TOP/evaluation/scripts/input/sorted_words"
+FILE="$(dirname $0)/input/100M.txt"
+DICT="$(dirname $0)/input/sorted_words"
+
+echo $FILE $DICT
 
 cat "$FILE" | tr A-Z a-z | tr -cs A-Za-z '\n' | sort | uniq | comm -13 $DICT -
