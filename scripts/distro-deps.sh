ef52ca61bfb79315290032b994cff8368ba3ee61
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-04-12 11:55:57 +0000

    Distro deps

--- scripts/distro-deps.sh
+++ scripts/distro-deps.sh
@@ -14,8 +14,40 @@ if [[ $(uname) == 'Darwin' ]]; then
   exit 1
 fi
 
-echo "Running preparation sudo apt install:"
-echo "|-- running apt update..."
-sudo apt-get update &> $LOG_DIR/apt_update.log
-echo "|-- running apt install..."
-sudo apt-get install -y git libtool m4 automake pkg-config libffi-dev python3 python3-pip wamerican-insane bc bsdmainutils &> $LOG_DIR/apt_install.log
+
+if type lsb_release >/dev/null 2>&1 ; then
+   distro=$(lsb_release -i -s)
+elif [ -e /etc/os-release ] ; then
+   distro=$(awk -F= '$1 == "ID" {print $2}' /etc/os-release)
+fi
+
+# convert to lowercase
+distro=$(printf '%s\n' "$distro" | LC_ALL=C tr '[:upper:]' '[:lower:]')
+
+# now do different things depending on distro
+case "$distro" in
+   ubuntu*)  
+     echo "Running preparation sudo apt install:"
+     echo "|-- running apt update..."
+     sudo apt-get update &> $LOG_DIR/apt_update.log
+     echo "|-- running apt install..."
+     sudo apt-get install -y git libtool m4 automake pkg-config libffi-dev python3 python3-pip wamerican-insane bc bsdmainutils &> $LOG_DIR/apt_install.log
+     ;;
+   fedora*) 
+     echo "|-- running dnf install...."
+     dnf install git gcc python3-pip make automake autoconf libtool hostname bc procps -y  &> $LOG_DIR/dnf_install.log
+     ;;
+   arch*) 
+    echo "Updating mirrors"
+    pacman -Sy &> $LOG_DIR/pacman_update.log
+     echo "|-- running pacman install...."
+    yes | pacman -S git libtool m4 automake pkg-config python-pip libffi make autoconf gcc sudo inetutils bc
+    ;;
+   *)        echo "unknown distro: '$distro'" ; exit 1 ;;
+esac
+
+
+
+
+
+
