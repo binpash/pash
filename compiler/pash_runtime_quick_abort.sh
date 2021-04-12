7fa0d194fac43dc51a7fa144e8ce853ebf72545f
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-04-11 23:50:15 -0400

    Fix logging

--- compiler/pash_runtime_quick_abort.sh
+++ compiler/pash_runtime_quick_abort.sh
@@ -8,9 +8,16 @@ still_alive()
     jobs -p | tr '\n' ' '
 }
 
-log()
+log_qa()
 {
-    pash_redir_output echo "$$: (QAbort) " "$@"
+    log "(QAbort) " "$@"
+}
+
+log_time_from_qa()
+{
+    local start_time="$1"
+    local prefix="$2"
+    log_time_from "$start_time" "(QAbort) $prefix"
 }
 
 # Taken from: https://stackoverflow.com/a/20473191
@@ -41,10 +48,10 @@ spawn_eager()
     # "$RUNTIME_DIR/../runtime/eager" "$input" "$output" "$eager_file" </dev/null 1>/dev/null 2>/dev/null &
     "$RUNTIME_DIR/../runtime/dgsh_tee.sh" "$input" "$output" -I -f </dev/null 1>/dev/null 2>/dev/null &
     local eager_pid=$!
-    log "Spawned $name eager: $eager_pid with:"
-    log "  -- IN: $input"
-    log "  -- OUT: $output"
-    # log "  -- INTERM: $eager_file"
+    log_qa "Spawned $name eager: $eager_pid with:"
+    log_qa "  -- IN: $input"
+    log_qa "  -- OUT: $output"
+    # log_qa "  -- INTERM: $eager_file"
     echo "$eager_pid"
 }
 
@@ -55,10 +62,10 @@ spawn_tee()
     local output2=$3
     "$RUNTIME_DIR/../runtime/dgsh_tee" -i "$input" -o "$output1" -o "$output2" -f </dev/null 1>/dev/null 2>/dev/null &
     local tee_pid=$!
-    log "Spawned dgsh-tee: $tee_pid with:"
-    log "  -- IN: $input"
-    log "  -- OUT1: $output1"
-    log "  -- OUT1: $output2"
+    log_qa "Spawned dgsh-tee: $tee_pid with:"
+    log_qa "  -- IN: $input"
+    log_qa "  -- OUT1: $output1"
+    log_qa "  -- OUT1: $output2"
     echo "$tee_pid"
 }
 
@@ -150,7 +157,7 @@ if [ "$pash_execute_flag" -eq 1 ]; then
            "$pash_tee_stdout1" \
            "$pash_tee_stdout2" \
            "$pash_par_eager_output"
-    log_time_from "$pash_quick_abort_time_start" "Tempfile preparation"
+    log_time_from_qa "$pash_quick_abort_time_start" "Tempfile preparation"
 
     pash_quick_abort_time_after_tempfiles=$(date +"%s%N")
 
@@ -162,7 +169,7 @@ if [ "$pash_execute_flag" -eq 1 ]; then
         ${pash_sequential_script_file} \
         > "$pash_seq_output" < "$pash_seq_eager_output" &
     pash_seq_pid=$!
-    log "Sequential pid: $pash_seq_pid"
+    log_qa "Sequential pid: $pash_seq_pid"
 
     ## (E) The sequential output eager
     seq_output_eager_pid=$(spawn_eager "sequential output" "$pash_seq_output" "$pash_seq_eager2_output")
@@ -171,7 +178,7 @@ if [ "$pash_execute_flag" -eq 1 ]; then
     ## The redirections below are necessary to ensure that the background `cat` reads from stdin.
     { setsid cat > "$pash_tee_stdin" <&3 3<&- & } 3<&0
     pash_input_cat_pid=$!
-    log "Spawned input cat with pid: $pash_input_cat_pid"
+    log_qa "Spawned input cat with pid: $pash_input_cat_pid"
 
     ## (B) A `tee` that duplicates input to both the sequential and parallel
     # tee_pid=$(spawn_tee "$pash_tee_stdin" "$pash_tee_stdout1" "$pash_tee_stdout2")
@@ -187,28 +194,28 @@ if [ "$pash_execute_flag" -eq 1 ]; then
     ## Run the compiler
     setsid python3 "$RUNTIME_DIR/pash_runtime.py" ${pash_compiled_script_file} --var_file "${pash_runtime_shell_variables_file}" "${@:2}" &
     pash_compiler_pid=$!
-    log "Compiler pid: $pash_compiler_pid"
-    log_time_from "$pash_quick_abort_time_after_tempfiles" "Phases (A-F)"
+    log_qa "Compiler pid: $pash_compiler_pid"
+    log_time_from_qa "$pash_quick_abort_time_after_tempfiles" "Phases (A-F)"
 
     ## Wait until one of the two (original script, or compiler) die
     pash_quick_abort_time_before_wait=$(date +"%s%N")
-    ## TODO: Delete this loop if not necessary
+    ## TODO: Delete this loop if not necessary. It seems to be however...
     alive_pids=$(still_alive)
-    log "Still alive: $alive_pids"
+    log_qa "Still alive: $alive_pids"
     while `list_include_item "$alive_pids" "$pash_seq_pid"` && `list_include_item "$alive_pids" "$pash_compiler_pid"` ; do
         ## Wait for either of the two to complete
         wait -n "$pash_seq_pid" "$pash_compiler_pid"
         completed_pid_status=$?
-        log "Process exited with return code: $completed_pid_status"
+        log_qa "Process exited with return code: $completed_pid_status"
         alive_pids=$(still_alive)
-        log "Still alive: $alive_pids"
+        log_qa "Still alive: $alive_pids"
     done
-    log_time_from "$pash_quick_abort_time_before_wait" "Waiting"
+    log_time_from_qa "$pash_quick_abort_time_before_wait" "Waiting"
 
     ## If the sequential is still alive we want to see if the compiler succeeded
     if `list_include_item "$alive_pids" "$pash_seq_pid"` ; then
         pash_runtime_return_code=$completed_pid_status
-        log "Compilation was done first with return code: $pash_runtime_return_code"
+        log_qa "Compilation was done first with return code: $pash_runtime_return_code"
 
         ## We only want to run the parallel if the compiler succeeded.
         if [ "$pash_runtime_return_code" -eq 0 ]; then
@@ -217,15 +224,15 @@ if [ "$pash_execute_flag" -eq 1 ]; then
             ## Redirect the sequential output to /dev/null
             cat "$pash_seq_eager2_output" > /dev/null &
             seq_cat_pid=$!
-            log "seq to /dev/null cat pid: $seq_cat_pid"
+            log_qa "seq to /dev/null cat pid: $seq_cat_pid"
 
             ## Kill the sequential process tree
-            log "Killing sequential pid: $pash_seq_pid..."
+            log_qa "Killing sequential pid: $pash_seq_pid..."
             kill_pg "$pash_seq_pid"
             kill_status=$?
             wait_pg "$pash_seq_pid"
             seq_exit_status=$?
-            log "Sequential pid: $pash_seq_pid was killed successfully returning status $seq_exit_status."
+            log_qa "Sequential pid: $pash_seq_pid was killed successfully returning status $seq_exit_status."
 
             ## If kill failed it means it was already completed, 
             ## and therefore we do not need to run the parallel.
@@ -233,12 +240,12 @@ if [ "$pash_execute_flag" -eq 1 ]; then
             ## TOOD: Enable this optimization
             if true || [ "$kill_status" -eq 0 ]; then
                 ## (2) Run the parallel
-                log "Run parallel:"
-                log "  -- Runtime vars: $pash_runtime_shell_variables_file"
-                log "  -- Output vars: $pash_output_variables_file"
-                log "  -- Output set: ${pash_output_set_file}"
-                log "  -- Compiled script: ${pash_compiled_script_file}"
-                log "  -- Input: $pash_par_eager_output"
+                log_qa "Run parallel:"
+                log_qa "  -- Runtime vars: $pash_runtime_shell_variables_file"
+                log_qa "  -- Output vars: $pash_output_variables_file"
+                log_qa "  -- Output set: ${pash_output_set_file}"
+                log_qa "  -- Compiled script: ${pash_compiled_script_file}"
+                log_qa "  -- Input: $pash_par_eager_output"
 
                 "$RUNTIME_DIR/pash_wrap_vars.sh" \
                     $pash_runtime_shell_variables_file \
@@ -250,14 +257,14 @@ if [ "$pash_execute_flag" -eq 1 ]; then
                 ##       but no more after we started using dgsh-tee
 
                 pash_par_pid=$!
-                log "Parallel is running with pid: $pash_par_pid..."
+                log_qa "Parallel is running with pid: $pash_par_pid..."
                 # strace -p $pash_par_pid 2>> $PASH_REDIR
                 wait "$pash_par_pid"
                 pash_runtime_final_status=$?
-                log "Parallel is done with status: $pash_runtime_final_status"
+                log_qa "Parallel is done with status: $pash_runtime_final_status"
             else
                 ## TODO: Handle that case properly by enabling the optimization above.
-                log "ERROR: Shouldn't have reached that"
+                log_qa "ERROR: Shouldn't have reached that"
                 exit 1
             fi
         else
@@ -266,27 +273,27 @@ if [ "$pash_execute_flag" -eq 1 ]; then
             ## (1) Redirect the seq output to stdout
             cat "$pash_seq_eager2_output" &
             seq_output_cat_pid=$!
-            log "STDOUT cat pid: $seq_output_cat_pid"
+            log_qa "STDOUT cat pid: $seq_output_cat_pid"
 
-            log "Waiting for sequential: $pash_seq_pid"
+            log_qa "Waiting for sequential: $pash_seq_pid"
             wait "$pash_seq_pid"
             pash_runtime_final_status=$?
-            log "DONE Sequential: $pash_seq_pid exited with status: $pash_runtime_final_status"
+            log_qa "DONE Sequential: $pash_seq_pid exited with status: $pash_runtime_final_status"
 
             ## TODO: It is not clear if we also need to wait for the output cat to end.
-            log "Waiting for sequential output cat: $seq_output_cat_pid"
+            log_qa "Waiting for sequential output cat: $seq_output_cat_pid"
             wait "$seq_output_cat_pid"
-            log "DONE Waiting for sequential output cat: $seq_output_cat_pid"
+            log_qa "DONE Waiting for sequential output cat: $seq_output_cat_pid"
 
         fi
     else
         pash_runtime_final_status=$completed_pid_status
-        log "Sequential was done first with return code: $pash_runtime_final_status"
+        log_qa "Sequential was done first with return code: $pash_runtime_final_status"
 
         ## (1) Redirect the seq output to stdout
         cat "$pash_seq_eager2_output" &
         final_cat_pid=$!
-        log "STDOUT cat pid: $final_cat_pid"
+        log_qa "STDOUT cat pid: $final_cat_pid"
 
         ## We need to kill the compiler to not get delayed log output
         ## If this fails (meaning that compilation is done) we do not care
@@ -299,22 +306,22 @@ if [ "$pash_execute_flag" -eq 1 ]; then
     ## TODO: Not clear if this is needed or if it doesn indeed kill all the
     ##       processes and cleans up everything properly
     ## Kill the input process
-    log "Killing the input cat process: $pash_input_cat_pid"
+    log_qa "Killing the input cat process: $pash_input_cat_pid"
     kill_wait_pg "$pash_input_cat_pid"
     # kill -9 $pash_input_cat_pid 2> /dev/null
     # wait $pash_input_cat_pid 2> /dev/null
-    log "The input cat: $pash_input_cat_pid died!"
+    log_qa "The input cat: $pash_input_cat_pid died!"
     
 
     ## TODO: This (and the above) should not be needed actually, everything should be already done due to
     ##       sequential and parallel both having exited.
     ## Kill every spawned process
     still_alive_pids="$(still_alive)"
-    log "Killing all the still alive: $still_alive_pids"
+    log_qa "Killing all the still alive: $still_alive_pids"
     kill -15 "$still_alive_pids" 2> /dev/null
     wait $still_alive_pids 2> /dev/null
-    log "All the alive pids died: $still_alive_pids"
-    log_time_from "$pash_quick_abort_time_before_cleanup" "Cleanup"
+    log_qa "All the alive pids died: $still_alive_pids"
+    log_time_from_qa "$pash_quick_abort_time_before_cleanup" "Cleanup"
 
     ## Return the exit code
     (exit "$pash_runtime_final_status")
