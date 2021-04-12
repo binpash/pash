eaf5726a0795b9d1a4783c0701ac8250baabca17
Nikos Vasilakis <nikos@vasilak.is>
2021-04-12 12:00:25 -0400

    Improve setup scripts (#206)
    * Update setup
    
    * Teensy correction
    
    * Note for avoiding docker cache
    
    * Update scripts
    
    * Small shell-related fixes
    
    * Update vars
    
    * Distro deps
    
    Co-authored-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>

---
+++ scripts/distro-deps.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd $(dirname $0)
+PASH_TOP=${PASH_TOP:-$(git rev-parse --show-toplevel)}
+cd $PASH_TOP
+
+LOG_DIR=$PWD/install_logs
+mkdir -p $LOG_DIR
+
+if [[ $(uname) == 'Darwin' ]]; then
+  echo 'Currently pash can run only on Linux'
+  exit 1
+fi
+
+
+if type lsb_release >/dev/null 2>&1 ; then
+   distro=$(lsb_release -i -s)
+elif [ -e /etc/os-release ] ; then
+   distro=$(awk -F= '$1 == "ID" {print $2}' /etc/os-release)
+fi
+
+# convert to lowercase
+distro=$(printf '%s\n' "$distro" | LC_ALL=C tr '[:upper:]' '[:lower:]')
+
+# now do different things depending on distro
+case "$distro" in
+   ubuntu*)  
+     echo "Running preparation sudo apt install:"
+     echo "|-- running apt update..."
+     sudo apt-get update &> $LOG_DIR/apt_update.log
+     echo "|-- running apt install..."
+     sudo apt-get install -y git libtool m4 automake pkg-config libffi-dev python3 python3-pip wamerican-insane bc bsdmainutils &> $LOG_DIR/apt_install.log
+     ;;
+   fedora*) 
+     echo "|-- running dnf install...."
+     dnf install git gcc python3-pip make automake autoconf libtool hostname bc procps -y  &> $LOG_DIR/dnf_install.log
+     ;;
+   arch*) 
+    echo "Updating mirrors"
+    pacman -Sy &> $LOG_DIR/pacman_update.log
+     echo "|-- running pacman install...."
+    yes | pacman -S git libtool m4 automake pkg-config python-pip libffi make autoconf gcc sudo inetutils bc
+    ;;
+   *)        echo "unknown distro: '$distro'" ; exit 1 ;;
+esac
+
+
+
+
+
+
--- scripts/up.sh
+++ scripts/up.sh
@@ -1,42 +1,27 @@
 #!/usr/bin/env sh
 
-# TODO: Maybe first check if the repo is accessible via git?
+# clone and setup pash
+# N.b. This is a .sh script
+
+set -e
 
 # will install dependencies locally.
 PLATFORM=$(uname | tr '[:upper:]' '[:lower:]')
-URL='http://get.pash.ndr.md/'
+URL='https://github.com/andromeda/pash/archive/refs/heads/main.zip'
 VERSION='latest'
-DOWNLOADER='curl'
-alias curl='curl -s'
-
-download () {
-  command -v curl >/dev/null 2>&1 || { DOWNLOADER='wget'; alias curl='wget -qO- '; }
-}
+DL=$(command -v curl >/dev/null 2>&1 && echo curl || echo 'wget -qO-')
 
 cmd_exists () {
   command -v $1 >/dev/null 2>&1 && echo 'true' || echo 'false';
 }
 
-if [ $PLATFORM = 'darwin' ]; then
+if [ "$PLATFORM" = "darwin" ]; then
   echo 'PaSh is not yet well supported on OS X'
+  exit 1
 fi
 
-INSTALL=0
-while getopts 'i' opt; do
-    case $opt in
-        i) INSTALL=1 ;;
-        *) echo 'Error in command line parsing' >&2
-           exit 1
-    esac
-done
-shift "$(( OPTIND - 1 ))"
-
-[ -d "./pash" ] && mv ./pash ./pash-old-${RANDOM}
-curl -s $URL | tar xzf - --strip-components=1 -C .
-# FIXME: it's unclear we should always run as root
-# A first step would be to confirm dependencies are met
-# ./install.sh -p
-if [[ $INSTAll == 1 ]]; then
-  echo "This option does not work currently. Please open a ticket"
-fi
-echo 'Run `./scripts/install.sh`, with `-p` for installing packages if needed'
+git clone git@github.com:andromeda/pash.git
+cd pash/scripts
+# git checkout s3 # FIXME only for testing while PR is up
+bash distro-deps.sh
+bash setup-pash.sh
