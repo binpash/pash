1e698182350a937d24b9453ed698a9a8edbdd97b
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-04-13 08:16:28 +0000

    Setup fix on pash installation

--- scripts/up.sh
+++ scripts/up.sh
@@ -5,22 +5,22 @@
 
 set -e
 
-# will install dependencies locally.
-PLATFORM=$(uname | tr '[:upper:]' '[:lower:]')
-URL='https://github.com/andromeda/pash/archive/refs/heads/main.zip'
-VERSION='latest'
-DL=$(command -v curl >/dev/null 2>&1 && echo curl || echo 'wget -qO-')
-
-cmd_exists () {
-  command -v $1 >/dev/null 2>&1 && echo 'true' || echo 'false';
-}
-
-if [ "$PLATFORM" = "darwin" ]; then
-  echo 'PaSh is not yet well supported on OS X'
-  exit 1
-fi
-
-git clone git@github.com:andromeda/pash.git
+## will install dependencies locally.
+#PLATFORM=$(uname | tr '[:upper:]' '[:lower:]')
+#URL='https://github.com/andromeda/pash/archive/refs/heads/main.zip'
+#VERSION='latest'
+#DL=$(command -v curl >/dev/null 2>&1 && echo curl || echo 'wget -qO-')
+#
+#cmd_exists () {
+#  command -v $1 >/dev/null 2>&1 && echo 'true' || echo 'false';
+#}
+#
+#if [ "$PLATFORM" = "darwin" ]; then
+#  echo 'PaSh is not yet well supported on OS X'
+#  exit 1
+#fi
+#
+#git clone git@github.com:andromeda/pash.git
 cd pash/scripts
 # git checkout s3 # FIXME only for testing while PR is up
 bash distro-deps.sh
