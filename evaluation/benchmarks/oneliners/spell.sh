a3cba1a10c2b8f9172295fc5be09e734064f836a
Dimitris Karnikis <dkarnikis@gmail.com>
2021-04-13 20:54:00 +0300

    Handle non-utf8 characters on spell.sh (#220)

--- evaluation/benchmarks/oneliners/spell.sh
+++ evaluation/benchmarks/oneliners/spell.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 # Calculate mispelled words in an input
 # https://dl.acm.org/doi/10.1145/3532.315102
-
 IN=${IN:-$PASH_TOP/evaluation/benchmarks/oneliners/input/1G.txt}
 dict=${dict:-$PASH_TOP/evaluation/benchmarks/oneliners/input/dict.txt}
 
-
 cat $IN |
+    iconv -f utf-8 -t ascii//translit | # remove non utf8 characters
     # groff -t -e -mandoc -Tascii |  # remove formatting commands
     col -bx |                      # remove backspaces / linefeeds
     tr -cs A-Za-z '\n' |
