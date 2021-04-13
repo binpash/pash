3f293a262440b993bd5b637c144e128e811f9b0a
Dimitris Karnikis <dkarnikis@gmail.com>
2021-04-13 20:58:29 +0300

    Added dgsh, posh, bio, aliases for par.seq (#217)

--- evaluation/benchmarks/run.par.sh
+++ evaluation/benchmarks/run.par.sh
@@ -271,3 +271,244 @@ poets_pash(){
   done
   cd ..
 }
+
+
+
+# everything under this line is WIP
+
+
+dgsh_pash(){
+
+  times_file="par.res"
+  outputs_suffix="par.out"
+  outputs_dir="outputs"
+  pash_logs_dir="pash_logs"
+  width=16
+  if [ -e "dgsh/${times_file}" ]; then
+    echo "skipping dgsh/${times_file}"
+    return 0
+  fi
+
+  cd dgsh/
+
+  cd input/
+  ./setup.sh
+  cd ..
+
+  mkdir -p "$outputs_dir"
+  mkdir -p "$pash_logs_dir"
+
+  names_scripts=(
+    "compressionbench;1"
+    "gitstats;2"
+    "cmetrics;3"
+    "dublicatefiles;4"
+    "highlightwords;5"
+    # "wordproperties;6"
+    # "weatherreport;7"
+    "textproperties;8"
+    "staticsymbols;9"
+    # "hierarchymap;10"
+    # "plotgit;11"
+    "parallelword;12"
+    # "venuauthor;13"
+    # "2dfourier;14"
+    # "nuclear;15"
+    # "fft;16"
+    "reordercol;17"
+    "dirlisting;18"
+  )
+
+  touch "$times_file"
+  echo executing DGSH with pash $(date) | tee -a "$times_file"
+  echo '' >> "$times_file"
+
+  export VOC=/usr/share/dict/words
+  export IN=$PASH_TOP/evaluation/benchmarks/dgsh/input/
+  export FULL=$IN/dblp.xml
+  export MINI=$IN/mini.xml
+  export OUT=$PASH_TOP/evaluation/benchmarks/dgsh/input/
+  export BIN=/usr/local/bin
+
+  for name_script in ${names_scripts[@]}
+  do
+    IFS=";" read -r -a name_script_parsed <<< "${name_script}"
+    name="${name_script_parsed[0]}"
+    script="${name_script_parsed[1]}"
+    export IN="$PASH_TOP/evaluation/benchmarks/dgsh/input/genesis"
+    printf -v pad %30s
+    padded_script="${name}.sh:${pad}"
+    padded_script=${padded_script:0:30}
+
+    outputs_file="${outputs_dir}/${script}.${outputs_suffix}"
+    pash_log="${pash_logs_dir}/${script}.pash.log"
+
+    echo "${padded_script}" $({ time "$PASH_TOP/pa.sh" --r_split --dgsh_tee -d 1 -w "${width}" --log_file "${pash_log}" ${script}.sh > "$outputs_file"; } 2>&1) | tee -a "$times_file"
+  done
+  cd ..
+}
+
+
+aliases_pash(){
+
+  times_file="par.res"
+  outputs_suffix="par.out"
+  outputs_dir="outputs"
+  pash_logs_dir="pash_logs"
+  width=16
+  if [ -e "aliases/${times_file}" ]; then
+    echo "skipping aliases/${times_file}"
+    return 0
+  fi
+
+  cd aliases/
+
+  cd input/
+  ./setup.sh
+  cd ..
+
+  mkdir -p "$outputs_dir"
+  mkdir -p "$pash_logs_dir"
+
+  names_scripts=(
+    "compressionbench;1"
+    "gitstats;2"
+    "cmetrics;3"
+    "dublicatefiles;4"
+    "highlightwords;5"
+    # "wordproperties;6"
+    # "weatherreport;7"
+    "textproperties;8"
+    "staticsymbols;9"
+    # "hierarchymap;10"
+    # "plotgit;11"
+    "parallelword;12"
+    # "venuauthor;13"
+    # "2dfourier;14"
+    # "nuclear;15"
+    # "fft;16"
+    "reordercol;17"
+    "dirlisting;18"
+  )
+
+  touch "$times_file"
+  echo executing aliases with pash $(date) | tee -a "$times_file"
+  echo '' >> "$times_file"
+
+
+  for name_script in ${names_scripts[@]}
+  do
+    IFS=";" read -r -a name_script_parsed <<< "${name_script}"
+    name="${name_script_parsed[0]}"
+    script="${name_script_parsed[1]}"
+    export IN="$PASH_TOP/evaluation/benchmarks/aliases/input/genesis"
+    printf -v pad %30s
+    padded_script="${name}.sh:${pad}"
+    padded_script=${padded_script:0:30}
+
+    outputs_file="${outputs_dir}/${script}.${outputs_suffix}"
+    pash_log="${pash_logs_dir}/${script}.pash.log"
+
+    echo "${padded_script}" $({ time "$PASH_TOP/pa.sh" --r_split --dgsh_tee -d 1 -w "${width}" --log_file "${pash_log}" ${script}.sh > "$outputs_file"; } 2>&1) | tee -a "$times_file"
+  done
+  cd ..
+}
+
+
+
+posh_pash(){
+
+  times_file="par.res"
+  outputs_suffix="par.out"
+  outputs_dir="outputs"
+  pash_logs_dir="pash_logs"
+  width=16
+  if [ -e "posh/${times_file}" ]; then
+    echo "skipping posh/${times_file}"
+    return 0
+  fi
+
+  cd posh/
+
+  cd input/
+  ./setup.sh
+  cd ..
+
+  mkdir -p "$outputs_dir"
+  mkdir -p "$pash_logs_dir"
+
+  names_scripts=(
+    "discat;1"
+    "convert;2"
+    "raytracing;3"
+    # "zannotate;4" where is zannotate binary
+  )
+
+  touch "$times_file"
+  echo executing posh with pash $(date) | tee -a "$times_file"
+  echo '' >> "$times_file"
+
+  export OUT=$PASH_TOP/evaluation/benchmarks/posh/input/output
+  for name_script in ${names_scripts[@]}
+  do
+    IFS=";" read -r -a name_script_parsed <<< "${name_script}"
+    name="${name_script_parsed[0]}"
+    script="${name_script_parsed[1]}"
+    printf -v pad %30s
+    padded_script="${name}.sh:${pad}"
+    padded_script=${padded_script:0:30}
+
+    outputs_file="${outputs_dir}/${script}.${outputs_suffix}"
+    pash_log="${pash_logs_dir}/${script}.pash.log"
+
+    echo "${padded_script}" $({ time "$PASH_TOP/pa.sh" --r_split --dgsh_tee -d 1 -w "${width}" --log_file "${pash_log}" ${script}.sh > "$outputs_file"; } 2>&1) | tee -a "$times_file"
+  done
+  cd ..
+}
+
+bio_pash(){
+
+  times_file="par.res"
+  outputs_suffix="par.out"
+  outputs_dir="outputs"
+  pash_logs_dir="pash_logs"
+  width=16
+  if [ -e "bio/${times_file}" ]; then
+    echo "skipping bio/${times_file}"
+    return 0
+  fi
+
+  cd bio/
+
+  cd input/
+  ./setup.sh
+  cd ..
+
+  mkdir -p "$outputs_dir"
+  mkdir -p "$pash_logs_dir"
+
+  names_scripts=(
+    "bio4.sh;bio4"
+  )
+
+  touch "$times_file"
+  echo executing bio with pash $(date) | tee -a "$times_file"
+  echo '' >> "$times_file"
+
+  export OUT=$PASH_TOP/evaluation/benchmarks/bio/input/output
+  for name_script in ${names_scripts[@]}
+  do
+    IFS=";" read -r -a name_script_parsed <<< "${name_script}"
+    name="${name_script_parsed[0]}"
+    script="${name_script_parsed[1]}"
+    printf -v pad %30s
+    padded_script="${name}.sh:${pad}"
+    padded_script=${padded_script:0:30}
+
+    outputs_file="${outputs_dir}/${script}.${outputs_suffix}"
+    pash_log="${pash_logs_dir}/${script}.pash.log"
+
+    echo "${padded_script}" $({ time "$PASH_TOP/pa.sh" --r_split --dgsh_tee -d 1 -w "${width}" --log_file "${pash_log}" ${script}.sh > "$outputs_file"; } 2>&1) | tee -a "$times_file"
+  done
+  cd ..
+}
