845b0bb8fb4d382be11c8a59e1e871e4512c8bc7
Nikos Vasilakis <nikos@vasilak.is>
2021-04-13 16:29:45 -0400

    Correct path

--- evaluation/intro/demo-spell.sh
+++ evaluation/intro/demo-spell.sh
@@ -8,7 +8,7 @@
   echo "PASH_TOP not set, maybe $(git rev-parse --show-toplevel)?"
   exit
 }
-FILE="$PASH_TOP/input/100M.txt"
-DICT="$PASH_TOP/input/sorted_words"
+FILE="$PASH_TOP/evaluation/intro/input/100M.txt"
+DICT="$PASH_TOP/evaluation/intro/input/sorted_words"
 
 cat "$FILE" | tr A-Z a-z | tr -cs A-Za-z '\n' | sort | uniq | comm -13 $DICT -
