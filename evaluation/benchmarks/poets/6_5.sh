821f2a6bc4f5d81fc5cd174eb65b7f6b10ac0c90
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-04-19 23:25:46 -0400

    Comment out set -e in poets

--- evaluation/benchmarks/poets/6_5.sh
+++ evaluation/benchmarks/poets/6_5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # tag: 2-syllable words
-set -e
+# set -e
 IN=${IN:-$PASH_TOP/evaluation/benchmarks/poets/input/pg/}
 cat ${IN}* | tr -sc '[A-Z][a-z]' ' [\012*]' | grep -i '^[^aeiou]*[aeiou][^aeiou]*[aeiou][^aeiou]$' | sort | uniq -c | sed 5q
--- evaluation/benchmarks/poets/7_1.sh
+++ evaluation/benchmarks/poets/7_1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # tag: count_morphs
-set -e
+# set -e
 
 IN=${IN:-$PASH_TOP/evaluation/benchmarks/poets/input/pg/}
 # FIXME: `spell -v' does not exist; instead of spell: sed 's/ly$/-ly/g'
