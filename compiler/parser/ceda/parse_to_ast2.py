15b0165326b339c4391e14675b5b06b80f5a0c8a
Thurston Dang (pash docker) <tdang@mit.edu>
2021-04-21 22:34:16 +0000

    Obtain line numbers of shell script parsed per AST
    nes starting

--- compiler/parser/ceda/parse_to_ast2.py
+++ compiler/parser/ceda/parse_to_ast2.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import subprocess
 from ctypes import *
 
 from ast2a import of_node
@@ -18,6 +19,8 @@ LIBDASH_LIBRARY_PATH = os.path.join(PASH_TOP, "compiler/parser/libdash/src/.libs
 
 # This is a mix of dash.ml:parse_next and parse_to_json.ml.
 def parse_to_ast (inputPath, init=True):
+    lines = [];
+
     libdash = CDLL (LIBDASH_LIBRARY_PATH);
 
     if (init):
@@ -28,13 +31,35 @@ def parse_to_ast (inputPath, init=True):
     else:
         setinputfile (libdash, inputPath);
 
+        fp = open (inputPath, 'r');
+        for line in fp:
+            lines.append (line);
+        fp.close()
+
+    # struct parsefile *parsefile = &basepf;  /* current input file */
+    # Get the value of parsefile (not &parsefile)!
+    parsefile_ptr_ptr = addressof (parsefile.in_dll (libdash, "parsefile"));
+    parsefile_ptr = cast (parsefile_ptr_ptr, POINTER (POINTER (parsefile)));
+    parsefile_var = parsefile_ptr.contents;
+
     smark = init_stack (libdash);
 
     NEOF = addressof (c_int.in_dll (libdash, "tokpushback"));
     NERR = addressof (c_int.in_dll (libdash, "lasttoken"));
 
     while (True):
+        linno_before = parsefile_var.contents.linno - 1; # libdash is 1-indexed
+
+        nleft = parsefile_var.contents.nleft;
+        if (nleft != 0):
+            # Our assumption is that parsecmd_safe always parses complete line(s)
+            ## TODO: Fix this print message to be a log.
+            print ("Oops");
+            os.abort ();
+
         n_ptr_C = parsecmd_safe (libdash, False);
+
+        linno_after = parsefile_var.contents.linno - 1; # libdash is 1-indexed
 
         if (n_ptr_C == None): # Dash.Null
             pass;
@@ -46,6 +71,11 @@ def parse_to_ast (inputPath, init=True):
             n_ptr = cast (n_ptr_C, POINTER (union_node));
             new_ast = of_node (n_ptr);
 
-            yield new_ast
+            if (inputPath != "-"):
+                parsedLines = "".join (lines [linno_before:linno_after]);
+            else:
+                parsedLines = "# Cannot return parsed lines for stdin input";
+
+            yield (new_ast, parsedLines, linno_before, linno_after)
 
             pop_stack (libdash, smark);
