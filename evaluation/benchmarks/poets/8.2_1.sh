a38e8a25c2afd8c9031b8263743715352e5a748b
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-04-25 16:30:02 -0400

    Comment out set -e in poets (#231)

--- evaluation/benchmarks/poets/8.2_1.sh
+++ evaluation/benchmarks/poets/8.2_1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # tag: vowel_sequencies_gr_1K.sh
-set -e
+# set -e
 
 IN=${IN:-$PASH_TOP/evaluation/benchmarks/poets/input/pg/}
 cat ${IN}* | tr -sc '[A-Z][a-z]' '[\012*]' | tr -sc 'AEIOUaeiou' '[\012*]' | sort | uniq -c | awk "\$1 >= 1000"
