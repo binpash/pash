a952d5a74e71c40bb10e79ffe2380bc7177ad646
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-04-25 16:32:59 -0400

    Various interface fixes and improved robustness (#227)
    * Interface tests
    
    * Allow input arguments when calling PaSh
    
    * Support -c flag (Make sure that positional arguments are handled correctly for -c)
    
    * Parsing and printing robustness
    
    * Add new lines instead of semicolons to fix buggy printing
    
    * Parsing is now a generator instead of returning a list

--- compiler/pash.py
+++ compiler/pash.py
@@ -19,7 +19,8 @@ def main():
     args = parse_args()
 
     ## 1. Execute the POSIX shell parser that returns the AST in JSON
-    input_script_path = args.input
+    input_script_path = args.input[0]
+    input_script_arguments = args.input[1:]
     preprocessing_parsing_start_time = datetime.now()
     ast_objects = parse_shell_to_ast(input_script_path)
     preprocessing_parsing_end_time = datetime.now()
@@ -51,7 +52,7 @@ def main():
 
     ## 4. Execute the preprocessed version of the input script
     if(not args.preprocess_only):
-        execute_script(fname, args.debug, args.command)
+        execute_script(fname, args.debug, args.command, input_script_arguments)
 
 
 def parse_args():
@@ -59,7 +60,7 @@ def parse_args():
     if 'PASH_FROM_SH' in os.environ:
         prog_name = os.environ['PASH_FROM_SH']
     parser = argparse.ArgumentParser(prog_name)
-    parser.add_argument("input", nargs='?', help="the script to be compiled and executed")
+    parser.add_argument("input", nargs='*', help="the script to be compiled and executed (followed by any command-line arguments")
     parser.add_argument("--preprocess_only",
                         help="only preprocess the input script and not execute it",
                         action="store_true")
@@ -81,13 +82,21 @@ def parse_args():
     ## Make a directory for temporary files
     config.PASH_TMP_PREFIX = tempfile.mkdtemp(prefix="pash_")
     if args.command:
-        with open(config.config['runtime']['immediate'], 'w') as f:
+        _, fname = ptempfile()
+        with open(fname, 'w') as f:
             f.write(args.command)
-            args.input = f.name
+            ## If the shell is invoked with -c and arguments after it, then these arguments
+            ## need to be assigned to $0, $1, $2, ... and not $1, $2, $3, ...
+            if(len(args.input) > 0):
+                ## Assign $0
+                args.name = args.input[0]
+                args.input = args.input[1:]
+            args.input = [fname] + args.input
 
-    if (args.input == None):
+    if (len(args.input) == 0):
         parser.print_usage()
-        exit()
+        print("Error: PaSh does not yet support interactive mode!")
+        exit(1)
 
     ## Print all the arguments
     log("Arguments:")
@@ -108,15 +117,14 @@ def preprocess(ast_objects, config):
 
     return preprocessed_asts
 
-def execute_script(compiled_script_filename, debug_level, command):
+def execute_script(compiled_script_filename, debug_level, command, arguments):
     new_env = os.environ.copy()
     new_env["PASH_TMP_PREFIX"] = config.PASH_TMP_PREFIX
-    exec_obj = subprocess.run(["/usr/bin/env", "bash" ,compiled_script_filename], env=new_env)
+    ## TODO: Assign $0
+    exec_obj = subprocess.run(["/usr/bin/env", "bash", compiled_script_filename] + arguments, env=new_env)
     ## Delete the temp directory when not debugging
     if(debug_level == 0):
         shutil.rmtree(config.PASH_TMP_PREFIX)
-    if command:
-        os.remove(config.config['runtime']['immediate'])
     log("-" * 40) #log end marker
     ## Return the exit code of the executed script
     exit(exec_obj.returncode)
