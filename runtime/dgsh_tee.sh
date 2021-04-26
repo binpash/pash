1fb80d120371799e349b0d2c70b5369e48a05a22
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-04-26 17:17:18 -0400

    Make sure that all runtime wrappers kill children
    Make sure that eager deletes its child on killing
    
    Make sure that all wrapper runtime scripts kill their children

--- runtime/dgsh_tee.sh
+++ runtime/dgsh_tee.sh
@@ -6,5 +6,12 @@ args=${@:3}
 # Set a default DISH_TOP in this directory if it doesn't exist
 PASH_TOP=${PASH_TOP:-$(git rev-parse --show-toplevel)}
 
-$PASH_TOP/runtime/dgsh-tee -i "$input" -o "$output" $args
+cleanup()
+{
+    kill -SIGPIPE $dgsh_tee_pid > /dev/null 2>&1
+}
+trap cleanup EXIT
 
+$PASH_TOP/runtime/dgsh-tee -i "$input" -o "$output" $args &
+dgsh_tee_pid=$!
+wait $dgsh_tee_pid
--- runtime/eager.sh
+++ runtime/eager.sh
@@ -7,5 +7,13 @@ intermediate_file=${3?"ERROR: Eager: No intermediate file given"}
 # Set a default DISH_TOP in this directory if it doesn't exist
 PASH_TOP=${PASH_TOP:-$(git rev-parse --show-toplevel)}
 
-$PASH_TOP/runtime/eager "$input" "$output" "$intermediate_file"
+cleanup()
+{
+    kill -SIGPIPE $eager_pid > /dev/null 2>&1
+}
+trap cleanup EXIT
+
+$PASH_TOP/runtime/eager "$input" "$output" "$intermediate_file" &
+eager_pid=$!
+wait $eager_pid
 rm "$intermediate_file"
