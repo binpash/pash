7271ac6273dd7016a7315340b845c12e4a598add
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-05-18 15:19:26 -0400

    Add set-diff in gnu-parallel AE

--- evaluation/icfp-ae/gnu-parallel-case-study.sh
+++ evaluation/icfp-ae/gnu-parallel-case-study.sh
@@ -4,6 +4,7 @@
 export PASH_TOP=${PASH_TOP:-$(git rev-parse --show-toplevel --show-superproject-working-tree)}
 
 ## Setup inputs if they are not there already
+echo "Downloading/setting up input..."
 cd "$PASH_TOP/evaluation/benchmarks/oneliners/input"
 ./setup.sh
 
@@ -14,22 +15,22 @@ export IN=$(echo in{00..15})
 export IN=$(echo $IN)
 
 ## Setup input and dictionary
-export IN_FILE="$PASH_TOP/evaluation/benchmarks/oneliners/input/10M.txt"
+export IN_FILE="$PASH_TOP/evaluation/benchmarks/oneliners/input/1G.txt"
 export dict="$PASH_TOP/evaluation/benchmarks/oneliners/input/dict.txt"
 
 cd "$PASH_TOP/evaluation/micro/gnu-parallel"
 ## Split the files
 split -n l/$JOBS -d "$IN_FILE" in
 
-echo "Running bash..."
+echo "Running spell with bash..."
 time ./spell.sh > spell.bash.out
 
 run_spell_gnu_parallel()
 {
-    echo "Running gnu-parallel with block_size=$BLOCK_SIZE..."
+    echo "Running spell with gnu-parallel with block_size=$BLOCK_SIZE..."
     time ./spell.par.sh > spell.parallel.$BLOCK_SIZE.out
     echo "Comparing bash and gnu-parallel output"
-    diff spell.bash.out spell.parallel.$BLOCK_SIZE.out    
+    diff spell.bash.out spell.parallel.$BLOCK_SIZE.out | head
 }
 
 export BLOCK_SIZE=50K
@@ -41,7 +42,26 @@ run_spell_gnu_parallel
 export BLOCK_SIZE=250M
 run_spell_gnu_parallel
 
-echo "Running pash..."
-time "$PASH_TOP/pa.sh" -w $JOBS ./spell.sh > spell.pash.out
+echo "Running spell with pash..."
+time "$PASH_TOP/pa.sh" --r_split_batch_size 1000000 --r_split --dgsh_tee -w $JOBS ./spell.sh > spell.pash.out
 echo "Comparing bash and pash output"
-diff spell.bash.out spell.pash.out
+diff spell.bash.out spell.pash.out | head
+
+
+
+## Setup input for set-diff
+export IN_FILE="$PASH_TOP/evaluation/benchmarks/oneliners/input/3G.txt"
+split -n l/$JOBS -d "$IN_FILE" in
+
+echo "Running set-diff with bash..."
+time ./set-diff.sh > set-diff.bash.out
+
+echo "Running set-diff with gnu-parallel with block_size=$BLOCK_SIZE..."
+time ./set-diff.par.sh > set-diff.parallel.$BLOCK_SIZE.out
+echo "Comparing bash and gnu-parallel output"
+diff set-diff.bash.out set-diff.parallel.$BLOCK_SIZE.out | head
+
+echo "Running set-diff with pash..."
+time "$PASH_TOP/pa.sh" --r_split_batch_size 1000000 --r_split --dgsh_tee -w $JOBS ./set-diff.sh > set-diff.pash.out
+echo "Comparing bash and pash output"
+diff set-diff.bash.out set-diff.pash.out | head
--- evaluation/micro/gnu-parallel/set-diff.sh
+++ evaluation/micro/gnu-parallel/set-diff.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
-# Issue: Deadlocks as s2 is read from more than one parallel copy
-# mkfifo s1 s2
-# TEMP_C1="/tmp/{/}.out1"
-# TEMP_C2="/tmp/{/}.out2"
-# mkfifo ${TEMP1} ${TEMP2}
-# parallel "cat {} | cut -d ' ' -f 1 | sort > $TEMP_C1" ::: $IN &
-# sort -m ${TEMP1} > s1 &
-# parallel "cat {} | cut -d ' ' -f 1 | sort > $TEMP_C2" ::: $IN &
-# sort -m ${TEMP2} > s2 &
-# cat s1 | parallel -k --pipe --jobs ${JOBS} --block 100K "comm -23 - s2"
-# rm ${TEMP1} ${TEMP2}
-# rm s1 s2
+# Show the set-difference between two streams (i.e., elements in the first that are not in the second).
+# https://stackoverflow.com/questions/2509533/bash-linux-set-difference-between-two-text-files
 
-## Correct
-mkfifo s1
-rm -f s2
-TEMP_C1="/tmp/{/}.out1"
-TEMP_C2="/tmp/{/}.out2"
-mkfifo ${TEMP1} ${TEMP2}
-parallel "cat {} | cut -d ' ' -f 1 | sort > $TEMP_C1" ::: $IN &
-sort -m ${TEMP1} > s1 &
-parallel "cat {} | cut -d ' ' -f 1 | sort > $TEMP_C2" ::: $IN &
-sort -m ${TEMP2} > s2
-cat s1 | parallel -k --pipe --jobs ${JOBS} --block 250M "comm -23 - s2"
-rm ${TEMP1} ${TEMP2}
+IN=${IN:-$PASH_TOP/evaluation/benchmarks/oneliners/input/1G.txt}
+
+mkfifo s1 s2
+
+cat $IN |
+    cut -d ' ' -f 1 |
+    tr [:lower:] [:upper:] |
+    sort > s1 &
+
+cat $IN |
+    cut -d ' ' -f 1 |
+    sort > s2 &
+
+comm -23 s1 s2
+
 rm s1 s2
