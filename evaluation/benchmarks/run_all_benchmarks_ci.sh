2a447281d5388799527492e52252d356d7026e22
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-06-25 11:20:20 +0300

    Comments on script. Added execution results for CLI logging

--- evaluation/benchmarks/run_all_benchmarks_ci.sh
+++ evaluation/benchmarks/run_all_benchmarks_ci.sh
@@ -3,16 +3,23 @@ export PASH_TOP=${PASH_TOP:-$(git rev-parse --show-toplevel --show-superproject-
 ## This script is necessary to ensure that sourcing happens with bash
 source run.seq.sh
 source run.par.sh
-
+# total tests
+total=0
+# number of tests that passed
+passed=0
 compare_outputs(){
-  dir=$1
-  outputs=$(ls $dir | grep "seq" | sed 's/.seq.out$//')
-  for out in $outputs;
-  do
-    seq_output="${dir}/${out}.seq.out"
-    pash_output="${dir}/${out}.par.out"
-    diff -q "$seq_output" "$pash_output"
-  done
+    dir=$1
+    outputs=$(ls $dir | grep "seq" | sed 's/.seq.out$//')
+    for out in $outputs;
+    do
+        seq_output="${dir}/${out}.seq.out"
+        pash_output="${dir}/${out}.par.out"
+        res=$(diff -q "$seq_output" "$pash_output")
+        if [[ "${res}" -eq "" ]]; then
+            passed=$((passed + 1))
+        fi
+        total=$((total + 1))
+    done
 }
 
 #oneliners
@@ -62,26 +69,35 @@ EXEC=()
 rm -f $1/*.res
 # run bash
 b=$($1      | grep -v executing | sed 's\.sh:\bash\g' |  sed 's\,\.\g' | awk '{ print $1 "," $2}')
-#EXEC+=("$b")
-rm -rf output_res
 labels="group,Bash"
 for conf in "${configurations[@]}"; do
     for n_in in "${n_inputs[@]}"; do
+        # on each run, clean all the res files 
         rm -f $1/*.res
-        #echo "|-- Executing with pash --width ${n_in} ${conf}..."
+        # re-export the new config
         export PASH_FLAGS="${conf} -w ${n_in}"
+        # append the new labels for the plot
         labels="${labels},${conf}_${n_in}"
+        # execute the pash with the new config
         p=$($1_pash | grep -v executing | sed 's\.sh:\pash\g' |  sed 's\,\.\g' | awk '{ print $1 "," $2}' | awk '{sub(/[^,]*/,"");sub(/,/,"")} 1')
+        # store the results
         EXEC+=("$p")
     done
 done
+# concat all the results and merge them to create the final data for plotting
 labels=$(echo $labels | sed 's\--\\g' | sed -e 's/ /_/g')
 res="$b"
 for i in "${EXEC[@]}"
 do
     res=$(paste -d'@' <(echo "$res") <(echo "$i"))
 done
+# write the labels to the file
 echo "$labels" > results.time
+# write the data formatted
 echo -e "$res" | sed 's\@\,\g' >> results.time
+# compare the results
 compare_outputs "$1/outputs"
+# this is going to be written on the UI output log
 cat results.time
+# this is going to be written on the UI output log / CLI output
+echo "Summary: ${passed}/${total} tests passed."
