0697680e61fb94cb3dc5485323bdc87c318a94dc
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-06-27 12:22:43 -0400

    Add a test with spell-grep-f (#267)

---
+++ evaluation/tests/spell-grep.sh
@@ -0,0 +1,17 @@
+set_diff()
+{
+    grep -vx -f $1 -
+}
+
+dict=$PASH_TOP/evaluation/tests/input/sorted_words
+IN=$PASH_TOP/evaluation/tests/input/10M.txt
+
+cat $IN |
+    # groff -t -e -mandoc -Tascii |  # remove formatting commands
+    col -bx |                      # remove backspaces / linefeeds
+    tr -cs A-Za-z '\n' |
+    tr A-Z a-z |                   # map upper to lower case
+    tr -d '[:punct:]' |            # remove punctuation
+    sort |                         # put words in alphabetical order
+    uniq |                         # remove duplicate words
+    set_diff $dict                 # report words not in dictionary 
