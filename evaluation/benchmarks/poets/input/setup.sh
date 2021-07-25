7e6e522d8ae07b1d40a4a8e9a4f1567124d39c51
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-07-25 12:57:06 +0300

    Fixed curl not downloading genesis input file
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>

--- evaluation/benchmarks/poets/input/setup.sh
+++ evaluation/benchmarks/poets/input/setup.sh
@@ -3,10 +3,9 @@
 PASH_TOP=${PASH_TOP:-$(git rev-parse --show-toplevel)}
 
 [[ "$1" == "-c" ]] && { rm -rf genesis exodus pg; exit; }
-
 if [ ! -f ./genesis ]; then
-  curl -sf http://www.gutenberg.org/cache/epub/8001/pg8001.txt > genesis
-  "$PASH_TOP/scripts/append_nl_if_not.sh" genesis
+    curl https://www.gutenberg.org/cache/epub/8001/pg8001.txt > genesis
+    "$PASH_TOP/scripts/append_nl_if_not.sh" genesis
 fi 
 
 if [ ! -f ./exodus ]; then
