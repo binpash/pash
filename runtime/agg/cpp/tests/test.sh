2c9c253ba2cc15c30d0ff91521933d458e5928d4
Jan Bielak <janekb04@icloud.com>
2021-07-25 12:28:33 +0200

    Move aggregator binaries to dedicated directory
    Signed-off-by: Jan Bielak <janekb04@icloud.com>
    Signed-off-by: Jan Bielak <contact@janbielak.com>

--- runtime/agg/cpp/tests/test.sh
+++ runtime/agg/cpp/tests/test.sh
@@ -1,27 +1,27 @@
-./test-common.sh tr "'[a-z]' '\n'" ../tr
-./test-common.sh tr "A-Z a-z" ../tr
-./test-common.sh tr "-c '[A-Z]' '\n'" ../tr
-./test-common.sh tr "--complement '[1-9]\n*' '[a-z][A-Z]" ../tr
-./test-common.sh tr "--complement -t '[1-9]\n*' '[a-z][A-Z]" ../tr
-./test-common.sh tr "-d '\n'" ../tr
-./test-common.sh tr "-tcd '[1-9][a-z][A-Z]\n'" ../tr
+./test-common.sh tr "'[a-z]' '\n'" ../bin/tr
+./test-common.sh tr "A-Z a-z" ../bin/tr
+./test-common.sh tr "-c '[A-Z]' '\n'" ../bin/tr
+./test-common.sh tr "--complement '[1-9]\n*' '[a-z][A-Z]" ../bin/tr
+./test-common.sh tr "--complement -t '[1-9]\n*' '[a-z][A-Z]" ../bin/tr
+./test-common.sh tr "-d '\n'" ../bin/tr
+./test-common.sh tr "-tcd '[1-9][a-z][A-Z]\n'" ../bin/tr
 
-./test-common.sh wc "" ../wc
-./test-common.sh wc "-l" ../wc
-./test-common.sh wc "-w" ../wc
-./test-common.sh wc "-c" ../wc
-./test-common.sh wc "-m" ../wc
-./test-common.sh wc "-L" ../wc
-./test-common.sh wc "-lcm" ../wc
-./test-common.sh wc "-mlw" ../wc
-./test-common.sh wc "-mLc" ../wc
-./test-common.sh wc "-L -mc -w" ../wc
-./test-common.sh wc "--bytes -c --chars -L" ../wc
-./test-common.sh wc "-L --lines --words" ../wc
+./test-common.sh wc "" ../bin/wc
+./test-common.sh wc "-l" ../bin/wc
+./test-common.sh wc "-w" ../bin/wc
+./test-common.sh wc "-c" ../bin/wc
+./test-common.sh wc "-m" ../bin/wc
+./test-common.sh wc "-L" ../bin/wc
+./test-common.sh wc "-lcm" ../bin/wc
+./test-common.sh wc "-mlw" ../bin/wc
+./test-common.sh wc "-mLc" ../bin/wc
+./test-common.sh wc "-L -mc -w" ../bin/wc
+./test-common.sh wc "--bytes -c --chars -L" ../bin/wc
+./test-common.sh wc "-L --lines --words" ../bin/wc
 
-./test-common.sh uniq "" ../uniq
-./test-common.sh uniq "-c" ../uniq 
-./test-common.sh uniq "--count" ../uniq
+./test-common.sh uniq "" ../bin/uniq
+./test-common.sh uniq "-c" ../bin/uniq 
+./test-common.sh uniq "--count" ../bin/uniq
 
 # These tests are run during PASH_TOP/scripts/run_tests.sh
 # Make sure to build the aggregators using PASH_TOP/scripts/setup-pash.sh first
