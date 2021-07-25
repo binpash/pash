945f1d793c560685a5d04f1dcefe590efc4aa635
Dimitris Karnikis <dkarnikis@gmail.com>
2021-07-25 11:28:48 +0300

    Fix poets/8.2_2.sh  (#293)
    First calculate all the bigrams (4_3.sh script) and based on the generated output calculate all the bigrams that appear twice
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>

--- evaluation/benchmarks/poets/8.2_2.sh
+++ evaluation/benchmarks/poets/8.2_2.sh
@@ -2,6 +2,11 @@
 # tag: bigrams_appear_twice.sh
 # set -e
 
-# Bigrams that appear twice
+# Calculate the bigrams (based on 4_3.sh script)
 IN=${IN:-$PASH_TOP/evaluation/benchmarks/poets/input/pg/}
-ls ${IN} | sed "s;^;$IN;"| xargs cat | awk "\$1 == 2 {print \$2, \$3}" 
+OUT=${OUT:-$PASH_TOP/evaluation/benchmarks/poets/input/output/}
+ls ${IN} | sed "s;^;$IN;"| xargs cat | tr -sc '[A-Z][a-z]' '[\012*]' > ${OUT}.input.words
+tail +2 ${OUT}.input.words > ${OUT}.input.nextwords
+paste ${OUT}.input.words ${OUT}.input.nextwords | sort | uniq -c > ${OUT}.input.bigrams
+# find the bigrams that appear exactly twice 
+awk "\$1 == 2 {print \$2, \$3}" ${OUT}.input.bigrams
