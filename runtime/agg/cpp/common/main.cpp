710601b7e40baf26d79cbf5b3058c9274282da3b
Jan Bielak <contact@janbielak.com>
2021-07-27 13:09:23 +0200

    Remove output file argument from aggregators
    Signed-off-by: Jan Bielak <contact@janbielak.com>

--- runtime/agg/cpp/common/main.cpp
+++ runtime/agg/cpp/common/main.cpp
@@ -42,7 +42,6 @@ std::pair<int, char**> parse_options(int argc, char** argv, const opt_holder& op
 // Read by functions below it
 std::ifstream g_in1;
 std::ifstream g_in2;
-std::ofstream g_out;
 int g_argc;
 char** g_argv;
 
@@ -50,15 +49,14 @@ int main(int _argc, char** _argv)
 {
 	std::ios_base::sync_with_stdio(false);
 
-	if (_argc < 4)
+	if (_argc < 3)
 	{
-		std::cerr << "Usage: " << _argv[0] << " input1 input2 output [OPTION]...\n";
+		std::cerr << "Usage: " << _argv[0] << " input1 input2 [OPTION]...\n";
 		return EXIT_FAILURE;
 	}
 
 	g_in1.open(_argv[1], std::ios_base::in  | std::ios_base::binary);
 	g_in2.open(_argv[2], std::ios_base::in  | std::ios_base::binary);
-	g_out.open(_argv[3], std::ios_base::out | std::ios_base::binary);
 
 	if (g_in1.peek() == EOF)
 	{
@@ -71,7 +69,7 @@ int main(int _argc, char** _argv)
 		return EXIT_SUCCESS;
 	}
 
-	std::tie(g_argc, g_argv) = parse_options(_argc - 3, _argv + 3, g_options);
+	std::tie(g_argc, g_argv) = parse_options(_argc - 2, _argv + 2, g_options);
 
 	aggregate();
 
@@ -97,5 +95,5 @@ int main(int _argc, char** _argv)
 }
 [[nodiscard]] std::ostream& output() noexcept
 {
-	return g_out;
+	return std::cout;
 }
