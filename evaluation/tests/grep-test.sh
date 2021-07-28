46bb9da11b6371c1f51f45a816aad6ae40b2682e
Nikos Vasilakis <nikos@vasilak.is>
2021-07-28 16:57:53 +0300

    Add little custom aggregator tutorial (#300)
    * Add little custom aggregator tutorial
    * minor edits
    * Update tutorial
    * Point to files rather than copy
    * Rename "non-parallelizable pure" to "pure"
    * Correct annotations
    * Only point to files, not copy
    * Update path
    
    Signed-off-by: Nikos Vasilakis <nikos@vasilak.is>
    Co-authored-by: Konstantinos Kallas <konstantinos.kallas@hotmail.com>

--- evaluation/tests/grep-test.sh
+++ evaluation/tests/grep-test.sh
@@ -2,7 +2,7 @@
 
 ## This test contains all occurences of tr (to test the annotation)
 
-FILE="../evaluation/tests/input/1M.txt"
+FILE="$PASH_TOP/evaluation/tests/input/1M.txt"
 
 cat $FILE | grep "the"
 cat $FILE | grep -c "the"
