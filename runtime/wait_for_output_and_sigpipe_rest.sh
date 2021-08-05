5463c2f4c27562d1ab2749cea57686b476b5f6be
Dimitris Karnikis <dkarnikis@gmail.com>
2021-08-05 21:03:12 +0800

    Fixed ps command. Not quite sure if this is the best solution
    Signed-off-by: Dimitris Karnikis <dkarnikis@gmail.com>

--- runtime/wait_for_output_and_sigpipe_rest.sh
+++ runtime/wait_for_output_and_sigpipe_rest.sh
@@ -3,8 +3,25 @@
 ## TODO: Give it the output pid as an argument
 
 wait $@
-pids_to_kill="$(ps --ppid $$ |awk '{print $1}' | grep -E '[0-9]')"
+if type lsb_release >/dev/null 2>&1 ; then
+    distro=$(lsb_release -i -s)
+elif [ -e /etc/os-release ] ; then
+    distro=$(awk -F= '$1 == "ID" {print $2}' /etc/os-release)
+fi
+
+# convert to lowercase
+distro=$(printf '%s\n' "$distro" | LC_ALL=C tr '[:upper:]' '[:lower:]')
+# now do different things depending on distro
+case "$distro" in
+    freebsd*)  
+        # not sure at all about this one
+        pids_to_kill="$(ps -efl $$ |awk '{print $1}' | grep -E '[0-9]')"
+        ;;
+    *)
+        pids_to_kill="$(ps --ppid $$ |awk '{print $1}' | grep -E '[0-9]')"
+        ;;
+esac
 for pid in $pids_to_kill
 do
-        (> /dev/null 2>&1 kill -SIGPIPE $pid || true)
+    (> /dev/null 2>&1 kill -SIGPIPE $pid || true)
 done
