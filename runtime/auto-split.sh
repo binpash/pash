b71323b4c89f1a26319a9a4c61fc7d1d33ae4c83
Dimitris Karnikis <dkarnikis@gmail.com>
2021-08-05 21:02:42 +0800

    mktemp hacky fixes
    Signed-off-by: Dimitris Karnikis <dkarnikis@gmail.com>

--- runtime/auto-split.sh
+++ runtime/auto-split.sh
@@ -8,7 +8,24 @@ n_outputs="$#"
 # Set a default DISH_TOP in this directory if it doesn't exist
 PASH_TOP=${PASH_TOP:-$(git rev-parse --show-toplevel)}
 
-temp="$(mktemp --tmpdir -u pash_XXXXXXXXXX)"
+if type lsb_release >/dev/null 2>&1 ; then
+   distro=$(lsb_release -i -s)
+elif [ -e /etc/os-release ] ; then
+   distro=$(awk -F= '$1 == "ID" {print $2}' /etc/os-release)
+fi
+
+# convert to lowercase
+distro=$(printf '%s\n' "$distro" | LC_ALL=C tr '[:upper:]' '[:lower:]')
+# now do different things depending on distro
+case "$distro" in
+   freebsd*)  
+    temp="$(TMPDIR=/tmp mktemp -u pash_XXXXXXXXXX)"
+    ;;
+    *)
+    temp="$(mktemp --tmpdir -u pash_XXXXXXXXXX)"
+    ;;
+esac
+
 
 cat "$input" > "$temp"
 total_lines=$(wc -l $temp | cut -f 1 -d ' ')
