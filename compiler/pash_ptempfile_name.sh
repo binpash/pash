93b05e8249755afbe841ec19014e4db687979bbd
Dimitris Karnikis <dkarnikis@gmail.com>
2021-08-05 21:00:14 +0800

    Hacky fix to mktemp for pash tmp files. Someone could fix it better
    Signed-off-by: Dimitris Karnikis <dkarnikis@gmail.com>

--- compiler/pash_ptempfile_name.sh
+++ compiler/pash_ptempfile_name.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/env bash
+if type lsb_release >/dev/null 2>&1 ; then
+    distro=$(lsb_release -i -s)
+elif [ -e /etc/os-release ] ; then
+    distro=$(awk -F= '$1 == "ID" {print $2}' /etc/os-release)
+fi
 
-echo "$(mktemp --tmpdir="$PASH_TMP_PREFIX" -u pash_XXXXXXXXXX)"
+# convert to lowercase
+distro=$(printf '%s\n' "$distro" | LC_ALL=C tr '[:upper:]' '[:lower:]')
+# now do different things depending on distro
+case "$distro" in
+    freebsd*)  
+        # bsd is so fun :)
+        mkdir -p "$PASH_TMP_PREFIX"
+        tmp=$(mktemp -u pash_XXXXXXXXXX)
+        mv $tmp "$PASH_TMP_PREFIX"
+        echo "${PASH_TMP_PREFIX}/${tmp}"
+        ;;
+    *)
+        echo "$(mktemp --tmpdir="$PASH_TMP_PREFIX" -u pash_XXXXXXXXXX)"
+        ;;
+esac
