c013e72ee9ccac34da0b9b1066e9c5d4a593925d
Jan Bielak <contact@janbielak.com>
2021-08-12 20:59:09 +0200

    Detect failing base command in aggregator test script
    Signed-off-by: Jan Bielak <contact@janbielak.com>

--- runtime/agg/cpp/tests/test-common.sh
+++ runtime/agg/cpp/tests/test-common.sh
@@ -2,16 +2,22 @@ CMD="$1"
 FLG="$2"
 AGG="$3"
 
+cat $IN1 $IN2 | $CMD $FLG > reference
+
+if [ $? -ne 0 ]; then
+    cat "Base $CMD invocation failed ...FAIL"
+    exit 1
+fi
+
 cat $IN1 | $CMD $FLG > partial1
 cat $IN2 | $CMD $FLG > partial2
-cat $IN1 $IN2 | $CMD $FLG > reference
 
 $AGG partial1 partial2 $FLG > aggregated
 
 diff aggregated reference > log
 if [ $? -ne 0 ]; then
     cat log | head
-    echo $CMD "$FLG ...fail"
+    echo $CMD "$FLG ...FAIL"
 else
     echo $CMD "$FLG ...pass"
 fi
