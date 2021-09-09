bcfc1e144c2acb02e0749204a2c46da4ebfd960a
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-09-10 02:03:07 +0300

    Fix an issue with `set` (#317)
    * Test that checks set issue
    
    Signed-off-by: Konstantinos Kallas <konstantinos.kallas@hotmail.com>
    
    * Fix the set issue by never setting/unsetting c
    
    Signed-off-by: Konstantinos Kallas <konstantinos.kallas@hotmail.com>

--- compiler/pash_set_from_to.sh
+++ compiler/pash_set_from_to.sh
@@ -3,10 +3,10 @@
 from_set=${1?From set not given}
 to_set=${2?To set not given}
 
-## Finds the difference of set variables
-pash_set_to_add="$(comm -23 <(echo $to_set | sed -E 's/(.)/\1\n/g' | sort) <(echo $from_set | sed -E 's/(.)/\1\n/g' | sort))"
-pash_set_to_remove="$(comm -13 <(echo $to_set | sed -E 's/(.)/\1\n/g' | sort) <(echo $from_set | sed -E 's/(.)/\1\n/g' | sort))"
-# pash_redir_output echo "To add: $pash_set_to_add"
-# pash_redir_output echo "To remove: $pash_set_to_remove"
+## Finds the difference of set variables (removing the c one since it cannot be actually set and unset)
+pash_set_to_add="$(comm -23 <(echo $to_set | sed -E 's/(.)/\1\n/g' | sort) <(echo $from_set | sed -E 's/(.)/\1\n/g' | sort) | grep -v 'c' )"
+pash_set_to_remove="$(comm -13 <(echo $to_set | sed -E 's/(.)/\1\n/g' | sort) <(echo $from_set | sed -E 's/(.)/\1\n/g' | sort) | grep -v 'c' )"
+pash_redir_output echo "To add: $pash_set_to_add"
+pash_redir_output echo "To remove: $pash_set_to_remove"
 pash_redir_all_output set "-$pash_set_to_add"
 pash_redir_all_output set "+$pash_set_to_remove"
