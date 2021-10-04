f0ccd834859de55003697f4299540b3817aca80e
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-10-04 16:58:02 -0400

    Pull things that happen in the runtime to happen once in pa.sh
    Signed-off-by: Konstantinos Kallas <konstantinos.kallas@hotmail.com>

---
+++ compiler/pash_init_setup.sh
@@ -0,0 +1,184 @@
+## Get distro
+## TODO: Move that somewhere where it happens once (during installation)
+if type lsb_release >/dev/null 2>&1 ; then
+    distro=$(lsb_release -i -s)
+elif [ -e /etc/os-release ] ; then
+    distro=$(awk -F= '$1 == "ID" {print $2}' /etc/os-release)
+fi
+
+# convert to lowercase
+export distro=$(printf '%s\n' "$distro" | LC_ALL=C tr '[:upper:]' '[:lower:]')
+
+## File directory
+export RUNTIME_DIR=$(dirname "${BASH_SOURCE[0]}")
+export PASH_REDIR="&2"
+export PASH_DEBUG_LEVEL=0
+
+## Check flags
+export pash_output_time_flag=1
+export pash_execute_flag=1
+export pash_speculation_flag=0 # By default there is no speculation
+export pash_dry_run_compiler_flag=0
+export pash_assert_compiler_success_flag=0
+export pash_checking_speculation=0
+export pash_checking_log_file=0
+export pash_checking_debug_level=0
+export pash_avoid_pash_runtime_completion_flag=0
+for item in $@
+do
+    if [ "$pash_checking_speculation" -eq 1 ]; then
+        export pash_checking_speculation=0
+        if [ "no_spec" == "$item" ]; then
+            export pash_speculation_flag=0
+        elif [ "quick_abort" == "$item" ]; then
+            ## TODO: Fix how speculation interacts with dry_run, assert_compiler_success
+            export pash_speculation_flag=1
+        else
+            pash_redir_output echo "$$: Unknown value for option --speculation"
+            exit 1
+        fi
+    fi
+
+    if [ "$pash_checking_log_file" -eq 1 ]; then
+        export pash_checking_log_file=0
+        export PASH_REDIR="$item"
+    fi
+
+    if [ "$pash_checking_debug_level" -eq 1 ]; then
+        export pash_checking_debug_level=0
+        pash_redir_output echo "$$: $item"
+        export PASH_DEBUG_LEVEL=$item
+    fi
+
+    # We output time always 
+    # if [ "--output_time" == "$item" ]; then
+    #     pash_output_time_flag=1
+    # fi
+
+    if [ "--dry_run_compiler" == "$item" ]; then
+        export pash_dry_run_compiler_flag=1
+    fi
+
+    if [ "--assert_compiler_success" == "$item" ]; then
+        export pash_assert_compiler_success_flag=1
+    fi
+
+    if [ "--speculation" == "$item" ]; then
+        pash_checking_speculation=1
+    fi
+
+    if [ "--log_file" == "$item" ]; then
+        pash_checking_log_file=1
+    fi
+
+    if [ "--avoid_pash_runtime_completion" == "$item" ]; then
+        export pash_avoid_pash_runtime_completion_flag=1
+    fi
+
+    if [ "-d" == "$item" ] || [ "--debug" == "$item" ]; then
+        pash_checking_debug_level=1
+        pash_redir_output echo "$$: $item"
+    fi
+done
+
+## `pash_redir_output` and `pash_redir_all_output` are strictly for logging.
+##
+## They do not execute their arguments if there is no debugging.
+if [ "$PASH_DEBUG_LEVEL" -eq 0 ]; then
+    pash_redir_output()
+    {
+        :
+    }
+
+    pash_redir_all_output()
+    {
+        :
+    }
+
+    pash_redir_all_output_always_execute()
+    {
+        > /dev/null 2>&1 $@
+    }
+
+else
+    if [ "$PASH_REDIR" == '&2' ]; then
+        pash_redir_output()
+        {
+            >&2 $@
+        }
+
+        pash_redir_all_output()
+        {
+            >&2 $@
+        }
+
+        pash_redir_all_output_always_execute()
+        {
+            >&2 $@
+        }
+    else
+        pash_redir_output()
+        {
+            >>"$PASH_REDIR" $@
+        }
+
+        pash_redir_all_output()
+        {
+            >>"$PASH_REDIR" 2>&1 $@
+        }
+
+        pash_redir_all_output_always_execute()
+        {
+            >>"$PASH_REDIR" 2>&1 $@
+        }
+    fi
+fi
+
+## Even though this is strictly in (2) it happens here
+## because it is necessary for performing the rest of (1)
+##
+## These two functions are strictly for logging since when not debugging they do not execute their commands
+# pash_redir_output()
+# {
+#     ## TODO: We should properly allow for different debug levels
+#     ## TODO: This code is copied in pash_source_declare_vars.sh
+#     if [ "$PASH_DEBUG_LEVEL" -eq 0 ]; then
+#         :
+#     else
+#         if [ "$PASH_REDIR" == '&2' ]; then
+#             >&2 $@
+#         else
+#             >>"$PASH_REDIR" $@
+#         fi
+#     fi
+# }
+
+# pash_redir_all_output()
+# {
+#     if [ "$PASH_DEBUG_LEVEL" -eq 0 ]; then
+#         :
+#     else
+#         if [ "$PASH_REDIR" == '&2' ]; then
+#             >&2 $@
+#         else
+#             >>"$PASH_REDIR" 2>&1 $@
+#         fi
+#     fi
+# }
+
+# pash_redir_all_output_always_execute()
+# {
+#     if [ "$PASH_DEBUG_LEVEL" -eq 0 ]; then
+#         > /dev/null 2>&1 $@
+#     else
+#         if [ "$PASH_REDIR" == '&2' ]; then
+#             >&2 $@
+#         else
+#             >>"$PASH_REDIR" 2>&1 $@
+#         fi
+#     fi
+# }
+
+export -f pash_redir_output
+export -f pash_redir_all_output
+export -f pash_redir_all_output_always_execute
