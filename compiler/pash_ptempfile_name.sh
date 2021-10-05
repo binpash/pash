415aa3effec0c70df8956903b9ae49ff77ccd0e1
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-10-05 18:29:20 +0300

    Switched to dry run flag
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>

--- compiler/pash_ptempfile_name.sh
+++ compiler/pash_ptempfile_name.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
 distro=${1??Distro not given}
-# now do different things depending on distro
-tmp_file=$(TMPDIR=$PASH_TMP_PREFIX mktemp -t pash_XXXXXXXXXX)
-echo "${tmp_file}"
+TMPDIR=$PASH_TMP_PREFIX mktemp -u -t pash_XXXXXXXXXX
