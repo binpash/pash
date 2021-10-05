0b5e1e74133e151816c2c1ff53800f81d55118c5
Jan Bielak <contact@janbielak.com>
2021-10-05 19:22:12 +0200

    Fix sort aggregator
    Signed-off-by: Jan Bielak <contact@janbielak.com>

--- runtime/agg/cpp/aggregators/sort/agg-bsd.h
+++ runtime/agg/cpp/aggregators/sort/agg-bsd.h
@@ -19,38 +19,43 @@ bool compare(const std::string& str1, const std::string& str2)
 void aggregate() noexcept
 {
     std::string input1_top, input2_top;
-    bool refill1 = true, refill2 = true;
+    bool invalid1 = true, invalid2 = true;
     while (true)
     {
-        if (refill1)
+        if (invalid1)
         {
             std::getline(input1(), input1_top);
             if (!input1())
                 break;
-            refill1 = false;
+            invalid1 = false;
         }
-        if (refill2)
+        if (invalid2)
         {
             std::getline(input2(), input2_top);
             if (!input2())
                 break;
-            refill2 = false;
+            invalid2 = false;
         }
 
         if (compare(input1_top, input2_top)) // input1_top < input2_top
         {
             output() << input1_top << '\n';
-            refill1 = true;
+            invalid1 = true;
         }
         else
         {
             output() << input2_top << '\n';
-            refill2 = true;
+            invalid2 = true;
         }
     }
 
-    // At this point exactly one of the inputs is empty
-    // and the remaining input from the second one will get outputted
+    if(!invalid1)
+        output() << input1_top << '\n';
+    if(!invalid2)
+        output() << input2_top << '\n';
+
+    // At this point at least one of the inputs
+    // is empty, so the other one can sefely be forwarded
     output() << input1().rdbuf() << input2().rdbuf();
 }
 
