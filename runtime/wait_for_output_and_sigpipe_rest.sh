0a0c3f16c4516c2f5019639f8a194206b7ff19d1
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-10-07 13:14:45 -0400

    Fix wait to kill correct pids
    Signed-off-by: Konstantinos Kallas <konstantinos.kallas@hotmail.com>

--- runtime/wait_for_output_and_sigpipe_rest.sh
+++ runtime/wait_for_output_and_sigpipe_rest.sh
@@ -5,17 +5,22 @@ wait $@
 
 ## It is assumed that $distro is set when this is called.
 
+## Note: We need the || true after the grep so that it doesn't exit with error if it finds nothing.
+
 # now do different things depending on distro
 case "$distro" in
     freebsd*)  
         # not sure at all about this one
-        pids_to_kill="$(ps -efl $$ |awk '{print $1}' | grep -E '[0-9]')"
+        pids_to_kill="$(ps -efl $BASHPID |awk '{print $1}' | { grep -E '[0-9]' || true; } )"
         ;;
     *)
-        pids_to_kill="$(ps --ppid $$ |awk '{print $1}' | grep -E '[0-9]')"
+        pids_to_kill="$(ps --ppid $BASHPID |awk '{print $1}' | { grep -E '[0-9]' || true; } )"
         ;;
 esac
+
 for pid in $pids_to_kill
 do
     (> /dev/null 2>&1 kill -SIGPIPE $pid || true)
 done
+## Need to wait to not leave any zombies
+wait
