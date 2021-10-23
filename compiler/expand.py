023e607d86a219753b09273aee46f62a06660fee
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-10-23 18:45:37 -0400

    Fix a mini-bug
    Signed-off-by: Konstantinos Kallas <konstantinos.kallas@hotmail.com>

--- compiler/expand.py
+++ compiler/expand.py
@@ -282,7 +282,8 @@ def lookup_variable_inner(varname):
     if config.pash_args.expand_using_bash_mirror:
         return config.query_expand_variable_bash_mirror(varname)
     else:
-        return config['shell_variables'][varname]
+        _type, value = config.config['shell_variables'][varname]
+        return value
 
 def invalidate_variable(var, reason, config):
     config['shell_variables'][var] = [None, InvalidVariable(var, reason)]
