bb57c29c7d30a3e954eb731fe6d61123335054e4
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-11-12 11:50:26 -0500

    Revert two commits

--- evaluation/tests/interface_tests/cat-redir-fail.sh
+++
@@ -1,1 +0,0 @@
-cat < no.such.file
--- evaluation/tests/interface_tests/run.sh
+++ evaluation/tests/interface_tests/run.sh
@@ -255,12 +255,6 @@ test_set-dash()
     grep 'echo hello' set-dash-v-x.err | wc -l
 }
 
-test_cat_redir_fail()
-{
-    local shell=$1
-    $shell cat-redir-fail.sh
-}
-
 ## We run all tests composed with && to exit on the first that fails
 if [ "$#" -eq 0 ]; then
     run_test test1
@@ -294,7 +288,6 @@ if [ "$#" -eq 0 ]; then
     run_test test_cat_hyphen
     run_test test_trap
     run_test test_set-dash
-    run_test test_cat_redir_fail
 else
     for testname in $@
     do
