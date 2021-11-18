091f75d35a8927b2c9df2cca72422a60d0c0fc8a
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-11-18 15:54:04 -0500

    Add a check that disables parallel_pipelines of the last ast object since its exit code might be needed
    Signed-off-by: Konstantinos Kallas <konstantinos.kallas@hotmail.com>

--- compiler/ast_util.py
+++ compiler/ast_util.py
@@ -2,12 +2,13 @@ from definitions.ast_node import *
 
 ## This class is used by the preprocessor in ast_to_ir
 class PreprocessedAST:
-    def __init__(self, ast, replace_whole, non_maximal, something_replaced=True):
+    def __init__(self, ast, replace_whole, non_maximal, something_replaced=True, last_ast=False):
         assert(isinstance(ast, AstNode))
         self.ast = ast
         self.replace_whole = replace_whole
         self.non_maximal = non_maximal
         self.something_replaced = something_replaced
+        self.last_ast = last_ast
 
     def should_replace_whole_ast(self):
         return self.replace_whole
@@ -17,6 +18,9 @@ class PreprocessedAST:
     
     def will_anything_be_replaced(self):
         return self.something_replaced
+
+    def is_last_ast(self):
+        return self.last_ast
 
 ## This class represents text that was not modified at all by preprocessing, and therefore does not
 ## need to be unparsed.
