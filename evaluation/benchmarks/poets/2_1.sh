3182e4da85b7db7f9f6e71534392338fdf1d6930
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-11-24 15:16:07 -0500

    Alternative syntax for poets
    Signed-off-by: Konstantinos Kallas <konstantinos.kallas@hotmail.com>

--- evaluation/benchmarks/poets/2_1.sh
+++ evaluation/benchmarks/poets/2_1.sh
@@ -4,4 +4,20 @@
 
 # Merge upper and lower counts
 IN=${IN:-$PASH_TOP/evaluation/benchmarks/poets/input/pg/}
-ls ${IN} | sed "s;^;$IN;"| xargs cat | tr '[a-z]' '[A-Z]' | tr -sc '[A-Z]' '[\012*]' | sort | uniq -c
+# ls ${IN} | sed "s;^;$IN;"| xargs cat | tr '[a-z]' '[A-Z]' | tr -sc '[A-Z]' '[\012*]' | sort | uniq -c
+
+OUT=${OUT:-$PASH_TOP/evaluation/benchmarks/poets/output/2_1/}
+mkdir -p "$OUT"
+
+for input in $(ls ${IN} | sed "s;^;$IN;")
+do
+    cat "$input" | tr '[a-z]' '[A-Z]' | tr -sc '[A-Z]' '[\012*]' | sort | uniq -c > "${OUT}/${input}"
+done
+
+for output in $(ls ${OUT} | sed "s;^;$OUT;")
+do
+    cat $output
+done
+
+# Potentially rm -rf OUT
+# rm 
