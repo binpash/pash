2f11ebf795c5e556c2b81f4102ba3f06ae231acd
dkarnikis <dkarnikis@gmail.com>
2021-11-24 09:06:20 -0500

    cleanup on for-loop benchmarks
    Signed-off-by: dkarnikis <dkarnikis@gmail.com>

--- evaluation/benchmarks/for-loops/compress_files.sh
+++ evaluation/benchmarks/for-loops/compress_files.sh
@@ -1,18 +1,20 @@
+#!/bin/bash
 # compress all files in a directory
-
-IN=${IN:-./pcap/pcaps}
-OUT=${OUT:-./out}
-mkdir -p out
+IN=${IN:-$PASH_TOP/evaluation/benchmarks/for-loops/input/pcaps/}
+OUT=${OUT:-$PASH_TOP/evaluation/benchmarks/for-loops/output/compress}
+LOGS=${OUT}/logs
+mkdir -p ${OUT}/logs
 run_tests() {
-        tar -czf $OUT/$(basename $1).tar.gz $1
+    echo ${OUT}
+    name=$(basename $1).zip
+    zip -r ${OUT}/$name $1
 }
 
 export -f run_tests
+
 pkg_count=0
 for item in ${IN}/*;
 do
     pkg_count=$((pkg_count + 1));
-    run_tests $item > logs/$pkg_count.log
+    run_tests $item > ${LOGS}/$pkg_count.log
 done
-
-echo "done"
--- evaluation/benchmarks/for-loops/encrypt_files.sh
+++ evaluation/benchmarks/for-loops/encrypt_files.sh
@@ -1,18 +1,18 @@
+#!/bin/bash
 # encrypt all files in a directory 
-
-IN=${IN:-./pcap/pcaps}
-OUT=${OUT:-./out}
-rm -rf logs_enc
-mkdir -p logs_enc
+IN=${IN:-$PASH_TOP/evaluation/benchmarks/for-loops/input/pcap_data}
+OUT=${OUT:-$PASH_TOP/evaluation/benchmarks/for-loops/output/encrypt}
+LOGS=${OUT}/logs
+mkdir -p ${OUT} ${LOGS}
 run_tests() {
         echo 'key' | openssl enc -aes-256-cbc -e -md sha512 -in $item -out $OUT/$(basename $1).enc --pass stdin 
 }
+
 export -f run_tests
 pkg_count=0
+
 for item in ${IN}/*;
 do
     pkg_count=$((pkg_count + 1));
-    run_tests $item  > logs_enc/${pkg_count}.log
+    run_tests $item  > ${LOGS}/${pkg_count}.log
 done
-
-echo "done"
