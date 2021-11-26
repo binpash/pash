78b46467572d96028a661f9422553b86fba49993
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-11-26 08:57:47 -0500

    Fixes
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>

--- evaluation/benchmarks/for-loops/pcap.sh
+++ evaluation/benchmarks/for-loops/pcap.sh
@@ -6,11 +6,11 @@ LOGS=${OUT}/logs
 mkdir -p ${LOGS}
 run_tests() {
     INPUT=$1
-    /usr/sbin/tcpdump -nn -r ${INPUT} -A 'port 53'| sort | uniq |grep -Ev '(com|net|org|gov|mil|arpa)'
+    /usr/sbin/tcpdump -nn -r ${INPUT} -A 'port 53'| sort | uniq |grep -Ev '(com|net|org|gov|mil|arpa)' 2> /dev/null
     # extract URL
-    /usr/sbin/tcpdump -nn -r ${INPUT} -s 0 -v -n -l | egrep -i "POST /|GET /|Host:"
+    /usr/sbin/tcpdump -nn -r ${INPUT} -s 0 -v -n -l | egrep -i "POST /|GET /|Host:" 2> /dev/null
     # extract passwords
-    /usr/sbin/tcpdump -nn -r ${INPUT} -s 0 -A -n -l | egrep -i "POST /|pwd=|passwd=|password=|Host:"
+    /usr/sbin/tcpdump -nn -r ${INPUT} -s 0 -A -n -l | egrep -i "POST /|pwd=|passwd=|password=|Host:" 2> /dev/null
 }
 export -f run_tests
 
@@ -21,7 +21,5 @@ do
     pkg_count=$((pkg_count + 1));
     run_tests $item > ${LOGS}/${pkg_count}.log
 done
-
-
 
 echo 'done';
