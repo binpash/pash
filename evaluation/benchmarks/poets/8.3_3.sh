6ca88ad8d352351365fcb670ac7ef9f922079acd
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-12-01 11:22:40 -0500

    fixes to poets
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>

--- evaluation/benchmarks/poets/8.3_3.sh
+++ evaluation/benchmarks/poets/8.3_3.sh
@@ -11,14 +11,12 @@ INPUT2=${INPUT2:-$PASH_TOP/evaluation/benchmarks/poets/input/exodus}
 OUT=${OUT:-$PASH_TOP/evaluation/benchmarks/poets/output/8.3_3/}
 mkdir -p $OUT
 ENTRIES=${ENTRIES:-1000}
-
 run_tests() {
-    input=$1
     cat $IN/$input | tr -sc '[A-Z][a-z]' '[\012*]' | sort -u > ${OUT}/${input}1.types
     tr -sc '[A-Z][a-z]' '[\012*]' < ${INPUT2} | sort -u > ${OUT}/${input}2.types
-    sort $OUT/${input}1.types ${OUT}/${input}2.types ${OUT}/${input}2.types | uniq -c | head
-}
+    sort $OUT/${input}1.types ${OUT}/${input}2.types ${OUT}/${input}2.types | uniq -c | head 
 
+}
 export -f run_tests
 for input in $(ls ${IN} | head -n ${ENTRIES})
 do
