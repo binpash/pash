ac74fd8ef92efa7ec43bddbeb6dffe86a305c2c0
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2021-12-16 20:18:34 +0200

    Remove old script implementations from comment. Fix anagrams
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>

--- evaluation/benchmarks/poets/8.3_2.sh
+++ evaluation/benchmarks/poets/8.3_2.sh
@@ -3,30 +3,22 @@
 # set -e
 
 IN=${IN:-$PASH_TOP/evaluation/benchmarks/poets/input/pg/}
-## need to generate words
-#ls ${IN} | sed "s;^;$IN;"| xargs cat | tr -sc '[A-Z][a-z]' '[\012*]' > ${OUT}.words
-## need to generate types
-#sort -u ${OUT}.words > ${OUT}.types
-## Actual find anagram script
-#rev < ${OUT}.types > ${OUT}.types.rev
-#sort ${OUT}.types ${OUT}.types.rev | uniq -c | awk "\$1 >= 2 {print \$2}"
-
 OUT=${OUT:-$PASH_TOP/evaluation/benchmarks/poets/output/8.3_2/}
-mkdir -p "$OUT"
 ENTRIES=${ENTRIES:-1000}
-#ls ${IN} | sed "s;^;$IN;"| xargs cat | tr -sc '[A-Z][a-z]' '[\012*]' | tr -sc 'AEIOUaeiou' '[\012*]' | sort | uniq -c | awk "\$1 >= 1000"
+mkdir -p "$OUT"
 
-for input in $(ls ${IN} | head -n ${ENTRIES})
-do
+run_tests() {
+    input=$1
     cat $IN/$input | tr -sc '[A-Z][a-z]' '[\012*]' | sort -u > ${OUT}/${input}.types
     rev < ${OUT}/${input}.types > ${OUT}/${input}.types.rev
-    sort ${OUT}/${input}.types ${OUT}/${input}.types.rev | uniq -c | awk "\$1 >= 2 {print \$2}" > ${OUT}/${input}.out
-done
+    sort ${OUT}/${input}.types ${OUT}/${input}.types.rev | uniq -c | awk "\$1 >= 2 {print \$2}"
+}
 
-#for output in $(ls ${OUT} | sed "s;^;$OUT;")
-#do
-#    cat $output
-#done
+export -f run_tests
+for input in $(ls ${IN} | head -n ${ENTRIES})
+do
+    run_tests $input > ${OUT}/${input}.out
+done
 
 echo 'done';
 rm -rf "${OUT}"
--- evaluation/benchmarks/poets/test.sh
+++
@@ -1,5 +0,0 @@
-for i in {0..1000}
-do
-    echo "Number: $i"
-done
-
