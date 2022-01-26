f6c818d44ef491622abbf1a8c16a30b9eae11bf1
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2022-01-26 11:51:21 -0500

    Add 3 benchmark runs. Removed dgsh_tee flag
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>

--- evaluation/eval_script/run_all.sh
+++ evaluation/eval_script/run_all.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# go to benchmark directory
-RES_FOLDER=${PWD}/eval_results
+RES_FOLDER=${PWD}/eval_results/run
 rm -rf ${RES_FOLDER}
-mkdir -p ${RES_FOLDER}
+# go to benchmark directory
 cd ../benchmarks
+# use the small input for the benchmarks
 setup_flags='--small'
 
+# run all the scripts using bash
 run_bash() {
     ## This script is necessary to ensure that sourcing happens with bash
     source run.seq.sh
@@ -35,6 +36,7 @@ run_bash() {
     done
 }
 
+# run all the scripts using different configurations of pash/blish
 run_bench() {
     ## This script is necessary to ensure that sourcing happens with bash
     source run.par.sh
@@ -69,34 +71,46 @@ run_bench() {
     done
 }
 
-export PASH_ALL_FLAGS=(" "
-                       "--r_split --dgsh_tee --r_split_batch_size 1000000 --parallel_pipelines --profile_driven")
-export PASH_BENCHMARK=("oneliners" "unix50" "analytics-mts" "nlp" "max-temp" "web-index" "dependency_untangling")
-export PASH_MODE=("pash" 
-                  "blish")
+function run_all_benchmarks() {
+  # generate output folder for each run
+  export RES_FOLDER=$1
+  mkdir -p ${RES_FOLDER}
+  export PASH_ALL_FLAGS=(" "
+                         "--r_split --dgsh_tee --r_split_batch_size 1000000 --parallel_pipelines --profile_driven")
+  export PASH_BENCHMARK=("oneliners" "unix50" "analytics-mts" "nlp" "max-temp" "web-index" "dependency_untangling")
+  export PASH_MODE=("pash" 
+                    "blish")
 
-echo 'Running all benchmark for bash'
-time run_bash
+  echo 'Running all benchmark for bash'
+  time run_bash
 
-echo 'Running Blish benchmarks'
-time run_bench 
+  echo 'Running Blish benchmarks'
+  time run_bench 
 
-##### Figure 6
-export PASH_ALL_FLAGS=("--r_split --dgsh_tee --r_split_batch_size 1000000" 
-                       "--r_split --dgsh_tee --r_split_batch_size 1000000 --parallel_pipelines" )
-export PASH_BENCHMARK=("nlp" "max-temp" "dependency_untangling")
-export PASH_MODE=("blish_no_prof_no_du" 
-                  "blish_no_prof")
+  ##### Figure 6
+  export PASH_ALL_FLAGS=("--r_split --dgsh_tee --r_split_batch_size 1000000" 
+                         "--r_split --dgsh_tee --r_split_batch_size 1000000 --parallel_pipelines" )
+  export PASH_BENCHMARK=("nlp" "max-temp" "dependency_untangling")
+  export PASH_MODE=("blish_no_prof_no_du" 
+                    "blish_no_prof")
 
-time run_bench 
+  time run_bench 
 
-##### Figure 7
-export PASH_ALL_FLAGS=(
-"--dgsh_tee --parallel_pipelines --profile_driven" )
-export PASH_BENCHMARK=("oneliners" "unix50" "analytics-mts" "max-temp" "web-index")
-export PASH_MODE=("blish_no_comm")
+  ##### Figure 7
+  export PASH_ALL_FLAGS=(
+  #"--dgsh_tee  # omitted until it's fixed
+  "--parallel_pipelines --profile_driven" )
+  export PASH_BENCHMARK=("oneliners" "unix50" "analytics-mts" "max-temp" "web-index")
+  export PASH_MODE=("blish_no_comm")
 
-time run_bench 
+  time run_bench 
 
-# kill the hanging processes 
-pkill -f cat
+  # kill the hanging processes 
+  pkill -f cat
+}
+
+# run all the tests three times
+# the results will be stored at: $PASH_TOP/evaluation/eval_results/run{1,2,3}
+run_all_benchmarks ${RES_FOLDER}1
+run_all_benchmarks ${RES_FOLDER}2
+run_all_benchmarks ${RES_FOLDER}3
