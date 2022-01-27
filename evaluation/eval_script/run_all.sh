e4bfa7c2370e43bf2186a656883e27d405da5864
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2022-01-27 05:44:21 -0500

    Don't copy output/pash_logs
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>

--- evaluation/eval_script/run_all.sh
+++ evaluation/eval_script/run_all.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 RES_FOLDER=${PWD}/eval_results/run
-rm -rf ${RES_FOLDER}
 # go to benchmark directory
-cd ../benchmarks
+cd ${PASH_TOP}/evaluation/benchmarks
 # use the small input for the benchmarks
 setup_flags='--small'
 
@@ -17,22 +16,13 @@ run_bash() {
         export IN=
         export IN_PRE=
         bench=${PASH_BENCHMARK[$i]}
-        cd $bench
-        # remove all the time files
-        rm -rf outputs output pash_logs par.res
-        cd ..
         echo 'Running bash:' ${bench}
         bdir=${RES_FOLDER}/bash/${bench}
         mkdir -p ${bdir}
         # run the benchmark
         ${bench} ${setup_flags}
-        # copy the output
-        cp -r ${bench}/outputs/* ${bdir}/
         # copy the time file
-        cp ${bench}/seq.res ${bdir}/
-        cd ${bench}
-        rm -rf outputs output pash_logs *.res
-        cd ..
+        mv ${bench}/seq.res ${bdir}/
     done
 }
 
@@ -44,13 +34,8 @@ run_bench() {
     array_len=$((${#PASH_ALL_FLAGS[@]} -1))
     for i in $(seq 0 $bench_len)
     do
-        export IN=
-        export IN_PRE=
         bench=${PASH_BENCHMARK[$i]}
-        cd $bench
         # remove all the time files
-        rm -rf outputs output pash_logs par.res
-        cd ..
         for j in $(seq 0 $array_len)
         do
             export IN=
@@ -61,20 +46,26 @@ run_bench() {
             ${bench}_pash ${setup_flags}
             mkdir -p ${pdir}
             # move the folder to our dest
-            mv ${bench}/outputs ${pdir}/
+            rm -rf ${bench}/outputs
             # copy the time file
             mv ${bench}/par.res ${pdir}/
         done
-        cd ${bench}
-        rm -rf outputs output pash_logs *.res
-        cd ..
     done
 }
 
 function run_all_benchmarks() {
   # generate output folder for each run
   export RES_FOLDER=$1
+  # clean previous runs
+  rm -rf ${RES_FOLDER}
   mkdir -p ${RES_FOLDER}
+  cd ${PASH_TOP}/evaluation/benchmarks
+  # remove all res files from previous runs
+  find . -type d -name "outputs" | xargs rm -rf
+  find . -type d -name "output" | xargs rm -rf
+  find . -type d -name "pash_logs" | xargs rm -rf
+  find . -type f -name "*.res" | xargs rm -f
+  # start preparing from execution
   export PASH_ALL_FLAGS=(" "
                          "--r_split --dgsh_tee --r_split_batch_size 1000000 --parallel_pipelines --profile_driven")
   export PASH_BENCHMARK=("oneliners" "unix50" "analytics-mts" "nlp" "max-temp" "web-index" "dependency_untangling")
@@ -83,8 +74,7 @@ function run_all_benchmarks() {
 
   echo 'Running all benchmark for bash'
   time run_bash
-
-  echo 'Running Blish benchmarks'
+  echo 'Running Blish/Pash benchmarks'
   time run_bench 
 
   ##### Figure 6
@@ -108,9 +98,9 @@ function run_all_benchmarks() {
   # kill the hanging processes 
   pkill -f cat
 }
-
+out_folder=${RES_FOLDER}
 # run all the tests three times
 # the results will be stored at: $PASH_TOP/evaluation/eval_results/run{1,2,3}
-run_all_benchmarks ${RES_FOLDER}1
-run_all_benchmarks ${RES_FOLDER}2
-run_all_benchmarks ${RES_FOLDER}3
+run_all_benchmarks ${out_folder}1
+run_all_benchmarks ${out_folder}2
+run_all_benchmarks ${out_folder}3
