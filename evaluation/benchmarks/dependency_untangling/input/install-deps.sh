4f2c79d80b85ec68934d57fed21b810bf6078516
Dimitris Karnikis <dkarnikis@gmail.com>
2022-02-14 18:16:17 +0200

    Fixes (#442)
    * export default input for poets to 1060
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Remove old files
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * cleanup is moved to the run files
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Path fixes
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Fixes to cleanup and dataset fetching
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Strip spaces from generated output
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Fix output_time flag
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Fixed CI/CD bug related to timers
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * switch max-temp.sh to temp-analytics.sh
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * modified execution scripts to support --small flag
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Fix script names
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Fixed typo on setup for small inputs
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Fix on oneliners setup
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * setup fixes
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Add small setup for poets
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * fixes on web-index install-deps scripts
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * silence the output when downloading the datasets
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Rename poets benchmark to nlp
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Fixes to dataset/deps installation
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * fix on poets setup path
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * bugfix on setup installation on max-temp
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * move dgsh repo to binpash org
    
    Signed-off-by: Dimitris Karnikis <dkarnikis@gmail.com>

---
+++ evaluation/benchmarks/dependency_untangling/input/install-deps.sh
@@ -0,0 +1,34 @@
+IN=$PASH_TOP/evaluation/benchmarks/dependency_untangling/input/
+mkdir -p ${IN}/deps/
+# install dependencies
+pkgs='ffmpeg unrtf imagemagick libarchive-tools zstd liblzma-dev libbz2-dev zip unzip nodejs'
+
+if ! dpkg -s $pkgs >/dev/null 2>&1 ; then
+    sudo apt-get install $pkgs -y
+    echo 'Packages Installed'
+fi
+
+if [ ! -d ${IN}/deps/samtools-1.7 ]; then
+    cd ${IN}/deps/
+    wget https://github.com/samtools/samtools/archive/refs/tags/1.7.zip
+    unzip 1.7.zip
+    rm 1.7.zip
+    cd samtools-1.7
+    wget https://github.com/samtools/htslib/archive/refs/tags/1.7.zip
+    unzip 1.7.zip
+    autoheader            # Build config.h.in (this may generate a warning about
+    # AC_CONFIG_SUBDIRS - please ignore it).
+    autoconf -Wno-syntax  # Generate the configure script
+    ./configure           # Needed for choosing optional functionality
+    make
+    rm -rf 1.7.zip
+    echo 'Samtools installed'
+fi
+
+if [ ! -f ${IN}/deps/makedeb.deb ]; then
+    cd ${IN}/deps/
+    wget http://pac-n4.csail.mit.edu:81/pash_data/makedeb.deb
+    sudo dpkg -i makedeb.deb
+    echo 'Makedeb installed'
+fi
+
