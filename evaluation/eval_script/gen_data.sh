10a842f6ed4f49b91a046de8d7078d7a1237749f
Dimitris Karnikis <dkarnikis@gmail.com>
2022-02-18 16:57:22 +0200

    Evaluation script (#447)
    * export default input for poets to 1060
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Remove old files
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * cleanup is moved to the run files
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Path fixes
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Fixes to cleanup and dataset fetching
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Strip spaces from generated output
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Fix output_time flag
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Fixed CI/CD bug related to timers
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * switch max-temp.sh to temp-analytics.sh
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * modified execution scripts to support --small flag
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * OSDI artifact script
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Fix script names
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Fix run script names
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * install deps
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Fixed typo on setup for small inputs
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Fix on oneliners setup
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * setup fixes
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Add small setup for poets
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * fixes on web-index install-deps scripts
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * silence the output when downloading the datasets
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Fixes to evaluation script
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Rename poets benchmark to nlp
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Update poets to nlp
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Fixes to dataset/deps installation
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * fix on poets setup path
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Add 3 benchmark runs. Removed dgsh_tee flag
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * bugfix on setup installation on max-temp
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Don't copy output/pash_logs
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Script to prepare plot source data
    
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Plot scripts to generate final pdfs.
    
    Co-authored-by: Michael Greenberg <michael.greenberg@stevens.edu>
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>
    
    * Name fixes
    
    Signed-off-by: Dimitris Karnikis <dkarnikis@gmail.com>
    
    * README
    
    Signed-off-by: Dimitris Karnikis <dkarnikis@gmail.com>
    
    * bugfix
    
    Signed-off-by: Dimitris Karnikis <dkarnikis@gmail.com>
    
    Co-authored-by: Michael Greenberg <michael.greenberg@stevens.edu>

---
+++ evaluation/eval_script/gen_data.sh
@@ -0,0 +1,97 @@
+DATA=final.csv
+rm -f $DATA
+replace_string() {
+    sed -i -e 's/'$1'/'"$2"'/g' .tmp
+}
+
+prepare_run_data() {
+    DATA_FILE=$1.data.csv
+    FILES=$1.log.dat
+    echo $1.tmp
+    rm -f $DATA_FILE $FILES
+
+    # gather all results
+    find $1 -name "*.res" -type f > $FILES
+
+    # read each line of the file
+    while read p; do
+        lines=$(cat $p | wc -l)
+        if [[ $lines -gt 3 ]]; then
+            file="$(tail -n +3 $p)"
+        else
+            file="$(tail -n +2 $p)"
+        fi
+        echo "$file" > .tmp
+        bench=$(echo $p | awk -F '/' '{print $3}')
+        mode=$(echo $p | awk -F'/' '{print $2}')
+        # read the contents of each execution file
+        while read l; do
+            #l=$(echo $l | sed 's/ //g')
+            echo $l | grep --quiet :
+            res=$?
+            if [[ $res == 1 ]]; then
+                perf=$(echo $l | grep -Eo '[0-9]+.[0-9]+$')
+                script=$(echo $l | sed -e 's/'$perf'//g')
+            else
+                # get script name and performance
+                # strip the .sh and get fetch the script name
+                script=$(echo $l | awk -F ':' '{print $1}' | sed 's/...$//')
+                # get the execution time
+                perf=$(echo $l | awk -F ':' '{print $2}')
+            fi
+            echo $bench,$script,$mode,$perf | sed 's/ //g'>> $DATA_FILE
+        done < .tmp
+    done < $FILES
+    sort $DATA_FILE > $1.tmp
+    rm $DATA_FILE rm -f $FILES
+}
+
+cd eval_results
+prepare_run_data run1
+prepare_run_data run2
+prepare_run_data run3
+# merge all the results
+paste run1.tmp run2.tmp run3.tmp -d , | sed -s  's/,/ /g' | awk '{print $1,$2,$3, $4+$8+$12}' | awk ' {print $1','$2','$3','$4/3}' | tr ' ' ',' > .tmp
+# cleanup
+replace_string dependency_untangling for-loops 
+replace_string nlp NLP
+replace_string oneliners Classics
+replace_string unix50 Unix50
+replace_string analytics-mts COVID-mts
+replace_string web-index WebIndex
+replace_string max-temp AvgTemp
+replace_string temp-analytics AvgTemp
+replace_string Genomics_Computation Genomics
+replace_string Program_Inference ProgInf
+replace_string blish_no_prof_no_du 'pash_jit -prof -par_pipe'
+replace_string blish_no_prof 'pash_jit -prof'
+replace_string blish pash_jit
+perf=''
+# calculate the ratios
+while read p; do
+    # is this the bash entry
+    echo $p | grep --quiet bash
+    res=$?
+    # fetch the performance
+    if [[ $res == 0 ]]; then
+        perf=$(echo $p | awk -F ',' '{print $4}')
+    fi
+    # get the bench
+    bench=$(echo $p | awk -F ',' '{print $1}')
+    # get the script
+    script=$(echo $p | awk -F ',' '{print $2}')
+    # get the mode
+    mode=$(echo $p | awk -F ',' '{print $3}')
+    # get the time of the pash/blish configs
+    current_perf=$(echo $p | awk -F ',' '{print $4}')
+    # calculate the ratio
+    if [[ $res == 0 ]]; then
+        ratio=$perf
+    else
+        ratio=$(echo "$perf $current_perf" | awk '{print $1/$2}' )
+    fi
+    # replace the pash/blish time with the ratio
+    echo $bench,$script,$mode,$ratio >> $DATA
+done < .tmp
+rm -f .tmp
+mv $DATA ..
