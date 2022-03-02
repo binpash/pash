0892ad8ffb145f4ee6287c9ba0c2ffd239dc0c7d
Sage Gerard <sage@sagegerard.com>
2022-03-02 12:40:25 -0500

    Remove unhelpful compilation code
    Some code only calls `make` with no target specified. No `autogen.sh`,
    `configure`, etc.
    
    Maybe that's fine for the runtime, but it's not enough for the `libdash`
    derivative. I wonder if there's a different script that I haven't seen
    yet that does this better.

--- scripts/package/after-install.sh
+++ scripts/package/after-install.sh
@@ -13,25 +13,3 @@ say_task() {
 mkdir -vp "$logd"
 cd "$pashd/compiler/parser"
 ln -fs "$pashd/pa.sh" /usr/bin/pa.sh
-
-case $(bash "$pashd/scripts/distro.sh") in
-    freebsd*)
-	cp Makefile Makefile.backup
-        gsed -i 's/ make/ gmake/g' Makefile
-
-	say_task "compile libdash (FreeBSD)" libdash.log
-        gmake libdash &> "$logd/libdash.log"
-
-	say_task "compile runtime (FreeBSD)" runtime.log
-        gmake -C ../../runtime/ &> "$logd/runtime.log"
-        ;;
-    *)
-	say_task "compile libdash" libdash.log
-        make libdash &>"$logd/libdash.log"
-
-	say_task "compile runtime" runtime.log
-        make -C ../../runtime/ &>"$logd/runtime.log"
-        ;;
-esac
-
-wait
