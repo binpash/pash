3191b89618f3b85c95727a208e2c8c3bf8c47798
Sage Gerard <sage@sagegerard.com>
2022-03-02 21:07:50 -0500

    Compile libdash

--- scripts/package/after-install.sh
+++ scripts/package/after-install.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 
 pashd=/usr/lib/pash
 logd=/var/log/pash
+export PASH_TOP="$pashd"
 
 say_task() {
     printf 'pash: %s (log: %s/%s)\n' "$1" "$logd" "$2"
@@ -13,3 +14,7 @@ say_task() {
 mkdir -vp "$logd"
 cd "$pashd/compiler/parser"
 ln -fs "$pashd/pa.sh" /usr/bin/pa.sh
+
+cd "$pashd/compiler/parser"
+git clone https://github.com/angelhof/libdash/
+make libdash
