0068d64dcdd9d817795f1165fe91f79a24fb19fe
Tammam Mustafa <tammammusatafa@gmail.com>
2022-03-03 05:28:00 +0000

    improve dspash setup script

--- scripts/setup-dspash.sh
+++ scripts/setup-dspash.sh
@@ -2,9 +2,11 @@
 # TODO: install any extra needed python debs
 
 # TODO: install golang
+wget https://go.dev/dl/go1.17.7.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.7.linux-amd64.tar.gz
 echo -e '\nexport PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
 export PATH=$PATH:/usr/local/go/bin
+rm go1.17.7.linux-amd64.tar.gz
 
 # TODO: install cli tool
 GO111MODULE=on go get github.com/urfave/cli/v2
