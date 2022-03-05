65dbf1e996db1a0cbbb2cf2fbfd8aaa6d2621e7c
Sage Gerard <sage@sagegerard.com>
2022-03-04 19:29:50 -0500

    Pass version information to format hook

--- scripts/package/fire-hook.sh
+++ scripts/package/fire-hook.sh
@@ -5,15 +5,14 @@
 # example.
 
 set -eu
-event_name="$1"
-output_format="$2"
+event_name="$1"; shift
 
 here="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 event="$here/hooks/${event_name}"
 
 if [ -f "$event/hook" ]; then
-    # Pass directory name so the script don't need to figure out where
+    # Pass directory path so the script don't need to figure out where
     # it is.  -f implies the script does not have to be Bash, but use
     # of non-executables must raise an error.
-    "$event/hook" "$event" "$output_format"
+    "$event/hook" "$event" "$@"
 fi
