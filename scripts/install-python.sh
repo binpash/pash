8b65338781a710ca5c6749282d7ec498cd617842
Sage Gerard <sage@sagegerard.com>
2022-03-04 19:20:43 -0500

    Pull out code for merging later

--- scripts/install-python.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-set -euo pipefail
-PYTHON_PKG_DIR="$PASH_TOP/python_pkg_root"
-python3 -m pip install --root "$PYTHON_PKG_DIR" --ignore-installed -r "$PASH_TOP/requirements.txt"
-ln -frs "$(find "$PYTHON_PKG_DIR" -type d -name site-packages)" "$PASH_TOP/python_pkgs"
