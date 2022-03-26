0a89f5a7218cd8ac9dbcd3cf97410151a56e0ef8
Tammam Mustafa <tammammusatafa@gmail.com>
2022-03-26 20:37:52 +0000

    files for debug server

--- compiler/debug/app.py
+++ compiler/debug/app.py
@@ -1,8 +1,26 @@
 from flask import Flask, render_template
+from werkzeug.exceptions import abort
 
 app = Flask(__name__)
-
+worker_db = {}
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/<int:worker_id>')
+def get_worker_log(worker_id):
+    return render_template('wlog.html')
+
+def log():
+    addr = request.remote_addr
+    return 
+
+
+class Worker:
+    def __init__(self, id):
+        self.id = id
+        self.log = ""
+
+    def clear_log(self):
+        self.log = ""
