24e07f46f719bab1df4e1cbf774f833b2d52ee3d
Dimitris Karnikis <dkarnikis@gmail.com>
2022-04-06 18:41:35 +0300

    fix install dep
    Signed-off-by: Dimitris Karnikis <dkarnikis@gmail.com>

--- evaluation/benchmarks/dependency_untangling/input/install-deps.sh
+++ evaluation/benchmarks/dependency_untangling/input/install-deps.sh
@@ -25,10 +25,9 @@ if [ ! -d ${IN}/deps/samtools-1.7 ]; then
     echo 'Samtools installed'
 fi
 
-if [ ! -f ${IN}/deps/makedeb.deb ]; then
+if ! dpkg -s "makedeb-makepkg" ; then
     cd ${IN}/deps/
     wget http://pac-n4.csail.mit.edu:81/pash_data/makedeb.deb
     sudo dpkg -i makedeb.deb
     echo 'Makedeb installed'
 fi
-
