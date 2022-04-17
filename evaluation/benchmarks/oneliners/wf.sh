a9081ab819013ae52ad13cdd42567a154ba735e9
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-04-17 09:17:48 -0400

    Revert wf
    Signed-off-by: Konstantinos Kallas <konstantinos.kallas@hotmail.com>

--- evaluation/benchmarks/oneliners/wf.sh
+++ evaluation/benchmarks/oneliners/wf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Calculate the frequency of each word in the document, and sort by frequency
 
-IN=$PASH_TOP/evaluation/benchmarks/oneliners/input/1M.txt
+IN=${IN:-$PASH_TOP/evaluation/benchmarks/oneliners/input/1G.txt}
 
 cat $IN | tr -cs A-Za-z '\n' | tr A-Z a-z | sort | uniq -c | sort -rn 
