a6bf1686b37bbdc01f88442b6a50c776c0e8e7fb
Tammam Mustafa <tammammusatafa@gmail.com>
2022-04-20 20:53:46 +0000

    Remove cleanup in some runtime .sh file as they cause problems with distribution
    Signed-off-by: Tammam Mustafa <tammammusatafa@gmail.com>
    Signed-off-by: Forthoney <castlehoneyjung@gmail.com>

--- runtime/dgsh_tee.sh
+++ runtime/dgsh_tee.sh
@@ -6,12 +6,15 @@ args=${@:3}
 # Set a default DISH_TOP in this directory if it doesn't exist
 PASH_TOP=${PASH_TOP:-$(git rev-parse --show-toplevel)}
 
-cleanup()
-{
-    kill -SIGTERM $dgsh_tee_pid > /dev/null 2>&1
-}
-trap cleanup EXIT
+# TODO: Doable check if this is still needed. Turned off for distributed exection. 
+#   PR https://github.com/binpash/pash/pull/495 might've resolved it.
+# cleanup()
+# {
+#     kill -SIGTERM $dgsh_tee_pid > /dev/null 2>&1
+# }
+# trap cleanup EXIT
 
-$PASH_TOP/runtime/dgsh-tee -i "$input" -o "$output" $args &
-dgsh_tee_pid=$!
-wait $dgsh_tee_pid
+# $PASH_TOP/runtime/dgsh-tee -i "$input" -o "$output" $args &
+# dgsh_tee_pid=$!
+# wait $dgsh_tee_pid
+$PASH_TOP/runtime/dgsh-tee -i "$input" -o "$output" $args
--- runtime/eager.sh
+++ runtime/eager.sh
@@ -6,14 +6,16 @@ intermediate_file=${3?"ERROR: Eager: No intermediate file given"}
 
 # Set a default DISH_TOP in this directory if it doesn't exist
 PASH_TOP=${PASH_TOP:-$(git rev-parse --show-toplevel)}
+# TODO: Doable check if this is still needed. Turned off for distributed exection. 
+#   PR https://github.com/binpash/pash/pull/495 might've resolved it.
+# cleanup()
+# {
+#     kill -SIGPIPE $eager_pid > /dev/null 2>&1
+# }
+# trap cleanup EXIT
 
-cleanup()
-{
-    kill -SIGPIPE $eager_pid > /dev/null 2>&1
-}
-trap cleanup EXIT
-
-$PASH_TOP/runtime/eager "$input" "$output" "$intermediate_file" &
-eager_pid=$!
-wait $eager_pid
+# $PASH_TOP/runtime/eager "$input" "$output" "$intermediate_file" &
+# eager_pid=$!
+# wait $eager_pid
+$PASH_TOP/runtime/eager "$input" "$output" "$intermediate_file"
 rm "$intermediate_file"
