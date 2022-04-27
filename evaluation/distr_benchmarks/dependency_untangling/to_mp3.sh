f89cbddb78e993d9d251d31382207c0100ff7014
Tammam Mustafa <tammammusatafa@gmail.com>
2022-04-27 20:08:05 +0000

    add stateless and pure function annotations
    Signed-off-by: Tammam Mustafa <tammammusatafa@gmail.com>

--- evaluation/distr_benchmarks/dependency_untangling/to_mp3.sh
+++ evaluation/distr_benchmarks/dependency_untangling/to_mp3.sh
@@ -4,18 +4,18 @@ IN=${IN:-$PASH_TOP/evaluation/distr_benchmarks/dependency_untangling/input/wav}
 OUT=${OUT:-$PASH_TOP/evaluation/distr_benchmarks/dependency_untangling/input/output/mp3}
 LOGS=${OUT}/logs
 mkdir -p ${LOGS}
-trigrams_aux(){
+pure_func(){
     ffmpeg -y -i pipe:0 -f mp3 -ab 192000 pipe:1  2>/dev/null
 }
 
-export -f trigrams_aux
+export -f pure_func
 
 pkg_count=0
 for item in $(hdfs dfs -ls -C /for-loops/wav);
 do
     pkg_count=$((pkg_count + 1));
     out="$OUT/$(basename $item).mp3"
-    hdfs dfs -cat $item | trigrams_aux > $out
+    hdfs dfs -cat $item | pure_func > $out
 done
 
 echo 'done';
