e672a0d7b3a381bda2acdd770727a540168ad115
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-05-14 08:37:06 -0400

    Correct the web-index input downloading

--- evaluation/benchmarks/web-index/input/setup.sh
+++ evaluation/benchmarks/web-index/input/setup.sh
@@ -11,25 +11,30 @@ PASH_TOP=${PASH_TOP:-$(git rev-parse --show-toplevel)}
 
 setup_dataset() {
   rm -rf ../1-grams.txt ../2-grams.txt 
+  
+  ## Downloading the dataset needs to happen for both small and large
+  if [[ ! -d ./en ]]; then
+    wget $wiki_archive || eexit "cannot fetch wikipedia"
+    7za x wikipedia-en-html.tar.7z
+    tar -xvf wikipedia-en-html.tar
+    wget http://ndr.md/data/wikipedia/index.txt || eexit "cannot fetch wiki indices"
+  fi
+
   if [ "$1" = "--small" ]; then
     if [[ ! -d ./en ]]; then
       # 500 entries
       wget http://pac-n4.csail.mit.edu:81/pash_data/small/web-index.small.zip
       unzip web-index.small.zip
-      mv small/* .
+      mv small/500.txt .
       rm -rf small web-index.small.zip
     fi
   else
     # elif [ "$1" = "--full" ]; then
-    wget $wiki_archive || eexit "cannot fetch wikipedia"
-    7za x wikipedia-en-html.tar.7z
-    tar -xvf wikipedia-en-html.tar
-    wget http://ndr.md/data/wikipedia/index.txt || eexit "cannot fetch wiki indices"
     # the default full 
     # 1000 entries
     wget http://pac-n4.csail.mit.edu:81/pash_data/full/web-index.full.zip
     unzip web-index.full.zip
-    mv full/* .
+    mv full/1000.txt .
     rm -rf full web-index.full.zip
   fi
 }
