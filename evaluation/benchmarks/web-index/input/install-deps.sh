b5b0ae02cb9b24133d999dbb51c9b46e48ecf8ef
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-05-15 11:18:56 -0400

    Correctly install npm dependencies...

--- evaluation/benchmarks/web-index/input/install-deps.sh
+++ evaluation/benchmarks/web-index/input/install-deps.sh
@@ -17,6 +17,7 @@ fi
 if ! dpkg -s nodejs > /dev/null 2>&1 ; then
     # node version 10+ does not need external npm
     curl -fsSL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+    sudo apt-get install -y nodejs
 fi
 
 if [ ! -d node_modules ]; then
