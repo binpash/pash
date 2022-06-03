017ef5598fcd2768c9acb5c214ba6d5e6cd26d77
Tammam Mustafa <tammammusatafa@gmail.com>
2022-06-03 02:38:58 -0400

    fix trigrams nlp

--- evaluation/distr_benchmarks/nlp/6_1.sh
+++ evaluation/distr_benchmarks/nlp/6_1.sh
@@ -7,7 +7,7 @@ OUT=${OUT:-$PASH_TOP/evaluation/distr_benchmarks/nlp/output/6_1/}
 ENTRIES=${ENTRIES:-1060}
 mkdir -p "$OUT"
 
-trigrams() {
+pure_func() {
     input=$1
     TEMPDIR=$(mktemp -d)
     tr -sc '[A-Z][a-z]' '[\012*]' > ${TEMPDIR}/${input}.words
@@ -16,12 +16,12 @@ trigrams() {
     paste ${TEMPDIR}/${input}.words ${TEMPDIR}/${input}.nextwords ${TEMPDIR}/${input}.nextwords2 | sort | uniq -c
     rm -rf ${TEMPDIR}
 }
-export -f trigrams
+export -f pure_func
 
 for input in $(hdfs dfs -ls -C ${IN} | head -n ${ENTRIES} | xargs -n 1 -I arg1 basename arg1)
 do
-    hdfs dfs -cat $IN"/"$input | grep 'the land of' | trigrams ${input} | sort -nr | sed 5q > ${OUT}/${input}.out0
-    hdfs dfs -cat $IN"/"$input | grep 'And he said' | trigrams ${input} | sort -nr | sed 5q > ${OUT}/${input}.out1
+    hdfs dfs -cat $IN"/"$input | grep 'the land of' | pure_func ${input} | sort -nr | sed 5q > ${OUT}/${input}.out0
+    hdfs dfs -cat $IN"/"$input | grep 'And he said' | pure_func ${input} | sort -nr | sed 5q > ${OUT}/${input}.out1
 done
 
 echo 'done';
