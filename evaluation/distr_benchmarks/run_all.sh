a056c467a47fa1f2cfe59bc433af3a6f5504219b
Tammam Mustafa <tammammusatafa@gmail.com>
2022-06-07 21:25:19 +0000

    Add newly added benchmarks to the run all script

--- evaluation/distr_benchmarks/run_all.sh
+++ evaluation/distr_benchmarks/run_all.sh
@@ -1,5 +1,14 @@
 cd $PASH_TOP/evaluation/distr_benchmarks/oneliners
 bash run.distr.sh
 
+cd $PASH_TOP/evaluation/distr_benchmarks/unix50
+bash run.distr.sh
+
 cd $PASH_TOP/evaluation/distr_benchmarks/nlp
+bash run.distr.sh
+
+cd $PASH_TOP/evaluation/distr_benchmarks/analytics-mts
+bash run.distr.sh
+
+cd $PASH_TOP/evaluation/distr_benchmarks/dependency_untangling
 bash run.distr.sh
