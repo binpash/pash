9ee0791077b0de94683c83f6f542f3b5598048f0
Tammam Mustafa <tammammusatafa@gmail.com>
2022-06-08 19:51:05 +0000

    use gzip instead of zip for better streaming support

--- evaluation/distr_benchmarks/dependency_untangling/compress_files.sh
+++ evaluation/distr_benchmarks/dependency_untangling/compress_files.sh
@@ -4,16 +4,11 @@ IN=${IN:-/dependency_untangling/pcap_data/}
 OUT=${OUT:-$PASH_TOP/evaluation/distr_benchmarks/dependency_untangling/input/output/compress}
 
 mkdir -p ${OUT}
-pure_func() {
-    zip -r --
-}
-
-export -f pure_func
 
 for item in $(hdfs dfs -ls -C ${IN});
 do
     output_name=$(basename $item).zip
-    hdfs dfs -cat $item | pure_func > $OUT/$output_name
+    hdfs dfs -cat $item | gzip -c > $OUT/$output_name
 done
 
 echo 'done';
