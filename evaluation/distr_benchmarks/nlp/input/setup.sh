63fe6145111c13c3c0c1821d6b261f5db8a18883
Tammam Mustafa <tammammusatafa@gmail.com>
2022-06-08 19:55:04 +0000

    small changes to setup script

--- evaluation/distr_benchmarks/nlp/input/setup.sh
+++ evaluation/distr_benchmarks/nlp/input/setup.sh
@@ -4,21 +4,20 @@ PASH_TOP=${PASH_TOP:-$(git rev-parse --show-toplevel)}
 
 [[ "$1" == "-c" ]] && { rm -rf genesis exodus pg; exit; }
 
-setup_dataset() {
-  if [ ! -f ./genesis ]; then
-      curl -sf https://www.gutenberg.org/cache/epub/8001/pg8001.txt > genesis
-      "$PASH_TOP/scripts/append_nl_if_not.sh" genesis
-  fi 
+if [ ! -f ./genesis ]; then
+    curl -sf https://www.gutenberg.org/cache/epub/8001/pg8001.txt > genesis
+    "$PASH_TOP/scripts/append_nl_if_not.sh" genesis
+fi 
 
-  if [ ! -f ./exodus ]; then
-    curl -sf https://www.gutenberg.org/files/33420/33420-0.txt > exodus
-    "$PASH_TOP/scripts/append_nl_if_not.sh" exodus
-  fi
+if [ ! -f ./exodus ]; then
+  curl -sf https://www.gutenberg.org/files/33420/33420-0.txt > exodus
+  "$PASH_TOP/scripts/append_nl_if_not.sh" exodus
+fi
 
-  if [ ! -e ./pg ]; then
-    mkdir pg
-    cd pg
-  if [[ "$1" == "--gen-full" ]]; then
+if [ ! -e ./pg ]; then
+  mkdir pg
+  cd pg
+  if [[ "$1" == "--full" ]]; then
     echo 'N.b.: download/extraction will take about 10min'
     wget ndr.md/data/pg.tar.xz
     if [ $? -ne 0 ]; then
@@ -36,24 +35,16 @@ setup_dataset() {
     mv data/* .
     rm nlp.zip data -rf
   fi
-    for f in *.txt; do
-      "$PASH_TOP/scripts/append_nl_if_not.sh" $f
-    done
-    cd ..
-  fi
 
-  # Put files in hdfs
-  hdfs dfs -mkdir /nlp
-  hdfs dfs -put exodus /nlp/exodus
-  hdfs dfs -put genesis /nlp/genesis
-  hdfs dfs -put pg /nlp/pg
-}
+  for f in *.txt; do
+    "$PASH_TOP/scripts/append_nl_if_not.sh" $f
+  done
+  cd ..
+  
+fi
 
-source_var() {
-  if [[ "$1" == "--small" ]]; then
-    export ENTRIES=40
-  else
-    # 1% of the input
-    export ENTRIES=1060
-  fi
-}
+# Put files in hdfs
+hdfs dfs -mkdir /nlp
+hdfs dfs -put exodus /nlp/exodus
+hdfs dfs -put genesis /nlp/genesis
+hdfs dfs -put pg /nlp/pg
