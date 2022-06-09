57c138dfdbe93dede2fde3da416863160c3350f2
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2022-06-09 13:58:34 -0400

    Clean up and refactor input generation to avoid several issues with artifact (#564)
    * Refactor input downloading to clean steps
    
    Signed-off-by: Konstantinos Kallas <konstantinos.kallas@hotmail.com>
    
    * Some notes from the generation
    
    Signed-off-by: Konstantinos Kallas <konstantinos.kallas@hotmail.com>
    
    * Comments on lib functions
    
    Signed-off-by: Konstantinos Kallas <konstantinos.kallas@hotmail.com>
    
    * Change wget to curl
    
    Signed-off-by: Konstantinos Kallas <konstantinos.kallas@hotmail.com>
    
    * checkpoint
    
    Signed-off-by: Konstantinos Kallas <konstantinos.kallas@hotmail.com>
    
    * JPG downloading
    
    Signed-off-by: Konstantinos Kallas <konstantinos.kallas@hotmail.com>
    
    * nginx logs
    
    Signed-off-by: Konstantinos Kallas <konstantinos.kallas@hotmail.com>
    
    * Add a build library
    
    Signed-off-by: Konstantinos Kallas <konstantinos.kallas@hotmail.com>
    Signed-off-by: Forthoney <castlehoneyjung@gmail.com>

--- evaluation/benchmarks/dependency_untangling/input/setup.sh
+++ evaluation/benchmarks/dependency_untangling/input/setup.sh
@@ -7,77 +7,175 @@ IN=$PASH_TOP/evaluation/benchmarks/dependency_untangling/input/
 OUT=$PASH_TOP/evaluation/benchmarks/dependency_untangling/output/
 IN_NAME=$PASH_TOP/evaluation/benchmarks/dependency_untangling/input/100G.txt
 
+## Import the build library
+. "$IN/build_lib.sh"
+
 if [ "$1" == "-c" ]; then
-    rm -rf ${IN}/jpg
-    rm -rf ${IN}/log_data
-    rm -rf ${IN}/wav
-    rm -rf ${IN}/nginx-logs
-    rm -rf ${IN}/node_modules
-    rm -rf ${IN}/pcap_data
-    rm -rf ${IN}/pcaps
-    rm -rf ${IN}/packages
-    rm -rf ${IN}/mir-sa
-    rm -rf ${IN}/deps
-    rm -rf ${IN}/bio
-    rm -rf ${IN}/output
-    rm -rf ${OUT}
+    rm -rf "${IN}/jpg"
+    rm -rf "${IN}/log_data"
+    rm -rf "${IN}/wav"
+    rm -rf "${IN}/nginx-logs"
+    rm -rf "${IN}/node_modules"
+    rm -rf "${IN}/pcap_data"
+    rm -rf "${IN}/pcaps"
+    rm -rf "${IN}/packages"
+    rm -rf "${IN}/mir-sa"
+    rm -rf "${IN}/deps"
+    rm -rf "${IN}/bio"
+    rm -rf "${IN}/output"
+    rm -rf "${OUT}"
     exit 
 fi
 
+
+
+## Q: Can these checks be generated automatically? This would be great if
+##    the user just ran the command, and then if it succeeded, the test is generated.
+wav_step_1_done_check()
+{
+    local prefix="wav/file_example_WAV"
+    files_exist_done_check "${prefix}_1MG.wav.kernel" "${prefix}_2MG.wav.kernel" "${prefix}_5MG.wav.kernel" "${prefix}_10MG.wav.kernel"
+    return $?
+}
+
+## Q: Can we automatically check that this is idempotent?
+## For example, in the step below there were 2 non-idempotence issues:
+## - wget downloads wav.zip.2 if wav.zip already exists, so we need to use -O flag
+## - wav files need to be saved with .kernel suffix to make step 2 idempotent
+wav_step_1()
+{
+    curl -C - -o wav.zip http://pac-n4.csail.mit.edu:81/pash_data/wav.zip
+    unzip wav.zip
+    local prefix="wav/file_example_WAV"
+    ## Necessary so that the iteration in step 2 is idempotent
+    for f in "${prefix}_1MG.wav" "${prefix}_2MG.wav" "${prefix}_5MG.wav" "${prefix}_10MG.wav"; do
+        mv $f $f.kernel
+    done
+}
+export -f wav_step_1_done_check
+export -f wav_step_1
+
+wav_step_2_done_check()
+{
+    local prefix="wav/file_example_WAV"
+    for i in $(seq 0 "$WAV_DATA_FILES"); do
+        if ! files_exist_done_check "${prefix}_1MG.wav$i.wav" "${prefix}_2MG.wav$i.wav" "${prefix}_5MG.wav$i.wav" "${prefix}_10MG.wav$i.wav"; then
+            return 1
+        fi
+    done
+    echo "Done"
+    return 0
+}
+
+wav_step_2()
+{
+    for f in wav/*.kernel; do
+        for (( i = 0; i <= $WAV_DATA_FILES; i++)) do
+            echo copying to "$base_f$i.wav"
+            base_f=wav/$(basename "$f" .kernel)
+            cp "$f" "$base_f$i.wav"
+        done
+    done
+}
+export -f wav_step_2_done_check
+export -f wav_step_2
+
+
+jpg_step()
+{
+    curl -C - -o jpg.zip $JPG_DATA_LINK
+    unzip jpg.zip
+    rm -rf ${IN}/jpg.zip
+}
+
+jpg_step_done_check()
+{
+    number_of_files_in_dir $JPG_NUMBER jpg
+}
+export -f jpg_step
+export -f jpg_step_done_check
+
+nginx_logs_step_1()
+{
+    curl -C - -o nginx.zip http://pac-n4.csail.mit.edu:81/pash_data/nginx.zip
+    unzip nginx.zip
+    rm nginx.zip
+}
+
+nginx_logs_step_1_done_check()
+{
+    local prefix="nginx-logs/log"
+    for i in $(seq 0 7); do
+        if ! files_exist_done_check "${prefix}$i"; then
+            return 1
+        fi
+    done
+    return $?
+}
+
+export -f nginx_logs_step_1
+export -f nginx_logs_step_1_done_check
+
+nginx_logs_step_2()
+{
+    # generating analysis logs
+    mkdir -p ${IN}/log_data
+    for (( i = 1; i <=$LOG_DATA_FILES; i++)) do
+        for j in nginx-logs/*;do
+            n=$(basename $j)
+            cp $j  log_data/log${i}_${n}.log; 
+        done
+    done
+}
+
+
+nginx_logs_step_2_done_check()
+{
+    local prefix="log_data/log"
+    for j in $(seq 0 7); do
+        for i in $(seq 1 "$LOG_DATA_FILES"); do
+            if ! files_exist_done_check "${prefix}${i}_log${j}.log"; then
+                return 1
+            fi
+        done
+    done
+    echo "Done"
+    return 0
+}
+
+export -f nginx_logs_step_2_done_check
+export -f nginx_logs_step_2
+
+
 setup_dataset() {
   if [ "$1" == "--small" ]; then
-      LOG_DATA_FILES=6
-      WAV_DATA_FILES=20
+      export LOG_DATA_FILES=6
+      export WAV_DATA_FILES=20
       NODE_MODULE_LINK=http://pac-n4.csail.mit.edu:81/pash_data/small/node_modules.zip
       BIO_DATA_LINK=http://pac-n4.csail.mit.edu:81/pash_data/small/bio.zip
-      JPG_DATA_LINK=http://pac-n4.csail.mit.edu:81/pash_data/small/jpg.zip
+      export JPG_DATA_LINK=http://pac-n4.csail.mit.edu:81/pash_data/small/jpg.zip
+      export JPG_NUMBER=508
       PCAP_DATA_FILES=1
   else
-      LOG_DATA_FILES=84
-      WAV_DATA_FILES=120
+      export LOG_DATA_FILES=84
+      export WAV_DATA_FILES=120
       NODE_MODULE_LINK=http://pac-n4.csail.mit.edu:81/pash_data/full/node_modules.zip
       BIO_DATA_LINK=http://pac-n4.csail.mit.edu:81/pash_data/full/bio.zip
-      JPG_DATA_LINK=http://pac-n4.csail.mit.edu:81/pash_data/full/jpg.zip
+      export JPG_DATA_LINK=http://pac-n4.csail.mit.edu:81/pash_data/full/jpg.zip
+      export JPG_NUMBER=1624
       PCAP_DATA_FILES=15
   fi
   
-  if [ ! -d ${IN}/wav ]; then
-      wget http://pac-n4.csail.mit.edu:81/pash_data/wav.zip
-      unzip wav.zip && cd wav/
-      for f in *.wav; do
-          FILE=$(basename "$f")
-          for (( i = 0; i <= $WAV_DATA_FILES; i++)) do
-              echo copying to $f$i.wav
-              cp $f $f$i.wav
-          done
-      done
-      echo "WAV Generated"
-  fi
+  ## WAV
+  execute_step wav_step_1 wav_step_1_done_check "WAV zip download"
+  execute_step wav_step_2 wav_step_2_done_check "WAV file generation"
+
+  ## JPG
+  execute_step jpg_step jpg_step_done_check "JPG Downloading"
   
-  if [ ! -d ${IN}/jpg ]; then
-      cd ${IN}
-      wget $JPG_DATA_LINK
-      unzip jpg.zip
-      echo "JPG Generated"
-      rm -rf ${IN}/jpg.zip
-  fi
-  
-  # download the input for the nginx logs and populate the dataset
-  if [ ! -d ${IN}/log_data ]; then
-      cd $IN
-      wget http://pac-n4.csail.mit.edu:81/pash_data/nginx.zip
-      unzip nginx.zip 
-      rm nginx.zip
-      # generating analysis logs
-      mkdir -p ${IN}/log_data
-      for (( i = 1; i <=$LOG_DATA_FILES; i++)) do
-          for j in nginx-logs/*;do
-              n=$(basename $j)
-              cat $j > log_data/log${i}_${n}.log; 
-          done
-      done
-      echo "Logs Generated"
-  fi
+  ## nginx logs
+  execute_step nginx_logs_step_1 nginx_logs_step_1_done_check "NGINX logs Downloading"
+  execute_step nginx_logs_step_2 nginx_logs_step_2_done_check "NGINX logs generated"
   
   if [ ! -d ${IN}/bio ]; then                                                  
       if [ "$1" = "--small" ]; then
