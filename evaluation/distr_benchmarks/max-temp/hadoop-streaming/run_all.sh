8139554a8dfe1471417e2f0592a3a33ab722f044
Tammam Mustafa <tammammusatafa@gmail.com>
2022-06-16 19:48:34 +0000

    ignore checksum in hadoop streaming

--- evaluation/distr_benchmarks/max-temp/hadoop-streaming/run_all.sh
+++ evaluation/distr_benchmarks/max-temp/hadoop-streaming/run_all.sh
@@ -1,4 +1,4 @@
-hadoop jar $jarpath -files 1_map.sh,1_reduce.sh -input $infile -output $output_dirs/max -mapper 1_map.sh -reducer 1_reduce.sh &
-hadoop jar $jarpath -files 2_map.sh,2_reduce.sh -input $infile -output $output_dirs/min -mapper 2_map.sh -reducer 2_reduce.sh &
-hadoop jar $jarpath -files 3_map.sh,3_reduce.sh -input $infile -output $output_dirs/average -mapper 3_map.sh -reducer 3_reduce.sh &
+hadoop jar $jarpath -files 1_map.sh,1_reduce.sh -D dfs.checksum.type=NULL -input $infile -output $output_dirs/max -mapper 1_map.sh -reducer 1_reduce.sh &
+hadoop jar $jarpath -files 2_map.sh,2_reduce.sh -D dfs.checksum.type=NULL -input $infile -output $output_dirs/min -mapper 2_map.sh -reducer 2_reduce.sh &
+hadoop jar $jarpath -files 3_map.sh,3_reduce.sh -D dfs.checksum.type=NULL -input $infile -output $output_dirs/average -mapper 3_map.sh -reducer 3_reduce.sh &
 wait
