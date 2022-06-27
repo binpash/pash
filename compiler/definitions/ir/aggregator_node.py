613d090b3c46c36e4e503ed6463201f87bc09378
Felix Stutz <fstutz@mpi-inf.mpg.de>
2022-06-27 13:38:38 -0400

    First PR for new annotations and new model for DFG nodes (#583)
    * WIP: first changes for new annotations
    
    * Make parallelization use mapper info from new annotations
    
    * Make parallelization use mapper info from new annotations
    
    * Delete com_mapper field in dfg_node and use the info from new annotations
    
    * Use input info from new annotations
    
    * Use output info from new annotations
    
    * Make dfg_options use the information from new annotations and completely remove use of old aggregator com_aggregator
    
    * Make to_ast work for wf.sh with some hacks, e.g. to_ast for eager to handle special case of intermediate file as last operand and fixed parsing issue for newline
    
    * Remove com_mapper and com_aggregator from DFGNode
    
    * WIP: incorporating remodelled command invocations
    
    * Parsing with new dataflow node model works
    
    * WIP: parallelization
    
    * Rudimentary parallelization with new annotations works
    
    * Add way to specify where to find repository for annotations repository
    
    * Do not require flag `r_split` since we do consecutive chunks for now
    
    * 1st part of changes due to comments for PR
    
    * minor fix
    
    * 2nd part of changes due to comments for PR
    
    Co-authored-by: Felix Stutz <fstutz@mpi-sws.org>

--- compiler/definitions/ir/aggregator_node.py
+++ compiler/definitions/ir/aggregator_node.py
@@ -1,10 +1,13 @@
 from definitions.ir.dfg_node import *
+# from definitions.ir.nodes.arg import Arg
+from annotations_utils.util_cmd_invocations import get_command_invocation_prefix_from_dfg_node
+
 
 ## This class corresponds to a generic n-ary aggregator
 ##
 ## TODO: Do we need to do anything special for binary aggregators?
 class MapperAggregatorNode(DFGNode):
-    def __init__(self, old_node, input_ids, output_ids, name_string, new_options):
+    def __init__(self, old_node, input_ids, output_ids, name_string, new_options, flag_option_list):
 
         ## The name of the aggregator command
         name = Arg(string_to_argument(name_string))
@@ -17,36 +20,73 @@ class MapperAggregatorNode(DFGNode):
         super().__init__(input_ids,
                          output_ids, 
                          name,
-                         com_category, 
-                         com_options=old_node.com_options, 
+                         com_category,
+                         # BEGIN ANNO
+                         # OLD
+                         # com_options=old_node.com_options,
+                         # NEW
+                         com_options=new_options, # changed that all are already in there and not appended
+                         flag_option_list=flag_option_list,
+                         # END ANNO
                          com_redirs=com_redirs, 
                          com_assignments=old_node.com_assignments)
-        
+
         ## TODO: This assumes that all options from the old function are copied to the new.
         ##
         ## TODO: If we need a behavior where we don't keep the old flags, we can extend this
-        self.append_options(new_options)
+        # BEGIN ANNO
+        # OLD
+        # self.append_options(new_options)
+        # END ANNO
 
 
 class AggregatorNode(MapperAggregatorNode):
     def __init__(self, old_node, input_ids, output_ids):
 
+        # BEGIN ANNO
+        used_parallelizer = old_node.get_used_parallelizer()
+        cmd_inv_pref = get_command_invocation_prefix_from_dfg_node(old_node)
+        used_aggregator = used_parallelizer.get_actual_aggregator(cmd_inv_pref)
+        log(f'used_agg: {used_aggregator}')
+        log(f'old_node: {old_node}')
+        # END ANNO
+
         ## Check if an aggregator can be instantiated from the node
-        if(old_node.com_aggregator is None):
+        # BEGIN ANNO
+        # OLD
+        # if(old_node.com_aggregator is None):
+        # NEW
+        if(used_aggregator is None):
+        # END ANNO
             log("Error: Node:", old_node, "does not contain information to instantiate an aggregator!")
             raise Exception('No information to instantiate aggregator')
 
         ## The name of the aggregator command
-        agg_name_string = old_node.com_aggregator.name
-        new_options = old_node.com_aggregator.options
+        # BEGIN ANNO
+        # OLD
+        # agg_name_string = old_node.com_aggregator.name
+        # new_options = old_node.com_aggregator.options
+        # NEW
+        agg_name_string = used_aggregator.cmd_name
+        all_options_incl_new = [Arg.string_to_arg(el.get_name()) for el in used_aggregator.flag_option_list + used_aggregator.positional_config_list]
+        # TODO: zip is nicer
+        all_options_incl_new_right_format = [(i, all_options_incl_new[i]) for i in range(len(all_options_incl_new))]
+        # END ANNO
 
-        super().__init__(old_node, input_ids, output_ids, agg_name_string, new_options)
+        # BEGIN ANNO
+        # OLD
+        # super().__init__(old_node, input_ids, output_ids, agg_name_string, new_options)
+        # NEW
+        super().__init__(old_node, input_ids, output_ids, agg_name_string, all_options_incl_new_right_format,
+                         flag_option_list=used_aggregator.flag_option_list)
+        # END ANNO
 
         log("Generic Aggregator Created:", self)
 
 class MapperNode(MapperAggregatorNode):
     def __init__(self, old_node, input_ids, output_ids):
 
+        assert(False)
         ## Check if an mapper can be instantiated from the node
         if(old_node.com_mapper is None):
             log("Error: Node:", old_node, "does not contain information to instantiate a mapper!")
--- compiler/definitions/ir/nodes/pash_split.py
+++ compiler/definitions/ir/nodes/pash_split.py
@@ -1,3 +1,6 @@
+from datatypes_new.AccessKind import AccessKind
+from datatypes_new.CommandInvocationWithIOVars import CommandInvocationWithIOVars
+
 from definitions.ir.file_id import *
 from definitions.ir.dfg_node import *
 from ir_utils import string_to_argument
@@ -6,19 +9,30 @@ import config
 import os
 
 class Split(DFGNode):
-    def __init__(self, inputs, outputs, com_name, com_category, com_options = [], 
-                 com_redirs = [], com_assignments=[]):
-        super().__init__(inputs, outputs, com_name, com_category, 
-                         com_options=com_options, 
-                         com_redirs=com_redirs, 
-                         com_assignments=com_assignments)
+    def __init__(self,
+                 cmd_invocation_with_io_vars,
+                 com_redirs=[],
+                 com_assignments=[],
+                 parallelizer_list=None,
+                 cmd_related_properties=None):
+        # TODO []: default arguments!
+        super().__init__(cmd_invocation_with_io_vars=cmd_invocation_with_io_vars,
+                         com_redirs=com_redirs,
+                         com_assignments=com_assignments,
+                         parallelizer_list=parallelizer_list,
+                         cmd_related_properties=cmd_related_properties)
 
-## TODO: Make a proper splitter subclass of Node
 def make_split_file(input_id, out_ids):
     auto_split_bin = os.path.join(config.PASH_TOP, config.config['runtime']['auto_split_binary'])
-    com_name = Arg(string_to_argument(auto_split_bin))
-    com_category = "pure"
-    return Split([input_id],
-                 out_ids,
-                 com_name,
-                 com_category)
+    operand_list = [input_id]
+    operand_list.extend(out_ids)
+    access_map = {output_id: AccessKind.make_stream_output() for output_id in out_ids}
+    access_map[input_id] = AccessKind.make_stream_input()
+    cmd_inv_with_io_vars = CommandInvocationWithIOVars(
+        cmd_name=auto_split_bin,
+        flag_option_list=[],
+        operand_list=operand_list,
+        implicit_use_of_streaming_input=None,
+        implicit_use_of_streaming_output=None,
+        access_map=access_map)
+    return Split(cmd_inv_with_io_vars)
