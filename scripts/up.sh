a272d9d28ea55a7d3220c47a9deeb979fb414ee5
Nikos Vasilakis <nikos@vasilak.is>
2022-06-30 11:01:58 -0400

    More installation fixes (#584)
    Signed-off-by: Nikos Vasilakis <nikos@vasilak.is>

--- scripts/up.sh
+++ scripts/up.sh
@@ -7,10 +7,6 @@ set -e
 # will install dependencies locally.
 PLATFORM=$(uname | tr '[:upper:]' '[:lower:]')
 
-cmd_exists () {
-  command -v $1 >/dev/null 2>&1 && echo 'true' || echo 'false';
-}
-
 if [ "$PLATFORM" = "darwin" ]; then
   echo 'PaSh is not yet well supported on OS X'
   exit 1
@@ -20,14 +16,12 @@ set +e
 git clone --depth 1 git@github.com:binpash/pash.git
 if [ $? -ne 0 ]; then
   echo 'SSH clone failed; attempting HTTPS'
-  git clone --depth 1 https://github.com/andromeda/pash.git
+  git clone --depth 1 https://github.com/binpash/pash.git
 fi
 set -e
 
-cd pash/scripts
-
 if [ $(groups $(whoami) | grep -c "sudo\|root\|admin") -ge 1 ]; then
   # only run this if we are in the sudo group (or it's doomed to fail)
-  bash distro-deps.sh
+  bash ./pash/scripts/distro-deps.sh
 fi
-bash setup-pash.sh
+bash ./pash/scripts/setup-pash.sh
