e7966b32efe8e92c3b41ed6da869d15d6955aab3
Sage Gerard <sage@sagegerard.com>
2022-07-11 16:06:23 -0400

    Use consistent working directory path for make

--- scripts/package/shell.sh
+++ scripts/package/shell.sh
@@ -5,14 +5,13 @@ here="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 PASH_TOP=$(readlink -f "$here/../..")
 
 # We need FPM's source code to build a key Docker image.
-printf "COMPARE\n  >$here\n  >%s\n " "$(pwd)"
 if [ ! -d "$here/fpm" ]; then
     git clone --depth 1 git@github.com:jordansissel/fpm.git "$here/fpm"
 fi
 
 # Make a Docker image that knows about building packages.
 if ! docker image inspect fpm 2>&1 >/dev/null; then
-    make -C fpm docker-release-everything
+    make -C "$here/fpm" docker-release-everything
 fi
 
 mkdir -p "$here/output"
