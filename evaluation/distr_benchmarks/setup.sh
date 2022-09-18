63f2c2e8d1eaf1013ca469937ad7cf85f6c0f3c9
Tammam Mustafa <tammammusatafa@gmail.com>
2022-09-18 13:32:38 +0000

    setup file to install all inputs

---
+++ evaluation/distr_benchmarks/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd $PASH_TOP/evaluation/distr_benchmarks/oneliners/input
+bash setup.sh
+
+cd $PASH_TOP/evaluation/distr_benchmarks/unix50/input
+bash setup.sh
+
+cd $PASH_TOP/evaluation/distr_benchmarks/nlp/input
+bash setup.sh
+
+cd $PASH_TOP/evaluation/distr_benchmarks/analytics-mts/input
+bash setup.sh
+
+cd $PASH_TOP/evaluation/distr_benchmarks/dependency_untangling/input
+bash setup.sh
+
+cd $PASH_TOP/evaluation/distr_benchmarks/max-temp/input
+bash setup.sh
