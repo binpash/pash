5407161d84bad287a21cb0502ef2d838cbba48aa
FortHoney <castlehoneyjung@gmail.com>
2022-12-06 00:25:04 -0500

    prep for cli arguments
    Signed-off-by: Forthoney <castlehoneyjung@gmail.com>

--- evaluation/benchmarks/ml/network_generator.py
+++ evaluation/benchmarks/ml/network_generator.py
@@ -1,4 +1,5 @@
-from torch import nn, optim
+import sys
+from torch import nn, optim, save
 
 # Characteristics inherent to the dataset
 INPUT_SIZE = 28**2 # Images are 28px * 28px
@@ -7,9 +8,10 @@ OUTPUT_SIZE  = 10
 # Neural network characteristics
 HIDDEN_LAYER_SIZES = [128, 64]
 BATCH_SIZE = 32
-EPOCHS = 15
 
-def prepare_environment():
+FILENAMES = ['model.pt', 'criterion.pt', 'optimizer.pt']
+
+def generate_network():
     # Set up the model
     model = nn.Sequential(nn.Linear(INPUT_SIZE, HIDDEN_LAYER_SIZES[0]), 
                         nn.ReLU(),
@@ -22,3 +24,19 @@ def prepare_environment():
     optimizer = optim.SGD(model.parameters(), lr=0.004, momentum=0.9)
 
     return model, criterion, optimizer
+
+def save_network(model, criterion, optimizer, dest_dir):
+    for i, elem in enumerate([model, criterion, optimizer]):
+        save(elem, dest_dir + FILENAMES[i])
+        print(dest_dir + FILENAMES[i])
+
+def main():
+    if len(sys.argv) != 2:
+        print('Incorrect Usage')
+        exit(1)
+    
+    model, criterion, optimizer = generate_network()
+    save_network(model, criterion, optimizer, sys.argv[1])
+
+if (__name__ == '__main__'):
+    main()
