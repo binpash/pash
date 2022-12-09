619412ea77d21acf04c80f4e1dfc80893e91c503
FortHoney <castlehoneyjung@gmail.com>
2022-12-08 22:26:00 -0500

    fix echo bug
    Signed-off-by: Forthoney <castlehoneyjung@gmail.com>

--- evaluation/benchmarks/ml/build_network.sh
+++ evaluation/benchmarks/ml/build_network.sh
@@ -7,7 +7,7 @@ TRAIN_LOADER=${TRAIN_LOADER:-$PASH_TOP/evaluation/benchmarks/ml/dataloader/train
 PYTHON=${PYTHON:-`which python`}
 
 $PYTHON network_generator.py $INTERMED_DIR
-echo 'Finished generating network elements'
+echo "Done generating network elements"
 
 $PYTHON batchify.py $TRAIN_LOADER
-echo 'Produced batches'
+echo "Done producing batches"
