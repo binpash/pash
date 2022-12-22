2785d800575b8d533b49d2d2a2941fceeb0da7ea
FortHoney <castlehoneyjung@gmail.com>
2022-12-22 19:22:14 +0900

    finish breakdown of script

---
+++ evaluation/benchmarks/ml/sklearn/breakdown/fit_model.py
@@ -0,0 +1,14 @@
+import sys
+import numpy as np
+from pickle import dumps, load
+
+reg = load(sys.stdin)
+reg.coef_ = load(sys.stdin)
+reg.n_iter_ = load(sys.stdin)
+reg.classes_ = load(sys.stdin)
+reg.intercept = load(sys.stdin)
+
+if reg.fit_intercept:
+    reg.coef_ = load(sys.stdin)
+
+sys.stdout.buffer.write(dumps(reg))
