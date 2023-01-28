be5d4917de9f56da4a176eda1c8238a94090acf5
Forthoney <castlehoneyjung@gmail.com>
2023-01-28 15:16:49 -0500

    enable variable destination setting

--- evaluation/benchmarks/ml/sklearn/fit.sh
+++ evaluation/benchmarks/ml/sklearn/fit.sh
@@ -5,8 +5,7 @@ DIR=$PASH_TOP/evaluation/benchmarks/ml/sklearn
 TMP=$DIR/tmp
 SCRIPTS=$DIR/scripts
 
-# We can technically write to file via bash shovel, but for now, I write inside 
-# the python script
+# Ideally, we'll move on to piping rather than writing to a file
 MODEL=$TMP/model.obj
 X=$TMP/X_train.obj
 y=$TMP/y_train.obj
@@ -15,6 +14,8 @@ DUAL=false # should be converted to bool inside script
 MAX_SQ_SUM=$TMP/max_squared_sum.obj
 WARM_COEF=$TMP/warm_start_coef.obj
 C_=$TMP/C_.obj
+
+echo Destination: $1
 
 # Generating model & samples
 $PYTHON $SCRIPTS/gen_model.py
@@ -35,47 +36,4 @@ n_classes=$($PYTHON $SCRIPTS/reshape_classes.py $MODEL $CLASSES)
 $PYTHON $SCRIPTS/warm_start.py $MODEL $multiclass $n_classes | # pipes coefficients
 $PYTHON $SCRIPTS/fold_coef.py $MODEL $X $y $C_ $CLASSES $WARM_COEF $MAX_SQ_SUM $multiclass $penalty |
 $PYTHON $SCRIPTS/zip_coef.py $MODEL |
-$PYTHON $SCRIPTS/adjust_coef.py $MODEL $X $multiclass $n_classes
-
-
-# # takes solver, penalty, dual
-# # returns solver
-# $PYTHON $DIR/check_solver.py 
-
-# # takes model
-# # returns C_, penalty
-# $PYTHON $DIR/warning_checks.py 
-
-# # takes X, y, model, solver
-# # returns X, y
-# $PYTHON $DIR/val_data.py
-
-# # takes y 
-# # returns classes
-# $PYTHON $DIR/classes.py 
-
-# # takes X
-# # returns max_squared_sum
-# $PYTHON $DIR/rownorm.py
-
-# # takes model, solver, classes
-# # returns multi_class
-# $PYTHON $DIR/check_multiclass.py 
-
-# # TODO: fix implementation
-# # takes classes
-# # returns n_classes, classes reshaped
-# $PYTHON $DIR/reshape_classes.py
-
-# # takes model, multi_class, n_classes
-# # returns warm_start_coef
-# $PYTHON $DIR/warm_start.py 
-
-# # takes model, X, y, C_, classes_, warm_start_coef, prefer, max_squared_sum, 
-# # multi_class, solver, penalty, sample_weight, n_threads
-# # returns fold_coef
-# $PYTHON $DIR/fold_coef.py
-
-# # takes fold_coef
-# # returns fold_coef, n_iter
-# $PYTHON $DIR/zip_coef.py 
+$PYTHON $SCRIPTS/adjust_coef.py $MODEL $X $multiclass $n_classes $1
