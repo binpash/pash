732ff21f1a9e5485780319702e2300900c79ad94
Forthoney <castlehoneyjung@gmail.com>
2023-01-28 15:17:03 -0500

    refactor to make unpacking clearer
    Signed-off-by: Forthoney <castlehoneyjung@gmail.com>

--- evaluation/benchmarks/ml/sklearn/scripts/classes.py
+++ evaluation/benchmarks/ml/sklearn/scripts/classes.py
@@ -3,15 +3,18 @@ import sys
 import numpy as np
 import pickle
 
-with open(sys.argv[2], 'rb') as file:
+model_file, y_file = sys.argv[1:]
+
+with open(y_file, 'rb') as file:
     y = pickle.load(file)
 
 try:
+    # Can probably optimize, only opening the file once
     _logistic.check_classification_targets(y)
-    with open(sys.argv[1], 'rb') as file:
+    with open(model_file, 'rb') as file:
         model = pickle.load(file)
         model.classes_ = np.unique(y)
-    with open(sys.argv[1], 'wb') as file:
+    with open(model_file, 'wb') as file:
         pickle.dump(model, file)
 except:
     exit(1)
