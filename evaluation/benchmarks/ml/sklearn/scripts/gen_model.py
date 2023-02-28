42e25a0f9cf9d899fa38238cce1d8c9cb3c4c6fc
Forthoney <castlehoneyjung@gmail.com>
2023-02-28 08:51:51 -0500

    allow for variation in max_iter

--- evaluation/benchmarks/ml/sklearn/scripts/gen_model.py
+++ evaluation/benchmarks/ml/sklearn/scripts/gen_model.py
@@ -1,6 +1,7 @@
 from sklearn.linear_model import LogisticRegression
 import pickle
+import sys
 
-reg = LogisticRegression(max_iter=10000)
+reg = LogisticRegression(max_iter=int(sys.argv[1]))
 with open('./tmp/model.obj', 'w+b') as file:
     pickle.dump(reg, file)
