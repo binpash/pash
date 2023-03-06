b1afd46eb1cee8d0f4b251ebb52f6b000a5d5de5
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2023-03-06 09:53:07 -0500

    Add df regions to their own files and only edges to the partial order file

--- compiler/util_spec.py
+++
@@ -1,9 +0,0 @@
-
-##
-## This file contains utility functions useful for the speculative execution component
-##
-
-def initialize_po_file(trans_options) -> None:
-    ## Initializae the partial order file
-    open(trans_options.get_partial_order_file(), 'w').close()
-
