1c96ba1180e5d4e84f931048ae50de4266dad0b5
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2023-03-08 14:02:25 -0500

    add the alternative in a comment, it might interfere with antikythera and other CI scripts and there is no need to mess with this now

--- scripts/up.sh
+++ scripts/up.sh
@@ -12,19 +12,22 @@ if [ "$PLATFORM" = "darwin" ]; then
   exit 1
 fi
 
-## Download the latest PaSh release tarball to avoid using git
-curl -s https://api.github.com/repos/binpash/pash/releases/latest | 
-  grep "tarball_url" | 
-  cut -d : -f 2,3 | 
-  tr -d \" | 
-  tr -d , | 
-  wget -i - -O pash.tar.gz
-## Find the name of the top directory
-pash_dir_name=`tar -tzf pash.tar.gz | head -1 | cut -f1 -d"/"`
-tar -xzf pash.tar.gz
-mv "$pash_dir_name" pash
-# Alternative:
-# git clone https://github.com/binpash/pash.git
+git clone https://github.com/binpash/pash.git
+## TODO: Instead of using git, we could download the latest tarball
+##       though this would need care because `pash` is not a git directory
+##       then, potentially interfering with the automatic setting of PASH_TOP
+##       in some of our CI, etc
+# ## Download the latest PaSh release tarball to avoid using git
+# curl -s https://api.github.com/repos/binpash/pash/releases/latest | 
+#   grep "tarball_url" | 
+#   cut -d : -f 2,3 | 
+#   tr -d \" | 
+#   tr -d , | 
+#   wget -i - -O pash.tar.gz
+# ## Find the name of the top directory
+# pash_dir_name=`tar -tzf pash.tar.gz | head -1 | cut -f1 -d"/"`
+# tar -xzf pash.tar.gz
+# mv "$pash_dir_name" pash
 
 if [ $(groups $(whoami) | grep -c "sudo\|root\|admin") -ge 1 ]; then
   # only run this if we are in the sudo group (or it's doomed to fail)
