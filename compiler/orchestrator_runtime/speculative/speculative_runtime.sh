8a4d0b8b73388340e8d042c6303823c41962496a
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2023-04-24 10:58:03 -0400

    restore variables

--- compiler/orchestrator_runtime/speculative/speculative_runtime.sh
+++ compiler/orchestrator_runtime/speculative/speculative_runtime.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 
-## TODO: Ask the scheduler to let us know when a command has been committed and what is its exit code.
 ## TODO: Define the client in pash_spec_init_setup (which should be sourced by pash_init_setup)
-
-## TODO: Then we need to extend the scheduler to also support this protocol (unix sockets only) and 
-##       Respond when the command is actually done.
 
 export pash_speculative_command_id=$1
 
 pash_redir_output echo "$$: (2) Before asking the scheduler for cmd: ${pash_speculative_command_id} exit code..."
+
+## TODO: Correctly save variables
+## Save the shell variables to a file (necessary for expansion)
+export pash_runtime_shell_variables_file="${PASH_TMP_PREFIX}/variables_$RANDOM$RANDOM$RANDOM"
+source "$RUNTIME_DIR/pash_declare_vars.sh" "$pash_runtime_shell_variables_file"
+pash_redir_output echo "$$: (1) Bash variables saved in: $pash_runtime_shell_variables_file"
+
+
 ## Send and receive from daemon
 msg="Wait:${pash_speculative_command_id}"
 daemon_response=$(pash_spec_communicate_scheduler "$msg") # Blocking step, daemon will not send response until it's safe to continue
@@ -18,7 +22,9 @@ daemon_response=$(pash_spec_communicate_scheduler "$msg") # Blocking step, daemo
 if [[ "$daemon_response" == *"OK:"* ]]; then
     # shellcheck disable=SC2206
     response_args=($daemon_response)
+    pash_redir_output echo "$$: (2) Scheduler responded: $daemon_response"
     cmd_exit_code=${response_args[1]}
+    output_variable_file=${response_args[2]}
 elif [ -z "$daemon_response" ]; then
     ## Trouble... Daemon crashed, rip
     pash_redir_output echo "$$: ERROR: (2) Scheduler crashed!"
@@ -32,7 +38,10 @@ fi
 pash_redir_output echo "$$: (2) Scheduler returned exit code: ${cmd_exit_code} for cmd with id: ${pash_speculative_command_id}."
 
 
-## TODO: Figure out if this exits properly (prob not)
 pash_runtime_final_status=${cmd_exit_code}
+
+## TODO: Restore the variables
+pash_redir_output echo "$$: (2) Recovering script variables from: $output_variable_file"
+source "$RUNTIME_DIR/pash_source_declare_vars.sh" "$output_variable_file"
 
 ## TODO: Also need to use wrap_vars maybe to `set` properly etc
