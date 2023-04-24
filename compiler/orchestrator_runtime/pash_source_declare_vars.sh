059b5b7308087cd0dde8bfcab989adfb8ed982da
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2023-04-24 14:29:28 -0400

    nits

--- compiler/orchestrator_runtime/pash_source_declare_vars.sh
+++ compiler/orchestrator_runtime/pash_source_declare_vars.sh
@@ -11,8 +11,9 @@
 
 filter_vars_file()
 {
-    cat "$1" | grep -v "^declare -\([A-Za-z]\|-\)* \(pash\|BASH\|LINENO\|EUID\|GROUPS\)" |
-        grep -v "LS_COLORS"
+    cat "$1" | grep -v "^declare -\([A-Za-z]\|-\)* \(pash\|BASH\|LINENO\|EUID\|GROUPS\)" 
+    # The extension below is done for the speculative pash
+    # | grep -v "LS_COLORS"
 }
 
 ## TODO: Error handling if the argument is empty?
--- compiler/orchestrator_runtime/speculative/speculative_runtime.sh
+++ compiler/orchestrator_runtime/speculative/speculative_runtime.sh
@@ -11,6 +11,9 @@ export pash_runtime_shell_variables_file="${PASH_TMP_PREFIX}/variables_$RANDOM$R
 source "$RUNTIME_DIR/pash_declare_vars.sh" "$pash_runtime_shell_variables_file"
 pash_redir_output echo "$$: (1) Bash variables saved in: $pash_runtime_shell_variables_file"
 
+## TODO: We want to send the environment to the scheduler.
+##       Once the scheduler determines if there are environment changes, it can then
+##       decide to rerun or not the speculated commands with the new environment.
 
 ## Send and receive from daemon
 msg="Wait:${pash_speculative_command_id}"
@@ -38,8 +41,8 @@ pash_redir_output echo "$$: (2) Scheduler returned exit code: ${cmd_exit_code} f
 
 pash_runtime_final_status=${cmd_exit_code}
 
-## TODO: Restore the variables
+## TODO: Restore the variables (doesn't work currently because variables are printed using `env`)
 pash_redir_output echo "$$: (2) Recovering script variables from: $output_variable_file"
-source "$RUNTIME_DIR/pash_source_declare_vars.sh" "$output_variable_file"
+# source "$RUNTIME_DIR/pash_source_declare_vars.sh" "$output_variable_file"
 
 ## TODO: Also need to use wrap_vars maybe to `set` properly etc
