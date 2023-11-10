692316dc9a4c2bf597095bb66ae70202bf16cb3a
Nikos Pagonas <nikospagonas00@gmail.com>
2023-11-10 15:31:27 -0500

    Add TCP communication primitives & configuration

---
+++ runtime/serverless/aws/s3-get-object.py
@@ -0,0 +1,26 @@
+import boto3
+import sys
+import time
+
+object_key, outfile = sys.argv[1:]
+print("Remote read", object_key)
+session = boto3.Session()
+
+s3 = session.client("s3")
+
+try:
+    response = s3.get_object(Bucket="nikpag", Key=object_key)
+except Exception as e:
+    print(e)
+
+batch = 1000
+
+with open(outfile, "wb") as f:
+    while True:
+        x = response["Body"].read(batch)
+
+        if not x:
+            break
+
+        f.write(x)
+        f.flush()
