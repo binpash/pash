3d6e6897d7eab4e59e9c0c3a63976d7feead38c0
Forthoney <castlehoneyjung@gmail.com>
2023-11-23 15:06:17 -0500

    add a couple compiler funcs

--- compiler/shell_ast/ast_node.py
+++ compiler/shell_ast/ast_node.py
@@ -8,9 +8,11 @@ unsave and expensive typecheck
 from abc import ABC, abstractmethod
 from copy import deepcopy
 from typing import Any, Protocol, Tuple, TypedDict, TYPE_CHECKING
+from pash_annotations.annotation_generation.datatypes.InputOutputInfo import InputOutputInfo
+from pash_annotations.datatypes.CommandInvocationInitial import CommandInvocationInitial
 
 import shasta.ast_node
-from shasta.ast_node import Command, make_typed_semi_sequence
+from shasta.ast_node import make_typed_semi_sequence
 from shasta.json_to_ast import to_ast_node
 
 from shell_ast.ast_util import (
@@ -20,7 +22,7 @@ from shell_ast.ast_util import (
     make_increment_var,
     export_pash_loop_iters_for_current_context,
 )
-from ir import IR
+from ir import IR, compile_command_to_DFG
 from util import log
 
 if TYPE_CHECKING:
@@ -50,9 +52,10 @@ class PashNode(Protocol):
     ) -> PreprocessedAST:
         raise NotImplementedError
 
-    def preprocess_close_node(
-        self, trans_state: "TransState", last_object: bool = False
-    ) -> Tuple["PashNode", bool]:
+    def _preprocess_close_node(self, trans_state: "TransState", last_object: bool):
+        raise NotImplementedError
+
+    def compile(self, file_id_gen, config) -> IR:
         raise NotImplementedError
 
 
@@ -61,14 +64,8 @@ class PashMixin(ABC):
     Mixin that extends functionality of shasta's Commands
     """
 
-    @abstractmethod
-    def preprocess(
-        self, trans_state: "TransState", last_object: bool
-    ) -> PreprocessedAST:
-        pass
-
-    def preprocess_close_node(
-        self, trans_state: "TransState", last_object: bool = False
+    def _preprocess_close_node(
+        self: PashNode, trans_state: "TransState", last_object: bool = False
     ) -> Tuple[PashNode, bool]:
         preprocessed_ast_object = self.preprocess(trans_state, last_object=last_object)
         preprocessed_ast = preprocessed_ast_object.ast
@@ -84,9 +81,53 @@ class PashMixin(ABC):
             something_replaced = preprocessed_ast_object.will_anything_be_replaced()
         return final_ast, something_replaced
 
-    @abstractmethod
-    def compile(self):
-        pass
+    def _compile_arg_char(self, arg, file_id_gen, config):
+        """
+        Compile an arg char recursively if it contains quotes or command substitution.
+
+        Currently being extended to also expand any arguments taht are safe to expand.
+        """
+        ## Compile the arg char
+        if isinstance(arg, shasta.ast_node.CArgChar) or isinstance(
+            arg, shasta.ast_node.EArgChar
+        ):
+            # Single character or escape
+            return arg
+        elif isinstance(arg, shasta.ast_node.BArgChar):
+            # TODO: I probably have to redirect the input of the compiled
+            #       node (IR) to be closed, and the output to be
+            #       redirected to some file that we will use to write to
+            #       the command argument to complete the command
+            #       substitution.
+            arg.node = arg.node.compile(arg.node, file_id_gen, config)
+            return arg
+        elif isinstance(arg, shasta.ast_node.QArgChar):
+            arg.arg = self._compile_command_argument(arg.arg, file_id_gen, config)
+            return arg
+        else:
+            log(f"Unknown arg_char: {arg}")
+            ## TODO: Complete this
+            return arg
+
+    def _compile_command_to_dfg(file_id_gen, command_name, options, redirections):
+
+
+    def _compile_command_argument(self, arg, file_id_gen, config):
+        return [self._compile_arg_char(char, file_id_gen, config) for char in arg]
+
+    def _compile_assignments(self, assignments, file_id_gen, config):
+        return [
+            [
+                assignment[0],
+                self._compile_command_argument(assignment[1], file_id_gen, config),
+            ]
+            for assignment in assignments
+        ]
+
+    def _compile_redirection(self, redir, file_id_gen, config):
+        file_arg = self._compile_command_argument(redir.arg, file_id_gen, config)
+        redir.arg = file_arg
+        return redir
 
 
 class PipeNode(shasta.ast_node.PipeNode, PashMixin):
@@ -104,6 +145,21 @@ class PipeNode(shasta.ast_node.PipeNode, PashMixin):
             last_ast=last_object,
         )
         return preprocessed_ast_object
+
+    def compile(self, file_id_gen, config):
+        compiled_ir = self._combine_pipe(
+            [item.compile(file_id_gen, config) for item in self.items]
+        )
+        compiled_ir.set_ast(self.json())
+        compiled_ir.set_background(self.is_background)
+        return compiled_ir
+
+    def _combine_pipe(self, items):
+        combined_nodes = items[0]
+        for item in items[1:]:
+            combined_nodes.pipe_append(item)
+
+        return combined_nodes
 
 
 class CommandNode(shasta.ast_node.CommandNode, PashMixin):
@@ -124,6 +180,44 @@ class CommandNode(shasta.ast_node.CommandNode, PashMixin):
                 self, replace_whole=True, non_maximal=False, last_ast=last_object
             )
 
+    def compile(self, file_id_gen, config):
+        compiled_assignments = [
+            self._compile_assignments(self.assignments, file_id_gen, config)
+        ]
+        compiled_redirs = [
+            self._compile_redirection(redir, file_id_gen, config)
+            for redir in self.redir_list
+        ]
+
+        assert len(ast_node.arguments) > 0
+
+        args = self.arguments
+        command_name = args[0]
+        options = [
+            self._compile_command_argument(arg, file_id_gen, config) for arg in args[1:]
+        ]
+
+        try:
+            ir = compile_command_to_DFG(file_id_gen, command_name, options, compiled_redirs)
+            compiled_ast = ir
+        except ValueError as err:
+            log("Command not compiled to DFG:", err)
+            ## TODO: Maybe we want to fail here instead of waiting for later?
+            ##       Is there any case where a non-compiled command is fine?
+            # log(traceback.format_exc())
+            compiled_arguments = compile_command_arguments(arguments, fileIdGen, config)
+            compiled_ast = make_kv(
+                type(ast_node).NodeName,
+                [
+                    ast_node.line_number,
+                    compiled_assignments,
+                    compiled_arguments,
+                    compiled_redirections,
+                ],
+            )
+
+        return compiled_ast
+
 
 class SubshellNode(shasta.ast_node.SubshellNode, PashMixin):
     body: PashNode
@@ -133,7 +227,7 @@ class SubshellNode(shasta.ast_node.SubshellNode, PashMixin):
         self.body = pash_node_from(body)
 
     def preprocess(self, trans_state: "TransState", last_object: bool = False):
-        preprocessed_body, something_replaced = self.preprocess_close_node(
+        preprocessed_body, something_replaced = self._preprocess_close_node(
             trans_state, last_object=last_object
         )
         self.body = preprocessed_body
@@ -157,10 +251,10 @@ class AndNode(shasta.ast_node.AndNode, PashMixin):
         self.right_operand = pash_node_from(right_operand)
 
     def preprocess(self, trans_state: "TransState", last_object: bool = False):
-        preprocessed_l, sth_replaced_l = self.left_operand.preprocess_close_node(
+        preprocessed_l, sth_replaced_l = self.left_operand(
             trans_state, last_object=last_object
         )
-        preprocessed_r, sth_replaced_r = self.right_operand.preprocess_close_node(
+        preprocessed_r, sth_replaced_r = self.right_operand(
             trans_state, last_object=last_object
         )
         self.left_operand = preprocessed_l
@@ -188,10 +282,10 @@ class OrNode(shasta.ast_node.OrNode, PashMixin):
     def preprocess(
         self, trans_state: "TransState", last_object: bool = False
     ) -> PreprocessedAST:
-        preprocessed_l, sth_replaced_l = self.left_operand.preprocess_close_node(
+        preprocessed_l, sth_replaced_l = self.left_operand(
             trans_state, last_object=last_object
         )
-        preprocessed_r, sth_replaced_r = self.right_operand.preprocess_close_node(
+        preprocessed_r, sth_replaced_r = self.right_operand(
             trans_state, last_object=last_object
         )
         self.left_operand = preprocessed_l
@@ -221,12 +315,8 @@ class SemiNode(shasta.ast_node.SemiNode, PashMixin):
         self, trans_state: "TransState", last_object: bool
     ) -> PreprocessedAST:
         # TODO: Is it valid that only the right one is considered the last command?
-        preprocessed_l, sth_replaced_l = self.left_operand.preprocess_close_node(
-            trans_state, last_object
-        )
-        preprocessed_r, sth_replaced_r = self.right_operand.preprocess_close_node(
-            trans_state, last_object
-        )
+        preprocessed_l, sth_replaced_l = self.left_operand(trans_state, last_object)
+        preprocessed_r, sth_replaced_r = self.right_operand(trans_state, last_object)
         self.left_operand = preprocessed_l
         self.right_operand = preprocessed_r
         sth_replaced = sth_replaced_l or sth_replaced_r
@@ -249,7 +339,7 @@ class NotNode(shasta.ast_node.NotNode, PashMixin):
     def preprocess(
         self, trans_state: "TransState", last_object: bool = False
     ) -> PreprocessedAST:
-        preprocessed_body, sth_replaced = self.body.preprocess_close_node(
+        preprocessed_body, sth_replaced = self.body(
             trans_state, last_object=last_object
         )
         self.body = preprocessed_body
@@ -274,9 +364,7 @@ class RedirNode(shasta.ast_node.RedirNode, PashMixin):
     def preprocess(
         self, trans_state: "TransState", last_object: bool
     ) -> PreprocessedAST:
-        preprocessed_node, sth_replaced = self.node.preprocess_close_node(
-            trans_state, last_object
-        )
+        preprocessed_node, sth_replaced = self.node(trans_state, last_object)
         self.node = preprocessed_node
         return PreprocessedAST(
             self,
@@ -336,7 +424,7 @@ class ForNode(shasta.ast_node.ForNode, PashMixin):
     ) -> PreprocessedAST:
         # If we are in a loop, we push the loop identifier into the loop context
         loop_id = trans_state.enter_loop()
-        preprocessed_body, sth_replaced = self.body.preprocess_close_node(
+        preprocessed_body, sth_replaced = self.body._preprocess_close_node(
             trans_state, last_object
         )
         # TODO: Then send this iteration identifier when talking to the spec scheduler
@@ -410,12 +498,8 @@ class WhileNode(shasta.ast_node.WhileNode, PashMixin):
     ) -> PreprocessedAST:
         trans_state.enter_loop()
 
-        preprocessed_test, sth_replaced_test = self.test.preprocess_close_node(
-            trans_state, last_object
-        )
-        preprocessed_body, sth_replaced_body = self.body.preprocess_close_node(
-            trans_state, last_object
-        )
+        preprocessed_test, sth_replaced_test = self.test(trans_state, last_object)
+        preprocessed_body, sth_replaced_body = self.body(trans_state, last_object)
         self.test = preprocessed_test
         self.body = preprocessed_body
         sth_replaced = sth_replaced_test or sth_replaced_body
@@ -444,15 +528,9 @@ class IfNode(shasta.ast_node.IfNode, PashMixin):
     def preprocess(
         self, trans_state: "TransState", last_object: bool
     ) -> PreprocessedAST:
-        preprocess_cond, sth_replaced_cond = self.cond.preprocess_close_node(
-            trans_state, last_object
-        )
-        preprocess_then, sth_replaced_then = self.then_b.preprocess_close_node(
-            trans_state, last_object
-        )
-        preprocess_else, sth_replaced_else = self.else_b.preprocess_close_node(
-            trans_state, last_object
-        )
+        preprocess_cond, sth_replaced_cond = self.cond(trans_state, last_object)
+        preprocess_then, sth_replaced_then = self.then_b(trans_state, last_object)
+        preprocess_else, sth_replaced_else = self.else_b(trans_state, last_object)
         self.cond = preprocess_cond
         self.then_b = preprocess_then
         self.else_b = preprocess_else
@@ -497,14 +575,12 @@ class CaseNode(shasta.ast_node.CaseNode, PashMixin):
         )
 
     def _preprocess_case(self, case, trans_state, last_object):
-        preprocessed_body, sth_replaced = case["cbody"].preprocess_close_node(
-            trans_state, last_object
-        )
+        preprocessed_body, sth_replaced = case["cbody"](trans_state, last_object)
         case["cbody"] = preprocessed_body
         return case, sth_replaced
 
 
-def pash_node_from(shasta_node: Command) -> PashNode:
+def pash_node_from(shasta_node: shasta.ast_node.AstNode) -> PashNode:
     pash_node_class = globals()[f"{shasta_node.NodeName}Node"]
     args = vars(shasta_node)
     return pash_node_class(**args)
