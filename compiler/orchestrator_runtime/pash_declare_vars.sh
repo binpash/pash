f11e18848fd2f1130573a027daedb0f17f40ed29
Guest <di_jin@brown.edu>
2024-03-01 01:54:59 -0500

    remove debugging output that lacks proper environment when called from hs for now

--- compiler/orchestrator_runtime/pash_declare_vars.sh
+++ compiler/orchestrator_runtime/pash_declare_vars.sh
@@ -2,7 +2,7 @@
 
 vars_file="${1?File not given}"
 
-pash_redir_output echo "Writing vars to: $vars_file"
+# pash_redir_output echo "Writing vars to: $vars_file"
 
 declare -p > "$vars_file"
 ## KK  2021-11-23 We don't actually need to export functions in the vars file. 
