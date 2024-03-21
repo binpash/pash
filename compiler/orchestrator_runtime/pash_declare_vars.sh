4644312a1ac272bd3f262208712bd768b3008554
Guest <di_jin@brown.edu>
2024-03-21 15:08:24 -0400

    dump cwd in the env state

--- compiler/orchestrator_runtime/pash_declare_vars.sh
+++ compiler/orchestrator_runtime/pash_declare_vars.sh
@@ -4,7 +4,5 @@ vars_file="${1?File not given}"
 
 # pash_redir_output echo "Writing vars to: $vars_file"
 
-declare -p > "$vars_file"
-## KK  2021-11-23 We don't actually need to export functions in the vars file. 
-##                We never expand them in the compiler
-declare -f >> "$vars_file"
+echo "cd ${PWD}" > "$vars_file"
+declare -p >> "$vars_file"
