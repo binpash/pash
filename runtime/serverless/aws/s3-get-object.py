8058aa749f81e79e870ed80113f6db4541367997
Nikos Pagonas <nikospagonas00@gmail.com>
2024-04-14 17:28:18 -0400

    Use generic name for S3 bucket

--- runtime/serverless/aws/s3-get-object.py
+++ runtime/serverless/aws/s3-get-object.py
@@ -9,7 +9,7 @@ session = boto3.Session()
 s3 = session.client("s3")
 
 try:
-    response = s3.get_object(Bucket="nikpag", Key=object_key)
+    response = s3.get_object(Bucket="bucket", Key=object_key)
 except Exception as e:
     print(e)
 
