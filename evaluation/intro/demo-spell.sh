2bdf282f09a8a21c2c719fded8d19af7cdbc845c
Nikos Pagonas <nikospagonas00@gmail.com>
2024-04-15 10:39:51 -0400

    Use 200M.txt for evaluation

--- evaluation/intro/demo-spell.sh
+++ evaluation/intro/demo-spell.sh
@@ -6,7 +6,7 @@ cd "$(dirname $0)"
   echo "PASH_TOP not set, maybe $(git rev-parse --show-toplevel)?"
   exit
 }
-FILE="input/1K.txt"
+FILE="input/200M.txt"
 DICT="input/sorted_words"
 
 cat "$FILE" | tr A-Z a-z | tr -cs A-Za-z '\n' | sort | uniq | comm -13 $DICT -
