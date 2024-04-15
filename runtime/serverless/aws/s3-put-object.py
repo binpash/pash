d10e6fde6aa1bfff7e89461692cc0aadd5c239ed
Nikos Pagonas <nikospagonas00@gmail.com>
2024-04-15 10:39:16 -0400

    Use BUCKET and QUEUE env variables

--- runtime/serverless/aws/s3-put-object.py
+++ runtime/serverless/aws/s3-put-object.py
@@ -2,27 +2,39 @@ import boto3
 import sys
 import json
 import os
+import time
 
+AWS_ACCOUNT_ID=os.environ.get("AWS_ACCOUNT_ID")
+BUCKET=os.environ.get("AWS_BUCKET")
+QUEUE=os.environ.get("AWS_QUEUE")
 object_key, infile = sys.argv[1:]
-print("[s3-put-object.py] Remote write",object_key)
-session = boto3.Session()
 
+print(f"[s3-put-object.py] Start putting {object_key}")
+start_time = time.time()
+
+session = boto3.Session()
 s3 = session.client("s3")
+
 
 # Prepare the data to be stored in the S3 object
 with open(infile, "rb") as file:
     object_data = file.read()
 
 # Upload the object to S3
-s3.put_object(Bucket="bucket", Key=object_key, Body=object_data)
+s3.put_object(Bucket=BUCKET, Key=object_key, Body=object_data)
 
+end_time = time.time()
+total_time = end_time - start_time
+total_time = round(total_time, 3)
+
+print(f"[s3-put-object.py] Finish putting {object_key} in {total_time} seconds")
 
 # notify the main shell that job is done
 sqs_client = boto3.client("sqs")
 message_body = {"message": "done","output_file_id":object_key}
 try:
     response = sqs_client.send_message(
-        QueueUrl=f"https://sqs.us-east-1.amazonaws.com/{os.environ.get('AWS_ACCOUNT_ID')}/queue ", MessageBody=json.dumps(message_body)
+        QueueUrl=f"https://sqs.us-east-1.amazonaws.com/{AWS_ACCOUNT_ID}/{QUEUE}", MessageBody=json.dumps(message_body)
     )
 except Exception as e:
     print(e)
