c3da4786081d08d8115cb2ae7162c73c5bb7d4f1
Nikos Pagonas <nikospagonas00@gmail.com>
2024-05-04 11:53:56 -0400

    Change unix50 structure, setup inputs

---
+++ evaluation/benchmarks/unix50/scripts/30.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export IN_PRE=${IN_PRE:-$PASH_TOP/evaluation/benchmarks/unix50/input}
+IN98=$IN_PRE/9.8.txt
+# 9.8: TELE-communications
+cat "$IN9_8" | tr -c '[a-z][A-Z]' '\n' | grep '[A-Z]' | sed 1d | sed 2d | sed 3d | sed 4d | tr -c '[A-Z]' '\n' | tr -d '\n'
