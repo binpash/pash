8a1d08df07ddb97aaf74fd98350e760265e7250f
Nikos Pagonas <nikospagonas00@gmail.com>
2024-05-04 21:53:46 -0400

    Add timing support for oneliners

--- evaluation/benchmarks/oneliners/run.sh
+++ evaluation/benchmarks/oneliners/run.sh
@@ -10,16 +10,17 @@ cd "$(dirname "$0")" || exit 1
 BENCHMARK_DIR=$PASH_TOP/evaluation/benchmarks/oneliners
 INPUTS_DIR=$BENCHMARK_DIR/inputs
 OUTPUTS_DIR=$BENCHMARK_DIR/outputs
+TIMES_DIR=$BENCHMARK_DIR/times
 SCRIPTS_DIR=$BENCHMARK_DIR/scripts
 
 SCRIPTS_INPUTS=(
-    # nfa-regex.sh:1M.txt
-    # shortest-scripts.sh:all_cmds.txt
-    # sort-sort.sh:1M.txt
-    # sort.sh:1M.txt
-    # spell.sh:1M.txt
-    # top-n.sh:1M.txt
-    wf.sh:1M.txt
+  nfa-regex.sh:1G.txt
+  shortest-scripts.sh:all_cmds.txt
+  sort-sort.sh:1G.txt
+  sort.sh:1G.txt
+  spell.sh:1G.txt
+  top-n.sh:1G.txt
+  wf.sh:1G.txt
 )
 
 ENVIRONMENT=Local
@@ -33,6 +34,9 @@ do
   SCRIPT_PATH="${SCRIPTS_DIR}/${SCRIPT}"
   INPUT=${INPUTS_DIR}/$(echo "$SCRIPT_INPUT" | cut -d: -f2)
   OUTPUT="${OUTPUTS_DIR}/${SCRIPT}__env${ENVIRONMENT}__mem${MEMORY}__sys${SYSTEM}__w${WIDTH}.out"
+  TIME="${TIMES_DIR}/${SCRIPT}__env${ENVIRONMENT}__mem${MEMORY}__sys${SYSTEM}__w${WIDTH}.time"
 
-  IN=$INPUT "$SCRIPT_PATH" >"$OUTPUT"
+  echo "Running $SCRIPT on $INPUT"
+
+  { time IN=$INPUT "$SCRIPT_PATH" >"$OUTPUT" ; } 2>"$TIME"
 done
