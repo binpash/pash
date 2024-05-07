e48ddbf65e52f3b36d9229dbd4854ce4a306c643
sethsabar <sethsabar@gmail.com>
2024-05-07 15:35:33 +0000

    bash evaluation script

--- evaluation/benchmarks/run_benchmarks_bash.sh
+++ evaluation/benchmarks/run_benchmarks_bash.sh
@@ -34,8 +34,8 @@ oneliners(){
   mkdir -p "$outputs_dir"
 
   scripts_inputs=(
-      "nfa-regex;10M.txt" # 100M
-      "sort;100M.txt"
+      "nfa-regex;100M.txt" # 100M
+      "sort;3G.txt"
       "top-n;100M.txt"
       "wf;100M.txt"
       "spell;100M.txt"
@@ -88,8 +88,8 @@ oneliners_pash(){
   mkdir -p "$pash_logs_dir"
 
   scripts_inputs=(
-      "nfa-regex;10M.txt" # 100M.txt"
-      "sort;100M.txt" # 3G.txt
+      "nfa-regex;100M.txt"
+      "sort;3G.txt"
       "top-n;100M.txt"
       "wf;100M.txt"
       "spell;100M.txt"
@@ -128,146 +128,7 @@ oneliners_pash(){
   cd ..
 }
 
-# oneliners --full
-# oneliners_pash --full
+oneliners --full
+oneliners_pash --full
 
-# compare_outputs "oneliners/outputs"
-
-nlp(){
-  times_file="seq.res"
-  outputs_suffix="seq.out"
-  outputs_dir="outputs"
-  if [ -e "nlp/${times_file}" ]; then
-    echo "skipping nlp/${times_file}"
-    return 0
-  fi
-
-  cd nlp/
-
-  install_deps_source_setup $1
-
-  mkdir -p "$outputs_dir"
-
-  names_scripts=(
-    "1syllable_words;6_4"
-    "2syllable_words;6_5"
-    "4letter_words;6_2"
-    "bigrams_appear_twice;8.2_2"
-    "bigrams;4_3"
-    "compare_exodus_genesis;8.3_3"
-    "count_consonant_seq;7_2"
-    # "count_morphs;7_1"
-    "count_trigrams;4_3b"
-    "count_vowel_seq;2_2"
-    "count_words;1_1"
-    "find_anagrams;8.3_2"
-    "merge_upper;2_1"
-    "sort;3_1"
-    "sort_words_by_folding;3_2"
-    "sort_words_by_num_of_syllables;8_1"
-    "sort_words_by_rhyming;3_3"
-    # "trigram_rec;6_1"
-    "uppercase_by_token;6_1_1"
-    "uppercase_by_type;6_1_2"
-    "verses_2om_3om_2instances;6_7"
-    "vowel_sequencies_gr_1K;8.2_1"
-    "words_no_vowels;6_3"
-  )
-
-  touch "$times_file"
-  echo executing Unix-for-nlp $(date) | tee -a "$times_file"
-  echo '' >> "$times_file"
-
-  source_var $1
-
-  for name_script in ${names_scripts[@]}
-  do
-    IFS=";" read -r -a name_script_parsed <<< "${name_script}"
-    name="${name_script_parsed[0]}"
-    script="${name_script_parsed[1]}"
-    printf -v pad %30s
-    padded_script="${name}.sh:${pad}"
-    padded_script=${padded_script:0:30}
-
-    outputs_file="${outputs_dir}/${script}.${outputs_suffix}"
-
-    echo "${padded_script}" $({ time ./${script}.sh > "$outputs_file"; } 2>&1) | tee -a "$times_file"
-  done
-  cd ..
-}
-
-nlp_pash(){
-  times_file="par.res"
-  outputs_suffix="par.out"
-  time_suffix="par.time"
-  outputs_dir="outputs"
-  pash_logs_dir="pash_logs"
-  width=16
-  if [ -e "nlp/${times_file}" ]; then
-    echo "skipping nlp/${times_file}"
-    return 0
-  fi
-
-  cd nlp/
-
-  install_deps_source_setup $1
-
-  mkdir -p "$outputs_dir"
-  mkdir -p "$pash_logs_dir"
-
-  names_scripts=(
-    "1syllable_words;6_4"
-    "2syllable_words;6_5"
-    "4letter_words;6_2"
-    "bigrams_appear_twice;8.2_2"
-    "bigrams;4_3"
-    "compare_exodus_genesis;8.3_3"
-    "count_consonant_seq;7_2"
-    "count_morphs;7_1"
-    "count_trigrams;4_3b"
-    "count_vowel_seq;2_2"
-    "count_words;1_1"
-    "find_anagrams;8.3_2"
-    "merge_upper;2_1"
-    "sort;3_1"
-    "sort_words_by_folding;3_2"
-    "sort_words_by_num_of_syllables;8_1"
-    "sort_words_by_rhyming;3_3"
-    # "trigram_rec;6_1"
-    "uppercase_by_token;6_1_1"
-    "uppercase_by_type;6_1_2"
-    "verses_2om_3om_2instances;6_7"
-    "vowel_sequencies_gr_1K;8.2_1"
-    "words_no_vowels;6_3"
-  )
-
-  touch "$times_file"
-  echo executing Unix-for-nlp with pash $(date) | tee -a "$times_file"
-  echo '' >> "$times_file"
-
-  source_var $1
-
-  for name_script in ${names_scripts[@]}
-  do
-    IFS=";" read -r -a name_script_parsed <<< "${name_script}"
-    name="${name_script_parsed[0]}"
-    script="${name_script_parsed[1]}"
-    printf -v pad %30s
-    padded_script="${name}.sh:${pad}"
-    padded_script=${padded_script:0:30}
-
-    outputs_file="${outputs_dir}/${script}.${outputs_suffix}"
-    pash_log="${pash_logs_dir}/${script}.pash.log"
-    single_time_file="${outputs_dir}/${script}.${time_suffix}"
-
-    echo -n "${padded_script}" | tee -a "$times_file"
-    { time "$PASH_TOP/pa.sh" -w "${width}" $PASH_FLAGS --log_file "${pash_log}" ${script}.sh > "$outputs_file"; } 2> "${single_time_file}"
-    cat "${single_time_file}" | tee -a "$times_file"
-  done
-  cd ..
-}
-
-nlp --full
-nlp_pash --full
-
-compare_outputs "nlp/outputs"
+compare_outputs "oneliners/outputs"
