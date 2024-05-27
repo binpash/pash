064a46ccc0bfd5e90d9dfe5a17a931c62f3b7759
Nikos Pagonas <nikospagonas00@gmail.com>
2024-05-27 18:12:46 +0300

    Add oneliners button

---
+++ evaluation/benchmarks/oneliners/download.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+cd "$(dirname "$0")" || exit 1
+
+[ -z "$PASH_TOP" ] && {
+  echo "PASH_TOP not set, maybe $(git rev-parse --show-toplevel)?"
+  exit
+}
+
+[ -z "$AWS_ACCOUNT_ID" ] && {
+  echo "AWS_ACCOUNT_ID not set"
+  exit
+}
+
+[ -z "$AWS_BUCKET" ] && {
+  echo "AWS_BUCKET not set"
+  exit
+}
+
+mkdir -p outputs/
+
+BENCHMARK_DIR="$PASH_TOP/evaluation/benchmarks/oneliners"
+OUTPUTS_DIR="$BENCHMARK_DIR/outputs"
+
+S3_BUCKET_PREFIX="s3://$AWS_BUCKET"
+S3_BENCHMARK_DIR="oneliners"
+S3_OUTPUTS_DIR="$S3_BENCHMARK_DIR/outputs"
+
+CONFIGS=(
+  # AWS:2048M:Bash:1
+  AWS:2048M:Splash:1
+  # AWS:2048M:Splash:2
+  # AWS:2048M:Splash:4
+  # AWS:2048M:Splash:8
+  # AWS:2048M:Splash:16
+  # AWS:2048M:Splash:32
+  # AWS:2048M:Splash:64
+)
+
+SCRIPTS=(
+    nfa-regex.sh
+    # shortest-scripts.sh
+    # sort-sort.sh
+    # sort.sh
+    # spell.sh
+    # top-n.sh
+    # wf.sh
+)
+
+if [[ "$*" == *"--small"* ]]
+then
+    INPUT_TYPE=".small"
+else
+    INPUT_TYPE=""
+fi
+
+for CONFIG in "${CONFIGS[@]}"
+do
+    for SCRIPT in "${SCRIPTS[@]}"
+    do
+        ENVIRONMENT=$(echo "$CONFIG" | cut -d: -f1)
+        MEMORY=$(echo "$CONFIG" | cut -d: -f2)
+        SYSTEM=$(echo "$CONFIG" | cut -d: -f3)
+        WIDTH=$(echo "$CONFIG" | cut -d: -f4)
+
+        OUTPUT="${SCRIPT}__env${ENVIRONMENT}__mem${MEMORY}__sys${SYSTEM}__w${WIDTH}${INPUT_TYPE}.out"
+
+        OUTPUT_PATH="$OUTPUTS_DIR/$OUTPUT"
+        S3_URI=$S3_BUCKET_PREFIX/$S3_OUTPUTS_DIR/$OUTPUT
+
+        echo "Downloading $S3_URI to $OUTPUT_PATH"
+
+        aws s3 cp  "$S3_URI" "$OUTPUTS_DIR/$OUTPUT"
+    done
+done
--- evaluation/benchmarks/oneliners/run.sh
+++ evaluation/benchmarks/oneliners/run.sh
@@ -7,36 +7,108 @@ cd "$(dirname "$0")" || exit 1
   exit
 }
 
-BENCHMARK_DIR=$PASH_TOP/evaluation/benchmarks/oneliners
-INPUTS_DIR=$BENCHMARK_DIR/inputs
-OUTPUTS_DIR=$BENCHMARK_DIR/outputs
-TIMES_DIR=$BENCHMARK_DIR/times
-SCRIPTS_DIR=$BENCHMARK_DIR/scripts
+[ -z "$AWS_ACCOUNT_ID" ] && {
+  echo "AWS_ACCOUNT_ID not set"
+  exit
+}
 
-SCRIPTS_INPUTS=(
-  nfa-regex.sh:1G.txt
-  shortest-scripts.sh:all_cmds.txt
-  sort-sort.sh:1G.txt
-  sort.sh:1G.txt
-  spell.sh:1G.txt
-  top-n.sh:1G.txt
-  wf.sh:1G.txt
+[ -z "$AWS_BUCKET" ] && {
+  echo "AWS_BUCKET not set"
+  exit
+}
+
+[ -z "$AWS_QUEUE" ] && {
+  echo "AWS_QUEUE not set"
+  exit
+}
+
+mkdir -p {outputs,times}/
+
+BENCHMARK_DIR="$PASH_TOP/evaluation/benchmarks/oneliners"
+INPUTS_DIR="$BENCHMARK_DIR/inputs"
+OUTPUTS_DIR="$BENCHMARK_DIR/outputs"
+TIMES_DIR="$BENCHMARK_DIR/times"
+SCRIPTS_DIR="$BENCHMARK_DIR/scripts"
+
+S3_BENCHMARK_DIR="oneliners"
+S3_INPUTS_DIR="$S3_BENCHMARK_DIR/inputs"
+S3_OUTPUTS_DIR="$S3_BENCHMARK_DIR/outputs"
+
+# ENVIRONMENT, MEMORY, SYSTEM, WIDTH
+CONFIGS=(
+  # Local:16G:Bash:1
+  # AWS:2048M:Bash:1
+  AWS:2048M:Splash:1
+  # AWS:2048M:Splash:2
+  # AWS:2048M:Splash:4
+  # AWS:2048M:Splash:8
+  # AWS:2048M:Splash:16
+  # AWS:2048M:Splash:32
+  # AWS:2048M:Splash:64
 )
 
-ENVIRONMENT=Local
-MEMORY=16G
-SYSTEM=Bash
-WIDTH=1
+if [[ "$*" == *"--small"* ]]
+then
+  SCRIPTS_INPUTS=(
+    nfa-regex.sh:1M.txt
+    # shortest-scripts.sh:all_cmds.txt
+    # sort-sort.sh:1M.txt
+    # sort.sh:1M.txt
+    # spell.sh:1M.txt
+    # top-n.sh:1M.txt
+    # wf.sh:1M.txt
+  )
+  INPUT_TYPE=".small"
+else
+  SCRIPTS_INPUTS=(
+    # nfa-regex.sh:1G.txt
+    # shortest-scripts.sh:all_cmds.txt
+    # sort-sort.sh:1G.txt
+    # sort.sh:1G.txt
+    # spell.sh:1G.txt
+    # top-n.sh:1G.txt
+    # wf.sh:1G.txt
+  )
+  INPUT_TYPE=""
+fi
 
-for SCRIPT_INPUT in "${SCRIPTS_INPUTS[@]}"
+for CONFIG in "${CONFIGS[@]}"
 do
-  SCRIPT=$(echo "$SCRIPT_INPUT" | cut -d: -f1)
-  SCRIPT_PATH="${SCRIPTS_DIR}/${SCRIPT}"
-  INPUT=${INPUTS_DIR}/$(echo "$SCRIPT_INPUT" | cut -d: -f2)
-  OUTPUT="${OUTPUTS_DIR}/${SCRIPT}__env${ENVIRONMENT}__mem${MEMORY}__sys${SYSTEM}__w${WIDTH}.out"
-  TIME="${TIMES_DIR}/${SCRIPT}__env${ENVIRONMENT}__mem${MEMORY}__sys${SYSTEM}__w${WIDTH}.time"
+  for SCRIPT_INPUT in "${SCRIPTS_INPUTS[@]}"
+  do
+    ENVIRONMENT=$(echo "$CONFIG" | cut -d: -f1)
+    MEMORY=$(echo "$CONFIG" | cut -d: -f2)
+    SYSTEM=$(echo "$CONFIG" | cut -d: -f3)
+    WIDTH=$(echo "$CONFIG" | cut -d: -f4)
 
-  echo "Running $SCRIPT on $INPUT"
+    SCRIPT=$(echo "$SCRIPT_INPUT" | cut -d: -f1)
+    INPUT=$(echo "$SCRIPT_INPUT" | cut -d: -f2)
 
-  { time IN=$INPUT "$SCRIPT_PATH" >"$OUTPUT" ; } 2>"$TIME"
+    OUTPUT="${SCRIPT}__env${ENVIRONMENT}__mem${MEMORY}__sys${SYSTEM}__w${WIDTH}${INPUT_TYPE}.out"
+    TIME="${SCRIPT}__env${ENVIRONMENT}__mem${MEMORY}__sys${SYSTEM}__w${WIDTH}${INPUT_TYPE}.time"
+
+    SCRIPT_PATH="${SCRIPTS_DIR}/${SCRIPT}"
+    INPUT_PATH="${INPUTS_DIR}/${INPUT}"
+    OUTPUT_PATH="${OUTPUTS_DIR}/${OUTPUT}"
+    TIME_PATH="${TIMES_DIR}/${TIME}"
+
+    S3_INPUT_PATH="${S3_INPUTS_DIR}/${INPUT}"
+    S3_OUTPUT_PATH="${S3_OUTPUTS_DIR}/${OUTPUT}"
+
+    echo "CONFIG: $CONFIG, SCRIPT_INPUT: $SCRIPT_INPUT"
+
+    if [[ $ENVIRONMENT == "Local" ]]
+    then
+      { time IN=$INPUT_PATH bash "$SCRIPT_PATH" >"$OUTPUT_PATH" ; } 2>"$TIME_PATH"
+    elif [[ $ENVIRONMENT == "AWS" && $SYSTEM == "Bash" ]]
+    then
+      echo "Not implemented yet"
+    elif [[ $ENVIRONMENT == "AWS" && $SYSTEM == "Splash" ]]
+    then
+      python3 "$PASH_TOP"/scripts/serverless/delete-log-streams.py
+      { time IN=$S3_INPUT_PATH "$PASH_TOP"/pa.sh -w "$WIDTH" --serverless_exec "$SCRIPT_PATH" --sls_output "$S3_OUTPUT_PATH" ; } 2>"$TIME_PATH"
+    fi
+
+    grep real "$TIME_PATH"
+  done
 done
