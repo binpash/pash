65771c85941c64ffc1f7382649c1ff67562cb040
yizhengx <yizhengx@bu.edu>
2024-05-29 22:14:29 +0800

    Add covid-mts button

--- evaluation/benchmarks/covid-mts/inputs.sh
+++ evaluation/benchmarks/covid-mts/inputs.sh
@@ -1,40 +1,15 @@
 #!/bin/bash
 
-cd "$(dirname "$0")" || exit 1
+cd "$(realpath $(dirname "$0"))"
+mkdir -p inputs
+cd inputs
 
-inputs_dir="inputs"
+# if [ ! -f ./in.csv ]; then
+#     curl -f 'https://atlas-group.cs.brown.edu/data/covid-mts/in.csv.gz'> in.csv.gz
+#     gzip -d in.csv.gz
+# fi
 
-# Function to display usage
-usage() {
-  echo "Usage: $0 [--small] [--help]"
-  echo "  --small      : Download the small dataset"
-  echo "  --help       : Show this message and exit"
-}
-
-# Function to set up the dataset
-setup_dataset() {
-  if [[ ! -f "$inputs_dir/in_small.csv" && "$1" == "--small" ]]
-  then
-    echo "Downloading and decompressing small-size dataset..."
-    # TODO: Not implemented; link is broken
-    # curl -sf 'http://pac-n4.csail.mit.edu:81/pash_data/small/in_small.csv' > "$inputs_dir/in_small.csv"
-  elif [[ ! -f "$inputs_dir/in.csv" ]]
-  then
-    echo "Downloading and decompressing full-size dataset..."
-    curl -sf 'https://www.balab.aueb.gr/~dds/oasa-2021-01-08.bz2' | bzip2 -d > "$inputs_dir/in.csv"
-  fi
-}
-
-case "$1" in
-  --help)
-    usage
-    ;;
-  --small)
-    setup_dataset --small
-    ;;
-  *)
-    setup_dataset --full
-    ;;
-esac
-
-exit 0
+if [ ! -f ./in_small.csv ]; then
+    curl -f 'https://atlas-group.cs.brown.edu/data/covid-mts/in_small.csv.gz' > in_small.csv.gz
+    gzip -d in_small.csv.gz
+fi
--- evaluation/benchmarks/covid-mts/run.sh
+++ evaluation/benchmarks/covid-mts/run.sh
@@ -7,33 +7,106 @@ cd "$(dirname "$0")" || exit 1
   exit
 }
 
-BENCHMARK_DIR="${PASH_TOP}/evaluation/benchmarks/covid-mts"
-INPUTS_DIR="${BENCHMARK_DIR}/inputs"
-OUTPUTS_DIR="${BENCHMARK_DIR}/outputs"
-TIMES_DIR=$BENCHMARK_DIR/times
-SCRIPTS_DIR="${BENCHMARK_DIR}/scripts"
+[ -z "$AWS_ACCOUNT_ID" ] && {
+  echo "AWS_ACCOUNT_ID not set"
+  exit
+}
 
-SCRIPTS=(
-  "1.sh"
-  "2.sh"
-  "3.sh"
-  "4.sh"
-  "5.sh"
+[ -z "$AWS_BUCKET" ] && {
+  echo "AWS_BUCKET not set"
+  exit
+}
+
+[ -z "$AWS_QUEUE" ] && {
+  echo "AWS_QUEUE not set"
+  exit
+}
+
+mkdir -p {outputs,times}/
+
+BENCHMARK_DIR="$PASH_TOP/evaluation/benchmarks/covid-mts"
+INPUTS_DIR="$BENCHMARK_DIR/inputs"
+OUTPUTS_DIR="$BENCHMARK_DIR/outputs"
+TIMES_DIR="$BENCHMARK_DIR/times"
+SCRIPTS_DIR="$BENCHMARK_DIR/scripts"
+
+S3_BENCHMARK_DIR="covid-mts"
+S3_INPUTS_DIR="$S3_BENCHMARK_DIR/inputs"
+S3_OUTPUTS_DIR="$S3_BENCHMARK_DIR/outputs"
+
+# ENVIRONMENT, MEMORY, SYSTEM, WIDTH
+CONFIGS=(
+  Local:16G:Bash:1
+  # AWS:2048M:Bash:1
+  # AWS:2048M:Splash:1
+  # AWS:2048M:Splash:2
+  # AWS:2048M:Splash:4
+  # AWS:2048M:Splash:8
+  # AWS:2048M:Splash:16
+  # AWS:2048M:Splash:32
+  # AWS:2048M:Splash:64
 )
 
-ENVIRONMENT=Local
-MEMORY=16G
-SYSTEM=Bash
-WIDTH=1
-INPUT="${INPUTS_DIR}/in.csv"
+if [[ "$*" == *"--small"* ]]
+then
+  SCRIPTS_INPUTS=(
+    1.sh:in_small.csv
+    2.sh:in_small.csv
+    3.sh:in_small.csv
+    4.sh:in_small.csv
+    # 5.sh:in_small.csv
+  )
+  INPUT_TYPE=".small"
+else
+  SCRIPTS_INPUTS=(
+    1.sh:in.csv
+    2.sh:in.csv
+    3.sh:in.csv
+    4.sh:in.csv
+    # 5.sh:in.csv
+  )
+  INPUT_TYPE=""
+fi
 
-for SCRIPT in "${SCRIPTS[@]}"
+for CONFIG in "${CONFIGS[@]}"
 do
+  for SCRIPT_INPUT in "${SCRIPTS_INPUTS[@]}"
+  do
+    ENVIRONMENT=$(echo "$CONFIG" | cut -d: -f1)
+    MEMORY=$(echo "$CONFIG" | cut -d: -f2)
+    SYSTEM=$(echo "$CONFIG" | cut -d: -f3)
+    WIDTH=$(echo "$CONFIG" | cut -d: -f4)
+
+    SCRIPT=$(echo "$SCRIPT_INPUT" | cut -d: -f1)
+    INPUT=$(echo "$SCRIPT_INPUT" | cut -d: -f2)
+
+    OUTPUT="${SCRIPT}__env${ENVIRONMENT}__mem${MEMORY}__sys${SYSTEM}__w${WIDTH}${INPUT_TYPE}.out"
+    TIME="${SCRIPT}__env${ENVIRONMENT}__mem${MEMORY}__sys${SYSTEM}__w${WIDTH}${INPUT_TYPE}.time"
+
     SCRIPT_PATH="${SCRIPTS_DIR}/${SCRIPT}"
-    OUTPUT="${OUTPUTS_DIR}/${SCRIPT}__env${ENVIRONMENT}__mem${MEMORY}__sys${SYSTEM}__w${WIDTH}.out"
-    TIME="${TIMES_DIR}/${SCRIPT}__env${ENVIRONMENT}__mem${MEMORY}__sys${SYSTEM}__w${WIDTH}.time"
+    INPUT_PATH="${INPUTS_DIR}/${INPUT}"
+    OUTPUT_PATH="${OUTPUTS_DIR}/${OUTPUT}"
+    TIME_PATH="${TIMES_DIR}/${TIME}"
 
-    echo "Running $SCRIPT on $INPUT"
+    S3_INPUT_PATH="${S3_INPUTS_DIR}/${INPUT}"
+    S3_OUTPUT_PATH="${S3_OUTPUTS_DIR}/${OUTPUT}"
 
-    { time IN=$INPUT "$SCRIPT_PATH" >"$OUTPUT" ; } 2>"$TIME"
+    echo "CONFIG: $CONFIG, SCRIPT_INPUT: $SCRIPT_INPUT"
+
+    if [[ $ENVIRONMENT == "Local" ]]
+    then
+      { time IN=$INPUT_PATH bash "$SCRIPT_PATH" >"$OUTPUT_PATH" ; } 2>"$TIME_PATH"
+    elif [[ $ENVIRONMENT == "AWS" && $SYSTEM == "Bash" ]]
+    then
+      echo "Not implemented yet"
+    elif [[ $ENVIRONMENT == "AWS" && $SYSTEM == "Splash" ]]
+    then
+      echo "Running $SCRIPT with $ENVIRONMENT:$SYSTEM on $S3_INPUT_PATH"
+      python3 "$PASH_TOP"/scripts/serverless/delete-log-streams.py
+      time IN=$S3_INPUT_PATH "$PASH_TOP"/pa.sh -w "$WIDTH" --serverless_exec "$SCRIPT_PATH" --sls_output "$S3_OUTPUT_PATH"
+      { time IN=$S3_INPUT_PATH "$PASH_TOP"/pa.sh -w "$WIDTH" --serverless_exec "$SCRIPT_PATH" --sls_output "$S3_OUTPUT_PATH" ; } 2>"$TIME_PATH"
+    fi
+
+    grep real "$TIME_PATH"
+  done
 done
