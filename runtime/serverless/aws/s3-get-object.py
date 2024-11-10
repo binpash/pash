d44a509c5284435eb8bc395a9ae11062289657fa
yizhengx <yizhengx@bu.edu>
2024-11-10 14:04:21 -0500

    Improve s3 read/write

--- runtime/serverless/aws/s3-get-object.py
+++ runtime/serverless/aws/s3-get-object.py
@@ -9,25 +9,9 @@ object_key, outfile = sys.argv[1:]
 print(f"[s3-get-object.py] Start getting {object_key}")
 start_time = time.time()
 
-session = boto3.Session()
-s3 = session.client("s3")
-
-try:
-    response = s3.get_object(Bucket=BUCKET, Key=object_key)
-except Exception as e:
-    print(e)
-
-batch = 10000
-
-with open(outfile, "wb") as f:
-    while True:
-        x = response["Body"].read(batch)
-
-        if not x:
-            break
-
-        f.write(x)
-        f.flush()
+s3 = boto3.client('s3')
+with open(outfile, 'wb') as f:
+    s3.download_fileobj(BUCKET, object_key, f)
 
 end_time = time.time()
 total_time = end_time - start_time
--- runtime/serverless/aws/s3-put-object.py
+++ runtime/serverless/aws/s3-put-object.py
@@ -13,36 +13,18 @@ if object_key == "/dev/null":
     print(f"[s3-put-object.py] Get /dev/null as key, ignore.")
     exit(0)
 
-print(f"[s3-put-object.py] Start putting {object_key}")
-start_time = time.time()
-
 session = boto3.Session()
-s3 = session.client("s3")
 
-
-# Prepare the data to be stored in the S3 object
-with open(infile, "rb") as file:
-    object_data = file.read()
-
-# Upload the object to S3
-s3.put_object(Bucket=BUCKET, Key=object_key, Body=object_data)
+s3 = boto3.client('s3')
+start_time = time.time()
+with open(infile, "rb") as f:
+    s3.upload_fileobj(f, BUCKET, object_key)
 
 end_time = time.time()
 total_time = end_time - start_time
 total_time = round(total_time, 3)
 
 print(f"[s3-put-object.py] Finish putting {object_key} in {total_time} seconds")
-
-# notify the main shell that job is done
-# sqs_client = boto3.client("sqs")
-# message_body = {"message": "done","output_file_id":object_key}
-# try:
-#     response = sqs_client.send_message(
-#         QueueUrl=f"https://sqs.us-east-1.amazonaws.com/{AWS_ACCOUNT_ID}/{QUEUE}", MessageBody=json.dumps(message_body)
-#     )
-# except Exception as e:
-#     print(e)
-#   
 
 dynamodb = session.client("dynamodb")
 try:
