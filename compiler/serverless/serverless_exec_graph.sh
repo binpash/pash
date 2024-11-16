b47a66834730c087d44571bffd36221df41f87ed
yizhengx <yizhengx@bu.edu>
2024-11-16 11:53:04 -0500

    Serverless manager invoke and wait

--- compiler/serverless/serverless_exec_graph.sh
+++ compiler/serverless/serverless_exec_graph.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 ir_file=$1
-# python3 $PASH_TOP/compiler/serverless/serverless_executor.py $1 $declared_functions
 
 response=($(echo "Exec-Graph: $ir_file $declared_functions" | nc -U "$SERVERLESS_SOCKET"))
 
 status=${response[0]} #do something if false
-execution_random_id=${response[1]}
+folder_id=${response[1]}
+script_id=${response[2]}
 
 if [ "$status" == "ERR" ]; then
     echo "Error in launching serverless execution"
     exit 1
 fi
-
-python3 $PASH_TOP/compiler/serverless/serverless_wait_msg.py $execution_random_id
