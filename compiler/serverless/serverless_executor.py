2bbbaea02764f68ffdc886811d9e6423e52192c2
yizhengx <yizhengx@bu.edu>
2024-11-18 21:16:45 -0500

    Add concurrency limit and report concurrent workers

--- compiler/serverless/serverless_executor.py
+++ compiler/serverless/serverless_executor.py
@@ -204,34 +204,59 @@ def handler(request, conn):
         conn.sendall(bytes_message)
         conn.close()
 class ThreadSafeCounter:
-    def __init__(self, initial=0):
-        self.value = initial
+    def __init__(self, concurrency_limit=1000):
+        self.concurrency_limit = concurrency_limit
+        self.value = 0
+        self.total_time = 0
         self._lock = threading.Lock()
+        self.counter_history = []
 
     def increment(self):
-        with self._lock:
-            self.value += 1
+        # When attempting to increment the counter:
+        #   we need to check if the counter is already at the limit
+        success = True
+        while True:
+            if not success:
+                time.sleep(1)
+            with self._lock:
+                if self.value == self.concurrency_limit:
+                    success = False
+                    continue
+                self.value += 1
+                # log(f"[Serverless Manager] Counter value: {self.value}")
+                success = True
+                self.counter_history.append((time.time(), self.value))
+                break
         return self.value
     
-    def decrement(self):
+    def decrement(self, time_taken=0):
         with self._lock:
             self.value -= 1
+            self.total_time += time_taken
+            self.counter_history.append((time.time(), self.value))
         return self.value
 
     def get_value(self):
         with self._lock:
-            return self.value
+            return self.value, self.total_time
+    
+    def print_history(self):
+        with self._lock:
+            for time, value in self.counter_history:
+                log(f"[Serverless Manager] Counter value: {value} at time: {time}")
+        return
 
 class ServerlessManager:
     def __init__(self):
         create_dynamo_table()
         self.worker_threads = queue.Queue(maxsize=1000)
-        self.counter = ThreadSafeCounter()
         self.lambda_config = BotocoreConfig(
             read_timeout=900,
             connect_timeout=60,
             retries={"max_attempts": 0}
         )
+        self.concurrency_limit = 1000
+        self.counter = ThreadSafeCounter(self.concurrency_limit)
 
     def invoke_lambda(self, folder_ids, script_ids):
         # log(f"[Serverless Manager] Try to invoke lambda response with batches {script_ids} && {folder_ids}")
@@ -277,9 +302,12 @@ class ServerlessManager:
             request, conn = self.sls_socket.get_next_cmd()
 
             if request.startswith("Done"):
-                while self.counter.get_value() > 0:
+                counter, total_time = self.counter.get_value()
+                while counter > 0:
                     time.sleep(1)
-                # self.force_thread.join()
+                    counter, total_time = self.counter.get_value()
+                self.counter.print_history()
+                log(f"[Serverless Manager] All jobs are done with total time: {total_time}")
                 response_msg = f"All jobs are done, shutting down the serverless manager"
                 self.sls_socket.respond(response_msg, conn)
                 self.sls_socket.close()
@@ -288,9 +316,9 @@ class ServerlessManager:
             # multi-threaded
             client_thread = threading.Thread(target=self.handler, args=(request, conn))
             client_thread.start()
-            # self.worker_threads.put(client_thread)
 
     def handler(self, request, conn):
+        # print(f"[Serverless Manager] Received request: {request}")
         args = request.split(':', 1)[1].strip()
         ir_filename, declared_functions_file = args.split()
 
@@ -311,15 +339,17 @@ class ServerlessManager:
                     continue
                 s3.put_object(Bucket=bucket, Key=f'sls-scripts/{s3_folder_id}/{script_id}.sh', Body=script)
 
-            self.counter.increment()
             response_msg = f"OK {s3_folder_id} {main_subgraph_script_id}"
             bytes_message = response_msg.encode('utf-8')
             conn.sendall(bytes_message)
             conn.close()
+            start = time.time()
+            self.counter.increment()
             self.invoke_lambda([s3_folder_id], [main_subgraph_script_id])
+            end = time.time()
             if len(script_id_to_script) > 2:
                 wait_msg_done_dynamo(s3_folder_id)
-            self.counter.decrement()
+            self.counter.decrement(time_taken=end-start)
         
         except Exception as e:
             log(f"[Serverless Manager] Failed to add job to a queue: {e}")
