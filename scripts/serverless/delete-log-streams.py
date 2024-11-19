328f19a90877dc4336c6b5bd24aa13e9d3314a91
yizhengx <yizhengx@bu.edu>
2024-11-18 23:30:01 -0500

    Improve get/delete logs

--- scripts/serverless/delete-log-streams.py
+++ scripts/serverless/delete-log-streams.py
@@ -2,6 +2,9 @@ import os
 import boto3
 from datetime import datetime
 client = boto3.client('logs')
-response = (client.describe_log_streams(logGroupName="/aws/lambda/lambda"))
-for log_stream in response["logStreams"]:
-    client.delete_log_stream(logGroupName="/aws/lambda/lambda", logStreamName=log_stream["logStreamName"])
+while True:
+    response = (client.describe_log_streams(logGroupName="/aws/lambda/lambda"))
+    if not response["logStreams"] or len(response["logStreams"]) == 0:
+        break
+    for log_stream in response["logStreams"]:
+        client.delete_log_stream(logGroupName="/aws/lambda/lambda", logStreamName=log_stream["logStreamName"])
--- scripts/serverless/get-log-streams.py
+++ scripts/serverless/get-log-streams.py
@@ -2,18 +2,22 @@ import os
 import boto3
 from datetime import datetime
 client = boto3.client('logs')
-response = (client.describe_log_streams(logGroupName="/aws/lambda/lambda"))
 
-folder = "tmp"
+folder = os.getenv("LOG_FOLDER")
 if not os.path.exists(folder):
     os.mkdir(folder)
 
-for log_stream in response["logStreams"]:
-    file_name = ""
-    with open(f"{folder}/temp.log", "w") as f:
-        log_stream_response = client.get_log_events(logGroupName="/aws/lambda/lambda", logStreamName=log_stream["logStreamName"])
-        file_name = log_stream["logStreamName"].split("]")[1]
-        for log in log_stream_response["events"]:
-            f.write(f"[{datetime.fromtimestamp(log['timestamp']/1000.0)}] {log['message']}")
-        f.write("=========================================================================\n\n\n")
-    os.rename(f"{folder}/temp.log", f"{folder}/aws_lambda_log_{file_name}.log")
+while True:
+    response = (client.describe_log_streams(logGroupName="/aws/lambda/lambda"))
+    if not response["logStreams"] or len(response["logStreams"]) == 0:
+        break
+    for log_stream in response["logStreams"]:
+        file_name = ""
+        with open(f"{folder}/temp.log", "w") as f:
+            log_stream_response = client.get_log_events(logGroupName="/aws/lambda/lambda", logStreamName=log_stream["logStreamName"])
+            file_name = log_stream["logStreamName"].split("]")[1]
+            for log in log_stream_response["events"]:
+                f.write(f"[{datetime.fromtimestamp(log['timestamp']/1000.0)}] {log['message']}")
+            f.write("=========================================================================\n\n\n")
+        os.rename(f"{folder}/temp.log", f"{folder}/aws_lambda_log_{file_name}.log")
+        client.delete_log_stream(logGroupName="/aws/lambda/lambda", logStreamName=log_stream["logStreamName"])
