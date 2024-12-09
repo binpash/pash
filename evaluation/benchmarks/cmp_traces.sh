5181755d96b4e367aa03551a645571a62908152f
Evangelos Lamprou <vagos@lamprou.xyz>
2024-12-09 15:15:21 -0500

    caruca-eval update cmp_traces script
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- evaluation/benchmarks/cmp_traces.sh
+++ evaluation/benchmarks/cmp_traces.sh
@@ -3,5 +3,5 @@
 for file in logs_original/*; do
     file=$(basename $file)
     echo "Comparing: $file"
-    diff <(grep "caruca class" logs_original/$file) <(grep "caruca class" logs_caruca/$file)
+    sdiff -l <(grep "caruca class" logs_original/$file) <(grep "caruca class" logs_caruca/$file)
 done
