6b77e7a00626bc70283f0e7161b328d8acb752f1
Evangelos Lamprou <vagos@lamprou.xyz>
2024-12-09 21:26:59 -0500

    caruca-eval make cmp_traces comapre ios
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- evaluation/benchmarks/cmp_traces.sh
+++ evaluation/benchmarks/cmp_traces.sh
@@ -3,5 +3,5 @@
 for file in logs_original/*; do
     file=$(basename $file)
     echo "Comparing: $file"
-    sdiff -l <(grep "caruca class" logs_original/$file) <(grep "caruca class" logs_caruca/$file)
+    sdiff -l <(grep "caruca" logs_original/$file) <(grep "caruca" logs_caruca/$file)
 done
