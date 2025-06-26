9ac3f150586d7ae37b0a512a53d3cf1df110dddd
yizhengx <yizhengx@bu.edu>
2025-06-26 15:10:48 -0400

    Fix unix50

--- evaluation/benchmarks/unix50/input/setup.sh
+++ evaluation/benchmarks/unix50/input/setup.sh
@@ -4,9 +4,6 @@
 
 PASH_TOP=${PASH_TOP:-$(git rev-parse --show-toplevel)}
 
-## FIXME: These inputs are already 1G when downloaded
-## FIXME: Also, wget is not silent like curl in the other setup scripts.
-
 inputs=(
 1 10 11 12 2 3 4 5 6 7 8 9.1 9.2 9.3 9.4 9.5 9.6 9.7 9.8 9.9
 )
@@ -20,19 +17,18 @@ if [[ "$1" == "-c" ]]; then
 fi
 
 setup_dataset() {
-    # generate small inputs 
+    # generate small inputs
     if [ "$#" -eq 1 ] && [ "$1" = "--small" ]; then
-      if [ ! -d ./small ]; then                                                          
-         mkdir small                                                               
-      fi                                                                                 
+      if [ ! -d ./small ]; then
+         mkdir small
+      fi
       cd small || exit 1
     fi
 
     for input in ${inputs[@]}
-    do 
-        rm -f "${input}.txt"
+    do
         if [ ! -f "${input}.txt" ]; then
-          wget -q --no-check-certificate "https://atlas.cs.brown.edu/data/unix50/${input}.txt" 
+          wget -q --no-check-certificate "https://atlas.cs.brown.edu/data/unix50/small/${input}_1M.txt" -O ${input}.txt
           "$PASH_TOP/scripts/append_nl_if_not.sh" "${input}.txt"
         fi
     done
@@ -40,19 +36,8 @@ setup_dataset() {
     if [ "$#" -eq 1 ] && [ "$1" = "--small" ]; then
       cd .. || exit 1
     fi
-  
-    # FIXME: Calling this script with --full is not idempotent.
-    if [ "$#" -eq 1 ] && [ "$1" = "--full" ]; then
-        for file in *.txt; do
-            echo '' > temp.txt
-            for (( i = 0; i < 10; i++ )); do
-                cat $file >> temp.txt
-            done
-            mv temp.txt $file
-        done
-    fi
 
-    if [ "$#" -eq 1 ] && [ "$1" = "--gen-full" ]; then
+    if [ "$#" -eq 1 ] && [ "$1" = "--full" ]; then
         echo Generting full-size inputs
 
         for file in *.txt; do
@@ -62,6 +47,7 @@ setup_dataset() {
             for (( i = 0; i < max ; i++ )); do
                 cat $file >> $new_file;
             done
+            mv $new_file $file
         done
     fi
 }
@@ -70,7 +56,7 @@ source_var() {
   if [[ "$1" == "--small" ]]; then
     export IN_PRE=$PASH_TOP/evaluation/benchmarks/unix50/input/small
   else
-    # FIXME this is the input prefix; do we want all to be IN 
+    # FIXME this is the input prefix; do we want all to be IN
     export IN_PRE=$PASH_TOP/evaluation/benchmarks/unix50/input
   fi
 }
