8cfb239c469aa798c4df74e2ed7fb525a94d7c61
yizhengx <yizhengx@bu.edu>
2025-06-26 15:08:30 -0400

    Fix web-index

--- evaluation/benchmarks/web-index/input/setup.sh
+++ evaluation/benchmarks/web-index/input/setup.sh
@@ -2,7 +2,6 @@
 
 #set -e
 
-wiki_archive="https://dumps.wikimedia.org/other/static_html_dumps/current/en/wikipedia-en-html.tar.7z"
 PASH_TOP=${PASH_TOP:-$(git rev-parse --show-toplevel)}
 
 . "$PASH_TOP/scripts/utils.sh"
@@ -10,43 +9,37 @@ PASH_TOP=${PASH_TOP:-$(git rev-parse --show-toplevel)}
 [ "$1" = "-c" ] && rm-files en/ *.7z *.tar  500.txt 1000.txt full small
 
 setup_dataset() {
-  rm -rf ../1-grams.txt ../2-grams.txt 
-  
+  rm -rf ../1-grams.txt ../2-grams.txt
+
   ## Downloading the dataset needs to happen for both small and large
-  if [[ ! -d ./en ]]; then
-    wget $wiki_archive || eexit "cannot fetch wikipedia"
-    7za x wikipedia-en-html.tar.7z
-    tar -xvf wikipedia-en-html.tar
-    # wget http://ndr.md/data/wikipedia/index.txt # || eexit "cannot fetch wiki indices"
-    # It is actually OK if we don't have this index since we download the 500/1000 below
+  if [[ -d ./articles ]]; then
+    exit 0
   fi
 
   if [ "$1" = "--small" ]; then
-    # 500 entries
-    # wget http://pac-n4.csail.mit.edu:81/pash_data/small/web-index.small.zip
-    # unzip web-index.small.zip
-    # mv small/500.txt .
-    # rm -rf small web-index.small.zip
-
-    wget https://atlas.cs.brown.edu/data/wikipedia/input_small/index.txt -O 500.txt || eexit "cannot fetch small index"
-  else
-    # elif [ "$1" = "--full" ]; then
-    # the default full 
-    # 1000 entries
-    # wget http://pac-n4.csail.mit.edu:81/pash_data/full/web-index.full.zip
-    # unzip web-index.full.zip
-    # mv full/1000.txt .
-    # rm -rf full web-index.full.zip
-    wget "https://atlas.cs.brown.edu/data/wikipedia/index1g.txt" -O 1000.txt || eexit "cannot fetch full index"
+    wiki_archive="https://atlas.cs.brown.edu/data/wikipedia/wikipedia1g.tar.gz"
+    wget -O wikipedia.tar.gz $wiki_archive || eexit "cannot fetch wikipedia"
+    tar -xf wikipedia.tar.gz -C .
+    mv articles1g articles
+    wget -O index.txt https://atlas.cs.brown.edu/data/wikipedia/index1g.txt
   fi
+
+  if [ "$1" = "--full" ]; then
+    wiki_archive="https://atlas.cs.brown.edu/data/wikipedia/wikipedia10g.tar.gz"
+    wget $wiki_archive -O wikipedia10g.tar.gz || eexit "cannot fetch full wiki"
+    tar -xvzf wikipedia10g.tar.gz
+    rm wikipedia10g.tar.gz
+    wget -O index.txt https://atlas.cs.brown.edu/data/wikipedia/index10g.txt
+  fi
+
 }
 
 source_var() {
   export WEB_INDEX_DIR=$PASH_TOP/evaluation/benchmarks/web-index/input
-  export WIKI=$PASH_TOP/evaluation/benchmarks/web-index/input/
+  export WIKI=$PASH_TOP/evaluation/benchmarks/web-index/input/articles/
   if [[ "$1" == "--small" ]]; then
-    export IN=$PASH_TOP/evaluation/benchmarks/web-index/input/500.txt
+    export IN=$PASH_TOP/evaluation/benchmarks/web-index/input/index.txt
   else
-    export IN=$PASH_TOP/evaluation/benchmarks/web-index/input/1000.txt
+    export IN=$PASH_TOP/evaluation/benchmarks/web-index/input/index.txt
   fi
 }
