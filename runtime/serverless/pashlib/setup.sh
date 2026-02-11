fe64c870a41d193d26fb5661cbff56ca927aa72c
Yizheng Xie <yizhengx@bu.edu>
2026-02-11 21:19:46 +0800

    Update lambda package

--- runtime/serverless/pashlib/setup.sh
+++ runtime/serverless/pashlib/setup.sh
@@ -1,36 +1,5 @@
-# Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+#!/bin/bash
+set -euo pipefail
 
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-sudo apt install zip
-
-sudo apt install awscli
-
-# Download and install nvm:
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-
-# in lieu of restarting the shell
-\. "$HOME/.nvm/nvm.sh"
-
-# Download and install Node.js:
-nvm install 24
-
-# Verify the Node.js version:
-node -v # Should print "v24.11.0".
-
-# Verify npm version:
-npm -v # Should print "11.6.1".
-
-npm install -g serverless@3
+curl https://sh.rustup.rs -sSf | sh
+source "$HOME/.cargo/env"
